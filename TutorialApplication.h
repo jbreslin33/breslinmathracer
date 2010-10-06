712923a287c3a57a94a416e22bd1256aa89e0abb
jbreslin33 <jbreslin33@localhost>
2010-10-06 08:30:18 -0400

    have SinbadCharacterController working with key strokes need mouse moves. using the most simple BaseApplication along with Tut and Sinbad.

---
+++ TutorialApplication.h
@@ -0,0 +1,44 @@
+/*
+-----------------------------------------------------------------------------
+Filename:    TutorialApplication.h
+-----------------------------------------------------------------------------
+
+This source file is part of the
+   ___                 __    __ _ _    _ 
+  /___\__ _ _ __ ___  / / /\ \ (_) | _(_)
+ //  // _` | '__/ _ \ \ \/  \/ / | |/ / |
+/ \_// (_| | | |  __/  \  /\  /| |   <| |
+\___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
+      |___/                              
+      Tutorial Framework
+      http://www.ogre3d.org/tikiwiki/
+-----------------------------------------------------------------------------
+*/
+#ifndef __TutorialApplication_h_
+#define __TutorialApplication_h_
+
+#include "BaseApplication.h"
+#include "SinbadCharacterController.h"
+
+class TutorialApplication : public BaseApplication
+{
+public:
+    TutorialApplication(void);
+    virtual ~TutorialApplication(void);
+
+	SinbadCharacterController* mChara;
+
+protected:
+    virtual void createScene(void);
+    
+        // Ogre::FrameListener
+    virtual bool frameRenderingQueued(const Ogre::FrameEvent& evt);
+
+    // OIS::KeyListener
+    virtual bool keyPressed( const OIS::KeyEvent &arg );
+    virtual bool keyReleased( const OIS::KeyEvent &arg );
+
+
+};
+
+#endif // #ifndef __TutorialApplication_h_
