78debea73b6f45458183655ccd777b689374bb98
jbreslin33 <jbreslin33@localhost>
2013-11-02 23:21:08 -0400

    u

--- src/src/game/number_pad.php
+++ src/src/game/number_pad.php
@@ -13,59 +13,11 @@ Extends: Dungeon,
 	createControlObject: function()
 	{
 		this.parent();
-/*
-        	this.mControlObject.mHideOnQuestionSolved = false;
-        	this.mControlObject.createMountPoint(0,-5,-41);
-
-        	this.mControlObject.showQuestionObject(false);
-
-        	
-		//text question mountee
-        	var questionMountee = new QuestionShape(100,50,300,300,this,this.mQuiz.getSpecificQuestion(0),"","orange","questionMountee");
-        	questionMountee.setMountable(true);
-        	questionMountee.setHideOnDrop(true);
-        	this.addToShapeArray(questionMountee);
-        	this.mControlObject.setStartingMountee(questionMountee);
-
-        	//do the mount
-        	this.mControlObject.mount(questionMountee,0);
-
-        	questionMountee.setBackgroundColor("transparent");
-        	questionMountee.showQuestion(true);
-
-		//sync with mounter
-		questionMountee.setCopyQuestionFromMounter(true);
-*/
 	},
 
 	createQuestionShapes: function()
 	{
-                count = 0;
-                for (i = 0; i < numberOfRows; i++)
-                {
-                        var openPoint = this.getOpenPoint2D(40,735,75,375,50,7);
-                        var shape;
-                        this.addToShapeArray(shape = new QuestionShape(50,50,openPoint.mX,openPoint.mY,this,this.mQuiz.getSpecificQuestion(count),"/images/treasure/gold_coin_head.png","","question"));
-                        shape.createMountPoint(0,-5,-41);
-                        shape.showQuestion(false);
-
-                        //numberMount to go on top let's make it small and draw it on top
-                        var questionMountee = new QuestionShape(1,1,100,100,this,this.mQuiz.getSpecificQuestion(count),"","orange","questionMountee");
-                        questionMountee.setMountable(true);
-                        this.addToShapeArray(questionMountee);
-        		shape.setStartingMountee(questionMountee);
-                        questionMountee.showQuestion(false);
-
-                        //do the mount
-                        shape.mount(questionMountee,0);
-
-                        questionMountee.setBackgroundColor("transparent");
-
-			//evaluate questions
-			questionMountee.setEvaluateQuestions(false);
-
-                        count++;
-                }
+		//this.parent();
 	},
 
 	createChasers: function()
