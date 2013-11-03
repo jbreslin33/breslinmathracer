f98146e5e74f88419b0f87e7f3a9672cd1957f54
jbreslin33 <jbreslin33@localhost>
2013-11-02 22:04:49 -0400

    stratingt ot play

--- src/web/navigation/top_links.php
+++ src/web/navigation/top_links.php
@@ -1,6 +1,6 @@
 <ul>
 <li><a href="/web/home/home.php">Home</a></li>
-<li><a href="/web/game/chooser.php">Play</a></li>
+<li><a href="/web/game/play.php">Play</a></li>
 <li><a href="/web/insert/insert.php">Inserts</a></li>
 <li><a href="/web/select/select.php">Selects</a></li>
 <li><a href="/web/stats/stats.php">Stats</a></li>
