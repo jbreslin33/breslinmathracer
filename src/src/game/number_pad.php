32a0f0bd4f074943f7467c21e9845ce819505b8d
jbreslin33 <jbreslin33@localhost>
2013-11-02 23:16:08 -0400

    took out some stuff

--- src/src/game/number_pad.php
+++ src/src/game/number_pad.php
@@ -69,7 +69,7 @@ Extends: Dungeon,
 
 	createChasers: function()
 	{
-                for (i = 0; i < 1; i++)
+                for (i = 0; i < 0; i++)
                 {
                         var openPoint = this.getOpenPoint2D(40,735,75,375,50,7);
                         var shape = new ShapeChaser(50,50,openPoint.mX,openPoint.mY,this,"/images/monster/red_monster.png","","chaser");
@@ -79,6 +79,7 @@ Extends: Dungeon,
 
 	createKey: function(image_source)
 	{
+/*
         	var keyQuestion = new Question('Pick up key.',"key");
         	this.mQuiz.mQuestionArray.push(keyQuestion);
 
@@ -89,10 +90,12 @@ Extends: Dungeon,
        	 	key.mMountable = true;
         	key.setHideOnQuestionSolved(false);
         	this.addToShapeArray(key);
+*/
 	},
 
 	createDoor: function(image_source_closed,image_source_open)
 	{
+/*
         	var doorQuestion = new Question('Open door with key.',"door");
         	this.mQuiz.mQuestionArray.push(doorQuestion);
 
@@ -101,7 +104,7 @@ Extends: Dungeon,
         	door.mUrl = '/src/database/goto_next_level.php';
         	door.mOpenOnQuestionSolved = true;
         	this.addToShapeArray(door);
-
+*/
 	}
 		
 });
