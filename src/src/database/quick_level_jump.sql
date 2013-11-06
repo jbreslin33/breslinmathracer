53870bf4426361f5791d5e3b1366a51eedaf623f
jbreslin33 <jbreslin33@localhost>
2013-11-05 20:57:50 -0500

    error ...

--- src/src/database/quick_level_jump.sql
+++ src/src/database/quick_level_jump.sql
@@ -1,1 +1,1 @@
-insert into levels_transactions (advancement_time, user_id, level_id) values (current_timestamp,13,14);
+insert into levels_transactions (advancement_time, user_id, level_id) values (current_timestamp,13,4.3);
