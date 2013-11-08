1ad47c358e4a3a617901c70a4507b93668112b5e
jbreslin33 <jbreslin33@localhost>
2013-11-07 22:21:05 -0500

    in app

--- src/web/game/standard_bottom.php
+++ src/web/game/standard_bottom.php
@@ -1,14 +1,3 @@
-  	//RESET GAME TO START
-        //GAME.resetGame();
-
-        //START UPDATING
-        var t=setInterval("GAME.update()",32)
-
-        //brian - update score in database
-        var t=setInterval("GAME.updateScore()",1000)
-				
-	//check if game has gone on too long
-        var t=setInterval("GAME.checkTime()",1000)
 }
 );
 
