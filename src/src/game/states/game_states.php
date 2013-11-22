95c7c258609942bf60efef12b1ea79cef9dbccb3
jbreslin33 <jbreslin33@localhost>
2013-11-22 17:47:32 -0500

    u

--- src/src/game/states/game_states.php
+++ src/src/game/states/game_states.php
@@ -1,3 +1,33 @@
+var GLOBAL_PAD_GAME = new Class(
+{
+Extends: State,
+
+initialize: function()
+{
+},
+
+log: function(msg)
+{
+        setTimeout(function()
+        {
+                throw new Error(msg);
+        }, 0);
+},
+
+enter: function(game)
+{
+},
+
+execute: function(game)
+{
+},
+
+exit: function(game)
+{
+}
+
+});
+
 var INIT_PAD_GAME = new Class(
 {
 Extends: State,
