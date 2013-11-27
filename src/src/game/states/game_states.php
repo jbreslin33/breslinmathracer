cdf1d9a7cefe4858aa2d3e0e5b6dd8c0f6538049
jbresiln33 <jbresiln33@localhost>
2013-11-27 15:14:47 -0500

    next. kids a little lower maybe from 300 to 350?

--- src/src/game/states/game_states.php
+++ src/src/game/states/game_states.php
@@ -748,20 +748,35 @@ enter: function(game)
         game.mCorrectAnswerBar.mMesh.value = '';
         game.mCorrectAnswerBar.mMesh.innerHTML = 'HOORAY!';
         game.showCorrectAnswerBar();
+ 
 	game.mVictoryShape_0.setVisibility(true);
-	game.mVictoryShape_1.setVisibility(true);
-	game.mVictoryShape_2.setVisibility(true);
-	game.mVictoryShape_3.setVisibility(true);
-	game.mVictoryShape_4.setVisibility(true);
-	game.mVictoryShape_5.setVisibility(true);
-	game.mVictoryShape_6.setVisibility(true);
-	game.mVictoryShape_7.setVisibility(true);
-	game.mVictoryShape_8.setVisibility(true);
-	game.mVictoryShape_9.setVisibility(true);
-	game.mVictoryShape_10.setVisibility(true);
-	game.mVictoryShape_11.setVisibility(true);
-	game.mVictoryShape_12.setVisibility(true);
-	game.mVictoryShape_13.setVisibility(true);
+        game.mVictoryShape_0.setPosition(50,300);
+        game.mVictoryShape_1.setVisibility(true);
+        game.mVictoryShape_1.setPosition(100,300);
+        game.mVictoryShape_2.setVisibility(true);
+        game.mVictoryShape_2.setPosition(150,300);
+        game.mVictoryShape_3.setVisibility(true);
+        game.mVictoryShape_3.setPosition(200,300);
+        game.mVictoryShape_4.setVisibility(true);
+        game.mVictoryShape_4.setPosition(250,300);
+        game.mVictoryShape_5.setVisibility(true);
+        game.mVictoryShape_5.setPosition(300,300);
+        game.mVictoryShape_6.setVisibility(true);
+        game.mVictoryShape_6.setPosition(350,300);
+        game.mVictoryShape_7.setVisibility(true);
+        game.mVictoryShape_7.setPosition(400,300);
+        game.mVictoryShape_8.setVisibility(true);
+        game.mVictoryShape_8.setPosition(450,300);
+        game.mVictoryShape_9.setVisibility(true);
+        game.mVictoryShape_9.setPosition(500,300);
+        game.mVictoryShape_10.setVisibility(true);
+        game.mVictoryShape_10.setPosition(550,300);
+        game.mVictoryShape_11.setVisibility(true);
+        game.mVictoryShape_11.setPosition(600,300);
+        game.mVictoryShape_12.setVisibility(true);
+        game.mVictoryShape_12.setPosition(650,300);
+        game.mVictoryShape_13.setVisibility(true);
+        game.mVictoryShape_13.setPosition(700,300);
 },
 
 execute: function(game)
