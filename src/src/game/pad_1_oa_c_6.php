ad6820da1d268c1bf58e716e4de7ac2e0aa382ad
jbreslin33 <jbreslin33@localhost>
2013-12-02 10:58:24 -0500

    add

---
+++ src/src/game/pad_1_oa_c_6.php
@@ -0,0 +1,89 @@
+var Pad_k_oa_a_5 = new Class(
+{
+
+Extends: Pad,
+
+	initialize: function(application)
+	{
+       		this.parent(application);
+		this.mApplication.mHud.mGameName.setText('<font size="2">ADD</font>');
+	},
+
+	createQuestions: function()
+        {
+ 		this.parent();
+
+		this.log('createQuestions in Pad_k_oa_a_5');		
+
+		this.mQuiz.mQuestionPoolArray.push(new Question('1 + 1 =','2'));
+                this.mQuiz.mQuestionPoolArray.push(new Question('2 + 2 =','4'));
+                this.mQuiz.mQuestionPoolArray.push(new Question('2 + 1 =','3'));
+                this.mQuiz.mQuestionPoolArray.push(new Question('1 + 2 =','3'));
+                this.mQuiz.mQuestionPoolArray.push(new Question('3 + 1 =','4'));
+                this.mQuiz.mQuestionPoolArray.push(new Question('1 + 3 =','4'));
+                this.mQuiz.mQuestionPoolArray.push(new Question('4 + 1 =','5'));
+                this.mQuiz.mQuestionPoolArray.push(new Question('1 + 4 =','5'));
+                this.mQuiz.mQuestionPoolArray.push(new Question('2 + 3 =','5'));
+                this.mQuiz.mQuestionPoolArray.push(new Question('3 + 2 =','5'));
+                this.mQuiz.mQuestionPoolArray.push(new Question('0 + 0 =','0')); 
+                this.mQuiz.mQuestionPoolArray.push(new Question('1 + 0 =','1'));
+                this.mQuiz.mQuestionPoolArray.push(new Question('2 + 0 =','2'));
+                this.mQuiz.mQuestionPoolArray.push(new Question('3 + 0 =','3'));
+                this.mQuiz.mQuestionPoolArray.push(new Question('4 + 0 =','4'));
+                this.mQuiz.mQuestionPoolArray.push(new Question('5 + 0 =','0'));
+                this.mQuiz.mQuestionPoolArray.push(new Question('0 + 1 =','1'));
+                this.mQuiz.mQuestionPoolArray.push(new Question('0 + 2 =','2'));
+                this.mQuiz.mQuestionPoolArray.push(new Question('0 + 3 =','3'));
+                this.mQuiz.mQuestionPoolArray.push(new Question('0 + 4 =','4'));
+                this.mQuiz.mQuestionPoolArray.push(new Question('0 + 5 =','5'));
+
+		var totalNewGoal       = parseInt(this.mScoreNeeded / 2);
+		var totalNew           = 0;
+		var newQuestionElement = 0;
+
+		if (this.mApplication.mNextLevelID == 14)
+		{
+			newQuestionElement = 0;	
+		}		
+		if (this.mApplication.mNextLevelID == 14.01)
+		{
+			newQuestionElement = 1;	
+		}		
+		if (this.mApplication.mNextLevelID == 14.02)
+		{
+			newQuestionElement = 2;	
+		}		
+		if (this.mApplication.mNextLevelID == 14.03)
+		{
+			newQuestionElement = 3;	
+		}		
+
+		while (totalNew < totalNewGoal)
+		{	
+			//reset vars and arrays
+			totalNew = 0;
+			for (d = 0; d < this.mQuiz.mQuestionArray.length; d++)
+			{
+				this.mQuiz.mQuestionArray[d] = 0;
+			} 
+			this.mQuiz.mQuestionArray = 0;
+			this.mQuiz.mQuestionArray = new Array();
+
+			for (s = 0; s < this.mScoreNeeded; s++)
+			{	
+				//50% chance of asking newest question
+				var randomChance = Math.floor((Math.random()*2));		
+				if (randomChance == 0)
+				{
+					this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[newQuestionElement]);
+					totalNew++;
+				}	
+				if (randomChance == 1)
+				{
+					var randomElement = Math.floor((Math.random()*newQuestionElement));		
+					this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[randomElement]);
+				}
+			}
+		}
+	}
+});
