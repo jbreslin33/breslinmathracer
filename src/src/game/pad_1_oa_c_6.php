f27a09b04298f4a050003a183324c73ee073201e
jbreslin33 <jbreslin33@localhost>
2013-12-02 11:10:43 -0500

    adding add

--- src/src/game/pad_1_oa_c_6.php
+++ src/src/game/pad_1_oa_c_6.php
@@ -1,4 +1,4 @@
-var Pad_1_oa_c_6 = new Class(
+var Pad_k_oa_a_5 = new Class(
 {
 
 Extends: Pad,
@@ -12,8 +12,6 @@ Extends: Pad,
 	createQuestions: function()
         {
  		this.parent();
-
-		this.log('createQuestions in Pad_k_oa_a_5');		
 
 		this.mQuiz.mQuestionPoolArray.push(new Question('1 + 1 =','2'));
                 this.mQuiz.mQuestionPoolArray.push(new Question('2 + 2 =','4'));
@@ -41,21 +39,89 @@ Extends: Pad,
 		var totalNew           = 0;
 		var newQuestionElement = 0;
 
-		if (this.mApplication.mNextLevelID == 100)
+		if (this.mApplication.mNextLevelID == 14)
 		{
 			newQuestionElement = 0;	
 		}		
-		if (this.mApplication.mNextLevelID == 100.01)
+		if (this.mApplication.mNextLevelID == 14.01)
 		{
 			newQuestionElement = 1;	
 		}		
-		if (this.mApplication.mNextLevelID == 100.02)
+		if (this.mApplication.mNextLevelID == 14.02)
 		{
 			newQuestionElement = 2;	
 		}		
-		if (this.mApplication.mNextLevelID == 100.03)
+		if (this.mApplication.mNextLevelID == 14.03)
 		{
 			newQuestionElement = 3;	
+		}		
+		if (this.mApplication.mNextLevelID == 14.04)
+		{
+			newQuestionElement = 4;	
+		}		
+		if (this.mApplication.mNextLevelID == 14.05)
+		{
+			newQuestionElement = 5;	
+		}		
+		if (this.mApplication.mNextLevelID == 14.06)
+		{
+			newQuestionElement = 6;	
+		}		
+		if (this.mApplication.mNextLevelID == 14.07)
+		{
+			newQuestionElement = 7;	
+		}		
+		if (this.mApplication.mNextLevelID == 14.08)
+		{
+			newQuestionElement = 8;	
+		}		
+		if (this.mApplication.mNextLevelID == 14.09)
+		{
+			newQuestionElement = 9;	
+		}		
+		if (this.mApplication.mNextLevelID == 14.10)
+		{
+			newQuestionElement = 10;	
+		}		
+		if (this.mApplication.mNextLevelID == 14.11)
+		{
+			newQuestionElement = 11;	
+		}		
+		if (this.mApplication.mNextLevelID == 14.12)
+		{
+			newQuestionElement = 12;	
+		}		
+		if (this.mApplication.mNextLevelID == 14.13)
+		{
+			newQuestionElement = 13;	
+		}		
+		if (this.mApplication.mNextLevelID == 14.14)
+		{
+			newQuestionElement = 14;	
+		}		
+		if (this.mApplication.mNextLevelID == 14.15)
+		{
+			newQuestionElement = 15;	
+		}		
+		if (this.mApplication.mNextLevelID == 14.16)
+		{
+			newQuestionElement = 16;	
+		}		
+		if (this.mApplication.mNextLevelID == 14.17)
+		{
+			newQuestionElement = 17;	
+		}		
+		if (this.mApplication.mNextLevelID == 14.18)
+		{
+			newQuestionElement = 18;	
+		}		
+		if (this.mApplication.mNextLevelID == 14.19)
+		{
+			newQuestionElement = 19;	
+		}		
+		if (this.mApplication.mNextLevelID == 14.20)
+		{
+			newQuestionElement = 20;	
 		}		
 
 		while (totalNew < totalNewGoal)
