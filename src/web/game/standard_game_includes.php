106c81176104f859f7b36a2c06bf489e459e21cf
jbreslin33 <jbreslin33@localhost>
2013-12-10 18:44:07 -0500

    renames

--- src/web/game/standard_game_includes.php
+++ src/web/game/standard_game_includes.php
@@ -1,13 +1,19 @@
 <script type="text/javascript" src="/src/math/point2D.php"></script>
+
 <script type="text/javascript" src="/src/bounds/bounds.php"></script>
+
 <script type="text/javascript" src="/src/fsm/state.php"></script>
 <script type="text/javascript" src="/src/fsm/state_machine.php"></script>
+
 <script type="text/javascript" src="/src/application/application.php"></script>
 <script type="text/javascript" src="/src/application/states/application_states.php"></script>
+
 <script type="text/javascript" src="/src/game/game.php"></script>
-<script type="text/javascript" src="/src/game/states/game_states.php"></script>
+<script type="text/javascript" src="/src/game/states/states.php"></script>
+
 <script type="text/javascript" src="/src/animation/animation.php"></script>
 <script type="text/javascript" src="/src/animation/animation_advanced.php"></script>
+
 <script type="text/javascript" src="/src/shape/shape.php"></script>
 <script type="text/javascript" src="/src/shape/question_shape.php"></script>
 <script type="text/javascript" src="/src/shape/shape_player.php"></script>
@@ -16,19 +22,25 @@
 <script type="text/javascript" src="/src/shape/shape_ai.php"></script>
 <script type="text/javascript" src="/src/shape/shape_chaser.php"></script>
 <script type="text/javascript" src="/src/shape/shape_victory.php"></script>
+
 <script type="text/javascript" src="/src/div/div.php"></script>
 <script type="text/javascript" src="/src/question/question.php"></script>
+
 <script type="text/javascript" src="/src/quiz/quiz.php"></script>
+
 <script type="text/javascript" src="/src/hud/hud.php"></script>
-<script type="text/javascript" src="/src/game/game.php"></script>
-<script type="text/javascript" src="/src/game/game_quiz.php"></script>
-<script type="text/javascript" src="/src/game/pad/pad.php"></script>
+
 <script type="text/javascript" src="/src/game/game/dungeon/dungeon.php"></script>
+<script type="text/javascript" src="/src/game/game/dungeon/states/states.php"></script>
 <script type="text/javascript" src="/src/game/dungeon/k_cc_a_1.php"></script>
 <script type="text/javascript" src="/src/game/dungeon/k_cc_a_2.php"></script>
+
+<script type="text/javascript" src="/src/game/pad/pad.php"></script>
+<script type="text/javascript" src="/src/game/game/pad/states/states.php"></script>
 <script type="text/javascript" src="/src/game/pad/k_cc_a_3.php"></script>
 <script type="text/javascript" src="/src/game/pad/k_oa_a_5.php"></script>
 <script type="text/javascript" src="/src/game/pad/1_oa_c_6.php"></script>
 <script type="text/javascript" src="/src/game/pad/2_oa_b_2.php"></script>
 <script type="text/javascript" src="/src/game/pad/3_oa_c_7.php"></script>
+
 <script type="text/javascript" src="/web/game/standard_game_hud.php"></script>
