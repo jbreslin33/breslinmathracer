e698bf6d752519b2460a2c011fd38f7c15b6eb17
jbreslin33 <jbreslin33@localhost>
2013-12-11 14:23:30 -0500

    u

--- src/src/game/dungeon/dungeon.php
+++ src/src/game/dungeon/dungeon.php
@@ -188,7 +188,7 @@ Extends: Game,
  		//just wait here until what???
         	if (this.mApplication.mAdvanceToNextLevelConfirmation)
         	{
-                	this.mPadStateMachine.changeState(this.mSHOW_LEVEL_PASSED);
+                	this.mDungeonStateMachine.changeState(this.mSHOW_LEVEL_PASSED);
         	}
         },
 
