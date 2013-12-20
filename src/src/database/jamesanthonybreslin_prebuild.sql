8b4ccf90f3e547ca9c4b04e7e820208b9038b948
jbreslin33 <jbreslin33@localhost>
2013-12-19 22:20:37 -0500

    u

--- src/src/database/jamesanthonybreslin_prebuild.sql
+++ src/src/database/jamesanthonybreslin_prebuild.sql
@@ -118,55 +118,6 @@ ALTER SEQUENCE level_attempts_id_seq OWNED BY levelattempts.id;
 
 
 --
--- Name: levels_standards_clusters_domains_grades; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE levels_standards_clusters_domains_grades (
-    level_id double precision NOT NULL,
-    standard_cluster_domain_grade_id integer NOT NULL
-);
-
-
-ALTER TABLE public.levels_standards_clusters_domains_grades OWNER TO postgres;
-
---
--- Name: multiplication; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE multiplication (
-    id integer NOT NULL,
-    score_needed integer DEFAULT 10 NOT NULL,
-    factor_min integer NOT NULL,
-    factor_max integer NOT NULL,
-    number_of_factors integer DEFAULT 2 NOT NULL,
-    level_id double precision NOT NULL
-);
-
-
-ALTER TABLE public.multiplication OWNER TO postgres;
-
---
--- Name: multiplication_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE multiplication_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.multiplication_id_seq OWNER TO postgres;
-
---
--- Name: multiplication_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE multiplication_id_seq OWNED BY multiplication.id;
-
-
---
 -- Name: passwords; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -360,13 +311,6 @@ ALTER TABLE ONLY levelattempts ALTER COLUMN id SET DEFAULT nextval('level_attemp
 -- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY multiplication ALTER COLUMN id SET DEFAULT nextval('multiplication_id_seq'::regclass);
-
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
 ALTER TABLE ONLY passwords ALTER COLUMN id SET DEFAULT nextval('passwords_id_seq'::regclass);
 
 
@@ -411,17 +355,6 @@ SELECT pg_catalog.setval('error_log_id_seq', 1, false);
 --
 
 COPY learning_standards (id, ref_id, progression, levels) FROM stdin;
-k.cc.a.1	CA9EE2E34F384E95A5FA26769C5864B8	1.000	11
-k.cc.a.2	5E6A3E3B939B4577B104FA8658206E9E	2.000	6
-k.cc.a.3	C11F30815A9C49B9A83B61A285EA11F9	3.000	1
-k.cc.c.6	66626D8AEE4E474B8CFEC8A4B68AA51C	4.000	1
-k.cc.c.7	C9B9CAD5BDE84CE2A7A0C441A3DF1A2D	5.000	1
-k.oa.a.4	0CFFCBC851984A4281C23D34FC400445	7.000	18
-k.oa.a.5	1353E9D5614D460FA32E67853B6BA6D8	8.000	42
-k.nbt.a.1	ED150B29EFD14FF8B655FA3F2CA4FE6D	8.000	9
-1.oa.c.6	6C33D2BEC1AC431C8FC4BF9FD4DD3DCA	9.000	79
-2.oa.b.2	800715566B824BB3A5A8C464E961C2B4	10.000	304
-3.oa.c.7	3D384CB2349B41299A3B5A133AB9E3F8	11.000	218
 \.
 
 
@@ -441,73 +374,10 @@ COPY levelattempts (id, start_time, end_time, user_id, level, ref_id, score, tim
 
 
 --
--- Data for Name: levels_standards_clusters_domains_grades; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY levels_standards_clusters_domains_grades (level_id, standard_cluster_domain_grade_id) FROM stdin;
-\.
-
-
---
--- Data for Name: multiplication; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY multiplication (id, score_needed, factor_min, factor_max, number_of_factors, level_id) FROM stdin;
-1	10	0	1	2	600
-2	10	0	2	2	600.009999999999991
-3	10	0	3	2	600.019999999999982
-4	10	0	4	2	600.029999999999973
-5	10	0	5	2	600.039999999999964
-6	10	0	6	2	600.049999999999955
-7	10	0	7	2	600.059999999999945
-8	10	0	8	2	600.07000000000005
-9	10	0	9	2	600.080000000000041
-10	10	0	10	2	600.090000000000032
-11	10	0	11	2	600.100000000000023
-12	10	0	12	2	600.110000000000014
-\.
-
-
---
--- Name: multiplication_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('multiplication_id_seq', 12, true);
-
-
---
 -- Data for Name: passwords; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY passwords (id, password) FROM stdin;
-1	ahh
-2	abs
-3	ace
-4	add
-5	aft
-6	ape
-7	and
-8	aim
-9	aid
-10	air
-11	all
-12	amp
-13	ant
-14	app
-15	apt
-16	arc
-17	arf
-18	ark
-19	arm
-20	art
-21	ash
-22	ask
-23	ate
-24	ave
-25	awe
-26	axe
-27	aye
-28	ays
 \.
 
 
@@ -515,7 +385,7 @@ COPY passwords (id, password) FROM stdin;
 -- Name: passwords_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('passwords_id_seq', 28, true);
+SELECT pg_catalog.setval('passwords_id_seq', 1, false);
 
 
 --
@@ -618,22 +488,6 @@ ALTER TABLE ONLY learning_standards
 
 ALTER TABLE ONLY levelattempts
     ADD CONSTRAINT levelattempts_pkey PRIMARY KEY (id);
-
-
---
--- Name: levels_standards_clusters_domains_grades_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
---
-
-ALTER TABLE ONLY levels_standards_clusters_domains_grades
-    ADD CONSTRAINT levels_standards_clusters_domains_grades_pkey PRIMARY KEY (level_id, standard_cluster_domain_grade_id);
-
-
---
--- Name: multiplication_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
---
-
-ALTER TABLE ONLY multiplication
-    ADD CONSTRAINT multiplication_pkey PRIMARY KEY (id);
 
 
 --
