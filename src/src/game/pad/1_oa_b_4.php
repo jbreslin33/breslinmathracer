8b224ea3d5eedd9e820cfee94d8b7cd027f6c529
jbreslin33 <jbreslin33@localhost>
2014-01-06 17:37:14 -0500

    home. next mod on b4

--- src/src/game/pad/1_oa_b_4.php
+++ src/src/game/pad/1_oa_b_4.php
@@ -6,55 +6,47 @@ Extends: Pad,
 	initialize: function(application)
 	{
        		this.parent(application);
-	       	
-		//input pad
-                this.mInputPad = new NumberPad(application);
+		
+		this.setScoreNeeded(4);
+
+		this.mThresholdTime = 120000;
+
+                //input pad
+                this.mInputPad = new BigQuestionNumberPad(application);
+
+		//word problems
+		this.mWordProblems = new WordProblems();
 	},
+
+	createCorrectAnswerBar: function()
+        {
+                //question bar header
+                if (!this.mCorrectAnswerBarHeader)
+                {
+                        this.mCorrectAnswerBarHeader = new Shape(150,50,300,50,this,"","","");
+                        this.mCorrectAnswerBarHeader.mMesh.innerHTML = '';
+                }
+
+                //question bar
+                if (!this.mCorrectAnswerBar)
+                {
+                        this.mCorrectAnswerBar = new Shape(200,200,50,100,this,"","","");
+                        this.mCorrectAnswerBar.mMesh.innerHTML = '';
+                }
+        },
 
 	createQuestions: function()
         {
  		this.parent();
+		
+		var totalAddition        = 0;
+		var totalSubtraction     = 0;
+		var totalAdditionGoal    = parseInt( parseInt(this.mScoreNeeded / 2) - parseInt(1));
+		var totalSubtractionGoal = parseInt( parseInt(this.mScoreNeeded / 2) - parseInt(1));
 
-		//add
-                this.mQuiz.mQuestionPoolArray.push(new Question('9 + ? = 10','1','9 + 1 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('8 + ? = 10','2','8 + 2 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('7 + ? = 10','3','7 + 3 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('6 + ? = 10','4','6 + 4 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('5 + ? = 10','5','5 + 5 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('4 + ? = 10','6','4 + 6 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('3 + ? = 10','7','3 + 7 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('2 + ? = 10','8','2 + 8 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('1 + ? = 10','9','1 + 9 = 10'));
-		//9
-
-		this.mQuiz.mQuestionPoolArray.push(new Question('1 + ? = 10','9','1 + 9 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('2 + ? = 10','8','2 + 8 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('3 + ? = 10','7','3 + 7 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('4 + ? = 10','6','4 + 6 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('5 + ? = 10','5','5 + 5 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('6 + ? = 10','4','6 + 4 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('7 + ? = 10','3','7 + 3 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('8 + ? = 10','2','8 + 2 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('9 + ? = 10','1','9 + 1 = 10'));
-		//18
-
-		var totalNewGoal       = parseInt(this.mScoreNeeded / 2);
-		var totalNew           = 0;
-		var newQuestionElement = 0;
-   		var elementCounter     = 0;
-                
-                for (i = 0; i <= 17; i++)
-                {
-                        if (this.mApplication.mLevel == i)
-                        {
-                                newQuestionElement = elementCounter;
-                        }
-                        elementCounter++;
-                }
-
-		while (totalNew < totalNewGoal)
+		while (totalAddition < totalAdditionGoal || totalSubtraction < totalSubtractionGoal)
 		{	
-			//reset vars and arrays
+			//RESET as we have either just started or failed to reach totalNewGoal
 			totalNew = 0;
 			for (d = 0; d < this.mQuiz.mQuestionArray.length; d++)
 			{
@@ -63,34 +55,22 @@ Extends: Pad,
 			this.mQuiz.mQuestionArray = 0;
 			this.mQuiz.mQuestionArray = new Array();
 
+			//ADD questions
 			for (s = 0; s < this.mScoreNeeded; s++)
 			{	
 				//50% chance of asking newest question
 				var randomChance = Math.floor((Math.random()*2));		
 				if (randomChance == 0)
 				{
-					this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[newQuestionElement]);
-					totalNew++;
+					this.mQuiz.mQuestionArray.push(this.mWordProblems.getAdditionQuestion(20,2));
+					totalAddition++;
 				}	
 				if (randomChance == 1)
 				{
-					var randomElement = Math.floor((Math.random()*newQuestionElement));		
-					this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[randomElement]);
+					this.mQuiz.mQuestionArray.push(this.mWordProblems.getSubtractionQuestion(20));
+					totalSubtraction++;
 				}
 			}
 		}
-
-	},
- 
-	showCorrectAnswer: function()
-        {
-		this.parent();
-                this.mCorrectAnswerBar.mMesh.innerHTML = '' + this.mQuiz.getQuestion().getShowAnswer();
-        },
-
-	showCorrectAnswerOutOfTime: function()
-        {
-                this.parent();
-                this.mCorrectAnswerBar.mMesh.innerHTML = '' + this.mQuiz.getQuestion().getShowAnswer();
-        }
+	}
 });
