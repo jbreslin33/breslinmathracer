d68fd459d2bff1bdd5fa1c22b2480f702220625e
jbreslin33 <jbreslin33@localhost>
2014-01-06 09:52:03 -0500

    commutative.

--- src/src/game/pad/1_oa_b_3.php
+++ src/src/game/pad/1_oa_b_3.php
@@ -9,6 +9,8 @@ Extends: Pad,
 	       	
                 //input pad
                 this.mInputPad = new LongQuestionNumberPad(application);
+
+                this.mThresholdTime = 60000;
         },
 
         createCorrectAnswerBar: function()
@@ -54,39 +56,39 @@ Extends: Pad,
  		this.parent();
 
 		//commutative
+		//10
+                this.mQuiz.mQuestionPoolArray.push(new Question('9 + 1 = 10 and 1 + ? = 10','9','9 + 1 = 10 and 1 + 9 = 10'));
+                this.mQuiz.mQuestionPoolArray.push(new Question('8 + 2 = 10 and 2 + ? = 10','8','8 + 2 = 10 and 2 + 8 = 10'));
+                this.mQuiz.mQuestionPoolArray.push(new Question('7 + 3 = 10 and 3 + ? = 10','7','7 + 3 = 10 and 3 + 7 = 10'));
+                this.mQuiz.mQuestionPoolArray.push(new Question('6 + 4 = 10 and 4 + ? = 10','6','6 + 4 = 10 and 4 + 6 = 10'));
 
-                this.mQuiz.mQuestionPoolArray.push(new Question('8 + 3 = 11 and 3 + ? = 11','8','8 + 3 = 11 and 3 + 8 = 11'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('8 + 3 = 11 and 3 + ? = 11','8','8 + 3 = 11 and 3 + 8 = 11'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('8 + 3 = 11 and 3 + ? = 11','8','8 + 3 = 11 and 3 + 8 = 11'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('8 + 3 = 11 and 3 + ? = 11','8','8 + 3 = 11 and 3 + 8 = 11'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('8 + 3 = 11 and 3 + ? = 11','8','8 + 3 = 11 and 3 + 8 = 11'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('8 + 3 = 11 and 3 + ? = 11','8','8 + 3 = 11 and 3 + 8 = 11'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('8 + 3 = 11 and 3 + ? = 11','8','8 + 3 = 11 and 3 + 8 = 11'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('8 + 3 = 11 and 3 + ? = 11','8','8 + 3 = 11 and 3 + 8 = 11'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('8 + 3 = 11 and 3 + ? = 11','8','8 + 3 = 11 and 3 + 8 = 11'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('8 + 3 = 11 and 3 + ? = 11','8','8 + 3 = 11 and 3 + 8 = 11'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('8 + 3 = 11 and 3 + ? = 11','8','8 + 3 = 11 and 3 + 8 = 11'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('8 + 3 = 11 and 3 + ? = 11','8','8 + 3 = 11 and 3 + 8 = 11'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('8 + 3 = 11 and 3 + ? = 11','8','8 + 3 = 11 and 3 + 8 = 11'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('8 + 3 = 11 and 3 + ? = 11','8','8 + 3 = 11 and 3 + 8 = 11'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('8 + 3 = 11 and 3 + ? = 11','8','8 + 3 = 11 and 3 + 8 = 11'));
+		//3
+                this.mQuiz.mQuestionPoolArray.push(new Question('1 + 2  = 3 and 2 + ? = 3','1','1 + 2 = 3 and 2 + 1 = 3'));
 
-		//18
+		//4
+                this.mQuiz.mQuestionPoolArray.push(new Question('1 + 3  = 4 and 3 + ? = 4','1','1 + 3 = 4 and 3 + 1 = 4'));
+	
+		//5	
+                this.mQuiz.mQuestionPoolArray.push(new Question('2 + 3  = 5 and 3 + ? = 5','2','2 + 3 = 5 and 3 + 2 = 5'));
+
+		//6
+                this.mQuiz.mQuestionPoolArray.push(new Question('2 + 4  = 6 and 4 + ? = 6','2','2 + 4 = 6 and 4 + 2 = 6'));
+
+		//7
+                this.mQuiz.mQuestionPoolArray.push(new Question('3 + 4  = 7 and 4 + ? = 7','3','3 + 4 = 7 and 4 + 3 = 7'));
+
+		//8
+                this.mQuiz.mQuestionPoolArray.push(new Question('3 + 5  = 8 and 5 + ? = 8','3','3 + 5 = 8 and 5 + 3 = 8'));
+		
+		//9
+                this.mQuiz.mQuestionPoolArray.push(new Question('4 + 5  = 9 and 5 + ? = 9','4','4 + 5 = 9 and 5 + 4 = 9'));
+
+
+		//associative
 
 		var totalNewGoal       = parseInt(this.mScoreNeeded / 2);
 		var totalNew           = 0;
-		var newQuestionElement = 0;
-   		var elementCounter     = 0;
                 
-                for (i = 0; i <= 17; i++)
-                {
-                        if (this.mApplication.mLevel == i)
-                        {
-                                newQuestionElement = elementCounter;
-                        }
-                        elementCounter++;
-                }
-
 		while (totalNew < totalNewGoal)
 		{	
 			//reset vars and arrays
@@ -104,12 +106,12 @@ Extends: Pad,
 				var randomChance = Math.floor((Math.random()*2));		
 				if (randomChance == 0)
 				{
-					this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[newQuestionElement]);
+					this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[this.mApplication.mLevel]);
 					totalNew++;
 				}	
 				if (randomChance == 1)
 				{
-					var randomElement = Math.floor((Math.random()*newQuestionElement));		
+					var randomElement = Math.floor((Math.random()*this.mApplication.mLevel));		
 					this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[randomElement]);
 				}
 			}
