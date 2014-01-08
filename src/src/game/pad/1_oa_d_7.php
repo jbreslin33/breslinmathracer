a168b827da49681999a58e2cdb2085db773590a2
jbreslin33 <jbreslin33@localhost>
2014-01-08 16:51:57 -0500

    still working on d_7 but need to fix kb5

--- src/src/game/pad/1_oa_d_7.php
+++ src/src/game/pad/1_oa_d_7.php
@@ -14,51 +14,58 @@ Extends: Pad,
 		this.mInputPad = new ButtonMultipleChoicePad(application);
 	},
 
-	showQuestion: function()
-	{
-		this.mInputPad.showQuestion();	
-	},
- 
-	showCorrectAnswer: function()
-	{
-		this.parent();
-        	this.mCorrectAnswerBar.mMesh.innerHTML = '' + this.mQuiz.getQuestion().getAnswer();
-	},
-   
 	createQuestions: function()
         {
 		this.parent();
 
-              	for (i = 0; d < this.mQuiz.mQuestionArray.length; i++)
+		this.log('createQuestions');
+
+              	for (i = 0; i < this.mQuiz.mQuestionArray.length; i++)
                	{
                         this.mQuiz.mQuestionArray[i] = 0;
                 }
                 this.mQuiz.mQuestionArray = 0;
                 this.mQuiz.mQuestionArray = new Array();
-
-		var question = new Question('7 + 1 = 9','false');
-		this.mQuiz.mQuestionArray.push(question);
-		
-		var question = new Question('7 + 1 = 9','false');
-		question.setChoice('A','true');
-		question.setChoice('B','false');
-		this.mQuiz.mQuestionArray.push(question);
-              	
-		for (d = 0; d < this.mQuiz.mQuestionArray.length; d++)
-		{
-			this.mQuiz.mQuestionArray[i].setChoice('B','false');
-
-		}
 	
-	},
+		var equalOrNot = Math.floor((Math.random()*2));
+		var addendA = 0;
+		var addendB = 0;
+		var sumReal = 100;
+		var sumFake = 100;
 
-	//state overides
-	showCorrectAnswerOutOfTime: function()
-        {
-                this.mCorrectAnswerStartTime = this.mTimeSinceEpoch;
-                this.mInputPad.hide();
-                this.mCorrectAnswerBar.mMesh.innerHTML = '' + this.mQuiz.getQuestion().getAnswer();
-                this.showCorrectAnswerBar();
-                this.showClockShape();
-        }
+              	for (i = 0; i < this.mScoreNeeded; i++)
+		{
+			if (equalOrNot == 0)
+			{
+				while(sumReal > 10)
+				{
+					addendA = Math.floor((Math.random()*10)+1);
+					addendA = Math.floor((Math.random()*10)+1);
+					sumReal = parseInt(addendA + addendB);
+				
+					var question = new Question('' + addendA + ' + ' + addendB + ' = ' + sumReal,'true');
+					this.mQuiz.mQuestionArray.push(question);
+				}
+			}
+			else if (equalOrNot == 1)
+			{
+				while(sumReal > 10 || sumFake == sumReal )
+				{
+					addendA = Math.floor((Math.random()*10)+1);
+					addendA = Math.floor((Math.random()*10)+1);
+					sumReal = parseInt(addendA + addendB);
+					sumFake = Math.floor((Math.random()*10)+1);
+				
+					var question = new Question('' + addendA + ' + ' + addendB + ' = ' + sumFake,'false');
+					this.mQuiz.mQuestionArray.push(question);
+				}
+			}
+		}
+
+		for (i = 0; i < this.mQuiz.mQuestionArray.length; i++)
+		{
+			this.mQuiz.mQuestionArray[i].setChoice('A','true');
+			this.mQuiz.mQuestionArray[i].setChoice('B','false');
+		}
+	}
 });
