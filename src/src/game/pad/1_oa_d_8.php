270e555a6d4f8d428569e504001d491ddec5eaae
jbreslin33 <jbreslin33@localhost>
2014-01-12 14:46:31 -0500

    in the middle

--- src/src/game/pad/1_oa_d_8.php
+++ src/src/game/pad/1_oa_d_8.php
@@ -37,14 +37,68 @@ Extends: Pad,
                 this.mQuiz.mQuestionArray = new Array();
 
 		var minusOrNot = Math.floor((Math.random()*2));
+		var StandardFormOrNot = Math.floor((Math.random()*2));
+		var missingVar = Math.floor((Math.random()*3)+1);
 		var varA = 0;
 		var varB = 0;
-		var varC = 0;
+		var varC = 100;
 
               	for (i = 0; i < this.mScoreNeeded; i++)
 		{
+			//addition
 			if (minusOrNot == 0)
 			{
+				while(VarC > 20)
+				{
+					varA = Math.floor((Math.random()*20)+1);
+					varB = Math.floor((Math.random()*20)+1);
+					varC = parseInt(VarA + VarB);
+				}
+				//ok we have an equation with sum < 20  in the form a+b=c
+
+				//a+b=c
+				if (standardFormOrNot == 0)
+				{
+					if (missingVar == 0)
+					{
+						var question = new Question('? + ' + VarB + ' = ' + VarC,'' + VarA);
+						this.mQuiz.mQuestionArray.push(question);
+					}
+					else if (missingVar == 1)
+					{
+						var question = new Question('' + VarA + ' ? ' = ' + VarC,'' + VarB);
+						this.mQuiz.mQuestionArray.push(question);
+					}
+					else if (missingVar == 2)
+					{
+						var question = new Question('' + VarA + ' + ' + VarB + ' = ' + VarC,'' + VarC);
+						this.mQuiz.mQuestionArray.push(question);
+					}
+				
+				}
+				//c=a+b
+				else if (standardFormOrNot == 1)
+				{
+   					if (missingVar == 0)
+                                        {
+                                                var question = new Question('' + VarC + ' = ' + VarA + ' + ' + VarB,'' + VarC);
+                                                this.mQuiz.mQuestionArray.push(question);
+                                        }
+                                        else if (missingVar == 1)
+                                        {
+                                                var question = new Question('' + VarA + ' ? ' = ' + VarC,'' + VarB);
+                                                this.mQuiz.mQuestionArray.push(question);
+                                        }
+                                        else if (missingVar == 2)
+                                        {
+                                                var question = new Question('' + VarA + ' + ' + VarB + ' = ' + VarC,'' + VarC);
+                                                this.mQuiz.mQuestionArray.push(question);
+                                        }
+
+				
+				}
+
+
 				while(sumReal > 20)
 				{
 					varA = Math.floor((Math.random()*20)+1);
@@ -57,16 +111,6 @@ Extends: Pad,
 			}
 			else if (minusOrNot == 1)
 			{
-				while(sumReal > 10 || sumFake == sumReal )
-				{
-					addendA = Math.floor((Math.random()*10)+1);
-					addendA = Math.floor((Math.random()*10)+1);
-					sumReal = parseInt(addendA + addendB);
-					sumFake = Math.floor((Math.random()*10)+1);
-				
-					var question = new Question('' + addendA + ' + ' + addendB + ' = ' + sumFake,'false');
-					this.mQuiz.mQuestionArray.push(question);
-				}
 			}
 		}
 
