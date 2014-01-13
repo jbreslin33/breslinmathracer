64ae06abeef8b6b46d4e2e48bbcef3a5f0d4d5fa
jbreslin33 <jbreslin33@localhost>
2014-01-13 10:58:36 -0500

    1.nbt.c.6 complete. next standard.

--- src/src/game/pad/1_nbt_c_6.php
+++ src/src/game/pad/1_nbt_c_6.php
@@ -22,7 +22,6 @@ Extends: Pad,
 		var varA = 0;
 		var varB = 0;
 		var varC = 0;
-		var randomChance = 0;
 
 		//reset vars and arrays
 		for (d = 0; d < this.mQuiz.mQuestionArray.length; d++)
@@ -36,34 +35,15 @@ Extends: Pad,
 			varC = 100;
 			while (varC > 99 || varC < 0)
 			{	
-				//50% chance of different type of question 
-				randomChance = Math.floor((Math.random()*2));		
-
-				//10 more
-				if (randomChance == 0)
-				{
-					varA = Math.floor((Math.random()*99)+1);		
-					varB = 10		
-					varC = parseInt(varA + varB);
-				}	
-
-				//10 less
-				if (randomChance == 1)
-				{
-					varA = Math.floor((Math.random()*99)+1);		
-					varB = 10;		
-					varC = parseInt(varA - varB);
-				}
+				varA = Math.floor((Math.random()*9)+1);		
+				varA = varA * 10;
+				
+				varB = Math.floor((Math.random()*9)+1);		
+				varB = varB * 10;
+				varC = parseInt(varA - varB);
 			}
 			//ok we broke while now add question...
-			if (randomChance == 0)
-			{
-				this.mQuiz.mQuestionArray.push(new Question('' + varA + ' + ' +  varB + ' = ', '' + varC));
-			}
-			else
-			{
-				this.mQuiz.mQuestionArray.push(new Question('' + varA + ' - ' +  varB + ' = ', '' + varC));
-			}
+			this.mQuiz.mQuestionArray.push(new Question('' + varA + ' - ' +  varB + ' = ', '' + varC));
 		}
 	}
 });
