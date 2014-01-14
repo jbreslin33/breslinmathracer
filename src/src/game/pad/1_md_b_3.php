8aaf357486cdd143327150592f504f4dd57b541b
jbreslin33 <jbreslin33@localhost>
2014-01-14 17:50:54 -0500

    dumber than a tree trunk...

--- src/src/game/pad/1_md_b_3.php
+++ src/src/game/pad/1_md_b_3.php
@@ -12,6 +12,13 @@ Extends: Pad,
 
 		//input pad
 		this.mInputPad = new ButtonMultipleChoicePad(application);
+
+	},
+	
+	update: function()
+	{
+		this.parent();
+		this.updateClock();
 	},
 
 	reset: function()
@@ -73,7 +80,7 @@ Extends: Pad,
 
 		//1 circle
 		var question = new Question('What is this?','circle');
-		question.setChoice('A','circle');
+		question.setChoice('A','dddddcircle');
 		question.setChoice('B','cone');
 		this.mQuiz.mQuestionArray.push(question);
 	
@@ -136,22 +143,23 @@ Extends: Pad,
 
 	createQuestionShapes: function()
 	{
-		this.log('createQ');
 		this.destroyShapes();
 
 		this.mShapeArray = new Array();		
+		
+		this.drawClock();	
 
-                this.mShapeArray.push(new Shape(200,200,150,275,this,"/images/shapes/circle.png","",""));
-                this.mShapeArray.push(new Shape(200,200,150,275,this,"/images/shapes/cone.png","",""));
-                this.mShapeArray.push(new Shape(200,200,150,275,this,"/images/shapes/cube.jpg","",""));
-                this.mShapeArray.push(new Shape(200,200,150,275,this,"/images/shapes/cylinder.png","",""));
-                this.mShapeArray.push(new Shape(200,200,150,275,this,"/images/shapes/hexagon.png","",""));
-                this.mShapeArray.push(new Shape(200,200,150,275,this,"/images/shapes/rectangle.png","",""));
-                this.mShapeArray.push(new Shape(200,200,150,275,this,"/images/shapes/sphere.png","",""));
-                this.mShapeArray.push(new Shape(200,200,150,275,this,"/images/shapes/square.png","",""));
-                this.mShapeArray.push(new Shape(200,200,150,275,this,"/images/shapes/triangle.png","",""));
-                this.mShapeArray.push(new Shape(200,200,150,275,this,"/images/shapes/circle.png","",""));
-                	
+                this.mShapeArray.push(new Shape(200,200,450,275,this,"/images/shapes/circle.png","",""));
+                this.mShapeArray.push(new Shape(200,200,450,275,this,"/images/shapes/cone.png","",""));
+                this.mShapeArray.push(new Shape(200,200,450,275,this,"/images/shapes/cube.jpg","",""));
+                this.mShapeArray.push(new Shape(200,200,450,275,this,"/images/shapes/cylinder.png","",""));
+                this.mShapeArray.push(new Shape(200,200,450,275,this,"/images/shapes/hexagon.png","",""));
+                this.mShapeArray.push(new Shape(200,200,450,275,this,"/images/shapes/rectangle.png","",""));
+                this.mShapeArray.push(new Shape(200,200,450,275,this,"/images/shapes/sphere.png","",""));
+                this.mShapeArray.push(new Shape(200,200,450,275,this,"/images/shapes/square.png","",""));
+                this.mShapeArray.push(new Shape(200,200,450,275,this,"/images/shapes/triangle.png","",""));
+                this.mShapeArray.push(new Shape(200,200,450,275,this,"/images/shapes/circle.png","",""));
+               	
 		for (i = 0; i < this.mShapeArray.length; i++)
 		{
 			this.mShapeArray[i].setVisibility(false);
@@ -172,5 +180,42 @@ Extends: Pad,
                 this.showCorrectAnswerBar();
                 this.showClockShape();
         },
+
+	drawClock: function ()
+	{
+		canvas = Raphael("clock_id",200, 200);
+                var clock = canvas.circle(100,100,95);
+                clock.attr({"fill":"#f5f5f5","stroke":"#444444","stroke-width":"5"})  
+                var hour_sign;
+                for(i=0;i<12;i++)
+		{
+                	var start_x = 100+Math.round(80*Math.cos(30*i*Math.PI/180));
+                   	var start_y = 100+Math.round(80*Math.sin(30*i*Math.PI/180));
+                    	var end_x = 100+Math.round(90*Math.cos(30*i*Math.PI/180));
+                    	var end_y = 100+Math.round(90*Math.sin(30*i*Math.PI/180));    
+                    	hour_sign = canvas.path("M"+start_x+" "+start_y+"L"+end_x+" "+end_y);
+                }    
+                hour_hand = canvas.path("M100 100L100 50");
+                hour_hand.attr({stroke: "#444444", "stroke-width": 6});
+                minute_hand = canvas.path("M100 100L100 40");
+                minute_hand.attr({stroke: "#444444", "stroke-width": 4});
+                second_hand = canvas.path("M100 110L100 25");
+                second_hand.attr({stroke: "#444444", "stroke-width": 2});
+                var pin = canvas.circle(100, 100, 5);
+                pin.attr("fill", "#000000");    
+                this.updateClock()
+	},
+ 	
+	function: updateClock()
+	{
+                var now = new Date();
+                var hours = now.getHours();
+                var minutes = now.getMinutes();
+                var seconds = now.getSeconds();
+                hour_hand.rotate(30*hours+(minutes/2.5), 100, 100);
+                minute_hand.rotate(6*minutes, 100, 100);
+                second_hand.rotate(6*seconds, 100, 100);
+       }
+	
 
 });
