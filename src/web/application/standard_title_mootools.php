ff80fd9c78503260c4e67c92ee2567a76144306d
jbreslin33 <jbreslin33@localhost>
2014-01-14 20:10:09 -0500

    fixed paths.

--- src/web/application/standard_title_mootools.php
+++ src/web/application/standard_title_mootools.php
@@ -4,8 +4,8 @@
 <title>ABC AND YOU</title>
 
 <!-- mootools -->
-<script type="text/javascript" src="/src/mootools/mootools-core-1.4.5-full-compat.js"></script>
 <script type="text/javascript" src="/src/raphael/raphael-min.js"></script>
+<script type="text/javascript" src="/src/mootools/mootools-core-1.4.5-full-compat.js"></script>
 
 <?php
 include(getenv("DOCUMENT_ROOT") . "/web/login/check_login.php");
