76288667f54649e45b3e534e2332673a999f1770
jbreslin33 <jbreslin33@localhost>
2014-01-19 15:53:04 -0500

    no errors but no teachers...

--- src/web/insert/insert.php
+++ src/web/insert/insert.php
@@ -3,6 +3,8 @@
 <html>
 
 <head>
+<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+<meta content="utf-8" http-equiv="encoding">
 <link rel="stylesheet" type="text/css" href="<?php getenv("DOCUMENT_ROOT")?>/css/green_block.css" />
 </head>
 
