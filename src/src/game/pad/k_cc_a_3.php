b4640ff1516219eb698a07830dbb60dc067b4a01
jbreslin33 <jbreslin33@localhost>
2014-01-18 19:00:40 -0500

    counting objects giving 1 minute.

--- src/src/game/pad/k_cc_a_3.php
+++ src/src/game/pad/k_cc_a_3.php
@@ -11,7 +11,7 @@ Extends: Pad,
 		this.mCountShapeArray = new Array();
 	
 		//answers 
-                this.mThresholdTime = 10000;
+                this.mThresholdTime = 60000;
 
 		//input pad
 		this.mInputPad = new NumberPad(application,application.mGame);
