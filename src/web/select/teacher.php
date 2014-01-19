e0d346c3bc87fc0aea8ef74a0bda851802bcca1f
jbreslin33 <jbreslin33@localhost>
2014-01-19 16:07:56 -0500

    fixed teachers.

--- src/web/select/teacher.php
+++ src/web/select/teacher.php
@@ -6,12 +6,55 @@
 <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
 <meta content="utf-8" http-equiv="encoding">
 <link rel="stylesheet" type="text/css" href="<?php getenv("DOCUMENT_ROOT")?>/css/green_block.css" />
+<?php
+session_start();
+//db connection
+include(getenv("DOCUMENT_ROOT") . "/src/database/db_connect.php");
+$conn = dbConnect();
+?>
+
 </head>
 
 <body>
 
 <br><b><u>My Teachers:<u><b><br>
+<?php
+$query = "select teachers.id,  users.username, users.password, users.first_name, users.last_name from teachers join users on teachers.id = users.id where users.school_id = ";
+$query .= $_SESSION["school_id"];
+$query .= ";";
 
+$result = pg_query($conn,$query);
+dbErrorCheck($conn,$result);
+$numrows = pg_numrows($result);
+?>
+
+<table border="1">
+  <tr>
+   <th>ID</th>
+   <th>USERNAME</th>
+   <th>PASSWORD</th>
+   <th>FIRST NAME</th>
+   <th>LAST NAME</th>
+  </tr>
+
+<?php
+   // Loop on rows in the result set.
+
+   for($ri = 0; $ri < $numrows; $ri++) {
+    echo "<tr>\n";
+    $row = pg_fetch_array($result, $ri);
+    echo " <td>", $row["id"], "</td>
+   <td>", $row["username"], "</td>
+   <td>", $row["password"], "</td>
+   <td>", $row["first_name"], "</td>
+   <td>", $row["last_name"], "</td>
+  </tr>
+  ";
+   }
+   pg_close($conn);
+  ?>
+
+  </table>
 </body>
 
 </html>
