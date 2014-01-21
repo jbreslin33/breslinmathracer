f302a4f054aaaabda3691b6be3d38dcff423c615
jbreslin33 <jbreslin33@localhost>
2014-01-20 20:02:41 -0500

    monster shows up

--- src/src/game/pad/k_g_a_1.php
+++ src/src/game/pad/k_g_a_1.php
@@ -14,110 +14,52 @@ Extends: Pad,
 		this.mInputPad = new ButtonMultipleChoicePad(application);
 	},
 
-	reset: function()
-	{
-		this.parent();
-	
-		this.hideAllShapeArrays();
-	},
-	
-	destroyShapes: function()
-	{
-		this.parent();
-	
-		if (this.BesideShapeArray)
-		{
-			//shapes and array
-                	for (i = 0; i < this.mBesideShapeArray.length; i++)
-                	{
-                        	this.mBesideShapeArray[i].mDiv.mDiv.removeChild(this.mBesideShapeArray[i].mMesh);
-                        	document.body.removeChild(this.mBesideShapeArray[i].mDiv.mDiv);
-                       		this.mBesideShapeArray[i] = 0;
-                        
-				this.mAboveShapeArray[i].mDiv.mDiv.removeChild(this.mAboveShapeArray[i].mMesh);
-                        	document.body.removeChild(this.mAboveShapeArray[i].mDiv.mDiv);
-                        	this.mAboveShapeArray[i] = 0;
-			
-				this.mBehindShapeArray[i].mDiv.mDiv.removeChild(this.mBehindShapeArray[i].mMesh);
-                        	document.body.removeChild(this.mBehindShapeArray[i].mDiv.mDiv);
-                        	this.mBehindShapeArray[i] = 0;
-			
-				this.mInFrontOfShapeArray[i].mDiv.mDiv.removeChild(this.mInFrontOfShapeArray[i].mMesh);
-                        	document.body.removeChild(this.mInFrontOfShapeArray[i].mDiv.mDiv);
-                        	this.mInFrontOfShapeArray[i] = 0;
-			
-				this.mBelowShapeArray[i].mDiv.mDiv.removeChild(this.mBelowShapeArray[i].mMesh);
-                        	document.body.removeChild(this.mBelowShapeArray[i].mDiv.mDiv);
-                        	this.mBelowShapeArray[i] = 0;
-			
-				this.mNextToShapeArray[i].mDiv.mDiv.removeChild(this.mNextToShapeArray[i].mMesh);
-                        	document.body.removeChild(this.mNextToShapeArray[i].mDiv.mDiv);
-                        	this.mNextToShapeArray[i] = 0;
-			}
-                	this.mBesideShapeArray = 0;
-                	this.mAboveShapeArray = 0;
-                	this.mBehindShapeArray = 0;
-                	this.mInFrontOfShapeArray = 0;
-                	this.mBelowShapeArray = 0;
-                	this.mNextToShapeArray = 0;
-                }
-	},
-
 	showQuestion: function()
 	{
-		this.hideAllShapeArrays();
-
 		this.mInputPad.showQuestion();	
+		
+		for (i = 0; i < this.mShapeArray.length; i++)
+		{
+			this.mShapeArray[i].setVisibility(false);
+               		this.mShapeArray[i].mCollidable = false;
+               		this.mShapeArray[i].mCollisionOn = false;
+		}
 	
 		//show question shapes	
 		if (this.mQuiz.getQuestion().getAnswer() == 'beside')
 		{
-                	for (i = 0; i < this.mBesideShapeArray.length; i++)
-			{
-				this.mBesideShapeArray[i].setVisibility(true);
-			}
-		}
-                
+			this.mShapeArray[0].setVisibility(true);
+			this.mShapeArray[1].setVisibility(true);
+               	} 
 		if (this.mQuiz.getQuestion().getAnswer() == 'above')
                 {
-                        for (i = 0; i < this.mAboveShapeArray.length; i++)
-                        {
-                                this.mAboveShapeArray[i].setVisibility(true);
-                        }
-                }       
+			this.mShapeArray[2].setVisibility(true);
+			this.mShapeArray[3].setVisibility(true);
+		}       
 
 		if (this.mQuiz.getQuestion().getAnswer() == 'behind')
                 {
-                        for (i = 0; i < this.mBehindShapeArray.length; i++)
-                        {
-                                this.mBehindShapeArray[i].setVisibility(true);
-                        }
+			this.mShapeArray[4].setVisibility(true);
+			this.mShapeArray[5].setVisibility(true);
                 }       
 		
 		if (this.mQuiz.getQuestion().getAnswer() == 'in front of')
                 {
-                        for (i = 0; i < this.mInFrontOfShapeArray.length; i++)
-                        {
-                                this.mInFrontOfShapeArray[i].setVisibility(true);
-                        }
+			this.mShapeArray[6].setVisibility(true);
+			this.mShapeArray[7].setVisibility(true);
                 }       
 		
 		if (this.mQuiz.getQuestion().getAnswer() == 'below')
                 {
-                        for (i = 0; i < this.mBelowShapeArray.length; i++)
-                        {
-                                this.mBelowShapeArray[i].setVisibility(true);
-                        }
+			this.mShapeArray[8].setVisibility(true);
+			this.mShapeArray[9].setVisibility(true);
                 }       
 		
 		if (this.mQuiz.getQuestion().getAnswer() == 'next to')
                 {
-                        for (i = 0; i < this.mNextToShapeArray.length; i++)
-                        {
-                                this.mNextToShapeArray[i].setVisibility(true);
-                        }
+			this.mShapeArray[10].setVisibility(true);
+			this.mShapeArray[11].setVisibility(true);
                 }       
-	
 	},
  
 	showCorrectAnswer: function()
@@ -206,76 +148,39 @@ Extends: Pad,
 
 	createQuestionShapes: function()
 	{
-		this.destroyShapes();
-
 		this.mShapeArray = new Array();		
             
-	    	//shape arrays
-                this.mBesideShapeArray = new Array();
-                this.mAboveShapeArray = new Array();
-                this.mBehindShapeArray = new Array();
-                this.mInFrontOfShapeArray = new Array();
-                this.mBelowShapeArray = new Array();
-                this.mNextToShapeArray = new Array();
-	
 		//1 beside 
-                this.mBesideShapeArray.push(new Shape(50,50,200,250,this,"/images/monster/red_monster.png","",""));
-                this.mBesideShapeArray.push(new Shape(50,50,150,250,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,200,250,this,"/images/monster/red_monster.png","",""));
+                this.mShapeArray.push(new Shape(50,50,150,250,this,"/images/bus/kid.png","",""));
 
 		//2 above 
-                this.mAboveShapeArray.push(new Shape(50,50,150,200,this,"/images/monster/red_monster.png","",""));
-                this.mAboveShapeArray.push(new Shape(50,50,150,250,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,150,200,this,"/images/monster/red_monster.png","",""));
+                this.mShapeArray.push(new Shape(50,50,150,250,this,"/images/bus/kid.png","",""));
 	
 		//3 behind 
-                this.mBehindShapeArray.push(new Shape(50,50,150,250,this,"/images/monster/red_monster.png","",""));
-                this.mBehindShapeArray.push(new Shape(50,50,150,250,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,150,250,this,"/images/monster/red_monster.png","",""));
+                this.mShapeArray.push(new Shape(50,50,150,250,this,"/images/bus/kid.png","",""));
 
 		//4 in front of 
-                this.mInFrontOfShapeArray.push(new Shape(50,50,150,250,this,"/images/bus/kid.png","",""));
-                this.mInFrontOfShapeArray.push(new Shape(50,50,150,250,this,"/images/monster/red_monster.png","",""));
+                this.mShapeArray.push(new Shape(50,50,150,250,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,150,250,this,"/images/monster/red_monster.png","",""));
                
 		//5 below
-                this.mBelowShapeArray.push(new Shape(50,50,150,250,this,"/images/bus/kid.png","",""));
-                this.mBelowShapeArray.push(new Shape(50,50,150,300,this,"/images/monster/red_monster.png","",""));
+                this.mShapeArray.push(new Shape(50,50,150,250,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,150,300,this,"/images/monster/red_monster.png","",""));
 
 		//6 next to 
-                this.mNextToShapeArray.push(new Shape(50,50,100,250,this,"/images/monster/red_monster.png","",""));
-                this.mNextToShapeArray.push(new Shape(50,50,150,250,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,100,250,this,"/images/monster/red_monster.png","",""));
+                this.mShapeArray.push(new Shape(50,50,150,250,this,"/images/bus/kid.png","",""));
 
-		this.hideAllShapeArrays(); 
-	},
-
-
-	hideAllShapeArrays: function()
-	{
-		for (i = 0; i < this.mBesideShapeArray.length; i++)
+		for (i = 0; i < this.mShapeArray.length; i++)
 		{
-			this.mBesideShapeArray[i].setVisibility(false);
-               		this.mBesideShapeArray[i].mCollidable = false;
-               		this.mBesideShapeArray[i].mCollisionOn = false;
-			
-			this.mAboveShapeArray[i].setVisibility(false);
-               		this.mAboveShapeArray[i].mCollidable = false;
-               		this.mAboveShapeArray[i].mCollisionOn = false;
-			
-			this.mBehindShapeArray[i].setVisibility(false);
-               		this.mBehindShapeArray[i].mCollidable = false;
-               		this.mBehindShapeArray[i].mCollisionOn = false;
-			
-			this.mInFrontOfShapeArray[i].setVisibility(false);
-               		this.mInFrontOfShapeArray[i].mCollidable = false;
-               		this.mInFrontOfShapeArray[i].mCollisionOn = false;
-			
-			this.mBelowShapeArray[i].setVisibility(false);
-               		this.mBelowShapeArray[i].mCollidable = false;
-               		this.mBelowShapeArray[i].mCollisionOn = false;
-			
-			this.mNextToShapeArray[i].setVisibility(false);
-               		this.mNextToShapeArray[i].mCollidable = false;
-               		this.mNextToShapeArray[i].mCollisionOn = false;
-		}	
-
-	},	
+			this.mShapeArray[i].setVisibility(false);
+               		this.mShapeArray[i].mCollidable = false;
+               		this.mShapeArray[i].mCollisionOn = false;
+		}
+	},
 
 	//state overides
         showCorrectAnswerOutOfTime: function()
