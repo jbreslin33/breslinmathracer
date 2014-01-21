df9f83b811318b325af0375993562611d8fb28b4
jbreslin33 <jbreslin33@localhost>
2014-01-20 20:14:22 -0500

    next standard

--- src/src/game/pad/k_g_a_1.php
+++ src/src/game/pad/k_g_a_1.php
@@ -8,7 +8,7 @@ Extends: Pad,
        		this.parent(application);
 
 		//answers 
-                this.mThresholdTime = 10000;
+                this.mThresholdTime = 60000;
 
 		//input pad
 		this.mInputPad = new ButtonMultipleChoicePad(application);
