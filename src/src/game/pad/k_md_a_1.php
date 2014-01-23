dff42cef3c28949e2f27264e78ae90c8097633ca
jbreslin33 <jbreslin33@localhost>
2014-01-22 23:17:10 -0500

    u

--- src/src/game/pad/k_md_a_1.php
+++ src/src/game/pad/k_md_a_1.php
@@ -14,166 +14,86 @@ Extends: Pad,
 		this.mInputPad = new ButtonMultipleChoicePad(application);
 	},
 
-	reset: function()
-	{
-		this.parent();
-		
-		for (i = 0; i < this.mShapeArray.length; i++)
-		{
-			this.mShapeArray[i].setVisibility(false);
-		} 
-	},
-	
-	destroyShapes: function()
-	{
-		this.parent();
-
-		//shapes and array
-                for (i = 0; i < this.mShapeArray.length; i++)
-                {
-                        //back to div
-                        this.mShapeArray[i].mDiv.mDiv.removeChild(this.mShapeArray[i].mMesh);
-                        document.body.removeChild(this.mShapeArray[i].mDiv.mDiv);
-                        this.mShapeArray[i] = 0;
-			this.log('destroyShape:' + i);
-                }
-                this.mShapeArray = 0;
-	},
-
-	showQuestion: function()
-	{
-
-		for (i = 0; i < this.mShapeArray.length; i++)
-                {
-                        this.mShapeArray[i].setVisibility(false);
-                }
-                this.mShapeArray[this.mScore].setVisibility(false);
-
-		this.mInputPad.showQuestion();	
-		
-		//show shape	
-		this.mShapeArray[this.mScore].setVisibility(true);
-	},
- 
-	showCorrectAnswer: function()
-	{
-		this.parent();
-        	this.mCorrectAnswerBar.mMesh.innerHTML = '' + this.mQuiz.getQuestion().getAnswer();
-	},
-   
 	createQuestions: function()
         {
 		this.parent();
 
-              	for (d = 0; d < this.mQuiz.mQuestionArray.length; d++)
-               	{
-                        this.mQuiz.mQuestionArray[d] = 0;
-                }
-                this.mQuiz.mQuestionArray = 0;
-                this.mQuiz.mQuestionArray = new Array();
-	
-		//addendC = Math.floor((Math.random()*5)+2);
+	       	//answer pool
+                this.mQuiz.mAnswerPool.push('tall');
+                this.mQuiz.mAnswerPool.push('short');
+                this.mQuiz.mAnswerPool.push('heavy');
+                this.mQuiz.mAnswerPool.push('light');
+
+		this.mQuiz.resetQuestionArray();
 
 		//tall
 		var question = new Question('What is this?','tall');
-		question.setChoice('A','tall');
-		question.setChoice('B','short');
+		question.mAnswerPool = this.mQuiz.mAnswerPool;	
+		question.mShapeArray.push(this.mShapeArray[0]);
 		this.mQuiz.mQuestionArray.push(question);
 	
 		//short	
 		var question = new Question('What is this?','short');
-		question.setChoice('A','tall');
-		question.setChoice('B','short');
+		question.mAnswerPool = this.mQuiz.mAnswerPool;	
+		question.mShapeArray.push(this.mShapeArray[1]);
 		this.mQuiz.mQuestionArray.push(question);
 
 		//heavy
 		var question = new Question('What is this?','heavy');
-		question.setChoice('A','light');
-		question.setChoice('B','heavy');
+		question.mAnswerPool = this.mQuiz.mAnswerPool;	
+		question.mShapeArray.push(this.mShapeArray[2]);
 		this.mQuiz.mQuestionArray.push(question);
 		
 		//light
 		var question = new Question('What is this?','light');
-		question.setChoice('A','light');
-		question.setChoice('B','heavy');
-		this.mQuiz.mQuestionArray.push(question);
-		
-		//short	
-		var question = new Question('What is this?','short');
-		question.setChoice('A','tall');
-		question.setChoice('B','short');
-		this.mQuiz.mQuestionArray.push(question);
-		
-		//light
-		var question = new Question('What is this?','light');
-		question.setChoice('A','light');
-		question.setChoice('B','heavy');
-		this.mQuiz.mQuestionArray.push(question);
-
-		//heavy
-		var question = new Question('What is this?','heavy');
-		question.setChoice('A','light');
-		question.setChoice('B','heavy');
-		this.mQuiz.mQuestionArray.push(question);
-		
-		//short	
-		var question = new Question('What is this?','short');
-		question.setChoice('A','tall');
-		question.setChoice('B','short');
+		question.mAnswerPool = this.mQuiz.mAnswerPool;	
+		question.mShapeArray.push(this.mShapeArray[3]);
 		this.mQuiz.mQuestionArray.push(question);
 		
 		//tall
 		var question = new Question('What is this?','tall');
-		question.setChoice('A','tall');
-		question.setChoice('B','short');
+		question.mAnswerPool = this.mQuiz.mAnswerPool;	
+		question.mShapeArray.push(this.mShapeArray[0]);
+		this.mQuiz.mQuestionArray.push(question);
+	
+		//short	
+		var question = new Question('What is this?','short');
+		question.mAnswerPool = this.mQuiz.mAnswerPool;	
+		question.mShapeArray.push(this.mShapeArray[1]);
+		this.mQuiz.mQuestionArray.push(question);
+
+		//heavy
+		var question = new Question('What is this?','heavy');
+		question.mAnswerPool = this.mQuiz.mAnswerPool;	
+		question.mShapeArray.push(this.mShapeArray[2]);
 		this.mQuiz.mQuestionArray.push(question);
 		
 		//light
 		var question = new Question('What is this?','light');
-		question.setChoice('A','light');
-		question.setChoice('B','heavy');
+		question.mAnswerPool = this.mQuiz.mAnswerPool;	
+		question.mShapeArray.push(this.mShapeArray[3]);
 		this.mQuiz.mQuestionArray.push(question);
-
-		this.createQuestionShapes();
+		
+		//tall
+		var question = new Question('What is this?','tall');
+		question.mAnswerPool = this.mQuiz.mAnswerPool;	
+		question.mShapeArray.push(this.mShapeArray[0]);
+		this.mQuiz.mQuestionArray.push(question);
+	
+		//short	
+		var question = new Question('What is this?','short');
+		question.mAnswerPool = this.mQuiz.mAnswerPool;	
+		question.mShapeArray.push(this.mShapeArray[1]);
+		this.mQuiz.mQuestionArray.push(question);
 	},
 
 	createQuestionShapes: function()
 	{
-		this.log('createQ');
 		this.destroyShapes();
 
-		this.mShapeArray = new Array();		
-
                 this.mShapeArray.push(new Shape(200,200,150,275,this,"/images/attributes/giraffe.jpg","",""));
                 this.mShapeArray.push(new Shape(50,50,150,400,this,"/images/bus/kid.png","",""));
                 this.mShapeArray.push(new Shape(200,200,150,275,this,"/images/attributes/heavy.gif","",""));
                 this.mShapeArray.push(new Shape(50,50,150,400,this,"/images/attributes/feather.jpg","",""));
-                this.mShapeArray.push(new Shape(50,50,150,400,this,"/images/bus/kid.png","",""));
-                this.mShapeArray.push(new Shape(50,50,150,400,this,"/images/attributes/feather.jpg","",""));
-                this.mShapeArray.push(new Shape(200,200,150,275,this,"/images/attributes/heavy.gif","",""));
-                this.mShapeArray.push(new Shape(50,50,150,400,this,"/images/bus/kid.png","",""));
-                this.mShapeArray.push(new Shape(200,200,150,275,this,"/images/attributes/giraffe.jpg","",""));
-                this.mShapeArray.push(new Shape(50,50,150,400,this,"/images/attributes/feather.jpg","",""));
-                	
-		for (i = 0; i < this.mShapeArray.length; i++)
-		{
-			this.mShapeArray[i].setVisibility(false);
-               		this.mShapeArray[i].mCollidable = false;
-               		this.mShapeArray[i].mCollisionOn = false;
-		}	
-		
-		this.setScoreNeeded(this.mShapeArray.length);
-
-	},
-
-	//state overides
-	showCorrectAnswerOutOfTime: function()
-        {
-                this.mCorrectAnswerStartTime = this.mTimeSinceEpoch;
-                this.mInputPad.hide();
-                this.mCorrectAnswerBar.mMesh.innerHTML = '' + this.mQuiz.getQuestion().getAnswer();
-                this.showCorrectAnswerBar();
-                this.showClockShape();
-        }
-
+	}
 });
--- src/src/game/pad/k_nbt_a_1.php
+++ src/src/game/pad/k_nbt_a_1.php
@@ -38,12 +38,8 @@ Extends: Pad,
 		{	
 			//reset vars and arrays
 			totalNew = 0;
-			for (d = 0; d < this.mQuiz.mQuestionArray.length; d++)
-			{
-				this.mQuiz.mQuestionArray[d] = 0;
-			} 
-			this.mQuiz.mQuestionArray = 0;
-			this.mQuiz.mQuestionArray = new Array();
+
+			this.mQuiz.resetQuestionArray();
 
 			for (s = 0; s < this.mScoreNeeded; s++)
 			{	
