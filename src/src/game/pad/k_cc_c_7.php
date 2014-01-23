69fee7617a791091969ca3a8d52a0a20e680fbc9
jbreslin33 <jbreslin33@localhost>
2014-01-22 20:04:49 -0500

    u

--- src/src/game/pad/k_cc_c_7.php
+++ src/src/game/pad/k_cc_c_7.php
@@ -7,11 +7,6 @@ Extends: Pad,
 	{
        		this.parent(application);
 
-		//instead we need 2 widgets....
-		this.mCompareBarA = 0;									
-		this.mCompareBarB = 0;									
-		this.createCompareBars();
-	
 		//answers 
                 this.mThresholdTime = 60000;
 
@@ -19,83 +14,22 @@ Extends: Pad,
                 this.mInputPad = new ButtonMultipleChoicePad(application);
 	},
 
-	reset: function()
-	{
-		this.parent();
-
-                this.mCompareBarA.mMesh.innerHTML = '' + this.mQuiz.getQuestion().getVariableA();;
-                this.mCompareBarB.mMesh.innerHTML = '' + this.mQuiz.getQuestion().getVariableB();;
-	},
- 
-	createCompareBars: function()
-        {
-                //comparaBarA
-                if (!this.mCompareBarA)
-                {
-                        this.mCompareBarA = new Shape(150,50,250,100,this,"","","");
-                        this.mCompareBarA.mMesh.innerHTML = '';
-                }
-                //comparaBarB
-                if (!this.mCompareBarB)
-                {
-                        this.mCompareBarB = new Shape(150,50,500,100,this,"","","");
-                        this.mCompareBarB.mMesh.innerHTML = '';
-                }
-        },
-  
-	destroyShapes: function()
-        {
-                this.parent();
-
-                this.mCompareBarA.mDiv.mDiv.removeChild(this.mCompareBarA.mMesh);
-                document.body.removeChild(this.mCompareBarA.mDiv.mDiv);
-                this.mCompareBarA = 0;
-                
-		this.mCompareBarB.mDiv.mDiv.removeChild(this.mCompareBarB.mMesh);
-                document.body.removeChild(this.mCompareBarB.mDiv.mDiv);
-                this.mCompareBarB = 0;
-        },
-
-	showQuestion: function()
-        {
-  		this.mQuiz.getQuestion().setChoices();
-                this.mInputPad.showButtons();
-
-                this.mCompareBarA.mMesh.innerHTML = '' + this.mQuiz.getQuestion().getVariableA();;
-                this.mCompareBarB.mMesh.innerHTML = '' + this.mQuiz.getQuestion().getVariableB();;
-        },
- 
- 	showCorrectAnswer: function()
-        {
-                this.parent();
-                this.mCorrectAnswerBar.mMesh.innerHTML = '' + this.mQuiz.getQuestion().getAnswer();
-        },
-
-        showCorrectAnswerOutOfTime: function()
-        {
-                this.parent();
-                this.mCorrectAnswerBar.mMesh.innerHTML = '' + this.mQuiz.getQuestion().getAnswer();
-        },
-
 	createQuestions: function()
         {
 		this.parent();
 
-                //answer pool
+		//answer pool
                 this.mQuiz.mAnswerPool.push('is greater than');
                 this.mQuiz.mAnswerPool.push('is equal to');
                 this.mQuiz.mAnswerPool.push('is less than');
-   
-		var totalCountGoal       = parseInt(this.mScoreNeeded * 10);
-		var totalCount           = 0;
+		
 		var greaterThans = 0;
 		var lessThans = 0;
 		var equalTos = 0;
 
-		while (totalCount < totalCountGoal || greaterThans < 2 || lessThans < 2 || equalTos < 2)
+		while (greaterThans < 2 || lessThans < 2 || equalTos < 2)
 		{	
 			//reset vars and arrays
-			totalCount = 0;
 			for (d = 0; d < this.mQuiz.mQuestionArray.length; d++)
 			{
 				this.mQuiz.mQuestionArray[d] = 0;
@@ -125,12 +59,73 @@ Extends: Pad,
 					lessThans++;
 				}
 
-				var question = new QuestionCompare('Compare', '' + comparison, objectsToCountA, objectsToCountB);
+				var question = new QuestionCompare('Compare?', '' + comparison, objectsToCountA, objectsToCountB);
 				this.mQuiz.mQuestionArray.push(question);
-				question.mAnswerPool = this.mQuiz.mAnswerPool;
+				question.mAnswerPool = this.mQuiz.mAnswerPool;	
 
-				totalCount = parseInt(totalCount + objectsToCountA);
+				//add shapes
+				for (a = 0; a < objectsToCountA; a++)
+				{
+					question.mShapeArray.push(this.mShapeArray[a])
+				}
+				for (b = 0; b < objectsToCountB; b++)
+				{
+					question.mShapeArray.push(this.mShapeArray[b+20])
+				}
 			}
 		}
 	},
+	
+	createQuestionShapes: function()
+	{
+		//A
+                this.mShapeArray.push(new Shape(50,50,25,50,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,25,100,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,25,150,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,25,200,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,25,250,this,"/images/bus/kid.png","",""));
+                	
+                this.mShapeArray.push(new Shape(50,50,75,50,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,75,100,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,75,150,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,75,200,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,75,250,this,"/images/bus/kid.png","",""));
+                
+		this.mShapeArray.push(new Shape(50,50,125,50,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,125,100,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,125,150,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,125,200,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,125,250,this,"/images/bus/kid.png","",""));
+		
+		this.mShapeArray.push(new Shape(50,50,175,50,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,175,100,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,175,150,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,175,200,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,175,250,this,"/images/bus/kid.png","",""));
+
+		//B
+		this.mShapeArray.push(new Shape(50,50,525,50,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,525,100,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,525,150,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,525,200,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,525,250,this,"/images/bus/kid.png","",""));
+                        
+                this.mShapeArray.push(new Shape(50,50,575,50,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,575,100,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,575,150,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,575,200,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,575,250,this,"/images/bus/kid.png","",""));
+                
+                this.mShapeArray.push(new Shape(50,50,625,50,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,625,100,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,625,150,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,625,200,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,625,250,this,"/images/bus/kid.png","",""));
+                
+                this.mShapeArray.push(new Shape(50,50,675,50,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,675,100,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,675,150,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,675,200,this,"/images/bus/kid.png","",""));
+                this.mShapeArray.push(new Shape(50,50,675,250,this,"/images/bus/kid.png","",""));
+	}
 });
