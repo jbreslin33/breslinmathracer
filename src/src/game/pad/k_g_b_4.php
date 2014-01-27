56e980b9f63c4d0a92581fe9491c620927bbd07b
jbresiln33 <jbresiln33@localhost>
2014-01-26 22:00:31 -0500

    works.

--- src/src/game/pad/k_g_b_4.php
+++ src/src/game/pad/k_g_b_4.php
@@ -23,7 +23,7 @@ Extends: Pad,
 		
 		this.mQuiz.resetQuestionPoolArray();
 
-		if (this.mApplication.mLevel < 11)
+		if (this.mApplication.mLevel <= 5)
 		{
 	       		//answer pool
                 	this.mQuiz.mAnswerPool.push('0');
@@ -114,6 +114,98 @@ Extends: Pad,
                         	}
 			}	
 		}  
+
+	        if (this.mApplication.mLevel >= 6)
+                {
+                        //answer pool
+                        this.mQuiz.mAnswerPool.push('0');
+                        this.mQuiz.mAnswerPool.push('1');
+                        this.mQuiz.mAnswerPool.push('2');
+                        this.mQuiz.mAnswerPool.push('3');
+                        this.mQuiz.mAnswerPool.push('4');
+                        this.mQuiz.mAnswerPool.push('5');
+                        this.mQuiz.mAnswerPool.push('6');
+                        this.mQuiz.mAnswerPool.push('7');
+                        this.mQuiz.mAnswerPool.push('8');
+                        this.mQuiz.mAnswerPool.push('9');
+                        this.mQuiz.mAnswerPool.push('10');
+                        this.mQuiz.mAnswerPool.push('11');
+                        this.mQuiz.mAnswerPool.push('12');
+
+                        //cube
+                        var question = new Question('How many corners?','12');
+                        question.mAnswerPool = this.mQuiz.mAnswerPool;
+                        question.mShapeArray.push(this.mShapeArray[3]);
+                        this.mQuiz.mQuestionPoolArray.push(question);
+
+                        //hexagon
+                        var question = new Question('How many corners?','6');
+                        question.mAnswerPool = this.mQuiz.mAnswerPool;
+                        question.mShapeArray.push(this.mShapeArray[6]);
+                        this.mQuiz.mQuestionPoolArray.push(question);
+
+                        //rectangle
+                        var question = new Question('How many corners?','4');
+                        question.mAnswerPool = this.mQuiz.mAnswerPool;
+                        question.mShapeArray.push(this.mShapeArray[7]);
+                        this.mQuiz.mQuestionPoolArray.push(question);
+
+                        //square
+                        var question = new Question('How many corners?','4');
+                        question.mAnswerPool = this.mQuiz.mAnswerPool;
+                        question.mShapeArray.push(this.mShapeArray[9]);
+                        this.mQuiz.mQuestionPoolArray.push(question);
+
+                        //triangle
+                        var question = new Question('How many corners?','3');
+                        question.mAnswerPool = this.mQuiz.mAnswerPool;
+                        question.mShapeArray.push(this.mShapeArray[10]);
+                        this.mQuiz.mQuestionPoolArray.push(question);
+
+	                var totalCubes = 0;
+                        var totalHexagons = 0;
+                        var totalRectangles = 0;
+                        var totalSquares = 0;
+                        var totalTriangles = 0;
+
+                        while (totalCubes < 2 || totalHexagons < 2 || totalRectangles < 2 || totalSquares < 2 || totalTriangles < 2)
+                        {
+                                totalCubes = 0;
+                                totalHexagons = 0;
+                                totalRectangles = 0;
+                                totalSquares = 0;
+                                totalTriangles = 0;
+
+                                this.mQuiz.resetQuestionArray();
+
+                                for (i = 0; i < this.mScoreNeeded; i++)
+                                {
+                                        var element = Math.floor((Math.random()*this.mQuiz.mQuestionPoolArray.length));
+                                        this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[element]);
+
+                                        if (element == 0)
+                                        {
+                                                totalCubes++;
+                                        }
+                                        if (element == 1)
+                                        {
+                                                totalHexagons++;
+                                        }
+                                        if (element == 2)
+                                        {
+                                                totalRectangles++;
+                                        }
+                                        if (element == 3)
+                                        {
+                                                totalSquares++;
+                                        }
+                                        if (element == 4)
+                                        {
+                                                totalTriangles++;
+                                        }
+                                }
+                        }
+		}
 	},
 
 	createWorld: function()
