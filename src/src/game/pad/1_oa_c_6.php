f401f4a0049f09e9c47578a382405b5dd9abbc9a
jbreslin33 <jbreslin33@localhost>
2014-01-26 23:29:14 -0500

    now add strategy tips....

--- src/src/game/pad/1_oa_c_6.php
+++ src/src/game/pad/1_oa_c_6.php
@@ -7,6 +7,8 @@ Extends: Pad,
 	{
        		this.parent(application);
 
+		this.setScoreNeeded(20);
+
                 //input pad
                 this.mInputPad = new NumberPad(application);
 	},
@@ -158,30 +160,14 @@ Extends: Pad,
 		this.mQuiz.mQuestionPoolArray.push(new Question('10 - 1 =','9'));
 		//79
 
-		var totalNewGoal       = parseInt(this.mScoreNeeded / 2);
 		var totalNew           = 0;
-		var newQuestionElement = 0;
-   		var elementCounter     = 0;
                 
-                for (i = 0; i <= 78; i++)
-                {
-                        if (this.mApplication.mLevel == i)
-                        {
-                                newQuestionElement = elementCounter;
-                        }
-                        elementCounter++;
-                }
-
-		while (totalNew < totalNewGoal)
+		while (totalNew < this.mScoreNeeded * .45)
 		{	
 			//reset vars and arrays
 			totalNew = 0;
-			for (d = 0; d < this.mQuiz.mQuestionArray.length; d++)
-			{
-				this.mQuiz.mQuestionArray[d] = 0;
-			} 
-			this.mQuiz.mQuestionArray = 0;
-			this.mQuiz.mQuestionArray = new Array();
+	
+			this.mQuiz.resetQuestionArray();
 
 			for (s = 0; s < this.mScoreNeeded; s++)
 			{	
@@ -189,12 +175,12 @@ Extends: Pad,
 				var randomChance = Math.floor((Math.random()*2));		
 				if (randomChance == 0)
 				{
-					this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[newQuestionElement]);
+					this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[this.mApplication.mLevel - 1]);
 					totalNew++;
 				}	
 				if (randomChance == 1)
 				{
-					var randomElement = Math.floor((Math.random()*newQuestionElement));		
+					var randomElement = Math.floor((Math.random()*parseInt(this.mApplication.mLevel - 1)));		
 					this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[randomElement]);
 				}
 			}
