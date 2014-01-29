5b87ffc73e15f5b886d423d47724a82986d9d454
jbreslin33 <jbreslin33@localhost>
2014-01-28 20:00:58 -0500

    need symbols.

--- src/src/game/pad/1_nbt_b_3.php
+++ src/src/game/pad/1_nbt_b_3.php
@@ -7,143 +7,76 @@ Extends: Pad,
 	{
        		this.parent(application);
 
-		//instead we need 2 widgets....
-		this.mCompareBarA = 0;									
-		this.mCompareBarB = 0;									
-		this.createCompareBars();
-	
 		//answers 
-                this.mThresholdTime = 10000;
+                this.mThresholdTime = 60000;
+		
+		this.setScoreNeeded(20);
 
                 //input pad
-                this.mInputPad = new ButtonMultipleChoicePadImages(application);
+                this.mInputPad = new ButtonMultipleChoicePad(application);
 	},
-
-	reset: function()
-	{
-		this.parent();
-
-                this.mCompareBarA.mMesh.innerHTML = '' + this.mQuiz.getQuestion().getVariableA();;
-                this.mCompareBarB.mMesh.innerHTML = '' + this.mQuiz.getQuestion().getVariableB();;
-	},
- 
-	createCompareBars: function()
-        {
-                //comparaBarA
-                if (!this.mCompareBarA)
-                {
-                        this.mCompareBarA = new Shape(150,50,250,100,this,"","","");
-                        this.mCompareBarA.mMesh.innerHTML = '';
-                }
-                //comparaBarB
-                if (!this.mCompareBarB)
-                {
-                        this.mCompareBarB = new Shape(150,50,500,100,this,"","","");
-                        this.mCompareBarB.mMesh.innerHTML = '';
-                }
-        },
-  
-	destroyShapes: function()
-        {
-                this.parent();
-
-                this.mCompareBarA.mDiv.mDiv.removeChild(this.mCompareBarA.mMesh);
-                document.body.removeChild(this.mCompareBarA.mDiv.mDiv);
-                this.mCompareBarA = 0;
-                
-		this.mCompareBarB.mDiv.mDiv.removeChild(this.mCompareBarB.mMesh);
-                document.body.removeChild(this.mCompareBarB.mDiv.mDiv);
-                this.mCompareBarB = 0;
-        },
-
-	showQuestion: function()
-        {
-                this.mCompareBarA.mMesh.innerHTML = '' + this.mQuiz.getQuestion().getVariableA();;
-                this.mCompareBarB.mMesh.innerHTML = '' + this.mQuiz.getQuestion().getVariableB();;
-        },
- 
- 	showCorrectAnswer: function()
-        {
-                this.parent();
-		if (this.mQuiz.getQuestion().getAnswer() == 'A')
-		{
-                	this.mCorrectAnswerBar.mMesh.innerHTML = '=';
-		}
-		if (this.mQuiz.getQuestion().getAnswer() == 'B')
-		{
-                	this.mCorrectAnswerBar.mMesh.innerHTML = '>';
-		}
-		if (this.mQuiz.getQuestion().getAnswer() == 'C')
-		{
-                	this.mCorrectAnswerBar.mMesh.innerHTML = '<';
-		}
-                //this.mCorrectAnswerBar.mMesh.innerHTML = '' + this.mQuiz.getQuestion().getAnswer();
-        },
-
-        showCorrectAnswerOutOfTime: function()
-        {
-                this.parent();
-                this.mCorrectAnswerBar.mMesh.innerHTML = '' + this.mQuiz.getQuestion().getAnswer();
-        },
 
 	createQuestions: function()
         {
 		this.parent();
+
+		//answer pool
+                this.mQuiz.mAnswerPool.push('is greater than');
+                this.mQuiz.mAnswerPool.push('is equal to');
+                this.mQuiz.mAnswerPool.push('is less than');
 		
-		var totalCountGoal       = parseInt(this.mScoreNeeded * 10);
-		var totalCount           = 0;
 		var greaterThans = 0;
 		var lessThans = 0;
 		var equalTos = 0;
 
-		while (totalCount < totalCountGoal || greaterThans < 2 || lessThans < 2 || equalTos < 2)
+		while (greaterThans < 2 || lessThans < 2 || equalTos < 2)
 		{	
-			//reset vars and arrays
-			totalCount = 0;
-			for (d = 0; d < this.mQuiz.mQuestionArray.length; d++)
-			{
-				this.mQuiz.mQuestionArray[d] = 0;
-			} 
-			this.mQuiz.mQuestionArray = 0;
-			this.mQuiz.mQuestionArray = new Array();
+			this.mQuiz.resetQuestionArray();
 
 			for (s = 0; s < this.mScoreNeeded; s++)
 			{	
 				//random number to count from 0-20
-				var objectsToCountA = Math.floor((Math.random()*100));		
-				var objectsToCountB = Math.floor((Math.random()*100));		
+				var objectsToCountA = Math.floor((Math.random()*21));		
+				var objectsToCountB = Math.floor((Math.random()*21));		
 				var comparison = '';
 				if (objectsToCountA == objectsToCountB)
 				{
-					comparison = 'A';
+					comparison = 'is equal to';
 					equalTos++;
 				}
 				if (objectsToCountA > objectsToCountB)
 				{
-					comparison = 'B';
+					comparison = 'is greater than';
 					greaterThans++;
 				}
 				if (objectsToCountA < objectsToCountB)
 				{
-					comparison = 'C';
+					comparison = 'is less than';
 					lessThans++;
 				}
 
-				var question = new QuestionCompare('Compare', '' + comparison, objectsToCountA, objectsToCountB);
-
+				var question = new QuestionCompare('Compare?', '' + comparison, objectsToCountA, objectsToCountB);
 				this.mQuiz.mQuestionArray.push(question);
+				question.mAnswerPool = this.mQuiz.mAnswerPool;	
 
-				totalCount = parseInt(totalCount + objectsToCountA);
+				//add shapes
+				this.mShapeArray[ parseInt( s*2+2 ) ].mMesh.innerHTML = '' + objectsToCountA;
+				question.mShapeArray.push(this.mShapeArray[parseInt((s*2)+2)]);
 				
+				this.mShapeArray[parseInt((s*2)+3)].mMesh.innerHTML = '' + objectsToCountB;
+				question.mShapeArray.push(this.mShapeArray[parseInt((s*2)+3)]);
 			}
 		}
-    		
-		for (i = 0; i < this.mQuiz.mQuestionArray.length; i++)
-                {
-                        this.mQuiz.mQuestionArray[i].setChoice('A','A');
-                        this.mQuiz.mQuestionArray[i].setChoice('B','B');
-                        this.mQuiz.mQuestionArray[i].setChoice('C','C');
-                }
-
 	},
+	
+	createWorld: function()
+	{
+		this.parent();
+
+		for (i=0; i < this.mScoreNeeded; i++)
+		{	
+			this.mShapeArray.push(new Shape(150,50,300,100,this,"","",""));
+			this.mShapeArray.push(new Shape(150,50,550,100,this,"","",""));
+		}
+	}
 });
