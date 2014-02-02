57c8212cbd97e1a21876c5730f3237f88c26ca85
jbreslin33 <jbreslin33@localhost>
2014-02-02 12:38:33 -0500

    c

--- src/src/game/pad/1_md_b_3.php
+++ src/src/game/pad/1_md_b_3.php
@@ -24,41 +24,6 @@ Extends: Pad,
 		this.setClock(parseInt(tArray[0]),parseInt(tArray[1]));	
 	},
 
-	createQuestionShapes: function()
-	{
-		this.destroyShapes();
-
-		this.createClock(12,59);	
-	},
-
-	createClock: function(hours,minutes,seconds)
-	{
-		canvas = Raphael(25,200,200, 200);
-                clock = canvas.circle(100,100,95);
-                clock.attr({"fill":"#f5f5f5","stroke":"#444444","stroke-width":"5"})  
-                var hour_sign;
-                for(i=0;i<12;i++)
-		{
-                	var start_x = 100+Math.round(80*Math.cos(30*i*Math.PI/180));
-                    	var start_y = 100+Math.round(80*Math.sin(30*i*Math.PI/180));
-                    	var end_x = 100+Math.round(90*Math.cos(30*i*Math.PI/180));
-                    	var end_y = 100+Math.round(90*Math.sin(30*i*Math.PI/180));    
-                    	hour_sign = canvas.path("M"+start_x+" "+start_y+"L"+end_x+" "+end_y);
-                }    
-                this.hour_hand = canvas.path("M100 100L100 50");
-                this.hour_hand.attr({stroke: "#444444", "stroke-width": 6});
-
-                this.minute_hand = canvas.path("M100 100L100 40");
-                this.minute_hand.attr({stroke: "#444444", "stroke-width": 4});
-
-                var pin = canvas.circle(100, 100, 5);
-                pin.attr("fill", "#000000");    
-		
-		//reset transforms
- 		this.hour_hand.transform("");
- 		this.minute_hand.transform("");
-	},
-
 	setClock: function(hours,minutes)
 	{
 		//reset transforms
@@ -105,7 +70,40 @@ Extends: Pad,
 			var question = new Question('What time is it?', '' + h + ':' + m);
 			this.mQuiz.mQuestionArray.push(question);
 		}
+	},
 
-		this.createQuestionShapes();
+	createWorld: function()
+	{
+		this.parent();
+
+		this.createClock(12,59);	
+	},
+	
+	createClock: function(hours,minutes,seconds)
+	{
+		canvas = Raphael(25,200,200, 200);
+                clock = canvas.circle(100,100,95);
+                clock.attr({"fill":"#f5f5f5","stroke":"#444444","stroke-width":"5"})  
+                var hour_sign;
+                for(i=0;i<12;i++)
+		{
+                	var start_x = 100+Math.round(80*Math.cos(30*i*Math.PI/180));
+                    	var start_y = 100+Math.round(80*Math.sin(30*i*Math.PI/180));
+                    	var end_x = 100+Math.round(90*Math.cos(30*i*Math.PI/180));
+                    	var end_y = 100+Math.round(90*Math.sin(30*i*Math.PI/180));    
+                    	hour_sign = canvas.path("M"+start_x+" "+start_y+"L"+end_x+" "+end_y);
+                }    
+                this.hour_hand = canvas.path("M100 100L100 50");
+                this.hour_hand.attr({stroke: "#444444", "stroke-width": 6});
+
+                this.minute_hand = canvas.path("M100 100L100 40");
+                this.minute_hand.attr({stroke: "#444444", "stroke-width": 4});
+
+                var pin = canvas.circle(100, 100, 5);
+                pin.attr("fill", "#000000");    
+		
+		//reset transforms
+ 		this.hour_hand.transform("");
+ 		this.minute_hand.transform("");
 	}
 });
