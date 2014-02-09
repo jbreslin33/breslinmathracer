4581fac50ce8f1ca5f37b6e2636265bb868fe497
jbreslin33 <jbreslin33@localhost>
2014-02-08 23:00:48 -0500

    works.

--- src/src/game/pad/k_cc_b_5.php
+++ src/src/game/pad/k_cc_b_5.php
@@ -38,7 +38,7 @@ Extends: Pad,
                         
 			for (s = 0; s < parseInt(numberToCount); s++)
                         {
-                                question.mShapeArray.push(this.mShapeArray[s+2]);
+                                question.mShapeArray.push(this.mShapeArray[parseInt(s + this.mTotalGuiBars)]);
 			}
 			this.mQuiz.mQuestionArray.push(question);
 		}
@@ -80,19 +80,19 @@ Extends: Pad,
                         {
                                 if (i < 5)
                                 {
-                                        this.mShapeArray[i+2].setPosition(parseInt(i * 50 + 50), 50)
+                                        this.mShapeArray[parseInt(i + this.mTotalGuiBars)].setPosition(parseInt(i * 50 + 50), 50)
                                 }
                                 if (i >= 5 && i < 11)
                                 {
-                                        this.mShapeArray[i+2].setPosition(50,parseInt(i * 50 - 135))
+                                        this.mShapeArray[parseInt(i + this.mTotalGuiBars)].setPosition(50,parseInt(i * 50 - 135))
                                 }
                                 if (i >= 11 && i < 15)
                                 {
-                                        this.mShapeArray[i+2].setPosition(parseInt(i * 50 - 450 ), 365)
+                                        this.mShapeArray[parseInt(i + this.mTotalGuiBars)].setPosition(parseInt(i * 50 - 450 ), 365)
                                 }
                                 if (i >= 15 && i < 20)
                                 {
-                                        this.mShapeArray[i+2].setPosition(250,parseInt(i * 50 - 635))
+                                        this.mShapeArray[parseInt(i + this.mTotalGuiBars)].setPosition(250,parseInt(i * 50 - 635))
                                 }
                         }
 		}
@@ -112,16 +112,16 @@ Extends: Pad,
                 	this.mShapeArray.push(new Shape(50,50,75,200,this,"/images/bus/kid.png","",""));
                 	this.mShapeArray.push(new Shape(50,50,75,250,this,"/images/bus/kid.png","",""));
 
-			this.mShapeArray[2].setPosition (150,350);
-                        this.mShapeArray[3].setPosition (250,400);
-                        this.mShapeArray[4].setPosition (200,250);
-                        this.mShapeArray[5].setPosition (150,150);
-                        this.mShapeArray[6].setPosition (050,050);
-                        this.mShapeArray[7].setPosition (150,050);
-                        this.mShapeArray[8].setPosition (250,150);
-                        this.mShapeArray[9].setPosition (200,400);
-                        this.mShapeArray[10].setPosition (100,350);
-                        this.mShapeArray[11].setPosition (100,100);
+			this.mShapeArray[parseInt(this.mTotalGuiBars)].setPosition (150,350);
+                        this.mShapeArray[parseInt(1 + this.mTotalGuiBars)].setPosition (250,400);
+                        this.mShapeArray[parseInt(2 + this.mTotalGuiBars)].setPosition (200,250);
+                        this.mShapeArray[parseInt(3 + this.mTotalGuiBars)].setPosition (150,150);
+                        this.mShapeArray[parseInt(4 + this.mTotalGuiBars)].setPosition (050,050);
+                        this.mShapeArray[parseInt(5 + this.mTotalGuiBars)].setPosition (150,050);
+                        this.mShapeArray[parseInt(6 + this.mTotalGuiBars)].setPosition (250,150);
+                        this.mShapeArray[parseInt(7 + this.mTotalGuiBars)].setPosition (200,400);
+                        this.mShapeArray[parseInt(8 + this.mTotalGuiBars)].setPosition (100,350);
+                        this.mShapeArray[parseInt(9 + this.mTotalGuiBars)].setPosition (100,100);
 		} 
 	}
 });
