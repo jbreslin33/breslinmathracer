4be75e8b0a3ed4229cd5380611f5f7d5b13da1d2
jbreslin33 <jbreslin33@localhost>
2014-02-08 22:48:32 -0500

    k_cc_b_4b.php

--- src/src/game/pad/k_cc_b_4b.php
+++ src/src/game/pad/k_cc_b_4b.php
@@ -42,13 +42,13 @@ Extends: Pad,
                         
 			for (s = 0; s < parseInt(numberToCount * 2); s++)
                         {
-                                question.mShapeArray.push(this.mShapeArray[s+2]);
+                                question.mShapeArray.push(this.mShapeArray[parseInt(s + this.mTotalGuiBars)]);
 			}
 
 			this.mQuiz.mQuestionArray.push(question);
 		}
 	},
-	
+        
 	createWorld: function()
         {
 		this.parent();
@@ -56,51 +56,51 @@ Extends: Pad,
                 //one
                 this.mShapeArray.push(new Shape(50,50,38,80,this,"/images/bus/kid.png","",""));
                 this.mShapeArray.push(new Shape(50,50,45,45,this,"","",""));
-                this.mShapeArray[3].setText('one');
+                this.mShapeArray[parseInt(1 + this.mTotalGuiBars)].setText('one');
 
                 //two
                 this.mShapeArray.push(new Shape(50,50,83,80,this,"/images/bus/kid.png","",""));
                 this.mShapeArray.push(new Shape(50,50,93,45,this,"","",""));
-                this.mShapeArray[5].setText('two');
+                this.mShapeArray[parseInt(3 + this.mTotalGuiBars)].setText('two');
 
                 //three
                 this.mShapeArray.push(new Shape(50,50,135,80,this,"/images/bus/kid.png","",""));
                 this.mShapeArray.push(new Shape(50,50,140,45,this,"","",""));
-                this.mShapeArray[7].setText('three');
+                this.mShapeArray[parseInt(5 + this.mTotalGuiBars)].setText('three');
 
                 //four
                 this.mShapeArray.push(new Shape(50,50,185,80,this,"/images/bus/kid.png","",""));
                 this.mShapeArray.push(new Shape(50,50,190,45,this,"","",""));
-                this.mShapeArray[9].setText('four');
+                this.mShapeArray[parseInt(7 + this.mTotalGuiBars)].setText('four');
 
                 //five
                 this.mShapeArray.push(new Shape(50,50,235,80,this,"/images/bus/kid.png","",""));
                 this.mShapeArray.push(new Shape(50,50,240,45,this,"","",""));
-                this.mShapeArray[11].setText('five');
+                this.mShapeArray[parseInt(9 + this.mTotalGuiBars)].setText('five');
 
                 //six
                 this.mShapeArray.push(new Shape(50,50,38,180,this,"/images/bus/kid.png","",""));
                 this.mShapeArray.push(new Shape(50,50,45,145,this,"","",""));
-                this.mShapeArray[13].setText('six');
+                this.mShapeArray[parseInt(11 + this.mTotalGuiBars)].setText('six');
 
                 //seven
                 this.mShapeArray.push(new Shape(50,50,83,180,this,"/images/bus/kid.png","",""));
                 this.mShapeArray.push(new Shape(50,50,93,145,this,"","",""));
-                this.mShapeArray[15].setText('seven');
+                this.mShapeArray[parseInt(13 + this.mTotalGuiBars)].setText('seven');
 
                 //eight
                 this.mShapeArray.push(new Shape(50,50,135,180,this,"/images/bus/kid.png","",""));
                 this.mShapeArray.push(new Shape(50,50,140,145,this,"","",""));
-                this.mShapeArray[17].setText('eight');
+                this.mShapeArray[parseInt(15 + this.mTotalGuiBars)].setText('eight');
 
                 //nine
                 this.mShapeArray.push(new Shape(50,50,185,180,this,"/images/bus/kid.png","",""));
                 this.mShapeArray.push(new Shape(50,50,190,145,this,"","",""));
-                this.mShapeArray[19].setText('nine');
+                this.mShapeArray[parseInt(17 + this.mTotalGuiBars)].setText('nine');
 
                 //ten
                 this.mShapeArray.push(new Shape(50,50,235,180,this,"/images/bus/kid.png","",""));
                 this.mShapeArray.push(new Shape(50,50,240,145,this,"","",""));
-                this.mShapeArray[21].setText('ten');
+                this.mShapeArray[parseInt(19 + this.mTotalGuiBars)].setText('ten');
         }
 });
