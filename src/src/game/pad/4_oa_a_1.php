a6b34c4301a5f7df37c5660a23516b59b41dad42
jbreslin33 <jbreslin33@localhost>
2014-02-14 21:23:13 -0500

    u

--- src/src/game/pad/4_oa_a_1.php
+++ src/src/game/pad/4_oa_a_1.php
@@ -23,10 +23,6 @@ Extends: Pad,
                 this.mShapeArray[1].setSize(700,100);
                 this.mShapeArray[1].setPosition(380,80);
 
-                //move dont forget
-                //this.mShapeArray[8].setVisibility(false);
-                //this.mShapeArray[9].setVisibility(false);
-                
 		this.mShapeArray[1].mMesh.innerHTML = '' + this.mQuiz.getQuestion().getShowAnswer();
         },
 
@@ -49,57 +45,16 @@ Extends: Pad,
 	createQuestions: function()
         {
  		this.parent();
-
-		//add
-                this.mQuiz.mQuestionPoolArray.push(new Question('Interpret 35 = 5 × 7 as a statement that 35 is 5 times as many as ? and 7 times as many as 5.','7','Interpret 35 = 5 × 7 as a statement that 35 is 5 times as many as 7 and 7 times as many as 5.'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('8 + ? = 10','2','8 + 2 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('7 + ? = 10','3','7 + 3 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('6 + ? = 10','4','6 + 4 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('5 + ? = 10','5','5 + 5 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('4 + ? = 10','6','4 + 6 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('3 + ? = 10','7','3 + 7 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('2 + ? = 10','8','2 + 8 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('1 + ? = 10','9','1 + 9 = 10'));
-		//9
-
-		this.mQuiz.mQuestionPoolArray.push(new Question('1 + ? = 10','9','1 + 9 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('2 + ? = 10','8','2 + 8 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('3 + ? = 10','7','3 + 7 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('4 + ? = 10','6','4 + 6 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('5 + ? = 10','5','5 + 5 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('6 + ? = 10','4','6 + 4 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('7 + ? = 10','3','7 + 3 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('8 + ? = 10','2','8 + 2 = 10'));
-                this.mQuiz.mQuestionPoolArray.push(new Question('9 + ? = 10','1','9 + 1 = 10'));
-		//18
-               
-		//extra 
-		this.mQuiz.mQuestionPoolArray.push(new Question('9 + ? = 10','1','9 + 1 = 10'));
-
-		var totalNew           = 0;
                 
-		while (totalNew < this.mScoreNeeded * .4 || totalNew > this.mScoreNeeded * .6)
+		this.mQuiz.resetQuestionArray();
+
+		for (s = 0; s < this.mScoreNeeded; s++)
 		{	
-			//reset vars and arrays
-			totalNew = 0;
+			var a = Math.floor((Math.random()*8)+3);		
+			var b = Math.floor((Math.random()*8)+3);		
+			var x = a * b;
 
-			this.mQuiz.resetQuestionArray();
-
-			for (s = 0; s < this.mScoreNeeded; s++)
-			{	
-				//50% chance of asking newest question
-				var randomChance = Math.floor((Math.random()*2));		
-				if (randomChance == 0)
-				{
-					this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[parseInt(this.mApplication.mLevel-1)]);
-					totalNew++;
-				}	
-				if (randomChance == 1)
-				{
-					var randomElement = Math.floor((Math.random()*parseInt(this.mApplication.mLevel-1)));		
-					this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[randomElement]);
-				}
-			}
+               		this.mQuiz.mQuestionArray.push(new Question('Interpret ' + x + ' = ' + a + ' x ' + b + ' as a statement that ' + x + ' is ' + a + ' times as many as ? and ' + b + ' times as many as ' + a + '.','' + b,'Interpret ' + x + ' = ' + a + ' x ' + b + ' as a statement that ' + x + ' is ' + a + ' times as many as ? and ' + b + ' times as many as ' + a + '.'));
 		}
 	}
 });
