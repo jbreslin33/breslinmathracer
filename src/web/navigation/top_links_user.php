9b6eb6c8614e8598cbb4a0acea12d00dccc5221c
jbreslin33 <jbreslin33@localhost>
2014-02-17 16:59:57 -0500

    u

--- src/web/navigation/top_links_user.php
+++ src/web/navigation/top_links_user.php
@@ -2,7 +2,5 @@
 <li><a href="/web/application/application.php">Play</a></li>
 <li><a href="/web/select/select.php">Selects</a></li>
 <li><a href="/web/login/login_form.php">Logout</a></li>
+<li><a href="/web/update/update_ref_id_form.php">Update Ref ID</a></li>
 </ul>
-
-
-
---
+++ src/web/update/updaterefid.php
@@ -0,0 +1,47 @@
+<?php
+include(getenv("DOCUMENT_ROOT") . "/src/database/db_connect.php");
+include(getenv("DOCUMENT_ROOT") . "/src/database/set_level_session_variables.php");
+
+include(getenv("DOCUMENT_ROOT") . "/src/database/select_user_id.php");
+
+//db connection
+$conn = dbConnect();
+
+//start new session
+session_start();
+
+//let's set a var that will be false if there was a problem..
+$problem = "";
+
+$_SESSION["password"] = $_POST["password"];
+$_SESSION["username"] = $_POST["username"];
+$_SESSION["school_id"] = "1";
+$_SESSION["school_name"] = "visitationbvm";
+
+//set user sessions
+$user_id = selectUserID($conn, $_SESSION["username"], $_SESSION["password"]);
+if ($user_id)
+{
+	//set sessions
+        $_SESSION["user_id"] = $user_id;
+      	$_SESSION["Login"] = "YES";
+	setLevelSessionVariables($conn,$user_id);
+}
+else
+{
+        $_SESSION["Login"] = "NO";
+	$problem = "no_user";	
+       	$_SESSION["user_id"] = 0;
+}
+
+if ($problem == "")
+{
+        header("Location: /web/home/home.php");
+}
+else
+{
+        header("Location: login_form.php?message=$problem");
+}
+
+pg_close();
+?>
