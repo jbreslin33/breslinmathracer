60699d3471aded8bba6c7573d8722dcf2c613c40
jbreslin33 <jbreslin33@localhost>
2014-02-23 18:21:13 -0500

    u

--- src/web/update/updatestandardid.php
+++ src/web/update/updatestandardid.php
@@ -12,15 +12,38 @@ include(getenv("DOCUMENT_ROOT") . "/web/navigation/top_links.php");
 ?>
 
 <?php
-//update users SET ref_id = ( select ref_id from learning_standards where id = 'k.oa.a.2') where username = 'v1401';
-$query = "update users SET failed_attempts=0, level = 1,ref_id = ( select ref_id from learning_standards where id = '";
-$query .= $_POST["standardid"];
-$query .= "') where username = '";
-$query .= $_SESSION["username"];
-$query .= "';";
 
-$result = pg_query($conn,$query);
-dbErrorCheck($conn,$result);
+$response = "";
+
+$select = "select * from learning_standards where id = '";
+$select .= $_POST["standardid"];
+$select .= "';";
+        
+//get db result
+$selectResult = pg_query($conn,$select) or die('Could not connect: ' . pg_last_error());
+dbErrorCheck($conn,$selectResult);
+
+//get numer of rows
+$num = pg_num_rows($selectResult);
+
+if ($num > 0)
+{
+	$update = "update users SET failed_attempts=0, level = 1,ref_id = ( select ref_id from learning_standards where id = '";
+	$update .= $_POST["standardid"];
+	$update .= "') where username = '";
+	$update .= $_SESSION["username"];
+	$update .= "';";
+
+	$updateResult = pg_query($conn,$update);
+	$errorCheck = dbErrorCheck($conn,$updateResult);
+	
+	$response = "success";;
+}
+else
+{
+	$response = "standard does not exist";;
+}
+
 pg_close($conn);
 ?>
 
@@ -28,9 +51,8 @@ pg_close($conn);
 
 <body>
 
-<br><b><u>Sql update:<u><b><br>
 <?php
-echo $query;
+echo $response;
 ?>
 
 
