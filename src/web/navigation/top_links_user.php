de1502799df349d7fce1a60f4bac9556fc5376da
jbreslin33 <jbreslin33@localhost>
2014-02-23 21:01:53 -0500

    works.

--- src/web/navigation/top_links_user.php
+++ src/web/navigation/top_links_user.php
@@ -2,6 +2,5 @@
 <li><a href="/web/application/application.php">Play</a></li>
 <li><a href="/web/select/select.php">Selects</a></li>
 <li><a href="/web/login/login_form.php">Logout</a></li>
-<li><a href="/web/update/update_ref_id_form.php">Update Ref ID</a></li>
 <li><a href="/web/update/update_standard_id_form.php">Update Standard ID</a></li>
 </ul>
