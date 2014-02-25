2c98e9138139e87130e1ff7355a2985bb6faca54
jbreslin33 <jbreslin33@localhost>
2014-02-25 17:50:13 -0500

    2.nbt.a.2 mod it

--- src/src/application/application.php
+++ src/src/application/application.php
@@ -700,6 +700,19 @@ var Application = new Class(
                                	this.mGame = new g2_nbt_a_1(APPLICATION);
 			}	
 		}
+		if (this.mRef_id == 'EE88A59EFD4348C28C56A49E61A673A8')
+		{ 
+             		if (this.mGameName != "g2_nbt_a_2")
+                       	{
+				if (this.mGame)
+				{
+					this.mGame.destructor();
+					this.mGame = 0;
+				}
+                               	this.mGameName = "g2_nbt_a_2";
+                               	this.mGame = new g2_nbt_a_2(APPLICATION);
+			}	
+		}
 		if (this.mRef_id == '3D384CB2349B41299A3B5A133AB9E3F8')
 		{ 
              		if (this.mGameName != "g3_oa_c_7")
