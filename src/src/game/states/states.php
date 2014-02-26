44ee62675ca3acb9626733d66639e5f30f854875
jbreslin33 <jbreslin33@localhost>
2014-02-26 17:58:31 -0500

    logs

--- src/src/game/states/states.php
+++ src/src/game/states/states.php
@@ -31,7 +31,7 @@ initialize: function()
 
 enter: function(game)
 {
-	game.log('GAME::INIT_GAME');
+	//game.log('GAME::INIT_GAME');
 },
 
 execute: function(game)
@@ -55,7 +55,7 @@ initialize: function()
 
 enter: function(game)
 {
-	game.log('GAME::RESET_GAME');
+	//game.log('GAME::RESET_GAME');
 	game.resetGameEnter();
 },
 
@@ -79,7 +79,7 @@ initialize: function()
 
 enter: function(game)
 {
-	game.log('GAME::NORMAL_GAME');
+	//game.log('GAME::NORMAL_GAME');
 	game.reset();
 },
 
@@ -111,7 +111,7 @@ initialize: function()
 
 enter: function(game)
 {
-	game.log('GAME::LEVEL_PASSED');
+	//game.log('GAME::LEVEL_PASSED');
 	game.levelPassedEnter();
 },
 
@@ -137,7 +137,7 @@ initialize: function()
 
 enter: function(game)
 {
-	game.log('GAME::LEVEL_FAILED');
+	//game.log('GAME::LEVEL_FAILED');
         game.levelFailedEnter();
 },
 
@@ -164,7 +164,7 @@ initialize: function()
 
 enter: function(game)
 {
-	game.log('GAME::FIRST_TIME');
+	//game.log('GAME::FIRST_TIME');
         game.firstTimeEnter();
 },
 
@@ -214,7 +214,7 @@ initialize: function()
 
 enter: function(game)
 {
-	game.log('GAME::CORRECT_ANSWER');
+	//game.log('GAME::CORRECT_ANSWER');
         game.mQuiz.correctAnswer();
 },
 
@@ -246,7 +246,7 @@ initialize: function()
 
 enter: function(game)
 {
-	game.log('GAME::SHOW_CORRECT_ANSWER');
+	//game.log('GAME::SHOW_CORRECT_ANSWER');
         game.showCorrectAnswerEnter();
 },
 
@@ -281,7 +281,7 @@ initialize: function()
 
 enter: function(game)
 {
-	game.log('GAME::OUT_OF_TIME');
+	//game.log('GAME::OUT_OF_TIME');
         game.outOfTimeEnter();
 },
 
