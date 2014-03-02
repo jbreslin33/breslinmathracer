7e088c75641da64d1a81eab60594348bcdb45cad
jbreslin33 <jbreslin33@localhost>
2014-03-02 15:48:42 -0500

    others.

--- src/src/game/pad/2_nbt_a_3.php
+++ src/src/game/pad/2_nbt_a_3.php
@@ -43,188 +43,205 @@ Extends: NumberPad,
 		//just the question array reset
 		this.mQuiz.resetQuestionArray();
 
-		//loop thru and make potential questions
-		for (s = 0; s < this.mScoreNeeded; s++)
-		{	
-			var randomOnes     = Math.floor((Math.random()*10));		
-			var randomTens     = Math.floor((Math.random()*10));		
-			var randomHundreds = Math.floor((Math.random()*10));		
-			var randomNumber = parseInt((randomOnes * 1) + (randomTens * 10) + (randomHundreds * 100));	
+		//A name form to number form
+		//B name form to expanded form
+		//C number form to expanded form
+		//C number form to name form ----prob too dificult??? 
 
-			var onesText = '';
-			var tensText = '';
-			var hundredsText = '';
+		var totalA = 0;
+		var totalB = 5;
+		var totalC = 5;
 
-			//hundreds
-			if (randomHundreds == 0)
-			{
-			}
-			if (randomHundreds == 1)
-			{
-				hundredsText = 'one hundred ';
-			}
-			if (randomHundreds == 2)
-			{
-				hundredsText = 'two hundred ';
-			}
-			if (randomHundreds == 3)
-			{
-				hundredsText = 'three hundred ';
-			}
-			if (randomHundreds == 4)
-			{
-				hundredsText = 'four hundred ';
-			}
-			if (randomHundreds == 5)
-			{
-				hundredsText = 'five hundred ';
-			}
-			if (randomHundreds == 6)
-			{
-				hundredsText = 'six hundred ';
-			}
-			if (randomHundreds == 7)
-			{
-				hundredsText = 'seven hundred ';
-			}
-			if (randomHundreds == 8)
-			{
-				hundredsText = 'eight hundred ';
-			}
-			if (randomHundreds == 9)
-			{
-				hundredsText = 'nine hundred ';
-			}
+		while(totalA < this.mScoreNeeded * .2 || totalB < this.mScoreNeeded * .2 || totalC < this.mScoreNeeded * .2) 
+		{
+			this.mQuiz.resetQuestionArray();
+
+			//loop thru and make potential questions
+			for (s = 0; s < this.mScoreNeeded; s++)
+			{	
+				var randomChance = Math.floor((Math.random()*3));		
+				randomChance = 0;		
+				var randomOnes     = Math.floor((Math.random()*10));		
+				var randomTens     = Math.floor((Math.random()*10));		
+				var randomHundreds = Math.floor((Math.random()*10));		
+				var randomNumber   = parseInt((randomOnes * 1) + (randomTens * 10) + (randomHundreds * 100));	
+				var onesText = '';
+				var tensText = '';
+				var hundredsText = '';
+
+				//a name form to number form
+				if (randomChance == 0)
+				{
+					//hundreds
+					if (randomHundreds == 0)
+					{
+					}
+					if (randomHundreds == 1)
+					{
+						hundredsText = 'one hundred ';
+					}
+					if (randomHundreds == 2)
+					{
+						hundredsText = 'two hundred ';
+					}
+					if (randomHundreds == 3)
+					{
+						hundredsText = 'three hundred ';
+					}
+					if (randomHundreds == 4)
+					{
+						hundredsText = 'four hundred ';
+					}
+					if (randomHundreds == 5)
+					{
+						hundredsText = 'five hundred ';
+					}
+					if (randomHundreds == 6)
+					{
+						hundredsText = 'six hundred ';
+					}
+					if (randomHundreds == 7)
+					{
+						hundredsText = 'seven hundred ';
+					}
+					if (randomHundreds == 8)
+					{
+						hundredsText = 'eight hundred ';
+					}
+					if (randomHundreds == 9)
+					{
+						hundredsText = 'nine hundred ';
+					}
 		
-			//tens	
-			if (randomTens == 0)
-			{
-			}
-			if (randomTens == 1)
-			{
-				if (randomOnes == 1)
-				{
-					randomOnes = 0;	
-					tensText = 'eleven';		
-				}
-				if (randomOnes == 2)
-				{
-					randomOnes = 0;	
-					tensText = 'twelve';		
-				}
-				if (randomOnes == 3)
-				{
-					randomOnes = 0;	
-					tensText = 'thirteen';		
-				}
-				if (randomOnes == 4)
-				{
-					randomOnes = 0;	
-					tensText = 'fourteen';		
-				}
-				if (randomOnes == 5)
-				{
-					randomOnes = 0;	
-					tensText = 'fifteen';		
-				}
-				if (randomOnes == 6)
-				{
-					randomOnes = 0;	
-					tensText = 'sixteen';		
-				}
-				if (randomOnes == 7)
-				{
-					randomOnes = 0;	
-					tensText = 'seventeen';		
-				}
-				if (randomOnes == 8)
-				{
-					randomOnes = 0;	
-					tensText = 'eighteen';		
-				}
-				if (randomOnes == 9)
-				{
-					randomOnes = 0;	
-					tensText = 'nineteen';		
-				}
-			}
-			if (randomTens == 2)
-			{
-				tensText = 'twenty ';	
-			}
-			if (randomTens == 3)
-			{
-				tensText = 'thirty ';	
-			}
-			if (randomTens == 4)
-			{
-				tensText = 'forty ';	
-			}
-			if (randomTens == 5)
-			{
-				tensText = 'fifty ';	
-			}
-			if (randomTens == 6)
-			{
-				tensText = 'sixty ';	
-			}
-			if (randomTens == 7)
-			{
-				tensText = 'seventy ';	
-			}
-			if (randomTens == 8)
-			{
-				tensText = 'eighty ';	
-			}
-			if (randomTens == 9)
-			{
-				tensText = 'ninety ';	
-			}
+					//tens	
+					if (randomTens == 1)
+					{
+						if (randomOnes == 0)
+						{
+							randomOnes = 0;	
+							tensText = 'ten';		
+						}
+						if (randomOnes == 1)
+						{
+							randomOnes = 0;	
+							tensText = 'eleven';		
+						}
+						if (randomOnes == 2)
+						{
+							randomOnes = 0;	
+							tensText = 'twelve';		
+						}
+						if (randomOnes == 3)
+						{
+							randomOnes = 0;	
+							tensText = 'thirteen';		
+						}
+						if (randomOnes == 4)
+						{
+							randomOnes = 0;	
+							tensText = 'fourteen';		
+						}
+						if (randomOnes == 5)
+						{
+							randomOnes = 0;	
+							tensText = 'fifteen';		
+						}
+						if (randomOnes == 6)
+						{
+							randomOnes = 0;	
+							tensText = 'sixteen';		
+						}
+						if (randomOnes == 7)
+						{
+							randomOnes = 0;	
+							tensText = 'seventeen';		
+						}
+						if (randomOnes == 8)
+						{
+							randomOnes = 0;	
+							tensText = 'eighteen';		
+						}
+						if (randomOnes == 9)
+						{
+							randomOnes = 0;	
+							tensText = 'nineteen';		
+						}
+					}
+					if (randomTens == 2)
+					{
+						tensText = 'twenty ';	
+					}
+					if (randomTens == 3)
+					{
+						tensText = 'thirty ';	
+					}
+					if (randomTens == 4)
+					{
+						tensText = 'forty ';	
+					}
+					if (randomTens == 5)
+					{
+						tensText = 'fifty ';	
+					}
+					if (randomTens == 6)
+					{
+						tensText = 'sixty ';	
+					}
+					if (randomTens == 7)
+					{
+						tensText = 'seventy ';	
+					}
+					if (randomTens == 8)
+					{
+						tensText = 'eighty ';	
+					}
+					if (randomTens == 9)
+					{
+						tensText = 'ninety ';	
+					}
 			
-			//ones	
-			if (randomOnes == 0)
-			{
-			}
-			if (randomOnes == 1)
-			{
-				onesText = 'one';	
-			}
-			if (randomOnes == 2)
-			{
-				onesText = 'two';	
-			}
-			if (randomOnes == 3)
-			{
-				onesText = 'three';	
-			}
-			if (randomOnes == 4)
-			{
-				onesText = 'four';	
-			}
-			if (randomOnes == 5)
-			{
-				onesText = 'five';	
-			}
-			if (randomOnes == 6)
-			{
-				onesText = 'six';	
-			}
-			if (randomOnes == 7)
-			{
-				onesText = 'seven';	
-			}
-			if (randomOnes == 8)
-			{
-				onesText = 'eight';	
-			}
-			if (randomOnes == 9)
-			{
-				onesText = 'nine';
-			}
-
+					//ones	
+					if (randomOnes == 1)
+					{
+						onesText = 'one';	
+					}
+					if (randomOnes == 2)
+					{
+						onesText = 'two';	
+					}
+					if (randomOnes == 3)
+					{
+						onesText = 'three';	
+					}
+					if (randomOnes == 4)
+					{
+						onesText = 'four';	
+					}
+					if (randomOnes == 5)
+					{
+						onesText = 'five';	
+					}
+					if (randomOnes == 6)
+					{
+						onesText = 'six';	
+					}
+					if (randomOnes == 7)
+					{
+						onesText = 'seven';	
+					}
+					if (randomOnes == 8)
+					{
+						onesText = 'eight';	
+					}
+					if (randomOnes == 9)
+					{
+						onesText = 'nine';
+					}
+					totalA++;
+				}
 			var question = new Question('Write in numerical form: ' + hundredsText + '' + tensText + '' + onesText, '' + randomNumber);
-
 			this.mQuiz.mQuestionArray.push(question);
+			}
 		}
 	}
 });
