b0ff9748b7aedd21d26bd4cdaae3146ecb04c7d9
jbreslin33 <jbreslin33@localhost>
2014-03-03 17:30:07 -0500

    2.nbt.b.5 works.

--- src/src/game/pad/2_nbt_b_5.php
+++ src/src/game/pad/2_nbt_b_5.php
@@ -6,34 +6,58 @@ Extends: NumberPad,
 	initialize: function(application)
 	{
        		this.parent(application);
-
-		this.mThresholdTime = 6000;
 	},
 
 	createQuestions: function()
         {
  		this.parent();
 
+		var totalA = 0;
+		var totalB = 0;
+
 		var varA = 0;
 		var varB = 0;
 		var varC = 0;
 
-		this.mQuiz.resetQuestionArray();
+		var randomChance = 0;
 
-		for (s = 0; s < this.mScoreNeeded; s++)
-		{	
-			varC = 100;
-			while (varC > 99 || varC < 0)
+		while (totalA < this.mScoreNeeded * .4 || totalB < this.mScoreNeeded * .4)
+		{
+			this.mQuiz.resetQuestionArray();
+			totalA = 0;
+			totalB = 0;
+
+			for (s = 0; s < this.mScoreNeeded; s++)
 			{	
-				varA = Math.floor((Math.random()*9)+1);		
-				varA = varA * 10;
+				varC = 100;
+				randomChance = Math.floor((Math.random()*2));		
+
+				while (varC > 99 || varC < 0)
+				{	
+					varA = Math.floor((Math.random()*99));		
+					varB = Math.floor((Math.random()*99));		
 				
-				varB = Math.floor((Math.random()*9)+1);		
-				varB = varB * 10;
-				varC = parseInt(varA - varB);
+					if (randomChance == 0)
+					{
+						varC = parseInt(varA + varB);
+					}
+					else
+					{
+						varC = parseInt(varA - varB);
+					}
+				}
+   				if (randomChance == 0)
+                                {
+                                        this.mQuiz.mQuestionArray.push(new Question('' + varA + ' + ' +  varB + ' = ', '' + varC));
+                                        totalA++;
+                                }
+                                else
+                                {
+                                        this.mQuiz.mQuestionArray.push(new Question('' + varA + ' - ' +  varB + ' = ', '' + varC));
+                                        totalB++;
+                                }
 			}
 			//ok we broke while now add question...
-			this.mQuiz.mQuestionArray.push(new Question('' + varA + ' - ' +  varB + ' = ', '' + varC));
 		}
 	}
 });
