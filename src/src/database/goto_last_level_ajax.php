d8f4517e1956f68d3250e712802137e824ef68db
jbreslin33 <jbreslin33@localhost>
2014-03-04 23:56:32 -0500

    last lev works. what about next

--- src/src/database/goto_last_level_ajax.php
+++ src/src/database/goto_last_level_ajax.php
@@ -13,8 +13,16 @@ setLevelSessionVariablesBreslin($conn,$_SESSION["user_id"]);
 
 //fill php vars
 $returnString = "101,";
-$returnString .= $_SESSION["attempt_id"];
+$returnString .= $_SESSION["ref_id"];
+$returnString .= ",";
+$returnString .= $_SESSION["level"];
+$returnString .= ",";
+$returnString .= $_SESSION["standard"];
+$returnString .= ",";
+$returnString .= $_SESSION["progression"];
+$returnString .= ",";
+$returnString .= $_SESSION["levels"];
+$returnString .= ",";
+$returnString .= $_SESSION["failed_attempts"];
 echo $returnString;
-
 ?>
-
