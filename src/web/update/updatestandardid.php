fe1e2618b048c9dab6e16196573edf29a3052344
jbreslin33 <jbreslin33@localhost>
2014-03-05 17:19:24 -0500

    se

--- src/web/update/updatestandardid.php
+++ src/web/update/updatestandardid.php
@@ -1,22 +1,20 @@
 <!DOCTYPE html>
-
 <html>
-
 <head>
 <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
 <meta content="utf-8" http-equiv="encoding">
 <link rel="stylesheet" type="text/css" href="<?php getenv("DOCUMENT_ROOT")?>/css/green_block.css" />
 
 <?php
+$conn = dbConnect();
+
 include(getenv("DOCUMENT_ROOT") . "/web/navigation/top_links.php");
 include(getenv("DOCUMENT_ROOT") . "/src/database/set_level_session_variables.php");
-?>
 
-<?php
+//setLevelSessionVariablesRewind($conn,$_SESSION["user_id"]);
 
 $response = "";
 
-//new
 //insert into levelattempts (start_time,user_id,level,ref_id,transaction_code) VALUES (CURRENT_TIMESTAMP,7,1,( select ref_id from learning_standards where id = 'k.cc.a.1'),2);
 
         $insert = "insert into levelattempts (start_time, user_id,level,ref_id,transaction_code) VALUES (CURRENT_TIMESTAMP,";
@@ -28,23 +26,13 @@ $response = "";
 
         $insertResult = pg_query($conn,$insert) or die('Could not connect: ' . pg_last_error());
         dbErrorCheck($conn,$insertResult);
-
-
+	
 	$response = "Success";
-
-
 ?>
-
 </head>
-
 <body>
-
 <?php
 echo $response;
 ?>
-
-
 </body>
-
 </html>
-
