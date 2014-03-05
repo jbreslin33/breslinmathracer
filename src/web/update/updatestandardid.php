48f6a9566fbfaeca6f47ca79769629e968f56ad5
jbreslin33 <jbreslin33@localhost>
2014-03-05 17:26:36 -0500

    u

--- src/web/update/updatestandardid.php
+++ src/web/update/updatestandardid.php
@@ -6,33 +6,30 @@
 <link rel="stylesheet" type="text/css" href="<?php getenv("DOCUMENT_ROOT")?>/css/green_block.css" />
 
 <?php
-$conn = dbConnect();
+//include(getenv("DOCUMENT_ROOT") . "/src/database/db_connect.php");
+
+//start new session
+//session_start();
+
+//$conn = dbConnect();
 
 include(getenv("DOCUMENT_ROOT") . "/web/navigation/top_links.php");
-include(getenv("DOCUMENT_ROOT") . "/src/database/set_level_session_variables.php");
+//include(getenv("DOCUMENT_ROOT") . "/src/database/set_level_session_variables.php");
 
-//setLevelSessionVariablesRewind($conn,$_SESSION["user_id"]);
-
-$response = "";
-
-//insert into levelattempts (start_time,user_id,level,ref_id,transaction_code) VALUES (CURRENT_TIMESTAMP,7,1,( select ref_id from learning_standards where id = 'k.cc.a.1'),2);
-
-        $insert = "insert into levelattempts (start_time, user_id,level,ref_id,transaction_code) VALUES (CURRENT_TIMESTAMP,";
-	$insert .= $_SESSION["user_id"];
-	$insert .= ",1,'"; 
-	$insert .= "( select ref_id from learning_standards where id = '";
-	$insert .= $_POST["standardid"]; 
-	$insert .= "'),2);";
-
-        $insertResult = pg_query($conn,$insert) or die('Could not connect: ' . pg_last_error());
-        dbErrorCheck($conn,$insertResult);
+/*
+$insert = "insert into levelattempts (start_time, user_id,level,ref_id,transaction_code) VALUES (CURRENT_TIMESTAMP,";
+$insert .= $_SESSION["user_id"];
+$insert .= ",1,'"; 
+$insert .= "( select ref_id from learning_standards where id = '";
+$insert .= $_POST["standardid"]; 
+$insert .= "'),2);";
+*/
+//$insertResult = pg_query($conn,$insert) or die('Could not connect: ' . pg_last_error());
+//dbErrorCheck($conn,$insertResult);
 	
-	$response = "Success";
 ?>
 </head>
 <body>
-<?php
-echo $response;
-?>
+<b>hello</b>
 </body>
 </html>
