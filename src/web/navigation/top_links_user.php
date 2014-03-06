3b753c9d389af8ef509ef8e706540208f6a373ef
jbreslin33 <jbreslin33@localhost>
2014-03-06 17:22:46 -0500

    ready for mod

--- src/web/navigation/top_links_user.php
+++ src/web/navigation/top_links_user.php
@@ -5,6 +5,7 @@
 <li><a href="/web/update/update_standard_id_form.php">Update Standard ID</a></li>
 <li><a href="/web/update/update_username_form.php">Update Username</a></li>
 <li><a href="/web/update/update_password_form.php">Update Password</a></li>
+<li><a href="/web/update/update_first_name_form.php">Update First Name</a></li>
 <li><a href="/web/update/update_last_name_form.php">Update Last Name</a></li>
 <li><a href="/web/insert/upload.php">Upload School From Linux</a></li>
 </ul>
