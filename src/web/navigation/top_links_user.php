549c84d125686a1c4b014a58c40c5bbb966322b6
jbreslin33 <jbreslin33@localhost>
2014-03-06 14:26:13 -0500

    u

--- src/web/navigation/top_links_user.php
+++ src/web/navigation/top_links_user.php
@@ -3,6 +3,7 @@
 <li><a href="/web/select/select.php">Selects</a></li>
 <li><a href="/web/login/login_form.php">Logout</a></li>
 <li><a href="/web/update/update_standard_id_form.php">Update Standard ID</a></li>
+<li><a href="/web/update/update_username_form.php">Update Username</a></li>
 <li><a href="/web/update/update_password_form.php">Update Password</a></li>
 <li><a href="/web/insert/upload.php">Upload School From Linux</a></li>
 </ul>
