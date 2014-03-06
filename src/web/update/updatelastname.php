7f9ff6220f9e9f52d68f878bc9a0ec8599a166fb
jbreslin33 <jbreslin33@localhost>
2014-03-06 17:17:33 -0500

    last name works.

--- src/web/update/updatelastname.php
+++ src/web/update/updatelastname.php
@@ -16,8 +16,8 @@ include(getenv("DOCUMENT_ROOT") . "/src/database/set_level_session_variables.php
 
 $response = "";
 
-$update = "update users SET username = '";
-$update .= $_POST["username"];
+$update = "update users SET last_name = '";
+$update .= $_POST["last_name"];
 $update .= "' where id = '";
 $update .= $_POST["id"];
 $update .= "';";
