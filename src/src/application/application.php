ff7eba0f91406f6c51282ff37a93088385489814
jbreslin33 <jbreslin33@localhost>
2014-03-09 19:45:12 -0400

    ready for mod

--- src/src/application/application.php
+++ src/src/application/application.php
@@ -791,6 +791,19 @@ var Application = new Class(
                                	this.mGame = new g2_nbt_b_8(APPLICATION);
 			}	
 		}
+		if (this.mRef_id == '6B0AB3E999084B1B8FE700DD665EF86C')
+		{ 
+             		if (this.mGameName != "g2_md_a_1")
+                       	{
+				if (this.mGame)
+				{
+					this.mGame.destructor();
+					this.mGame = 0;
+				}
+                               	this.mGameName = "g2_md_a_1";
+                               	this.mGame = new g2_md_a_1(APPLICATION);
+			}	
+		}
 		if (this.mRef_id == 'ACB26A2ED7114E59911EE985D8D02B6D')
 		{ 
              		if (this.mGameName != "g3_oa_a_4")
