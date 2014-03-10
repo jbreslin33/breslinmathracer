ddbdbfba4a12f4d0424f09204217a92b9c533415
jbreslin33 <jbreslin33@localhost>
2014-03-09 20:10:45 -0400

    u

--- src/src/widgets/ruler.php
+++ src/src/widgets/ruler.php
@@ -7,8 +7,8 @@ var Ruler  = new Class(
 		//number pad
 		this.mTimerArray = new Array();
 
-		this.canvas = 0;
-		this.clock = 0;
+		this.mCanvas = 0;
+		this.mRuler = 0;
 
 		this.hour_sign = new Array();
 		this.hour_sign[0] = 0;
@@ -38,21 +38,21 @@ var Ruler  = new Class(
 	//fake virtual
 	createRuler: function()
 	{
-                this.canvas = Raphael(50,200,200,200);
-                this.clock = this.canvas.circle(100,100,95);
-                this.clock.attr({"fill":"#f5f5f5","stroke":"#444444","stroke-width":"5"})
+                this.mCanvas = Raphael(50,200,200,200);
+                this.mRuler = this.mCanvas.circle(100,100,95);
+                this.mRuler.attr({"fill":"#f5f5f5","stroke":"#444444","stroke-width":"5"})
                 for(i=0;i<12;i++)
                 {
                         var start_x = 100+Math.round(80*Math.cos(30*i*Math.PI/180));
                         var start_y = 100+Math.round(80*Math.sin(30*i*Math.PI/180));
                         var end_x = 100+Math.round(90*Math.cos(30*i*Math.PI/180));
                         var end_y = 100+Math.round(90*Math.sin(30*i*Math.PI/180));
-                        this.hour_sign[i] = this.canvas.path("M"+start_x+" "+start_y+"L"+end_x+" "+end_y);
+                        this.hour_sign[i] = this.mCanvas.path("M"+start_x+" "+start_y+"L"+end_x+" "+end_y);
                 }
-                this.minute_hand = this.canvas.path("M100 100L100 40");
+                this.minute_hand = this.mCanvas.path("M100 100L100 40");
                 this.minute_hand.attr({stroke: "#444444", "stroke-width": 4});
 
-                this.pin = this.canvas.circle(100, 100, 5);
+                this.pin = this.mCanvas.circle(100, 100, 5);
                 this.pin.attr("fill", "#000000");
 
                 //reset transforms
@@ -69,7 +69,7 @@ var Ruler  = new Class(
       	hide: function()
         {
                 //shapes and array
-		this.clock.hide();
+		this.mRuler.hide();
 		for (i=0;i<12;i++)
 		{
 			this.hour_sign[i].hide();
@@ -85,7 +85,7 @@ var Ruler  = new Class(
         show: function()
         {
                 //shapes and array
-		this.clock.show();
+		this.mRuler.show();
 		for (i=0;i<12;i++)
 		{
 			this.hour_sign[i].show();
