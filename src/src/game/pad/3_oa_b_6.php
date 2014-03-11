80ea707162de3d75e309c1fac6dc08fd18b765ea
jbreslin33 <jbreslin33@localhost>
2014-03-11 17:11:25 -0400

    u

--- src/src/game/pad/3_oa_b_6.php
+++ src/src/game/pad/3_oa_b_6.php
@@ -70,63 +70,20 @@ Extends: NumberPad,
         {
   		this.parent();
 
-		var totalA = 0; 
-		var totalB = 0; 
-		var totalC = 0; 
+		this.mQuiz.resetQuestionArray();
 
-		while(totalA < 5 || totalB < 5 || totalC < 5)  
+              	for (i = 0; i < this.mScoreNeeded; i++)
 		{
-			totalA = 0; 
-			totalB = 0; 
-			totalC = 0; 
-
-			this.mQuiz.resetQuestionArray();
-
-              		for (i = 0; i < this.mScoreNeeded; i++)
+			this.a = 0;
+			this.b = 0;
+			this.x = 0;
+			while (this.a == this.b)
 			{
-				var property = Math.floor((Math.random()*3));
-				//communative
-				if (property == 0)	
-				{
-					this.a = 0;
-					this.b = 0;
-					while (this.a == this.b)
-					{
-						this.a = Math.floor((Math.random()*10)+1);
-						this.b = Math.floor((Math.random()*10)+1);
-						var question = new Question('Using the Communative property of multiplication write this expression another way: ' + this.a + 'x' + this.b,'' + this.b + 'x' + this.a);
-						this.mQuiz.mQuestionArray.push(question);
-						totalA++;
-					}
-				}
-
-				//associative
-				if (property == 1)
-                                {
-                                        this.a = Math.floor((Math.random()*10)+1);
-                                        this.b = Math.floor((Math.random()*10)+1);
-                                        this.c = Math.floor((Math.random()*10)+1);
-                                        var question = new Question('Using the Associative property of multiplication write this expression another way: ' + this.a + 'x' + this.b + 'x' + this.c,'' + parseInt(this.a * this.b) + 'x' + this.c);
-					question.mAnswerArray.push('' + this.a + 'x' + parseInt(this.b * this.c));
-                                        this.mQuiz.mQuestionArray.push(question);
-                                        totalB++;
-                                }
-
-				//distributive
-				if (property == 2)
-                                {
-					this.x = 1000;
-					while (this.x > 100 || this.b + this.c > 10)
-					{
-                                        	this.a = Math.floor((Math.random()*10)+1);
-                                        	this.b = Math.floor((Math.random()*10)+1);
-                                        	this.c = Math.floor((Math.random()*10)+1);
-						this.x = parseInt(this.a * (this.b + this.c));
-					}
-                                        var question = new Question('Using the Distributive property of multiplication solve this: ' + this.a + 'x(' + this.b + '+' + this.c + ')','' + this.x);
-                                        this.mQuiz.mQuestionArray.push(question);
-                                        totalC++;
-                                }
+				this.a = Math.floor((Math.random()*10)+1);
+				this.b = Math.floor((Math.random()*10)+1);
+				var question = new Question('Find ' + this.a + '÷' + this.b + ' by finding the number that makes ' + this.a + ' when multiplied by ' + this.b + '.','' + this.x);
+				this.mQuiz.mQuestionArray.push(question);
+				totalA++;
                         }
 		}
 	}
