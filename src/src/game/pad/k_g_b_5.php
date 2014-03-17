ea27fc8da7be95c2e03d1a88efe1b76e2f48177b
jbreslin33 <jbreslin33@localhost>
2014-03-17 17:52:26 -0400

    nothing is the answer.

--- src/src/game/pad/k_g_b_5.php
+++ src/src/game/pad/k_g_b_5.php
@@ -12,33 +12,13 @@ Extends: NumberPad,
 	createQuestions: function()
         {
 		this.parent();
-		
-		var totalCount = 0;
+	
+		//just the question array reset
+		this.mQuiz.resetQuestionArray();
 
-		while (totalCount < parseInt(this.mScoreNeeded * 7) || totalCount > parseInt(this.mScoreNeeded * 13))
-		{	
-			//reset vars and arrays
-			totalCount = 0;
-
-			//just the question array reset
-			this.mQuiz.resetQuestionArray();
-
-			//loop thru and make potential questions
-			for (s = 0; s < this.mScoreNeeded; s++)
-			{	
-				//random number to count from 0-20
-				var objectsToCount = Math.floor((Math.random()*21));		
-
-				var question = new Question('How many kids?', '' + objectsToCount);
-				for (i = 0; i < objectsToCount; i++)
-				{
-					question.mShapeArray.push(this.mShapeArray[parseInt(i + this.mTotalGuiBars + this.mTotalInputBars)]);
-				}
-				this.mQuiz.mQuestionArray.push(question);
-
-				totalCount = parseInt(totalCount + objectsToCount);
-			}
-		}
+		var question = new Question('What?', 'Nothing');
+		this.mQuiz.mQuestionArray.push(question);
+	
                	//buffer
                 this.mQuiz.mQuestionArray.push(new Question('buf','buf'));
 	},
@@ -47,28 +27,6 @@ Extends: NumberPad,
 	{
 		this.parent();
 
-                this.mShapeArray.push(new Shape(50,50,25,50,this,"/images/bus/kid.png","",""));
-                this.mShapeArray.push(new Shape(50,50,25,100,this,"/images/bus/kid.png","",""));
-                this.mShapeArray.push(new Shape(50,50,25,150,this,"/images/bus/kid.png","",""));
-                this.mShapeArray.push(new Shape(50,50,25,200,this,"/images/bus/kid.png","",""));
-                this.mShapeArray.push(new Shape(50,50,25,250,this,"/images/bus/kid.png","",""));
-                	
-                this.mShapeArray.push(new Shape(50,50,75,50,this,"/images/bus/kid.png","",""));
-                this.mShapeArray.push(new Shape(50,50,75,100,this,"/images/bus/kid.png","",""));
-                this.mShapeArray.push(new Shape(50,50,75,150,this,"/images/bus/kid.png","",""));
-                this.mShapeArray.push(new Shape(50,50,75,200,this,"/images/bus/kid.png","",""));
-                this.mShapeArray.push(new Shape(50,50,75,250,this,"/images/bus/kid.png","",""));
-                
-		this.mShapeArray.push(new Shape(50,50,125,50,this,"/images/bus/kid.png","",""));
-                this.mShapeArray.push(new Shape(50,50,125,100,this,"/images/bus/kid.png","",""));
-                this.mShapeArray.push(new Shape(50,50,125,150,this,"/images/bus/kid.png","",""));
-                this.mShapeArray.push(new Shape(50,50,125,200,this,"/images/bus/kid.png","",""));
-                this.mShapeArray.push(new Shape(50,50,125,250,this,"/images/bus/kid.png","",""));
-		
-		this.mShapeArray.push(new Shape(50,50,175,50,this,"/images/bus/kid.png","",""));
-                this.mShapeArray.push(new Shape(50,50,175,100,this,"/images/bus/kid.png","",""));
-                this.mShapeArray.push(new Shape(50,50,175,150,this,"/images/bus/kid.png","",""));
-                this.mShapeArray.push(new Shape(50,50,175,200,this,"/images/bus/kid.png","",""));
                 this.mShapeArray.push(new Shape(50,50,175,250,this,"/images/bus/kid.png","",""));
 	}
 });
