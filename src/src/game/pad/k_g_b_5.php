dfc5261bbde3c0e3a49e8c404cb8d9c5ed23e3e8
jbresiln33 <jbresiln33@localhost>
2014-03-19 08:20:46 -0400

    u

--- src/src/game/pad/k_g_b_5.php
+++ src/src/game/pad/k_g_b_5.php
@@ -7,11 +7,6 @@ Extends: Game,
 	{
        		this.parent(application);
 		this.setScoreNeeded(1);
-		this.mRaphael = Raphael(0, 0, "100%", "100%");	
-                this.mRed    = this.mRaphael.circle(100, 100, 50);
-                this.mGreen  = this.mRaphael.circle(210, 100, 50);
-                this.mBlue   = this.mRaphael.circle(320, 100, 50);
-                this.mPurple = this.mRaphael.circle(430, 100, 50);
 
 	},
 
@@ -29,29 +24,26 @@ Extends: Game,
                 this.mQuiz.mQuestionArray.push(new Question('buf','buf'));
 	},
 
-	start: function()
-	{
-                    this.ox = this.attr("cx");
-                    this.oy = this.attr("cy");
-                    this.animate({r: 70, opacity: .25}, 500, ">");
-
-	},
-
-	move: function(dx,dy)
-	{
-        	this.attr({cx: this.ox + dx, cy: this.oy + dy});
-	},	
-
 	createWorld: function()
 	{
 		this.parent();
+    		var R = Raphael(0, 0, "100%", "100%"),
+                    r = R.circle(100, 100, 50).attr({fill: "hsb(0, 1, 1)", stroke: "none", opacity: .5}),
+                    g = R.circle(210, 100, 50).attr({fill: "hsb(.3, 1, 1)", stroke: "none", opacity: .5}),
+                    b = R.circle(320, 100, 50).attr({fill: "hsb(.6, 1, 1)", stroke: "none", opacity: .5}),
+                    p = R.circle(430, 100, 50).attr({fill: "hsb(.8, 1, 1)", stroke: "none", opacity: .5});
+                var start = function () {
+                    this.ox = this.attr("cx");
+                    this.oy = this.attr("cy");
+                    this.animate({r: 70, opacity: .25}, 500, ">");
+                },
                 move = function (dx, dy) {
                     this.attr({cx: this.ox + dx, cy: this.oy + dy});
                 },
                 up = function () {
                     this.animate({r: 50, opacity: .5}, 500, ">");
                 };
-                R.set(r, g, b, p).drag(move, start, up);
+                R.set(r, g, b, p).drag(move, start, up);                
 
                 this.mShapeArray.push(new Shape(50,50,175,250,this,"/images/bus/kid.png","",""));
 	}
