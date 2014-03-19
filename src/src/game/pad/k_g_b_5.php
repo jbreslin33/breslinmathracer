3edd7e2635cf310ccf2a11adc1ba72a68f3e5f57
jbresiln33 <jbresiln33@localhost>
2014-03-19 08:22:32 -0400

    in constructor

--- src/src/game/pad/k_g_b_5.php
+++ src/src/game/pad/k_g_b_5.php
@@ -7,6 +7,23 @@ Extends: Game,
 	{
        		this.parent(application);
 		this.setScoreNeeded(1);
+    		var R = Raphael(0, 0, "100%", "100%"),
+                    r = R.circle(100, 100, 50).attr({fill: "hsb(0, 1, 1)", stroke: "none", opacity: .5}),
+                    g = R.circle(210, 100, 50).attr({fill: "hsb(.3, 1, 1)", stroke: "none", opacity: .5}),
+                    b = R.circle(320, 100, 50).attr({fill: "hsb(.6, 1, 1)", stroke: "none", opacity: .5}),
+                    p = R.circle(430, 100, 50).attr({fill: "hsb(.8, 1, 1)", stroke: "none", opacity: .5});
+                var start = function () {
+                    this.ox = this.attr("cx");
+                    this.oy = this.attr("cy");
+                    this.animate({r: 70, opacity: .25}, 500, ">");
+                },
+                move = function (dx, dy) {
+                    this.attr({cx: this.ox + dx, cy: this.oy + dy});
+                },
+                up = function () {
+                    this.animate({r: 50, opacity: .5}, 500, ">");
+                };
+                R.set(r, g, b, p).drag(move, start, up);                
 
 	},
 
@@ -27,23 +44,6 @@ Extends: Game,
 	createWorld: function()
 	{
 		this.parent();
-    		var R = Raphael(0, 0, "100%", "100%"),
-                    r = R.circle(100, 100, 50).attr({fill: "hsb(0, 1, 1)", stroke: "none", opacity: .5}),
-                    g = R.circle(210, 100, 50).attr({fill: "hsb(.3, 1, 1)", stroke: "none", opacity: .5}),
-                    b = R.circle(320, 100, 50).attr({fill: "hsb(.6, 1, 1)", stroke: "none", opacity: .5}),
-                    p = R.circle(430, 100, 50).attr({fill: "hsb(.8, 1, 1)", stroke: "none", opacity: .5});
-                var start = function () {
-                    this.ox = this.attr("cx");
-                    this.oy = this.attr("cy");
-                    this.animate({r: 70, opacity: .25}, 500, ">");
-                },
-                move = function (dx, dy) {
-                    this.attr({cx: this.ox + dx, cy: this.oy + dy});
-                },
-                up = function () {
-                    this.animate({r: 50, opacity: .5}, 500, ">");
-                };
-                R.set(r, g, b, p).drag(move, start, up);                
 
                 this.mShapeArray.push(new Shape(50,50,175,250,this,"/images/bus/kid.png","",""));
 	}
