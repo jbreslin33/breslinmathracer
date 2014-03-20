cba7af504101331326f4841e8e0c792f78d1c69a
jbreslin33 <jbreslin33@localhost>
2014-03-20 09:50:16 -0400

    red triangle

--- src/src/game/pad/k_g_b_5.php
+++ src/src/game/pad/k_g_b_5.php
@@ -9,11 +9,15 @@ Extends: Game,
 		this.setScoreNeeded(1);
 
     		this.mRaphael = Raphael(10, 35, 760, 405),
+
                 this.mRedCircle = this.mRaphael.circle(100, 300, 50).attr({fill: "hsb(0, 1, 1)", stroke: "none", opacity: .5}),
                 this.mGreenCircle = this.mRaphael.circle(210, 300, 50).attr({fill: "hsb(.3, 1, 1)", stroke: "none", opacity: .5}),
                 this.mBlueCircle = this.mRaphael.circle(320, 300, 50).attr({fill: "hsb(.6, 1, 1)", stroke: "none", opacity: .5}),
                 this.mPurpleCircle = this.mRaphael.circle(430, 300, 50).attr({fill: "hsb(.8, 1, 1)", stroke: "none", opacity: .5}),
-		this.mRedRectangle = this.mRaphael.rect(10, 10, 50, 50).attr({fill: "hsb(.8, 1, 1)", stroke: "none", opacity: .5});
+
+		this.mRedRectangle = this.mRaphael.rect(10, 10, 50, 50).attr({fill: "hsb(0, 1, 1)", stroke: "none", opacity: .5}),
+
+		this.mRedTriangle = this.mRaphael.path("M0,100 L50,50 L100,100 z").attr({fill: "hsb(0, 1, 1)", stroke: "none", opacity: .5});
 
                 this.mRaphael.set(this.mRedCircle, this.mGreenCircle, this.mBlueCircle, this.mPurpleCircle).drag(this.moveCircle, this.startCircle, this.upCircle);                
                 this.mRaphael.set(this.mRedRectangle).drag(this.move, this.start, this.up);                
