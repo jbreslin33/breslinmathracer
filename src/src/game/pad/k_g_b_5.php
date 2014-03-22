878ddced819aeec78bdb3f206f53d87e16ce7460
jbreslin33 <jbreslin33@localhost>
2014-03-22 18:37:06 -0400

    4 tris

--- src/src/game/pad/k_g_b_5.php
+++ src/src/game/pad/k_g_b_5.php
@@ -16,6 +16,9 @@ Extends: Game,
 		this.mPurpleRectangle = new Rectangle(this.mRaphael,310,10,50,50,0,1,1,"none",.5); 
 
 		this.mRedTriangle  = new Triangle (this.mRaphael,0,150,50,100,100,150,0,1,1,"none",.5); 
+		this.mRedTriangle  = new Triangle (this.mRaphael,100,150,150,100,200,150,0,1,1,"none",.5); 
+		this.mRedTriangle  = new Triangle (this.mRaphael,200,150,250,100,300,150,0,1,1,"none",.5); 
+		this.mRedTriangle  = new Triangle (this.mRaphael,300,150,350,100,400,150,0,1,1,"none",.5); 
 
 		this.mRedCircle    = new Circle   (this.mRaphael,100,300,25,0,1,1,"none",.5); 
 		this.mGreenCircle  = new Circle   (this.mRaphael,210,300,25,.3,1,1,"none",.5); 
