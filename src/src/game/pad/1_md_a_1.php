41fb06bd6434683b1b414218b6fca49d3fc84483
jbreslin33 <jbreslin33@localhost>
2014-03-23 17:54:06 -0400

    close

--- src/src/game/pad/1_md_a_1.php
+++ src/src/game/pad/1_md_a_1.php
@@ -27,20 +27,26 @@ Extends: MultipleChoicePad,
 		this.mQuiz.resetQuestionArray();
 		this.mQuiz.resetQuestionPoolArray();
    
-		this.mQuiz.mAnswerPool.push('YES');
-                this.mQuiz.mAnswerPool.push('NO');
+		this.mQuiz.mAnswerPool.push('Red Green Blue');
+		this.mQuiz.mAnswerPool.push('Red Blue Green');
+                this.mQuiz.mAnswerPool.push('Green Blue Red');
+                this.mQuiz.mAnswerPool.push('Green Red Blue');
+                this.mQuiz.mAnswerPool.push('Blue Red Green');
+                this.mQuiz.mAnswerPool.push('Blue Green Red');
 
-		var question = new Question('Can you join the red and green triangles together to form a square?', 'YES');
+		var question = new Question('Order shapes by length from shortes to longest.', 'Red Green Blue');
 		question.mAnswerPool = this.mQuiz.mAnswerPool;
 		this.mQuiz.mQuestionArray.push(question);
 		question.mShapeArray.push(this.mShapeArray[parseInt(0 + this.mTotalGuiBars + this.mTotalInputBars)]);
 		question.mShapeArray.push(this.mShapeArray[parseInt(1 + this.mTotalGuiBars + this.mTotalInputBars)]);
+		question.mShapeArray.push(this.mShapeArray[parseInt(2 + this.mTotalGuiBars + this.mTotalInputBars)]);
 
-		var question = new Question('Can you join the red and green squares together to form a rectangle?', 'YES');
+		var question = new Question('Order shapes by length from shortes to longest.', 'Red Green Blue');
 		question.mAnswerPool = this.mQuiz.mAnswerPool;
 		this.mQuiz.mQuestionArray.push(question);
-		question.mShapeArray.push(this.mShapeArray[parseInt(2 + this.mTotalGuiBars + this.mTotalInputBars)]);
 		question.mShapeArray.push(this.mShapeArray[parseInt(3 + this.mTotalGuiBars + this.mTotalInputBars)]);
+		question.mShapeArray.push(this.mShapeArray[parseInt(4 + this.mTotalGuiBars + this.mTotalInputBars)]);
+		question.mShapeArray.push(this.mShapeArray[parseInt(5 + this.mTotalGuiBars + this.mTotalInputBars)]);
 	
                	//buffer
                 this.mQuiz.mQuestionArray.push(new Question('buf','buf'));
@@ -50,13 +56,12 @@ Extends: MultipleChoicePad,
 	{
 		this.parent();
 		
-		this.mShapeArray.push(new Triangle   (this,this.mRaphael,200,300,200,250,250,250,0,1,1,"none",.5,true)); 
-		this.mShapeArray.push(new Triangle   (this,this.mRaphael,300,300,350,250,350,300,.3,1,1,"none",.5,true)); 
-
-		//this.mShapeArray.push(new Rectangle(50,50,10,10,this,this.mRaphael,0,1,1,"none",.5,true));
-                //this.mShapeArray.push(new Rectangle(50,50,100,10,this,this.mRaphael,.3,1,1,"none",.5,true));
-
 		this.mShapeArray.push(new Rectangle(50,50,210,210,this,this.mRaphael,0,1,1,"none",.5,true));
                 this.mShapeArray.push(new Rectangle(50,50,300,310,this,this.mRaphael,.3,1,1,"none",.5,true));
+                this.mShapeArray.push(new Rectangle(50,50,410,310,this,this.mRaphael,.3,1,1,"none",.5,true));
+		
+		this.mShapeArray.push(new Rectangle(50,50,210,210,this,this.mRaphael,0,1,1,"none",.5,true));
+                this.mShapeArray.push(new Rectangle(50,50,300,310,this,this.mRaphael,.3,1,1,"none",.5,true));
+                this.mShapeArray.push(new Rectangle(50,50,410,310,this,this.mRaphael,.3,1,1,"none",.5,true));
 	}
 });
