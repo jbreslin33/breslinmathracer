74a11089a37078933328b659643cb7349c53079d
jbreslin33 <jbreslin33@localhost>
2014-03-26 17:28:30 -0400

    2 units

--- src/src/game/pad/2_md_a_1.php
+++ src/src/game/pad/2_md_a_1.php
@@ -32,7 +32,7 @@ Extends: NumberPad,
 		this.mQuiz.resetQuestionArray();
 
 		//number of shapes
-		var s = 8;  
+		var s = 9;  
  
 		for (i = 0; i < this.mScoreNeeded; i++)
 		{
@@ -53,6 +53,7 @@ Extends: NumberPad,
 			question.mShapeArray.push(this.mShapeArray[parseInt(i * s + 5 + this.mTotalGuiBars + this.mTotalInputBars)]);
 			question.mShapeArray.push(this.mShapeArray[parseInt(i * s + 6 + this.mTotalGuiBars + this.mTotalInputBars)]);
 			question.mShapeArray.push(this.mShapeArray[parseInt(i * s + 7 + this.mTotalGuiBars + this.mTotalInputBars)]);
+			question.mShapeArray.push(this.mShapeArray[parseInt(i * s + 8 + this.mTotalGuiBars + this.mTotalInputBars)]);
 		}
                	
 		//buffer
@@ -76,6 +77,11 @@ Extends: NumberPad,
 			this.mShapeArray.push(unitA);
 			unitA.setMountable(true);
 	
+			var textB = new Shape(5,5,245,200,this,"","","");
+			this.mShapeArray.push(textB);
+			textB.setMountable(true);
+			textB.setText('23');
+
                 	var unitB = new Rectangle(50,50,525,300,this,this.mRaphael,.4,1,1,"none",.5,true);
 			this.mShapeArray.push(unitB);
 			unitB.setMountable(true);
@@ -100,19 +106,21 @@ Extends: NumberPad,
 			var ruler = new Ruler(50,300,200,100,this,this.mRaphael,.6,1,1,"none",.5,true);
 			ruler.createMountPoint(0,30,30);
 			ruler.createMountPoint(1,0,17);
-			ruler.createMountPoint(2,0,67);
-			ruler.createMountPoint(3,0,117);
-			ruler.createMountPoint(4,0,167);
-			ruler.createMountPoint(5,0,217);
-			ruler.createMountPoint(6,0,267);
+			ruler.createMountPoint(2,30,80);
+			ruler.createMountPoint(3,0,67);
+			ruler.createMountPoint(4,0,117);
+			ruler.createMountPoint(5,0,167);
+			ruler.createMountPoint(6,0,217);
+			ruler.createMountPoint(7,0,267);
 			this.mShapeArray.push(ruler);
 			ruler.mount(textA,0);
 			ruler.mount(unitA,1);
-			ruler.mount(unitB,2);
-			ruler.mount(unitC,3);
-			ruler.mount(unitD,4);
-			ruler.mount(unitE,5);
-			ruler.mount(unitF,6);
+			ruler.mount(textB,2);
+			ruler.mount(unitB,3);
+			ruler.mount(unitC,4);
+			ruler.mount(unitD,5);
+			ruler.mount(unitE,6);
+			ruler.mount(unitF,7);
 		}
 	}
 });
