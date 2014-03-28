110e4e7e9a7f35418cfe68cf9742d4104cfb3b10
jbreslin33 <jbreslin33@localhost>
2014-03-28 17:18:56 -0400

    u

--- src/src/game/pad/1_g_a_1.php
+++ src/src/game/pad/1_g_a_1.php
@@ -22,41 +22,38 @@ Extends: MultipleChoicePad,
 	createQuestions: function()
         {
 		this.parent();
-		
-		//just the question array reset
-		this.mQuiz.resetQuestionArray();
-		this.mQuiz.resetQuestionPoolArray();
-   
-		this.mQuiz.mAnswerPool.push('YES');
-                this.mQuiz.mAnswerPool.push('NO');
-
-		var question = new Question('Can you join the red and green triangles together to form a square?', 'YES');
-		question.mAnswerPool = this.mQuiz.mAnswerPool;
-		this.mQuiz.mQuestionArray.push(question);
-		question.mShapeArray.push(this.mShapeArray[parseInt(0 + this.mTotalGuiBars + this.mTotalInputBars)]);
-		question.mShapeArray.push(this.mShapeArray[parseInt(1 + this.mTotalGuiBars + this.mTotalInputBars)]);
-
-		var question = new Question('Can you join the red and green squares together to form a rectangle?', 'YES');
-		question.mAnswerPool = this.mQuiz.mAnswerPool;
-		this.mQuiz.mQuestionArray.push(question);
-		question.mShapeArray.push(this.mShapeArray[parseInt(2 + this.mTotalGuiBars + this.mTotalInputBars)]);
-		question.mShapeArray.push(this.mShapeArray[parseInt(3 + this.mTotalGuiBars + this.mTotalInputBars)]);
 	
-               	//buffer
-                this.mQuiz.mQuestionArray.push(new Question('buf','buf'));
+		if (this.mApplication.mLevel == 1)	
+		{
+			//just the question array reset
+			this.mQuiz.resetQuestionArray();
+			this.mQuiz.resetQuestionPoolArray();
+   
+			var question = new Question('What makes this a triangle?', 'IT HAS 3 SIDES');
+			question.mAnswerPool.push('ITS GREEN');
+			question.mAnswerPool.push('IT HAS 3 SIDES'); 
+			this.mQuiz.mQuestionArray.push(question);
+			question.mShapeArray.push(this.mShapeArray[parseInt(0 + this.mTotalGuiBars + this.mTotalInputBars)]);
+
+			var question = new Question('Is this a triangle?', 'NO');
+			question.mAnswerPool.push('YES');
+			question.mAnswerPool.push('NO'); 
+			this.mQuiz.mQuestionArray.push(question);
+			question.mShapeArray.push(this.mShapeArray[parseInt(1 + this.mTotalGuiBars + this.mTotalInputBars)]);
+	
+               		//buffer
+                	this.mQuiz.mQuestionArray.push(new Question('buf','buf'));
+		}
 	},
 
 	createWorld: function()
 	{
 		this.parent();
 		
-		this.mShapeArray.push(new Triangle   (this,this.mRaphael,200,300,200,250,250,250,0,1,1,"none",.5,true)); 
-		this.mShapeArray.push(new Triangle   (this,this.mRaphael,300,300,350,250,350,300,.3,1,1,"none",.5,true)); 
-
-		//this.mShapeArray.push(new Rectangle(50,50,10,10,this,this.mRaphael,0,1,1,"none",.5,true));
-                //this.mShapeArray.push(new Rectangle(50,50,100,10,this,this.mRaphael,.3,1,1,"none",.5,true));
-
-		this.mShapeArray.push(new Rectangle(50,50,210,210,this,this.mRaphael,0,1,1,"none",.5,true));
-                this.mShapeArray.push(new Rectangle(50,50,300,310,this,this.mRaphael,.3,1,1,"none",.5,true));
+		if (this.mApplication.mLevel == 1)	
+		{
+			this.mShapeArray.push(new Triangle   (this,this.mRaphael,300,300,350,250,350,300,.3,1,1,"none",.5,true)); 
+ 			this.mShapeArray.push(new Rectangle(50,50,300,310,this,this.mRaphael,.3,1,1,"none",.5,true));
+		}
 	}
 });
