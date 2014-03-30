ede1d16f1df6547716dc71f7f553d5f71df62f6c
jbreslin33 <jbreslin33@localhost>
2014-03-29 20:25:48 -0400

    8

--- src/src/game/pad/3_oa_c_7_8.php
+++ src/src/game/pad/3_oa_c_7_8.php
@@ -13,29 +13,28 @@ Extends: NumberPad,
         {
  		this.parent();
 
-		this.mQuiz.mQuestionPoolArray.push(new Question('6 X 0 =','0'));
-		this.mQuiz.mQuestionPoolArray.push(new Question('6 X 1 =','6'));
-		this.mQuiz.mQuestionPoolArray.push(new Question('6 X 2 =','12'));
-		this.mQuiz.mQuestionPoolArray.push(new Question('6 X 3 =','18'));
-		this.mQuiz.mQuestionPoolArray.push(new Question('6 X 4 =','24'));
-		this.mQuiz.mQuestionPoolArray.push(new Question('6 X 5 =','30'));
-		this.mQuiz.mQuestionPoolArray.push(new Question('6 X 6 =','36'));
-		this.mQuiz.mQuestionPoolArray.push(new Question('6 X 7 =','42'));
-		this.mQuiz.mQuestionPoolArray.push(new Question('6 X 8 =','48'));
-		this.mQuiz.mQuestionPoolArray.push(new Question('6 X 9 =','54'));
-		this.mQuiz.mQuestionPoolArray.push(new Question('6 X 10 =','60'));
-		
-		this.mQuiz.mQuestionPoolArray.push(new Question('0 X 6 =','0'));
-		this.mQuiz.mQuestionPoolArray.push(new Question('1 X 6 =','6'));
-		this.mQuiz.mQuestionPoolArray.push(new Question('2 X 6 =','12'));
-		this.mQuiz.mQuestionPoolArray.push(new Question('3 X 6 =','18'));
-		this.mQuiz.mQuestionPoolArray.push(new Question('4 X 6 =','24'));
-		this.mQuiz.mQuestionPoolArray.push(new Question('5 X 6 =','30'));
-		this.mQuiz.mQuestionPoolArray.push(new Question('6 X 6 =','36'));
-		this.mQuiz.mQuestionPoolArray.push(new Question('7 X 6 =','42'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('8 X 7 =','56'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('8 X 9 =','72'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('7 X 8 =','56'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('9 X 8 =','72'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('8 X 4 =','32'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('8 X 3 =','24'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('4 X 8 =','32'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('3 X 8 =','24'));
 		this.mQuiz.mQuestionPoolArray.push(new Question('8 X 6 =','48'));
-		this.mQuiz.mQuestionPoolArray.push(new Question('9 X 6 =','54'));
-		this.mQuiz.mQuestionPoolArray.push(new Question('10 X 6 =','60'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('8 X 8 =','64'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('6 X 8 =','48'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('8 X 8 =','64'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('8 X 2 =','16'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('8 X 5 =','40'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('2 X 8 =','16'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('5 X 8 =','40'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('8 X 10 =','80'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('1 X 8 =','8'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('10 X 8 =','80'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('8 X 0 =','0'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('8 X 1 =','8'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('0 X 8 =','0'));
 		
   		var totalNew           = 0;
 
@@ -52,12 +51,12 @@ Extends: NumberPad,
                                 var randomChance = Math.floor((Math.random()*2));
                                 if (randomChance == 0)
                                 {
-                                        this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[parseInt(this.mApplication.mLevel-1)]);
+                                        this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[parseInt(this.mApplication.mLevel)]);
                                         totalNew++;
                                 }
                                 if (randomChance == 1)
                                 {
-                                        var randomElement = Math.floor((Math.random()*parseInt(this.mApplication.mLevel-1)));           
+                                        var randomElement = Math.floor((Math.random()*parseInt(this.mApplication.mLevel)));           
                                         this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[randomElement]);
                                 }
                         }
