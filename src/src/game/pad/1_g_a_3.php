40ee5e98ff9c4211b2834feef34568b86f83e713
jbresiln3 <jbresiln3@localhost>
2014-03-30 21:10:57 -0400

    u

--- src/src/game/pad/1_g_a_3.php
+++ src/src/game/pad/1_g_a_3.php
@@ -32,6 +32,7 @@ Extends: MultipleChoicePad,
 		question.mAnswerPool[1] = 'HALVES';
 		this.mQuiz.mQuestionArray.push(question);
 		question.mShapeArray.push(this.mShapeArray[parseInt(0 + this.mTotalGuiBars + this.mTotalInputBars)]);
+		question.mShapeArray.push(this.mShapeArray[parseInt(1 + this.mTotalGuiBars + this.mTotalInputBars)]);
 		
                	//buffer
                 this.mQuiz.mQuestionArray.push(new Question('buf','buf'));
@@ -44,5 +45,6 @@ Extends: MultipleChoicePad,
 		this.parent();
 		
 		this.mShapeArray.push(new Rectangle(50,50,210,210,this,this.mRaphael,1,1,1,"#19070B",.5,true));
+		this.mShapeArray.push(new Rectangle(50,50,260,210,this,this.mRaphael,.75,.75,.75,"#19070B",1,true));
 	}
 });
