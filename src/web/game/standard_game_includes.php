a78851fd7dd0c0c1f5176b22441e9d232ee44cec
jbreslin33 <jbreslin33@localhost>
2014-04-07 17:27:38 -0400

    u

--- src/web/game/standard_game_includes.php
+++ src/web/game/standard_game_includes.php
@@ -107,6 +107,7 @@
 <script type="text/javascript" src="/src/game/pad/2_nbt_b_8.php"></script>
 <script type="text/javascript" src="/src/game/pad/2_md_a_1.php"></script>
 <script type="text/javascript" src="/src/game/pad/2_md_a_2.php"></script>
+<script type="text/javascript" src="/src/game/pad/2_md_a_3.php"></script>
 <script type="text/javascript" src="/src/game/pad/2_g_a_2.php"></script>
 <script type="text/javascript" src="/src/game/pad/3_oa_a_1.php"></script>
 <script type="text/javascript" src="/src/game/pad/3_oa_a_2.php"></script>
