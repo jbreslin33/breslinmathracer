c943c9854667a0aa78643338ae6685bde69ce5b2
jbreslin33 <jbreslin33@localhost>
2014-04-10 17:28:58 -0400

    u

--- src/src/game/pad/2_md_b_5.php
+++ src/src/game/pad/2_md_b_5.php
@@ -21,7 +21,6 @@ Extends: NumberPad,
 	        this.mShapeArray[8].setVisibility(false);
 	        this.mShapeArray[9].setVisibility(false);
   		this.mShapeArray[1].mMesh.innerHTML = '' + this.mQuiz.getQuestion().getQuestion() + ' ANSWER: ' + this.mQuiz.getQuestion().getAnswer();
-
         },
 
         //outOfTime
