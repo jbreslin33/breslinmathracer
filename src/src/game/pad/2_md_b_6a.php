87dce4ef49b04b3bb4b9b21e2b726d9a1c9b3a50
jbreslnio33 <jbreslnio33@localhost>
2014-04-14 12:56:09 -0400

    u

--- src/src/game/pad/2_md_b_6a.php
+++ src/src/game/pad/2_md_b_6a.php
@@ -37,37 +37,8 @@ Extends: MultipleChoicePad,
 		question.mShapeArray.push(this.mShapeArray[parseInt(3 + this.mTotalGuiBars + this.mTotalInputBars)]);
 		question.mShapeArray.push(this.mShapeArray[parseInt(4 + this.mTotalGuiBars + this.mTotalInputBars)]);
 		question.mTipArray[0] = 'A rectangle has 4 sides that touch.';
-/*			
-		var question = new Question('This blue shape has three sides, is it a triangle?', 'no');
-		question.mAnswerPool.push('yes');
-		question.mAnswerPool.push('no'); 
-		this.mQuiz.mQuestionArray.push(question);
-		question.mShapeArray.push(this.mShapeArray[parseInt(4 + this.mTotalGuiBars + this.mTotalInputBars)]);
-		question.mShapeArray.push(this.mShapeArray[parseInt(5 + this.mTotalGuiBars + this.mTotalInputBars)]);
-		question.mShapeArray.push(this.mShapeArray[parseInt(6 + this.mTotalGuiBars + this.mTotalInputBars)]);
-		question.mTipArray[0] = 'A triangle has 3 sides that touch.';
-   
-		var question = new Question('What makes this a triangle?', 'It has 3 sides that touch');
-		question.mAnswerPool.push('It is green');
-		question.mAnswerPool.push('It has 3 sides that touch'); 
-		question.mAnswerPool.push('It has 3 sides'); 
-		this.mQuiz.mQuestionArray.push(question);
-		question.mShapeArray.push(this.mShapeArray[parseInt(7 + this.mTotalGuiBars + this.mTotalInputBars)]);
-
-		var question = new Question('Is this a triangle?', 'no');
-		question.mAnswerPool.push('yes');
-		question.mAnswerPool.push('no'); 
-		this.mQuiz.mQuestionArray.push(question);
-		question.mShapeArray.push(this.mShapeArray[parseInt(8 + this.mTotalGuiBars + this.mTotalInputBars)]);
-			
-		var question = new Question('What makes this a square?', 'It has 4 equal sides that touch');
-		question.mAnswerPool.push('It has 4 sides');
-		question.mAnswerPool.push('It has 4 equal sides');
-		question.mAnswerPool.push('It has 4 equal sides that touch');
-		this.mQuiz.mQuestionArray.push(question);
-		question.mShapeArray.push(this.mShapeArray[parseInt(9 + this.mTotalGuiBars + this.mTotalInputBars)]);
-*/	
-               	//buffer
+               	
+		//buffer
                 this.mQuiz.mQuestionArray.push(new Question('buf','buf'));
 		
 		//random	
@@ -83,25 +54,9 @@ Extends: MultipleChoicePad,
 		this.mShapeArray.push(new LineOne  (this,this.mRaphael,100,325,100,275,"#0000FF",false)); 
 		this.mShapeArray.push(new LineOne  (this,this.mRaphael,150,325,150,275,"#0000FF",false)); 
 
-		shape = new Shape(5,5,245,200,this,"","","");	
+		shape = new Shape(5,5,55,275,this,"","","");	
 		this.mShapeArray.push(shape);	
 		shape.setText('A');
 		
-/*
-                this.textG = new Shape(55,5,245,200,this,"","","");
-                        this.mGame.mShapeArray.push(this.textG);
-                        this.textG.setMountable(true);
-                        this.textG.setText('6 in');
-
-*/
-/*
-			this.mShapeArray.push(new LineOne  (this,this.mRaphael,100,300,150,250,"#0000FF",false)); 
-			this.mShapeArray.push(new LineOne  (this,this.mRaphael,150,250,150,300,"#0000FF",false)); 
-			this.mShapeArray.push(new LineOne  (this,this.mRaphael,150,300,125,300,"#0000FF",false)); 
-
-			this.mShapeArray.push(new Triangle (this,this.mRaphael,100,300,150,250,150,300,.3,1,1,"none",.5,false)); 
- 			this.mShapeArray.push(new Rectangle(50,50,100,310,this,this.mRaphael,.3,1,1,"none",.5,false));
- 			this.mShapeArray.push(new Rectangle(50,50,100,310,this,this.mRaphael,.3,1,1,"none",.5,false));
-*/
 	}
 });
