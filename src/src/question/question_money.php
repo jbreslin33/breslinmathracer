80c8b60ee9f1d50f4294dfbde4b971536f00cd2c
jbreslin33 <jbreslin33@localhost>
2014-04-16 08:54:48 -0400

    u

--- src/src/question/question_money.php
+++ src/src/question/question_money.php
@@ -1,18 +1,8 @@
 var QuestionMoney = new Class(
 {
-/*
-types
-0: a+b=x
-1: a-b=x
-2: b-a=x
-3: a*b=x
-4: a/b=x
-5: a+b+c=x
-6: a-b-c=x
-7: a*b+c=x
-*/
 Extends: Question,
-  	initialize: function(question,answer,minX,maxX,minA,maxA,minB,maxB,minC,maxC,textA,textB,textC,textD,type)
+		//Homer had 3 pennies, 4 nickels, 3 dimes, 3 quarters, and 3 dollars, how much money did Homer have?
+  	initialize: function(question,answer,minTotal,maxTotal,minPennies,maxPennies,minNickels,maxNickels,minDimes,maxDimes,minQuarters,maxQuarters,minDollars,maxDollars)
         {
                 this.parent(question,answer)
                 
@@ -22,29 +12,11 @@ Extends: Question,
                 var x = 100;
                 var questionText = '';
 
-		if (type < 4)
-		{
-                	while (x > maxX || x < minX || a < minA || a > maxA || b < minB || b > maxB)
-                	{
-                        	a = Math.floor((Math.random()* parseInt(maxA - minA + 1)));
-                        	b = Math.floor((Math.random()* parseInt(maxB - minB + 1)));
-				if (type == 0)
-				{
-                        		x = a + b;
-				}
-				else if (type == 1)
-				{
-					x = a - b;
-				}
-				else if (type == 2)
-				{
-					x = b - a;
-				}
-				else if (type == 3)
-				{
-                        		x = a * b;
-				}
-			}
+                while (x > maxX || x < minX || a < minA || a > maxA || b < minB || b > maxB)
+                {
+                       	a = Math.floor((Math.random()* parseInt(maxA - minA + 1)));
+                       	b = Math.floor((Math.random()* parseInt(maxB - minB + 1)));
+                       	x = a + b;
                 }
                 
 		//valid parameters so make the question...
@@ -58,10 +30,7 @@ Extends: Question,
 		this.mAnswerArray[0] = '' + x;
 
 		//auto tips
-		if (type == 0)
-		{
-                	this.mTipArray[2] = 'a + b = x';
-                	this.mTipArray[3] = '' + a + ' + ' + b + ' = ' + x;
-		}
+                this.mTipArray[2] = 'a + b = x';
+                this.mTipArray[3] = '' + a + ' + ' + b + ' = ' + x;
         }
 });
