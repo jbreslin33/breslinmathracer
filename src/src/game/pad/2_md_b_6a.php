03ce2c094ac4c01b42723396bcd7a1b16a1245dc
jbreslin33 <jbreslin33@localhost>
2014-04-15 20:32:46 -0400

    u

--- src/src/game/pad/2_md_b_6a.php
+++ src/src/game/pad/2_md_b_6a.php
@@ -19,21 +19,8 @@ Extends: MultipleChoicePad,
                 this.mShapeArray[1].setPosition(100,80);
         },
 
-	createQuestions: function()
-        {
-		this.parent();
-	
-		//just the question array reset
-		this.mQuiz.resetQuestionArray();
-		this.mQuiz.resetQuestionPoolArray();
-
-        	//*************** question 1 
-                var question = new Question('What is the value of the green line?', '2');
-                question.mAnswerPool.push('0');
-                question.mAnswerPool.push('1');
-                question.mAnswerPool.push('3');
-                question.mAnswerPool.push('4');
-                this.mQuiz.mQuestionArray.push(question);
+	includeGraph: function(question)
+	{
                 question.mShapeArray.push(this.mShapeArray[parseInt(0 + this.mTotalGuiBars + this.mTotalInputBars)]);
                 question.mShapeArray.push(this.mShapeArray[parseInt(1 + this.mTotalGuiBars + this.mTotalInputBars)]);
                 question.mShapeArray.push(this.mShapeArray[parseInt(2 + this.mTotalGuiBars + this.mTotalInputBars)]);
@@ -62,6 +49,25 @@ Extends: MultipleChoicePad,
                 question.mShapeArray.push(this.mShapeArray[parseInt(25 + this.mTotalGuiBars + this.mTotalInputBars)]);
                 question.mShapeArray.push(this.mShapeArray[parseInt(26 + this.mTotalGuiBars + this.mTotalInputBars)]);
                 question.mShapeArray.push(this.mShapeArray[parseInt(27 + this.mTotalGuiBars + this.mTotalInputBars)]);
+	},
+
+	createQuestions: function()
+        {
+		this.parent();
+	
+		//just the question array reset
+		this.mQuiz.resetQuestionArray();
+		this.mQuiz.resetQuestionPoolArray();
+
+        	//*************** question 1 
+                var question = new Question('What is the value of the green line?', '2');
+                question.mAnswerPool.push('0');
+                question.mAnswerPool.push('1');
+                question.mAnswerPool.push('3');
+                question.mAnswerPool.push('4');
+                this.mQuiz.mQuestionArray.push(question);
+
+		this.includeGraph(question);
 	
 		//buffer
                 this.mQuiz.mQuestionArray.push(new Question('buf','buf'));
