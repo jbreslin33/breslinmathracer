8241049c8971ef888fa04e1059cfe1e77a33e3df
jbreslin33 <jbreslin33@localhost>
2014-04-18 13:56:27 -0400

    u

--- src/src/game/pad/3_nbt_a_1.php
+++ src/src/game/pad/3_nbt_a_1.php
@@ -14,10 +14,7 @@ Extends: NumberPad,
 
 		var totalA = 0;
 		var totalB = 0;
-
-		var varA = 0;
-		var varB = 0;
-		var varC = 0;
+		var varA   = 0;
 
 		var randomChance = 0;
 
@@ -26,38 +23,26 @@ Extends: NumberPad,
 			this.mQuiz.resetQuestionArray();
 			totalA = 0;
 			totalB = 0;
+			varC = 0; 
 
 			for (s = 0; s < this.mScoreNeeded; s++)
 			{	
-				varC = 10000;
 				randomChance = Math.floor((Math.random()*2));		
+				varA = Math.floor((Math.random()*999));		
 
-				while (varC > 999 || varC < 0)
-				{	
-					varA = Math.floor((Math.random()*999));		
-					varB = Math.floor((Math.random()*999));		
-				
-					if (randomChance == 0)
-					{
-						varC = parseInt(varA + varB);
-					}
-					else
-					{
-						varC = parseInt(varA - varB);
-					}
-				}
-   				if (randomChance == 0)
-                                {
-                                        this.mQuiz.mQuestionArray.push(new Question('' + varA + ' + ' +  varB + ' = ', '' + varC));
+				if (randomChance == 0)
+				{
+					varC = Math.round(varA/10)*10
+                                        this.mQuiz.mQuestionArray.push(new Question('What is ' + varA + ' rounded to the nearest 10?', '' + varC));
                                         totalA++;
-                                }
-                                else
-                                {
-                                        this.mQuiz.mQuestionArray.push(new Question('' + varA + ' - ' +  varB + ' = ', '' + varC));
+				}
+				else
+				{
+					varC = Math.round(varA/100)*100
+                                        this.mQuiz.mQuestionArray.push(new Question('What is ' + varA + ' rounded to the nearest 100?', '' + varC));
                                         totalB++;
-                                }
+				}
 			}
-			//ok we broke while now add question...
 		}
 	}
 });
