9313e6b1fe31e4ab18a2ee91dae13ac7171569f8
jbreslin33 <jbreslin33@localhost>
2014-04-20 13:45:05 -0400

    u

--- src/src/game/pad/3_nf_a_2a.php
+++ src/src/game/pad/3_nf_a_2a.php
@@ -29,9 +29,17 @@ Extends: MultipleChoicePad,
                 	question.mShapeArray.push(this.mShapeArray[parseInt(3 + this.mTotalGuiBars + this.mTotalInputBars)]);
                 	question.mShapeArray.push(this.mShapeArray[parseInt(4 + this.mTotalGuiBars + this.mTotalInputBars)]);
                 	question.mShapeArray.push(this.mShapeArray[parseInt(5 + this.mTotalGuiBars + this.mTotalInputBars)]);
-			if (numerator == 1)
+			if (numerator == 0)
 			{
                 		question.mShapeArray.push(this.mShapeArray[parseInt(6 + this.mTotalGuiBars + this.mTotalInputBars)]);
+			}
+			if (numerator == 1)
+			{
+                		question.mShapeArray.push(this.mShapeArray[parseInt(7 + this.mTotalGuiBars + this.mTotalInputBars)]);
+			}
+			if (numerator == 2)
+			{
+                		question.mShapeArray.push(this.mShapeArray[parseInt(8 + this.mTotalGuiBars + this.mTotalInputBars)]);
 			}
 		} 
 	},
@@ -45,13 +53,28 @@ Extends: MultipleChoicePad,
 		this.mQuiz.resetQuestionPoolArray();
 
         	//*************** question 1 
-                var question = new Question('What is the value of V?', '1');
-                question.mAnswerPool.push('0');
-                question.mAnswerPool.push('2');
-                question.mAnswerPool.push('3');
-                question.mAnswerPool.push('4');
+                var question = new Question('What is the value of V?', '0/2');
+                question.mAnswerPool.push('1/2');
+                question.mAnswerPool.push('2/2');
+                question.mAnswerPool.push('0/3');
+                this.mQuiz.mQuestionArray.push(question);
+		this.includeGraph(question,0,2);
+
+        	//*************** question 2 
+                var question = new Question('What is the value of V?', '1/2');
+                question.mAnswerPool.push('0/2');
+                question.mAnswerPool.push('2/2');
+                question.mAnswerPool.push('1/3');
                 this.mQuiz.mQuestionArray.push(question);
 		this.includeGraph(question,1,2);
+
+        	//*************** question 3 
+                var question = new Question('What is the value of V?', '2/2');
+                question.mAnswerPool.push('0/2');
+                question.mAnswerPool.push('1/2');
+                question.mAnswerPool.push('2/3');
+                this.mQuiz.mQuestionArray.push(question);
+		this.includeGraph(question,2,2);
 
 		//buffer
                 this.mQuiz.mQuestionArray.push(new Question('buf','buf'));
@@ -64,7 +87,7 @@ Extends: MultipleChoicePad,
 	{
 		this.parent();
 	
-               	//denominator 2 
+               	//  denominator 2 
 		this.mShapeArray.push(new LineOne  (this,this.mRaphael,50,300,650,300,"#0000FF",false));
                 this.mShapeArray.push(new LineOne  (this,this.mRaphael,50,325,50,275,"#0000FF",false));
                 this.mShapeArray.push(new LineOne  (this,this.mRaphael,650,325,650,275,"#0000FF",false));
@@ -74,14 +97,24 @@ Extends: MultipleChoicePad,
                 this.mShapeArray.push(shape);
                 shape.setText('0');
                 
-		//0
+		//1
                 shape = new Shape(5,5,655,275,this,"","","");
                 this.mShapeArray.push(shape);
                 shape.setText('1');
                 
 		this.mShapeArray.push(new LineOne  (this,this.mRaphael,350,325,350,275,"#0000FF",false));
 		
-		//V
+		//V at 0/2
+                shape = new Shape(5,5,55,255,this,"","","");
+                this.mShapeArray.push(shape);
+                shape.setText('V');
+		
+		//V at 1/2 
+                shape = new Shape(5,5,355,255,this,"","","");
+                this.mShapeArray.push(shape);
+                shape.setText('V');
+		
+		//V at 2/2 
                 shape = new Shape(5,5,655,255,this,"","","");
                 this.mShapeArray.push(shape);
                 shape.setText('V');
