7754a5616be45a98ed5075072170ff98eb76affb
jbreslin33 <jbreslin33@localhost>
2014-04-22 18:58:07 -0400

    u

--- src/src/game/pad/3_nf_a_3b.php
+++ src/src/game/pad/3_nf_a_3b.php
@@ -7,7 +7,7 @@ Extends: MultipleChoicePad,
 	{
        		this.parent(application);
 
-		this.setScoreNeeded(6);
+		this.setScoreNeeded(10);
 	},
 
         createNumQuestion: function()
@@ -17,6 +17,12 @@ Extends: MultipleChoicePad,
                 this.mShapeArray.push(this.mNumQuestion);
                 this.mNumQuestion.mCollidable = false;
                 this.mNumQuestion.mCollisionOn = false;
+        },
+ 
+        showCorrectAnswerEnter: function()
+        {
+		this.parent();
+                this.mShapeArray[1].setPosition(200,200);
         },
 
 	createQuestions: function()
@@ -38,6 +44,34 @@ Extends: MultipleChoicePad,
     		question.mAnswerPool.push('1/3');
     		question.mAnswerPool.push('2/6');
     		question.mAnswerPool.push('3/4');
+		
+		// 1/2 3/6
+                question = new Question('What is an equivalent fraction for 1/2?', '3/6');
+                this.mQuiz.mQuestionArray.push(question);
+    		question.mAnswerPool.push('1/4');
+    		question.mAnswerPool.push('2/3');
+    		question.mAnswerPool.push('1/5');
+		
+		// 2/4 3/6
+                question = new Question('What is an equivalent fraction for 2/4?', '3/6');
+                this.mQuiz.mQuestionArray.push(question);
+    		question.mAnswerPool.push('1/3');
+    		question.mAnswerPool.push('2/6');
+    		question.mAnswerPool.push('3/4');
+		
+		// 3/6 2/4
+                question = new Question('What is an equivalent fraction for 3/6?', '2/4');
+                this.mQuiz.mQuestionArray.push(question);
+    		question.mAnswerPool.push('1/3');
+    		question.mAnswerPool.push('2/6');
+    		question.mAnswerPool.push('3/4');
+
+		// 3/6 1/2
+                question = new Question('What is an equivalent fraction for 3/6?', '1/2');
+                this.mQuiz.mQuestionArray.push(question);
+    		question.mAnswerPool.push('1/4');
+    		question.mAnswerPool.push('2/3');
+    		question.mAnswerPool.push('1/5');
 		
 		// 1/3 2/6
                 question = new Question('What is an equivalent fraction for 1/3?', '2/6');
