9fdb1f5b1713cc1f419d56c0d942a42e138a2ded
jbreslin33 <jbreslin33@localhost>
2014-04-24 17:01:05 -0400

    3.nf.a.3d works.

--- src/src/game/pad/3_nf_a_3d.php
+++ src/src/game/pad/3_nf_a_3d.php
@@ -7,7 +7,7 @@ Extends: MultipleChoicePadImages,
 	{
        		this.parent(application);
 		
-		this.setScoreNeeded(1);
+		this.setScoreNeeded(12);
 	},
 
 	showQuestion: function()
@@ -71,19 +71,187 @@ Extends: MultipleChoicePadImages,
 		
 		this.mQuiz.resetQuestionArray();
 
+		//1
 		var comparisonA = '1/2';
-		var comparisonB = '1/2';
+		var comparisonB = '2/4';
 
 		var question = new QuestionCompare('Compare?', 'is equal to', '' + comparisonA, '' + comparisonB);
 		this.mQuiz.mQuestionArray.push(question);
 		question.mAnswerPool = this.mQuiz.mAnswerPool;	
 
 		//add shapes
-		this.mShapeArray[ parseInt( s*2 + this.mTotalGuiBars + this.mTotalInputBars) ].mMesh.innerHTML = '' + comparisonA;
-		question.mShapeArray.push(this.mShapeArray[parseInt((s*2) + this.mTotalGuiBars + this.mTotalInputBars)]);
+		this.mShapeArray[ parseInt( 0 + this.mTotalGuiBars + this.mTotalInputBars) ].mMesh.innerHTML = '' + comparisonA;
+		question.mShapeArray.push(this.mShapeArray[parseInt(0 + this.mTotalGuiBars + this.mTotalInputBars)]);
 				
-		this.mShapeArray[parseInt((s*2) + this.mTotalGuiBars + this.mTotalInputBars + 1)].mMesh.innerHTML = '' + comparisonB;
-		question.mShapeArray.push(this.mShapeArray[parseInt((s*2) + this.mTotalGuiBars + this.mTotalInputBars + 1)]);
+		this.mShapeArray[parseInt(1 + this.mTotalGuiBars + this.mTotalInputBars)].mMesh.innerHTML = '' + comparisonB;
+		question.mShapeArray.push(this.mShapeArray[parseInt(1 + this.mTotalGuiBars + this.mTotalInputBars)]);
+
+                //2
+                var comparisonA = '2/4';
+                var comparisonB = '3/6';
+
+                var question = new QuestionCompare('Compare?', 'is equal to', '' + comparisonA, '' + comparisonB);
+                this.mQuiz.mQuestionArray.push(question);
+                question.mAnswerPool = this.mQuiz.mAnswerPool;
+
+                //add shapes
+                this.mShapeArray[ parseInt(2 + this.mTotalGuiBars + this.mTotalInputBars) ].mMesh.innerHTML = '' + comparisonA;
+                question.mShapeArray.push(this.mShapeArray[parseInt(2 + this.mTotalGuiBars + this.mTotalInputBars)]);
+
+                this.mShapeArray[parseInt(3 + this.mTotalGuiBars + this.mTotalInputBars)].mMesh.innerHTML = '' + comparisonB;
+                question.mShapeArray.push(this.mShapeArray[parseInt(3 + this.mTotalGuiBars + this.mTotalInputBars)]);
+
+                //3
+                var comparisonA = '1/2';
+                var comparisonB = '3/6';
+
+                var question = new QuestionCompare('Compare?', 'is equal to', '' + comparisonA, '' + comparisonB);
+                this.mQuiz.mQuestionArray.push(question);
+                question.mAnswerPool = this.mQuiz.mAnswerPool;
+
+                //add shapes
+                this.mShapeArray[ parseInt(4 + this.mTotalGuiBars + this.mTotalInputBars) ].mMesh.innerHTML = '' + comparisonA;
+                question.mShapeArray.push(this.mShapeArray[parseInt(4 + this.mTotalGuiBars + this.mTotalInputBars)]);
+
+                this.mShapeArray[parseInt(5 + this.mTotalGuiBars + this.mTotalInputBars)].mMesh.innerHTML = '' + comparisonB;
+                question.mShapeArray.push(this.mShapeArray[parseInt(5 + this.mTotalGuiBars + this.mTotalInputBars)]);
+
+                //4
+                var comparisonA = '1/2';
+                var comparisonB = '1/3';
+
+                var question = new QuestionCompare('Compare?', 'is greater than', '' + comparisonA, '' + comparisonB);
+                this.mQuiz.mQuestionArray.push(question);
+                question.mAnswerPool = this.mQuiz.mAnswerPool;
+
+                //add shapes
+                this.mShapeArray[ parseInt(6 + this.mTotalGuiBars + this.mTotalInputBars) ].mMesh.innerHTML = '' + comparisonA;
+                question.mShapeArray.push(this.mShapeArray[parseInt(6 + this.mTotalGuiBars + this.mTotalInputBars)]);
+
+                this.mShapeArray[parseInt(7 + this.mTotalGuiBars + this.mTotalInputBars)].mMesh.innerHTML = '' + comparisonB;
+                question.mShapeArray.push(this.mShapeArray[parseInt(7 + this.mTotalGuiBars + this.mTotalInputBars)]);
+
+
+           	//5
+                var comparisonA = '4/5';
+                var comparisonB = '3/4';
+
+                var question = new QuestionCompare('Compare?', 'is greater than', '' + comparisonA, '' + comparisonB);
+                this.mQuiz.mQuestionArray.push(question);
+                question.mAnswerPool = this.mQuiz.mAnswerPool;
+
+                //add shapes
+                this.mShapeArray[ parseInt(8 + this.mTotalGuiBars + this.mTotalInputBars) ].mMesh.innerHTML = '' + comparisonA;
+                question.mShapeArray.push(this.mShapeArray[parseInt(8 + this.mTotalGuiBars + this.mTotalInputBars)]);
+
+                this.mShapeArray[parseInt(9 + this.mTotalGuiBars + this.mTotalInputBars)].mMesh.innerHTML = '' + comparisonB;
+                question.mShapeArray.push(this.mShapeArray[parseInt(9 + this.mTotalGuiBars + this.mTotalInputBars)]);
+
+                //6
+                var comparisonA = '3/6';
+                var comparisonB = '2/5';
+                
+                var question = new QuestionCompare('Compare?', 'is greater than', '' + comparisonA, '' + comparisonB);
+                this.mQuiz.mQuestionArray.push(question);
+                question.mAnswerPool = this.mQuiz.mAnswerPool;
+        
+                //add shapes
+                this.mShapeArray[ parseInt(10 + this.mTotalGuiBars + this.mTotalInputBars) ].mMesh.innerHTML = '' + comparisonA;
+                question.mShapeArray.push(this.mShapeArray[parseInt(10 + this.mTotalGuiBars + this.mTotalInputBars)]);
+
+                this.mShapeArray[parseInt(11 + this.mTotalGuiBars + this.mTotalInputBars)].mMesh.innerHTML = '' + comparisonB;
+                question.mShapeArray.push(this.mShapeArray[parseInt(11 + this.mTotalGuiBars + this.mTotalInputBars)]);
+
+
+               	//7
+                var comparisonA = '1/2';
+                var comparisonB = '2/6';
+                
+                var question = new QuestionCompare('Compare?', 'is greater than', '' + comparisonA, '' + comparisonB);
+                this.mQuiz.mQuestionArray.push(question);
+                question.mAnswerPool = this.mQuiz.mAnswerPool;
+        
+                //add shapes
+                this.mShapeArray[ parseInt(12 + this.mTotalGuiBars + this.mTotalInputBars) ].mMesh.innerHTML = '' + comparisonA;
+                question.mShapeArray.push(this.mShapeArray[parseInt(12 + this.mTotalGuiBars + this.mTotalInputBars)]);
+                
+                this.mShapeArray[parseInt(13 + this.mTotalGuiBars + this.mTotalInputBars)].mMesh.innerHTML = '' + comparisonB;
+                question.mShapeArray.push(this.mShapeArray[parseInt(13 + this.mTotalGuiBars + this.mTotalInputBars)]);
+
+               	//8
+                var comparisonA = '3/4';
+                var comparisonB = '2/3';
+                
+                var question = new QuestionCompare('Compare?', 'is greater than', '' + comparisonA, '' + comparisonB);
+                this.mQuiz.mQuestionArray.push(question);
+                question.mAnswerPool = this.mQuiz.mAnswerPool;
+        
+                //add shapes
+                this.mShapeArray[ parseInt(14 + this.mTotalGuiBars + this.mTotalInputBars) ].mMesh.innerHTML = '' + comparisonA;
+                question.mShapeArray.push(this.mShapeArray[parseInt(14 + this.mTotalGuiBars + this.mTotalInputBars)]);
+                
+                this.mShapeArray[parseInt(15 + this.mTotalGuiBars + this.mTotalInputBars)].mMesh.innerHTML = '' + comparisonB;
+                question.mShapeArray.push(this.mShapeArray[parseInt(15 + this.mTotalGuiBars + this.mTotalInputBars)]);
+
+	        //9
+                var comparisonA = '4/6';
+                var comparisonB = '2/3';
+
+                var question = new QuestionCompare('Compare?', 'is equal to', '' + comparisonA, '' + comparisonB);
+                this.mQuiz.mQuestionArray.push(question);
+                question.mAnswerPool = this.mQuiz.mAnswerPool;
+
+                //add shapes
+                this.mShapeArray[ parseInt(16 + this.mTotalGuiBars + this.mTotalInputBars) ].mMesh.innerHTML = '' + comparisonA;
+                question.mShapeArray.push(this.mShapeArray[parseInt(16 + this.mTotalGuiBars + this.mTotalInputBars)]);
+
+                this.mShapeArray[parseInt(17 + this.mTotalGuiBars + this.mTotalInputBars)].mMesh.innerHTML = '' + comparisonB;
+                question.mShapeArray.push(this.mShapeArray[parseInt(17 + this.mTotalGuiBars + this.mTotalInputBars)]);
+
+               	//10
+                var comparisonA = '4/5';
+                var comparisonB = '2/3';
+
+                var question = new QuestionCompare('Compare?', 'is greater than', '' + comparisonA, '' + comparisonB);
+                this.mQuiz.mQuestionArray.push(question);
+                question.mAnswerPool = this.mQuiz.mAnswerPool;
+
+                //add shapes
+                this.mShapeArray[ parseInt(18 + this.mTotalGuiBars + this.mTotalInputBars) ].mMesh.innerHTML = '' + comparisonA;
+                question.mShapeArray.push(this.mShapeArray[parseInt(18 + this.mTotalGuiBars + this.mTotalInputBars)]);
+
+                this.mShapeArray[parseInt(19 + this.mTotalGuiBars + this.mTotalInputBars)].mMesh.innerHTML = '' + comparisonB;
+                question.mShapeArray.push(this.mShapeArray[parseInt(19 + this.mTotalGuiBars + this.mTotalInputBars)]);
+
+               	//11
+                var comparisonA = '4/6';
+                var comparisonB = '3/4';
+
+                var question = new QuestionCompare('Compare?', 'is less than', '' + comparisonA, '' + comparisonB);
+                this.mQuiz.mQuestionArray.push(question);
+                question.mAnswerPool = this.mQuiz.mAnswerPool;
+
+                //add shapes
+                this.mShapeArray[ parseInt(20 + this.mTotalGuiBars + this.mTotalInputBars) ].mMesh.innerHTML = '' + comparisonA;
+                question.mShapeArray.push(this.mShapeArray[parseInt(20 + this.mTotalGuiBars + this.mTotalInputBars)]);
+
+                this.mShapeArray[parseInt(21 + this.mTotalGuiBars + this.mTotalInputBars)].mMesh.innerHTML = '' + comparisonB;
+                question.mShapeArray.push(this.mShapeArray[parseInt(21 + this.mTotalGuiBars + this.mTotalInputBars)]);
+
+           	//12
+                var comparisonA = '2/3';
+                var comparisonB = '3/5';
+
+                var question = new QuestionCompare('Compare?', 'is greater than', '' + comparisonA, '' + comparisonB);
+                this.mQuiz.mQuestionArray.push(question);
+                question.mAnswerPool = this.mQuiz.mAnswerPool;
+
+                //add shapes
+                this.mShapeArray[ parseInt(22 + this.mTotalGuiBars + this.mTotalInputBars) ].mMesh.innerHTML = '' + comparisonA;
+                question.mShapeArray.push(this.mShapeArray[parseInt(22 + this.mTotalGuiBars + this.mTotalInputBars)]);
+
+                this.mShapeArray[parseInt(23 + this.mTotalGuiBars + this.mTotalInputBars)].mMesh.innerHTML = '' + comparisonB;
+                question.mShapeArray.push(this.mShapeArray[parseInt(23 + this.mTotalGuiBars + this.mTotalInputBars)]);
 
                 //buffer
                 this.mQuiz.mQuestionArray.push(new Question('buf','buf'));
