44beb9fe2b7eac61ae557d719710bb6b47eff613
bbreslin99 <bbreslin99@localhost>
2014-04-30 15:27:50 -0400

    dded new standard file

---
+++ src/src/game/pad/4_nbt_b_5.php
@@ -0,0 +1,49 @@
+var g4_nbt_b_4 = new Class(
+{
+Extends: NumberPad,
+	initialize: function(application)
+	{
+       		this.parent(application);
+	},
+
+	createQuestions: function()
+        {
+ 		this.parent();
+
+		var varA = 0;
+		var varB = 0;
+		var varC = 0;
+
+		this.mQuiz.resetQuestionArray();
+
+		for (s = 0; s < this.mScoreNeeded / 2; s++)
+		{	
+			varC = 10000;
+			while (varC > 999 || varC < 0)
+			{	
+				varC = 10000;
+				varA = Math.floor((Math.random()*999));		
+				varB = Math.floor((Math.random()*999));		
+				varC = parseInt(varA + varB);
+			}
+                        this.mQuiz.mQuestionArray.push(new Question('' + varA + ' + ' +  varB + ' = ', '' + varC));
+		}
+		for (s = 0; s < this.mScoreNeeded / 2; s++)
+		{
+			varC = 10000;
+			while (varC > 999 || varC < 0)
+			{	
+				varC = 10000;
+				varA = Math.floor((Math.random()*999));		
+				varB = Math.floor((Math.random()*999));		
+				varC = parseInt(varA - varB);
+			}
+                        this.mQuiz.mQuestionArray.push(new Question('' + varA + ' - ' +  varB + ' = ', '' + varC));
+		}
+                //buffer
+                this.mQuiz.mQuestionArray.push(new Question('buf','buf'));
+
+                //random
+                this.mQuiz.randomize(10);
+	}
+});
