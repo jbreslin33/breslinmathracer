e580921a47faa6f25a2cd0f9884df85d9a53600e
jbreslin33 <jbreslin33@localhost>
2014-05-12 09:29:34 -0400

    cleaned up 3.md.b.3, plan is to use one bar graph for quiz. then maybe another for a seperate quiz. this will make coding much easier.

--- src/src/game/pad/3_md_b_3.php
+++ src/src/game/pad/3_md_b_3.php
@@ -8,7 +8,7 @@ Extends: RulerPad,
        		this.parent(application);
 
 		// Creates canvas 640 × 480 at 10, 50
-		var r = Raphael(10, 50, 640, 480);
+		var r = Raphael(100, 100, 540, 480);
 		r.barchart(0, 0, 620, 260, [76, 70, 67, 71, 69], {})
    		txtattr = { font: "12px sans-serif" };
                 r.text(160, 10, "Single Series Chart").attr(txtattr);
@@ -18,53 +18,15 @@ Extends: RulerPad,
         {
  		this.parent();
 
-		var totalA = 0;
-		var totalB = 0;
+                this.mQuiz.resetQuestionArray();
 
-		var varA = 0;
-		var varB = 0;
-		var varC = 0;
+                //add 1
+                this.mQuiz.mQuestionArray.push(new Question('10 + 3 =','13'));
 
-		var randomChance = 0;
+                //buffer
+                this.mQuiz.mQuestionArray.push(new Question('buf','buf'));
 
-		while (totalA < this.mScoreNeeded * .4 || totalB < this.mScoreNeeded * .4)
-		{
-			this.mQuiz.resetQuestionArray();
-			totalA = 0;
-			totalB = 0;
-
-			for (s = 0; s < this.mScoreNeeded; s++)
-			{	
-				varC = 10000;
-				randomChance = Math.floor((Math.random()*2));		
-
-				while (varC > 999 || varC < 0)
-				{	
-					varA = Math.floor((Math.random()*999));		
-					varB = Math.floor((Math.random()*999));		
-				
-					if (randomChance == 0)
-					{
-						varC = parseInt(varA + varB);
-					}
-					else
-					{
-						varC = parseInt(varA - varB);
-					}
-				}
-   				if (randomChance == 0)
-                                {
-                                        this.mQuiz.mQuestionArray.push(new Question('' + varA + ' + ' +  varB + ' = ', '' + varC));
-                                        totalA++;
-                                }
-                                else
-                                {
-                                        this.mQuiz.mQuestionArray.push(new Question('' + varA + ' - ' +  varB + ' = ', '' + varC));
-                                        totalB++;
-                                }
-			}
-			//ok we broke while now add question...
-		}
+                //random
+                this.mQuiz.randomize(10);
 	}
-
 });
