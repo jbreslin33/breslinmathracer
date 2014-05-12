77417125458fc7dc3a2822aa42e759e7de19147c
jbreslin33 <jbreslin33@localhost>
2014-05-12 15:02:19 -0400

    adjusting xy again.

--- src/src/game/pad/3_md_b_3.php
+++ src/src/game/pad/3_md_b_3.php
@@ -9,9 +9,9 @@ Extends: RulerPad,
 
 		// Creates canvas 640 × 480 at 10, 50
 		var r = Raphael(380, 100, 540, 480);
-		r.barchart     (  0,   0, 380, 250, [76, 70, 67, 71, 69], {})
+		r.barchart     (  50,   0, 350, 250, [76, 70, 67, 71, 69], {})
    		txtattr = { font: "12px sans-serif" };
-                r.text(160, 10, "Single Series Chart").attr(txtattr);
+                r.text(0, 10, "Single Series Chart").attr(txtattr);
 	},
 
 	createQuestions: function()
