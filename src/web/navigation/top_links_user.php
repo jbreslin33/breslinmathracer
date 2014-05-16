866be0d25517a9058c52a8b5169126b65bdaaa29
jbreslin33 <jbreslin33@localhost>
2014-05-16 13:25:53 -0400

    got rid of application_ela and application_math links and files.

--- src/web/navigation/top_links_user.php
+++ src/web/navigation/top_links_user.php
@@ -1,6 +1,5 @@
 <ul>
-<li><a href="/web/application/application_math.php">Play Math</a></li>
-<li><a href="/web/application/application_ela.php">Play ELA</a></li>
+<li><a href="/web/application/application.php">Play</a></li>
 <li><a href="/web/select/select.php">Selects</a></li>
 <li><a href="/web/login/login_form.php">Logout</a></li>
 <li><a href="/web/update/update_standard_id_form.php">Update Standard ID</a></li>
