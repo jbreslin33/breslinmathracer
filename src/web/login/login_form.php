362e4dceac1d44d0d95ade65c942c40241e1687e
jbreslin33 <jbreslin33@localhost>
2014-05-17 19:44:02 -0400

    added donate button.

--- src/web/login/login_form.php
+++ src/web/login/login_form.php
@@ -50,5 +50,17 @@ echo "<br>";
 	<p><input type="submit" value="Log In" /></p>
 
 	</form>
+<br>
+<br>
+<br>
+<br>
+<br>
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="KNFRHXLYQYV3U">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
 </body>
 </html>
