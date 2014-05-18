397cb1d7b122febdafdce842b55f4e110944838f
jbreslin33 <jbreslin33@localhost>
2014-05-17 21:56:21 -0400

    5 and 10 on y axis set.

--- src/src/game/pad/3_md_b_3.php
+++ src/src/game/pad/3_md_b_3.php
@@ -9,10 +9,10 @@ Extends: RulerPad,
 
 	},
 	
-	createBarChart: function()
+	createBarChart: function(lions,tigers,bears,apes,birds)
 	{
 		this.r = Raphael(250, 100, 520, 480);
-		this.r.barchart     (100,   0, 420, 270, [76, 70, 67, 71, 69], {})
+		this.r.barchart     (100,   0, 420, 270, [lions, tigers, bears, apes, birds], {})
    		txtattr = { font: "12px sans-serif" };
                 this.r.text(30, 90, "Number").attr(txtattr);
                 this.r.text(30, 110, "of").attr(txtattr);
@@ -27,11 +27,11 @@ Extends: RulerPad,
 		this.r.text(390, 265, "Apes").attr(txtattr);
 		this.r.text(470, 265, "Birds").attr(txtattr);
 
-		this.r.path( "M100,225 L504,225" );
-                this.r.text(90, 225, "5").attr(txtattr);
+		this.r.path( "M100,205 L504,205" );
+                this.r.text(90, 205, "5").attr(txtattr);
 		
-		this.r.path( "M100,200 L504,200" );
-                this.r.text(90, 200, "10").attr(txtattr);
+		this.r.path( "M100,158 L504,158" );
+                this.r.text(90, 158, "10").attr(txtattr);
 	},
 
 	createQuestions: function()
@@ -40,7 +40,23 @@ Extends: RulerPad,
 
                 this.mQuiz.resetQuestionArray();
 
-		this.createBarChart();
+		var lions = Math.floor((Math.random()*7)+1);
+		lions = lions * 5;
+		
+		var tigers = Math.floor((Math.random()*7)+1);
+		tigers = tigers * 5;
+		
+		var bears = Math.floor((Math.random()*7)+1);
+		bears = bears * 5;
+		
+		var apes = Math.floor((Math.random()*7)+1);
+		apes = apes * 5;
+		
+		var birds = Math.floor((Math.random()*8)+1);
+		birds = birds * 5;
+
+		//this.createBarChart(lions,tigers,bears,apes,birds);
+		this.createBarChart(5,10,15,20,25);
 
                 //add 1
                 this.mQuiz.mQuestionArray.push(new Question('10 + 3 =','13'));
