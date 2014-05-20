320ec9fdb7c371157502d5c7a6f9fb741376e2e6
bbreslin99 <bbreslin99@localhost>
2014-05-19 20:13:20 -0400

    finished 4nfb3b

--- src/src/game/pad/4_nf_b_3b.php
+++ src/src/game/pad/4_nf_b_3b.php
@@ -12,7 +12,7 @@ Extends: NumberPad,
 	createNumQuestion: function()
         {
                 //question
-                this.mNumQuestion = new Shape(185,50,250,95,this,"","","");
+                this.mNumQuestion = new Shape(285,50,280,95,this,"","","");
                 this.mShapeArray.push(this.mNumQuestion);
                 this.mNumQuestion.mCollidable = false;
                 this.mNumQuestion.mCollisionOn = false;
@@ -23,7 +23,7 @@ Extends: NumberPad,
         {
                 this.parent();
 
-                this.mShapeArray[1].setSize(235,100);
+                this.mShapeArray[1].setSize(285,100);
                 //this.mShapeArray[1].setPosition(380,80);
 		this.mShapeArray[1].mMesh.innerHTML = '' + this.mQuiz.getQuestion().getQuestion() + ' ' + ' ? = ' + this.mQuiz.getQuestion().getAnswer();
 
@@ -45,6 +45,7 @@ Extends: NumberPad,
 		var varB = 0;
 		var varC = 0;
 		var varD = 0;
+		var varE = 0;
 		var varN = 0;
 
 
@@ -58,47 +59,29 @@ Extends: NumberPad,
 		this.mQuiz.resetQuestionArray();
 			
 
-		 for (s = 0; s < this.mScoreNeeded/2; s++)
+		 for (s = 0; s < this.mScoreNeeded; s++)
 		 {		
 
 			// get bottom number
 			varB = 6 + Math.floor(Math.random()*22);
 
 			// get top number
-			max = Math.floor(varB/2);
+			max = Math.floor(varB/3);
 			varA = 1 + Math.floor((Math.random()*max));
 
 			varC = 1 + Math.floor((Math.random()*max));
+
+			varE = 1 + Math.floor((Math.random()*max));
+
 			varD = varB;
 
-			answer = varA + varC;
+			answer = varA + varC + varE;
 				
-			question = new Question('' + varA + ' / ' +  varB + ' + ' + '' + varC + ' / ' +  varD + '=' + ' ?/ ' + varD, '' + answer);
+			question = new Question('' + varA + '/' +  varB + ' + ' + '' + varC + '/' +  varD + ' + ' + '' + '?' + '/' +  varD + ' = ' + answer + '/' + varD, '' + varE);
 			
                 	this.mQuiz.mQuestionArray.push(question);
                  }
 
-
-		 for (s = 0; s < this.mScoreNeeded/2; s++)
-		 {		
-
-			// get bottom number
-			varB = 6 + Math.floor(Math.random()*22);
-
-			// get top number
-			varA = 2 + Math.floor(Math.random()*(varB-2));
-			
-			varC = 1 + Math.floor(Math.random()*(varA-1));
-			//1 + Math.floor((Math.random()*max));
-			varD = varB;
-
-			answer = varA - varC;
-				
-			question = new Question('' + varA + ' / ' +  varB + ' - ' + '' + varC + ' / ' +  varD + '=' + ' ?/ ' + varD, '' + answer);
-			
-                	this.mQuiz.mQuestionArray.push(question);
-                 }
-			
 
 		//buffer
                 this.mQuiz.mQuestionArray.push(new Question('buf','buf'));
