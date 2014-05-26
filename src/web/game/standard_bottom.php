f5b30b91bb0df2acaf1e7f0483d986133d3fb2f8
jbreslin33 <jbreslin33@localhost>
2014-05-25 20:28:57 -0400

    testing out sound.

--- src/web/game/standard_bottom.php
+++ src/web/game/standard_bottom.php
@@ -6,6 +6,21 @@ window.onresize = function(event)
         APPLICATION.mWindow = window.getSize();
 }
 </script>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<audio controls id=audio_controls>
+    <source src="/audio/sound.mp3">
+    <source src="/audio/sound.oga">
+    <p>Your browser can’t play this audio content directly, but you can download it:</p>
+    <ul>
+      <li><a href="/audio/sound.mp3">Download as audio/mp3</a></li>
+      <li><a href="/audio/sound.oga">Download as audio/ogg</a></li>
+    </ul>
+</audio>
 
 </body>
 </html>
