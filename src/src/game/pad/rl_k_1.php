18b8204cee4aa4d0b4d3532038cd6f9c8ecb1afe
jbreslin33 <jbreslin33@localhost>
2014-05-26 13:17:56 -0400

    for sound in rl.k.1 we can produce different sounds for different questions just like we affiliate different shapes with different questions.

--- src/src/game/pad/rl_k_1.php
+++ src/src/game/pad/rl_k_1.php
@@ -8,17 +8,6 @@ Extends: MultipleChoicePad,
        		this.parent(application);
 
 		this.setScoreNeeded(2);
-
-		//sound shape
-		this.mVivaldi = new Shape(50,50,500,300,this,"AUDIO","","");		
-
-		//for new browsers
-		this.mVivaldi.mMesh.controls = true;
-		this.mVivaldi.mMesh.src="/audio/spring.mp3";
-	
-		//for old browsers	
-		this.mVivaldi.mMesh.setAttribute('href', '/audio/spring.mp3');
-                this.mVivaldi.mMesh.innerHTML = "Vivaldi!";
 	},
 
 	createNumQuestion: function()
@@ -47,19 +36,52 @@ Extends: MultipleChoicePad,
 		//1
 		question = new Question('The boy had a red ball.', 'red');
                 this.mQuiz.mQuestionArray.push(question);
+		question.mAnswerPool.push('red');
 		question.mAnswerPool.push('boy');
 		question.mAnswerPool.push('ball');
+		question.mShapeArray.push(this.mShapeArray[parseInt(this.mTotalGuiBars + this.mTotalInputBars)]);
 		
 		//2
 		question = new Question('The girl went home.', 'girl');
                 this.mQuiz.mQuestionArray.push(question);
+		question.mAnswerPool.push('girl');
 		question.mAnswerPool.push('went');
 		question.mAnswerPool.push('home');
+                question.mShapeArray.push(this.mShapeArray[parseInt(this.mTotalGuiBars + this.mTotalInputBars + 1)]);
 
 		//buffer
                 this.mQuiz.mQuestionArray.push(new Question('buf','buf'));
 
                 //random
                 this.mQuiz.randomize(10);
+	},
+
+	createWorld: function()
+        {
+                this.parent();
+
+		//1
+                this.mShapeArray.push(new Shape(50,50,500,300,this,"AUDIO","",""));
+
+		//for new browsers
+		this.mShapeArray[parseInt(this.mTotalGuiBars + this.mTotalInputBars)].mMesh.controls = true;
+		this.mShapeArray[parseInt(this.mTotalGuiBars + this.mTotalInputBars)].mMesh.src="/audio/spring.mp3";
+	
+		//for old browsers	
+		this.mShapeArray[parseInt(this.mTotalGuiBars + this.mTotalInputBars)].mMesh.setAttribute('href', '/audio/spring.mp3');
+                this.mShapeArray[parseInt(this.mTotalGuiBars + this.mTotalInputBars)].mMesh.innerHTML = "Play Question";
+
+
+		//2
+                this.mShapeArray.push(new Shape(50,50,500,300,this,"AUDIO","",""));
+
+		//for new browsers
+		this.mShapeArray[parseInt(this.mTotalGuiBars + this.mTotalInputBars + 1)].mMesh.controls = true;
+		this.mShapeArray[parseInt(this.mTotalGuiBars + this.mTotalInputBars + 1)].mMesh.src="/audio/canon.mp3";
+	
+		//for old browsers	
+		this.mShapeArray[parseInt(this.mTotalGuiBars + this.mTotalInputBars + 1)].mMesh.setAttribute('href', '/audio/canon.mp3');
+                this.mShapeArray[parseInt(this.mTotalGuiBars + this.mTotalInputBars + 1)].mMesh.innerHTML = "Play Question";
+
 	}
 });
