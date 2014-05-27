a2c21c55a29c6da7f026fe58ca7eb7b2ece74e7a
jbreslin33 <jbreslin33@localhost>
2014-05-27 17:46:46 -0400

    making progress on 3.md.b.4. next mark off inches and 1/4 inches

--- src/src/game/pad/3_md_b_4.php
+++ src/src/game/pad/3_md_b_4.php
@@ -10,7 +10,6 @@ Extends: RulerPad,
 
 		this.mRectangleArray       = new Array();	
 		this.mRulerCentimeterArray = new Array();	
-		this.mRulerInchArray       = new Array();	
 	},
    
         showCorrectAnswerEnter: function()
@@ -31,58 +30,33 @@ Extends: RulerPad,
 		for (i = 0; i < this.mScoreNeeded; i++)
 		{
 			var randomNumber = Math.floor((Math.random()*2));	
-			if (randomNumber == 0)
-			{
-				//get random heights.
-				var redHeightCode = Math.floor((Math.random()*3)+1);
-				var redHeight = parseInt(redHeightCode * 100);  
+			
+			//get random heights.
+			var redHeightCode = Math.floor((Math.random()*3)+1);
+			var redHeight = parseInt(redHeightCode * 100);  
 		
-				answer = '';	
-				if (redHeight == 100)
-				{
-					answer = '5 cm';	
-				}		
-				if (redHeight == 200)
-				{
-					answer = '10 cm';	
-				}		
-				if (redHeight == 300)
-				{
-					answer = '15 cm';	
-				}		
-					
-				question = new Question('What is the length of the red shape in centimeters? Write answer like this: 10 cm', '' + answer);
-				this.mQuiz.mQuestionArray.push(question);
-			
-				question.mShapeArray.push(this.mRectangleArray[i]);
-				this.mRectangleArray[i].setSize(50,redHeight);
-
-				this.mRulerCentimeterArray[i].addToQuestion(question);
-				question.mShapeArray.push(this.mRulerCentimeterArray[i]);
-			
-				this.mRulerInchArray[i].addToQuestion(question);
-				question.mShapeArray.push(this.mRulerInchArray[i]);
-			}
-			else
+			answer = '';	
+			if (redHeight == 100)
 			{
-				//get random heights.
-                        	var redHeightCode = Math.floor((Math.random()*3)+1);
-                        	var redHeight = parseInt(redHeightCode * 50);
+				answer = '5 cm';	
+			}		
+			if (redHeight == 200)
+			{
+				answer = '10 cm';	
+			}		
+			if (redHeight == 300)
+			{
+				answer = '15 cm';	
+			}		
+					
+			question = new Question('What is the length of the red shape in centimeters to the nearest 1/4 inch? Write answer like this: 1 3/4 in', '' + answer);
+			this.mQuiz.mQuestionArray.push(question);
+		
+			question.mShapeArray.push(this.mRectangleArray[i]);
+			this.mRectangleArray[i].setSize(50,redHeight);
 
-                        	answer = '' + redHeightCode + ' in';
-
-                        	question = new Question('What is the length of the red shape in inches? Write answer like this: 10 in', '' + answer);
-                        	this.mQuiz.mQuestionArray.push(question);
-
-                        	question.mShapeArray.push(this.mRectangleArray[i]);
-                        	this.mRectangleArray[i].setSize(50,redHeight);
-
-                        	this.mRulerCentimeterArray[i].addToQuestion(question);
-                        	question.mShapeArray.push(this.mRulerCentimeterArray[i]);
-
-                        	this.mRulerInchArray[i].addToQuestion(question);
-                        	question.mShapeArray.push(this.mRulerInchArray[i]);
-			}
+			this.mRulerCentimeterArray[i].addToQuestion(question);
+			question.mShapeArray.push(this.mRulerCentimeterArray[i]);
 		}
                	
 		//buffer
@@ -95,20 +69,13 @@ Extends: RulerPad,
 		
 		this.mRectangleArray.length = 0;
 		this.mRulerCentimeterArray.length = 0;	
-		this.mRulerInchArray.length = 0;	
 		
 		for (i = 0; i < this.mScoreNeeded; i++)
 		{
-
                         //the cm ruler
                         var rulerCentimeter = new RulerCentimeter(50,300,300,50,this,this.mRaphael,.6,1,1,"none",.5,true);
                         this.mShapeArray.push(rulerCentimeter);
 			this.mRulerCentimeterArray.push(rulerCentimeter);
-                        
-			//the inch ruler
-                        var rulerInch = new RulerInch(50,300,400,50,this,this.mRaphael,.6,1,1,"none",.5,true);
-                        this.mShapeArray.push(rulerInch);
-			this.mRulerInchArray.push(rulerInch);
                         
 			//red shape to measure
                         var redRectangle = new Rectangle(50,50,600,100,this,this.mRaphael,0,1,1,"none",.5,true);
