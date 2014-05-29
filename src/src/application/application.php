2d0fa87acb66bdeb631d3621864b13e4ff36cb97
jbreslin33 <jbreslin33@localhost>
2014-05-28 21:19:43 -0400

    3.md.c.6 ready for mod.

--- src/src/application/application.php
+++ src/src/application/application.php
@@ -1735,6 +1735,19 @@ var Application = new Class(
                                	this.mGame = new g3_md_c_5b(APPLICATION);
 			}	
 		}
+		if (this.mRef_id == '3.md.c.6')
+		{ 
+             		if (this.mGameName != "g3_md_c_6")
+                       	{
+				if (this.mGame)
+				{
+					this.mGame.destructor();
+					this.mGame = 0;
+				}
+                               	this.mGameName = "g3_md_c_6";
+                               	this.mGame = new g3_md_c_6(APPLICATION);
+			}	
+		}
 		if (this.mRef_id == '7828B4F15ABD40E19EF14DDE0EB399DF')
 		{ 
              		if (this.mGameName != "g4_oa_a_1")
