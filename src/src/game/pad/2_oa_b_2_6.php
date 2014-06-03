ae065cab74e5336b8dd111cebc0f7d0e5ccd3ebc
jbreslin33 <jbreslin33@localhost>
2014-06-02 20:59:12 -0400

    2.oa.b.2 done

--- src/src/game/pad/2_oa_b_2_6.php
+++ src/src/game/pad/2_oa_b_2_6.php
@@ -6,42 +6,35 @@ Extends: NumberPad,
 	initialize: function(application)
 	{
        		this.parent(application);
-	
 		this.mThresholdTime = 6000;
-	
-		this.setScoreNeeded(15);
+		this.setScoreNeeded(10);
 	},
   
 	createQuestions: function()
         {
  		this.parent();
                 
-		this.mQuiz.resetQuestionArray();
-
 		//add 1
-		this.mQuiz.mQuestionArray.push(new Question('10 + 6 =','16'));
-		this.mQuiz.mQuestionArray.push(new Question('11 + 6 =','17'));
-		this.mQuiz.mQuestionArray.push(new Question('12 + 6 =','18'));
-		this.mQuiz.mQuestionArray.push(new Question('14 + 6 =','19'));
-		this.mQuiz.mQuestionArray.push(new Question('14 + 6 =','20'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('10 + 6 =','16'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('11 + 6 =','17'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('12 + 6 =','18'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('14 + 6 =','19'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('14 + 6 =','20'));
 
 		//add 1  
-		this.mQuiz.mQuestionArray.push(new Question('6 + 10 =','16'));
-		this.mQuiz.mQuestionArray.push(new Question('6 + 11 =','17'));
-		this.mQuiz.mQuestionArray.push(new Question('6 + 12 =','18'));
-		this.mQuiz.mQuestionArray.push(new Question('6 + 13 =','19'));
-		this.mQuiz.mQuestionArray.push(new Question('6 + 14 =','20'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('6 + 10 =','16'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('6 + 11 =','17'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('6 + 12 =','18'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('6 + 13 =','19'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('6 + 14 =','20'));
 
 		//1
-		this.mQuiz.mQuestionArray.push(new Question('16 - 6 =','10'));
-		this.mQuiz.mQuestionArray.push(new Question('17 - 6 =','11'));
-		this.mQuiz.mQuestionArray.push(new Question('18 - 6 =','12'));
-		this.mQuiz.mQuestionArray.push(new Question('19 - 6 =','13'));
-		this.mQuiz.mQuestionArray.push(new Question('20 - 6 =','14'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('16 - 6 =','10'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('17 - 6 =','11'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('18 - 6 =','12'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('19 - 6 =','13'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('20 - 6 =','14'));
 	
-		//buffer
-                this.mQuiz.mQuestionArray.push(new Question('buf','buf'));
- 
                 var totalNew           = 0;
 
                 while (totalNew < this.mScoreNeeded * .4 || totalNew > this.mScoreNeeded * .6)
@@ -57,16 +50,17 @@ Extends: NumberPad,
                                 var randomChance = Math.floor((Math.random()*2));
                                 if (randomChance == 0)
                                 {
-                                        this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[this.mApplication.mLevel - 1]);
+                                        this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[this.mApplication.mLevel]);
                                         totalNew++;
                                 }
                                 if (randomChance == 1)
                                 {
-                                        var randomElement = Math.floor((Math.random()*parseInt(this.mApplication.mLevel - 1)));
+                                        var randomElement = Math.floor((Math.random()*parseInt(this.mApplication.mLevel)));
                                         this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[randomElement]);
                                 }
                         }
                 }
-	
+		//buffer
+                this.mQuiz.mQuestionArray.push(new Question('buf','buf'));
 	}
 });
--- src/src/game/pad/2_oa_b_2_8.php
+++ src/src/game/pad/2_oa_b_2_8.php
@@ -6,36 +6,29 @@ Extends: NumberPad,
 	initialize: function(application)
 	{
        		this.parent(application);
-	
 		this.mThresholdTime = 6000;
-	
-		this.setScoreNeeded(9);
+		this.setScoreNeeded(10);
 	},
   
 	createQuestions: function()
         {
  		this.parent();
                 
-		this.mQuiz.resetQuestionArray();
-
 		//add 1
-		this.mQuiz.mQuestionArray.push(new Question('10 + 8 =','18'));
-		this.mQuiz.mQuestionArray.push(new Question('11 + 8 =','19'));
-		this.mQuiz.mQuestionArray.push(new Question('12 + 8 =','20'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('10 + 8 =','18'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('11 + 8 =','19'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('12 + 8 =','20'));
 
 		//add 1  
-		this.mQuiz.mQuestionArray.push(new Question('8 + 10 =','18'));
-		this.mQuiz.mQuestionArray.push(new Question('8 + 11 =','19'));
-		this.mQuiz.mQuestionArray.push(new Question('8 + 12 =','20'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('8 + 10 =','18'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('8 + 11 =','19'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('8 + 12 =','20'));
 
 		//1
-		this.mQuiz.mQuestionArray.push(new Question('18 - 8 =','10'));
-		this.mQuiz.mQuestionArray.push(new Question('19 - 8 =','11'));
-		this.mQuiz.mQuestionArray.push(new Question('20 - 8 =','12'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('18 - 8 =','10'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('19 - 8 =','11'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('20 - 8 =','12'));
 	
-		//buffer
-                this.mQuiz.mQuestionArray.push(new Question('buf','buf'));
- 
                 var totalNew           = 0;
 
                 while (totalNew < this.mScoreNeeded * .4 || totalNew > this.mScoreNeeded * .6)
@@ -51,16 +44,17 @@ Extends: NumberPad,
                                 var randomChance = Math.floor((Math.random()*2));
                                 if (randomChance == 0)
                                 {
-                                        this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[this.mApplication.mLevel - 1]);
+                                        this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[this.mApplication.mLevel]);
                                         totalNew++;
                                 }
                                 if (randomChance == 1)
                                 {
-                                        var randomElement = Math.floor((Math.random()*parseInt(this.mApplication.mLevel - 1)));
+                                        var randomElement = Math.floor((Math.random()*parseInt(this.mApplication.mLevel)));
                                         this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[randomElement]);
                                 }
                         }
                 }
-	
+		//buffer
+                this.mQuiz.mQuestionArray.push(new Question('buf','buf'));
 	}
 });
