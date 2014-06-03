e55ce57f27948c567f24726c2f8c163d884cf968
jbreslin33 <jbreslin33@localhost>
2014-06-02 20:48:43 -0400

    fixed up to adding 3

--- src/src/game/pad/2_oa_b_2_2.php
+++ src/src/game/pad/2_oa_b_2_2.php
@@ -8,46 +8,45 @@ Extends: NumberPad,
        		this.parent(application);
 	
 		this.mThresholdTime = 6000;
+		this.setScoreNeeded(10);
 	},
   
 	createQuestions: function()
         {
  		this.parent();
                 
-		this.mQuiz.resetQuestionArray();
-
 		//add 1
-		this.mQuiz.mQuestionArray.push(new Question('10 + 2 =','12'));
-		this.mQuiz.mQuestionArray.push(new Question('11 + 2 =','13'));
-		this.mQuiz.mQuestionArray.push(new Question('12 + 2 =','14'));
-		this.mQuiz.mQuestionArray.push(new Question('13 + 2 =','15'));
-		this.mQuiz.mQuestionArray.push(new Question('14 + 2 =','16'));
-		this.mQuiz.mQuestionArray.push(new Question('15 + 2 =','17'));
-		this.mQuiz.mQuestionArray.push(new Question('16 + 2 =','18'));
-		this.mQuiz.mQuestionArray.push(new Question('17 + 2 =','19'));
-		this.mQuiz.mQuestionArray.push(new Question('18 + 2 =','20'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('10 + 2 =','12'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('11 + 2 =','13'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('12 + 2 =','14'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('13 + 2 =','15'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('14 + 2 =','16'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('15 + 2 =','17'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('16 + 2 =','18'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('17 + 2 =','19'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('18 + 2 =','20'));
 
 		//add 1  
-		this.mQuiz.mQuestionArray.push(new Question('2 + 10 =','12'));
-		this.mQuiz.mQuestionArray.push(new Question('2 + 11 =','13'));
-		this.mQuiz.mQuestionArray.push(new Question('2 + 12 =','14'));
-		this.mQuiz.mQuestionArray.push(new Question('2 + 13 =','15'));
-		this.mQuiz.mQuestionArray.push(new Question('2 + 14 =','16'));
-		this.mQuiz.mQuestionArray.push(new Question('2 + 15 =','17'));
-		this.mQuiz.mQuestionArray.push(new Question('2 + 16 =','18'));
-		this.mQuiz.mQuestionArray.push(new Question('2 + 17 =','19'));
-		this.mQuiz.mQuestionArray.push(new Question('2 + 18 =','20'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('2 + 10 =','12'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('2 + 11 =','13'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('2 + 12 =','14'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('2 + 13 =','15'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('2 + 14 =','16'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('2 + 15 =','17'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('2 + 16 =','18'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('2 + 17 =','19'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('2 + 18 =','20'));
 
 		//1
-		this.mQuiz.mQuestionArray.push(new Question('12 - 2 =','10'));
-		this.mQuiz.mQuestionArray.push(new Question('13 - 2 =','11'));
-		this.mQuiz.mQuestionArray.push(new Question('14 - 2 =','12'));
-		this.mQuiz.mQuestionArray.push(new Question('15 - 2 =','13'));
-		this.mQuiz.mQuestionArray.push(new Question('16 - 2 =','14'));
-		this.mQuiz.mQuestionArray.push(new Question('17 - 2 =','15'));
-		this.mQuiz.mQuestionArray.push(new Question('18 - 2 =','16'));
-		this.mQuiz.mQuestionArray.push(new Question('19 - 2 =','17'));
-		this.mQuiz.mQuestionArray.push(new Question('20 - 2 =','18'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('12 - 2 =','10'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('13 - 2 =','11'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('14 - 2 =','12'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('15 - 2 =','13'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('16 - 2 =','14'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('17 - 2 =','15'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('18 - 2 =','16'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('19 - 2 =','17'));
+		this.mQuiz.mQuestionPoolArray.push(new Question('20 - 2 =','18'));
 	
 		var totalNew           = 0;
 
@@ -64,16 +63,16 @@ Extends: NumberPad,
                                 var randomChance = Math.floor((Math.random()*2));
                                 if (randomChance == 0)
                                 {
-                                        this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[this.mApplication.mLevel - 1]);
+                                        this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[this.mApplication.mLevel]);
                                         totalNew++;
                                 }
                                 if (randomChance == 1)
                                 {
-                                        var randomElement = Math.floor((Math.random()*parseInt(this.mApplication.mLevel - 1)));
+                                        var randomElement = Math.floor((Math.random()*parseInt(this.mApplication.mLevel)));
                                         this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[randomElement]);
                                 }
                         }
                 }
-	
+		this.mQuiz.mQuestionArray.push(new Question('buf','buf'));
 	}
 });
