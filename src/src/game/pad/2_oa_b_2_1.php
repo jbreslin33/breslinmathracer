637f9d632a2dc64e621555cba2b20fbdccfc3df1
jbreslin33 <jbreslin33@localhost>
2014-06-02 20:48:43 -0400

    fixed up to adding 3

--- src/src/game/pad/2_oa_b_2_1.php
+++ src/src/game/pad/2_oa_b_2_1.php
@@ -5,6 +5,7 @@ Extends: NumberPad,
 	{
        		this.parent(application);
 		this.mThresholdTime = 6000;
+		this.setScoreNeeded(10);
 	},
   
 	createQuestions: function()
@@ -60,12 +61,12 @@ Extends: NumberPad,
                                 var randomChance = Math.floor((Math.random()*2));
                                 if (randomChance == 0)
                                 {
-                                        this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[this.mApplication.mLevel - 1]);
+                                        this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[this.mApplication.mLevel]);
                                         totalNew++;
                                 }
                                 if (randomChance == 1)
                                 {
-                                        var randomElement = Math.floor((Math.random()*parseInt(this.mApplication.mLevel - 1)));
+                                        var randomElement = Math.floor((Math.random()*parseInt(this.mApplication.mLevel)));
                                         this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[randomElement]);
                                 }
                         }
