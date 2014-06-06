cdb22d9cde55cf617d543755147bc53c4a281b55
bbreslin99 <bbreslin99@localhost>
2014-06-05 22:39:34 -0400

    finished 4oac5

--- src/src/game/pad/4_oa_c_5.php
+++ src/src/game/pad/4_oa_c_5.php
@@ -1,116 +1,95 @@
 var g4_oa_c_5 = new Class(
 {
-
 Extends: NumberPad,
-
 	initialize: function(application)
 	{
        		this.parent(application);
-             	this.mCorrectAnswerThresholdTime = 40000;
 	},
 
-        createNumQuestion: function()
+
+	createNumQuestion: function()
         {
-                this.parent();
-                this.mNumQuestion.setPosition(140,140);
-                this.mNumQuestion.setSize(200,200);
+                //question
+                this.mNumQuestion = new Shape(240,50,155,95,this,"","","");
+                this.mShapeArray.push(this.mNumQuestion);
+                this.mNumQuestion.mCollidable = false;
+                this.mNumQuestion.mCollisionOn = false;
         },
 
-     	//showCorrectAnswer
-        showCorrectAnswerEnter: function()
+	showCorrectAnswerEnter: function()
         {
 		this.parent();
-                
-                this.mShapeArray[1].setSize(700,100);
-                this.mShapeArray[1].setPosition(380,80);
-       
-		//move dont forget 
-	        this.mShapeArray[8].setVisibility(false);
-	        this.mShapeArray[9].setVisibility(false);
-        },
-
-        //outOfTime
-        outOfTimeEnter: function()
-        {
-		this.parent();
-
-                this.mShapeArray[0].setPosition(650,170);
-
-                this.mShapeArray[1].setSize(700,100);
-                this.mShapeArray[1].setPosition(380,80);
-	      
-		//move frantic clock 
-		this.mShapeArray[8].setPosition(650,300);
-        },
-
-    	tip: function()
-        {
-                if (this.mQuiz.getQuestion().mTipArray.length > 0)
-                {
-                        //tip header
-                        this.mShapeArray[2].setPosition(140,100);
-                        this.mShapeArray[2].setSize(200,10);
-                        this.mShapeArray[2].setVisibility(true);
-                        if (this.mQuiz.getQuestion().mTipArray.length == 1)
-                        {
-                                this.mShapeArray[2].mMesh.innerHTML = 'Tip:';
-                        }
-                        else
-                        {
-                                this.mShapeArray[2].mMesh.innerHTML = 'Tips:';
-                        }
-
-                        if (this.mQuiz.getQuestion().mTipArray.length > 0)
-                        {
-                                this.mShapeArray[3].setPosition(380,150);
-                                this.mShapeArray[3].setSize(700,10);
-                                this.mShapeArray[3].setVisibility(true);
-                                this.mShapeArray[3].mMesh.innerHTML = this.mQuiz.getQuestion().mTipArray[0];
-                        }
-                        if (this.mQuiz.getQuestion().mTipArray.length > 3)
-                        {
-                                this.mShapeArray[6].setPosition(380,180);
-                        	this.mShapeArray[6].setSize(700,10);
-                                this.mShapeArray[6].setVisibility(true);
-                                this.mShapeArray[6].mMesh.innerHTML = this.mQuiz.getQuestion().mTipArray[3];
-			}
-                }
+                this.mShapeArray[1].setPosition(360,100);
+		this.mShapeArray[1].setSize(250,100);
         },
 
 	createQuestions: function()
         {
  		this.parent();
 
-		//a
-		this.mQuiz.mQuestionPoolArray.push(new QuestionWord('','',2,100,2,10,2,10,'Tennis Steve had','diamonds. Athletic Steve had','times that amount of diamonds. How many diamonds does Athletic Steve have?',3));	
-		this.mQuiz.mQuestionPoolArray[this.mQuiz.mQuestionPoolArray.length -1].mTipArray[0] = 'Tennis Steve diamonds times AMOUNT = Athletic Steve diamonds';
-		
-		this.mQuiz.mQuestionPoolArray.push(new QuestionWord('','',2,100,2,10,2,10,'Tennis Steve had','diamonds. Athletic Steve had','times that amount of diamonds. How many diamonds does Athletic Steve have?',3));	
-		this.mQuiz.mQuestionPoolArray[this.mQuiz.mQuestionPoolArray.length -1].mTipArray[0] = 'Tennis Steve diamonds times AMOUNT = Athletic Steve diamonds';
+		var mult = 0;
+		var add = 0;
+		var exp = 0;
+		var rand = 0;
 
-		var totalA = 0;
-		var totalB = 0;
+		var num1 = 0;
+		var num2 = 0;
+		var num3 = 0;
+		var num4 = 0;
 
-		while (totalA < this.mScoreNeeded * .4 || totalB < this.mScoreNeeded * .4) 
+		var varA = 0;
+		var varB = 0;
+		var varC = 0;
+
+		this.mQuiz.resetQuestionArray();
+
+		rand = Math.floor((Math.random()*3));
+
+	    for (s = 0; s < this.mScoreNeeded; s++)
+	    {	
+
+		if (rand == 0)
 		{	
-			this.mQuiz.resetQuestionArray();
+				// pick number of digits (2 - 6)
+				num1 = 1 + Math.floor((Math.random()*19));
+				add = 1 + Math.floor((Math.random()*19));
+				num2 = num1 + add;
+				num3 = num2 + add;
+				num4 = num3 + add;
+				num5 = num4 + add;
 
-			//ADD questions
-			for (s = 0; s < this.mScoreNeeded; s++)
-			{	
-				//50% chance of asking newest question
-				var randomChance = Math.floor((Math.random()*2));		
-				if (randomChance == 0)
-				{
-       					this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[randomChance]);
-					totalA++;
-				}	
-				if (randomChance == 1)
-				{
-       					this.mQuiz.mQuestionArray.push(this.mQuiz.mQuestionPoolArray[randomChance]);
-					totalB++;
-				}
-			}
 		}
+		if (rand == 1)
+		{	
+                 
+				num1 = 1 + Math.floor((Math.random()*5));
+				mult = 2 + Math.floor((Math.random()*4));
+				num2 = num1 * mult;
+				num3 = num2 * mult;
+				num4 = num3 * mult;
+				num5 = num4 * mult;
+		}
+
+		if (rand == 2)
+		{	
+
+				num1 = 1 + Math.floor((Math.random()*5));
+				mult = 2 + Math.floor((Math.random()*4));
+				add = 1 + Math.floor((Math.random()*5));
+				num2 = (num1 * mult) + add;
+				num3 = (num2 * mult) + add;
+				num4 = (num3 * mult) + add;
+				num5 = (num4 * mult) + add;
+
+		}
+
+
+		 this.mQuiz.mQuestionArray.push(new Question(' Type the next number in this sequence:<br><br> ' + num1 + ' , ' +  num2 + ' , ' + num3 + ' , ' + num4 + ' , ?', '' + num5));
+	    }
+                //buffer
+                this.mQuiz.mQuestionArray.push(new Question('buf','buf'));
+
+                //random
+                //this.mQuiz.randomize(10);
 	}
 });
