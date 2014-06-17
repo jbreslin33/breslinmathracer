00879755269f0b5863ead8a51f6ea71ec9a7e4b5
jbreslin33 <jbreslin33@localhost>
2014-06-17 17:15:22 -0400

    moved some functions out of if statement for efficiency for 4.ao.a.3

--- src/src/game/pad/4_oa_a_3.php
+++ src/src/game/pad/4_oa_a_3.php
@@ -7,7 +7,7 @@ Extends: MultipleChoicePad,
 	{
        		this.parent(application);
 
-		this.setScoreNeeded(1);
+		this.setScoreNeeded(2);
 	},
 
         createInput: function()
@@ -40,6 +40,7 @@ Extends: MultipleChoicePad,
 
 	makeTypeA: function()
 	{
+		question = '';
 		randomChoice = Math.floor(Math.random()*2);
 		if (randomChoice == 0)
 		{	
@@ -47,8 +48,6 @@ Extends: MultipleChoicePad,
  			question.mAnswerPool.push('Find the number of red pencils: 9+8=17. Find the number of green pencils: 2+7=9. Find the total number of pencils: 9+17+9=35.');
                		question.mAnswerPool.push('Find the number green pencils: 9+8=17. Find the number of red pencils: 2x8=16. Find the total number of pencils: 9+17+16=42.');
                 	question.mAnswerPool.push('Find the number of red pencils: 9+8=17. Find the number of green pencils: 2x17=34. Find the total number of pencils: 9+17+34=60.');
-                	this.mQuiz.mQuestionArray.push(question);
-			question.mRandomChoices = true;
 		}
 		if (randomChoice == 1)
 		{	
@@ -56,24 +55,25 @@ Extends: MultipleChoicePad,
  			question.mAnswerPool.push('Find the number of red fish: 9+8=17. Find the number of green fish: 2+7=9. Find the total number of fish: 9+17+9=35.');
                		question.mAnswerPool.push('Find the number green fish: 9+8=17. Find the number of red fish: 2x8=16. Find the total number of fish: 9+17+16=42.');
                 	question.mAnswerPool.push('Find the number of red fish: 9+8=17. Find the number of green fish: 2x17=34. Find the total number of fish: 9+17+34=60.');
-                	this.mQuiz.mQuestionArray.push(question);
-			question.mRandomChoices = true;
 		}
+                this.mQuiz.mQuestionArray.push(question);
+		question.mRandomChoices = true;
 	},	
 
 	makeTypeB: function()
 	{
 		randomChoice = Math.floor(Math.random()*2);
 		randomChoice = 0;
+		question = '';
 		if (randomChoice == 0)
 		{	
 			question = new Question('There were 115 people at a party. There were 5 times as many people as there were men. There were twice as many women as men. The remaining people at the party were children. How many children at the party? What step cannot be used to solve the problem?','115-23=92 children');
 			question.mAnswerPool.push('115-23=92 children');
 			question.mAnswerPool.push('115/5=23 children');
 			question.mAnswerPool.push('23x2=46 children');
-                	this.mQuiz.mQuestionArray.push(question);
-			question.mRandomChoices = true;
 		}
+                this.mQuiz.mQuestionArray.push(question);
+		question.mRandomChoices = true;
 	},
 
 	createQuestions: function()
@@ -82,8 +82,8 @@ Extends: MultipleChoicePad,
 
                 this.mQuiz.resetQuestionArray();
 
-		this.makeTypeB();	
 		this.makeTypeA();	
+		this.makeTypeB();	
 
 		this.mQuiz.mQuestionArray.push(new Question('buf','buf'));
 	}
