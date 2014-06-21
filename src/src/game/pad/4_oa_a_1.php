211f63cfdeeafef254258360a881fa09650eabae
jbreslin33 <jbreslin33@localhost>
2014-06-21 11:42:52 -0400

    4.oa.a.1 fixed up.

--- src/src/game/pad/4_oa_a_1.php
+++ src/src/game/pad/4_oa_a_1.php
@@ -6,38 +6,35 @@ Extends: MultipleChoicePad,
 	initialize: function(application)
 	{
        		this.parent(application);
-		this.setScoreNeeded(12);
+		this.setScoreNeeded(5);
+
+		this.mCircles = new Circles(this);
+		this.mRaphael = Raphael(10, 35, 760, 405);
 	},
 
-	createNumQuestion: function()
-	{
-		this.parent();
-		this.mNumQuestion.setPosition(140,140);
-		this.mNumQuestion.setSize(200,200);
-	},	
+       	createNumQuestion: function()
+        {
+                this.parent();
+                this.mNumQuestion.setSize(650,200);
+                this.mNumQuestion.setPosition(350,140);
+        },
 
+        createInput: function()
+        {
+                this.parent();
+                this.mButtonA.setPosition(105,200);
+                this.mButtonB.setPosition(385,200);
+                this.mButtonC.setPosition(660,200);
+        },
+
+        //showCorrectAnswer
         showCorrectAnswerEnter: function()
         {
                 this.parent();
-
                 this.mShapeArray[1].setSize(650,200);
                 this.mShapeArray[1].setPosition(350,140);
-        },
-
-        //outOfTime
-        outOfTimeEnter: function()
-        {
-                this.parent();
-
-                this.mShapeArray[0].setPosition(650,170);
-
-                this.mShapeArray[1].setSize(700,100);
-                this.mShapeArray[1].setPosition(380,80);
-
-                //move frantic clock
-                this.mShapeArray[8].setPosition(650,300);
-		
-		this.mShapeArray[1].mMesh.innerHTML = '' + this.mQuiz.getQuestion().getShowAnswer();
+                this.mShapeArray[9].setSize(75,75);
+                this.mShapeArray[9].setPosition(75,175);
         },
 
 	makeTypeA: function()
@@ -96,6 +93,62 @@ Extends: MultipleChoicePad,
 		question.mRandomChoices = true;
 	},
 
+	makeTypeE: function()
+        {
+		question = '';
+                var a = 0;
+                var b = 0;
+                var c = 0;
+                var d = 0;
+                var e = 0;
+                var f = 0;
+
+                while ((a * b) == (c * d) || (a * b) == (e * f))
+                {
+                        a = Math.floor((Math.random()*8)+2);
+                        b = Math.floor((Math.random()*8)+2);
+                        c = Math.floor((Math.random()*8)+2);
+                        d = Math.floor((Math.random()*8)+2);
+                        e = Math.floor((Math.random()*8)+2);
+                        f = Math.floor((Math.random()*8)+2);
+                }
+                randomChoice = Math.floor(Math.random()*2);
+		randomChoice = 0;
+
+		if (randomChoice == 0)
+		{
+                	question = new Question('Lucy had wants to put ' + a + ' seeds in each of ' + b + ' pots of dirt. ' + a + 'x' + b + ' represents this as an expression. How would we best represent this expression in a picture?', 'A');
+		}
+
+                var correctLetterNumber = Math.floor(Math.random()*3);
+                if (correctLetterNumber == 0)
+                {
+                        question.setAnswer('A',0);
+                        this.mCircles.includeCircles(question,'A',a,b);
+                        this.mCircles.includeCircles(question,'B',c,d);
+                        this.mCircles.includeCircles(question,'C',e,f);
+
+                }
+                if (correctLetterNumber == 1)
+                {
+                        question.setAnswer('B',0);
+                        this.mCircles.includeCircles(question,'A',c,d);
+                        this.mCircles.includeCircles(question,'B',a,b);
+                        this.mCircles.includeCircles(question,'C',e,f);
+                }
+                if (correctLetterNumber == 2)
+                {
+                        question.setAnswer('C',0);
+                        this.mCircles.includeCircles(question,'A',c,d);
+                        this.mCircles.includeCircles(question,'B',e,f);
+                        this.mCircles.includeCircles(question,'C',a,b);
+                }
+                question.mAnswerPool.push('A');
+                question.mAnswerPool.push('B');
+                question.mAnswerPool.push('C');
+                this.mQuiz.mQuestionArray.push(question);
+        },
+
 	createQuestions: function()
         {
  		this.parent();
@@ -103,23 +156,18 @@ Extends: MultipleChoicePad,
 		this.mQuiz.resetQuestionArray();
 
 		this.makeTypeA();
-		this.makeTypeA();
-		this.makeTypeA();
-		this.makeTypeA();
-		this.makeTypeB();
-		this.makeTypeB();
-		this.makeTypeB();
 		this.makeTypeB();
 		this.makeTypeC();
-		this.makeTypeC();
-		this.makeTypeC();
-		this.makeTypeC();
 		this.makeTypeD();
-		this.makeTypeD();
-		this.makeTypeD();
-		this.makeTypeD();
+		this.makeTypeE();
 
                 this.mQuiz.mQuestionArray.push(new Question('buf','buf'));
-                this.mQuiz.randomize(10);
+                //this.mQuiz.randomize(10);
+	},
+
+	createWorld: function()
+	{
+		this.parent();
+		this.mCircles.createWorld();
 	}
 });
