1bb2b9a1a516e975123e16fecd9350d2829496e4
jbreslin33 <jbreslin33@localhost>
2014-06-26 17:18:50 -0400

    adding seperate sheet.

--- src/web/game/standard_game_includes.php
+++ src/web/game/standard_game_includes.php
@@ -213,4 +213,5 @@
 <script type="text/javascript" src="/src/game/pad/4_nf_c_7.php"></script>
 <script type="text/javascript" src="/src/game/pad/4_md_a_1.php"></script>
 <script type="text/javascript" src="/src/game/pad/5_oa_a_1.php"></script>
+<script type="text/javascript" src="/src/sheet/sheet_5_oa_a_1.php"></script>
 <script type="text/javascript" src="/web/game/standard_game_hud.php"></script>
