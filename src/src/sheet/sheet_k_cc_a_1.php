a126b1c23d3e664eb5572ec352f125e40426ad69
jbreslin33 <jbreslin33@localhost>
2014-07-01 14:08:04 -0400

    just adding shapes.

--- src/src/sheet/sheet_k_cc_a_1.php
+++ src/src/sheet/sheet_k_cc_a_1.php
@@ -8,12 +8,6 @@ Extends: Sheet,
         {
 		this.parent(game);
 
-		this.mItem1 = 0;		
-		this.mItem2 = 0;		
-		this.mItem3 = 0;		
-		this.mItem4 = 0;		
-		this.mItem5 = 0;		
-
 		this.mVictoryShape1 = 0; 
 		this.mVictoryShape2 = 0; 
 		this.mVictoryShape3 = 0; 
@@ -28,26 +22,12 @@ Extends: Sheet,
 	{
 		this.parent();
 
-		this.mItem1 = new TextItem(this,'0','1');     
-                this.addItem(this.mItem1);
-
-                this.mItem2 = new TextItem(this,'1','2');
-                this.addItem(this.mItem2);
-
-                this.mItem3 = new TextItem(this,'2','3');
-                this.addItem(this.mItem3);
-
-                this.mItem4 = new TextItem(this,'3','4');
-                this.addItem(this.mItem4);
-
-                this.mItem5 = new TextItem(this,'4','5');
-                this.addItem(this.mItem5);
-                	
-		this.mItem6 = new TextItem(this,'5','6');
-                this.addItem(this.mItem6);
-		
-		//this.mItem7 = new TextItem(this,'buf','buf');
-                //this.addItem(this.mItem7);
+		this.addItem(new TextItem(this,'0','1'));     
+		this.addItem(new TextItem(this,'1','2'));     
+		this.addItem(new TextItem(this,'2','3'));     
+		this.addItem(new TextItem(this,'3','4'));     
+		this.addItem(new TextItem(this,'4','5'));     
+		this.addItem(new TextItem(this,'5','6'));     
 	},
    
 	createVictoryShapes: function()
