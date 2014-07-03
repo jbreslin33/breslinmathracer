266791b4f4fa32deaff8587e8c6419c9279f69f8
jbreslin33 <jbreslin33@localhost>
2014-07-03 15:04:02 -0400

    buttons go up in the air.

--- src/src/item/item.php
+++ src/src/item/item.php
@@ -46,7 +46,7 @@ var Item = new Class(
 
 		//times for showContinueCorrect
                 this.mShowContinueCorrectStartTime = 0;
-                this.mShowContinueCorrectThresholdTime = 1000;
+                this.mShowContinueCorrectThresholdTime = 3000;
 
 		//continue button vars
 		this.mContinueCorrect = false;
@@ -268,7 +268,8 @@ var Item = new Class(
                 {
                         if (this.mButtonArray[i].getAnswer() != this.getAnswer())
                         {
-                                this.mButtonArray[i].setVisibility(false);
+                                //this.mButtonArray[i].setVisibility(false);
+                                this.mButtonArray[i].mFire = true;
                         }
                         if (this.mButtonArray[i].getAnswer() == this.getAnswer())
                         {
--- src/src/item/item_button.php
+++ src/src/item/item_button.php
@@ -5,6 +5,11 @@ Extends: Shape,
         {
                 this.parent(width,height,spawnX,spawnY,game,src,backgroundColor,message);
 
+                //ai
+                this.mAiCounter = 0;
+                this.mAiCounterDelay = 10;
+		this.mFire = false;
+
 		//lets make this not collide 
                 this.mCollidable  = false;
                 this.mCollisionOn = false;
@@ -19,6 +24,28 @@ Extends: Shape,
                         this.mMesh.addEvent('click',this.buttonHit);
                 }
         },
+
+        update: function(delta)
+        {
+                //run ai
+                if (this.mAiCounter > this.mAiCounterDelay)
+                {
+			if (this.mFire)
+			{
+                        	this.ai();
+			}
+                        this.mAiCounter = 0;
+                }
+                this.mAiCounter++;
+
+                this.parent(delta);
+        },
+
+	ai: function()
+	{
+        	this.mKey.mX = 0;
+                this.mKey.mY = -1;
+	},
 
 	setAnswer: function(answer)
 	{
