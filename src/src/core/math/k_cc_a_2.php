20f32093a831ba08355112a16533459cacfb4e7b
jbreslin33 <jbreslin33@localhost>
2014-07-04 15:11:22 -0400

    3 types.

--- src/src/core/math/k_cc_a_2.php
+++ src/src/core/math/k_cc_a_2.php
@@ -49,18 +49,32 @@ Extends: Sheet,
 
 		this.addItem(new i_k_cc_a_2_t_1(this));
 		this.addItem(new i_k_cc_a_2_t_2(this));
-		this.addItem(new i_k_cc_a_2_t_2(this));
-		this.addItem(new i_k_cc_a_2_t_2(this));
-		this.addItem(new i_k_cc_a_2_t_2(this));
-		this.addItem(new i_k_cc_a_2_t_2(this));
-		this.addItem(new i_k_cc_a_2_t_2(this));
-		this.addItem(new i_k_cc_a_2_t_2(this));
-		this.addItem(new i_k_cc_a_2_t_2(this));
-		this.addItem(new i_k_cc_a_2_t_2(this));
-		this.addItem(new i_k_cc_a_2_t_2(this));
-		this.addItem(new i_k_cc_a_2_t_2(this));
-		this.addItem(new i_k_cc_a_2_t_2(this));
-		this.addItem(new i_k_cc_a_2_t_2(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_3(this));
 		
 		this.randomize(10);
         }
@@ -106,7 +120,7 @@ Extends: ThreeButtonItem,
 });
 
 /*****************
-i_k_cc_a_2_t_2: This type will ask what comes next after a number from 0-99.
+i_k_cc_a_2_t_2: This type will ask what 2 numbers come next after a number from 0-99.
 ****************/
 var i_k_cc_a_2_t_2 = new Class(
 {
@@ -116,6 +130,46 @@ Extends: ThreeButtonItem,
                 this.parent(sheet);
                 this.mStandard = 'k.cc.a.2';
                 this.mType = 2;
+
+                var x = Math.floor(Math.random()*98);
+                var a = parseInt(x+1);
+                var b = 0;
+                var c = 0;
+
+                while (a == b || a == c || b == c || a < 0 || b < 0 || c < 0)
+                {
+                        b = Math.floor(Math.random()*7)-3;
+                        b = parseInt(a+b);
+                        c = Math.floor(Math.random()*7)-3;
+                        c = parseInt(a+c);
+                }
+
+                a = a + ',' + parseInt(a+1);
+                b = b + ',' + parseInt(b+1);
+                c = c + ',' + parseInt(c+1);
+
+                this.setQuestion('What comes after ' + x + '?');
+                this.setAnswer(a,0);
+
+                this.mButtonA.setAnswer(a);
+                this.mButtonB.setAnswer(b);
+                this.mButtonC.setAnswer(c);
+                this.shuffle(10);
+        }
+});
+
+
+/*****************
+i_k_cc_a_2_t_3: This type will ask what 3 numbers come next after a number from 0-99.
+****************/
+var i_k_cc_a_2_t_3 = new Class(
+{
+Extends: ThreeButtonItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet);
+                this.mStandard = 'k.cc.a.2';
+                this.mType = 3;
 
                 var x = Math.floor(Math.random()*98);
 		var a = parseInt(x+1);
@@ -143,4 +197,3 @@ Extends: ThreeButtonItem,
                 this.shuffle(10);
         }
 });
-
