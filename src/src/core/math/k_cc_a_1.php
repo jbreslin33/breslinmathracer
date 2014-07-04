a381318973af6d482b68c44d51f4f470bc2b4e10
jbreslin33 <jbreslin33@localhost>
2014-07-04 19:24:51 -0400

    type 2 works.

--- src/src/core/math/k_cc_a_1.php
+++ src/src/core/math/k_cc_a_1.php
@@ -47,9 +47,21 @@ Extends: Sheet,
               
 		this.setScoreNeeded(APPLICATION.mLevel); 
 
-		this.addItem(new i_k_cc_a_1_t_1(this));
+		this.addItem(new i_k_cc_a_1_t_2(this));
+		this.addItem(new i_k_cc_a_1_t_2(this));
+		this.addItem(new i_k_cc_a_1_t_2(this));
+		this.addItem(new i_k_cc_a_1_t_2(this));
+		this.addItem(new i_k_cc_a_1_t_2(this));
+		this.addItem(new i_k_cc_a_1_t_2(this));
+		this.addItem(new i_k_cc_a_1_t_2(this));
+		this.addItem(new i_k_cc_a_1_t_2(this));
+		this.addItem(new i_k_cc_a_1_t_2(this));
+		this.addItem(new i_k_cc_a_1_t_2(this));
+		this.addItem(new i_k_cc_a_1_t_2(this));
+		this.addItem(new i_k_cc_a_1_t_2(this));
+		this.addItem(new i_k_cc_a_1_t_2(this));
 		
-		//this.randomize(10);
+		this.randomize(10);
         }
 });
 
@@ -91,3 +103,41 @@ Extends: ThreeButtonItem,
                 this.shuffle(10);
         }
 });
+
+/*****************
+i_k_cc_a_1_t_2: This type will ask what comes next after a number ending in 9 from 0-99.
+****************/
+var i_k_cc_a_1_t_2 = new Class(
+{
+Extends: ThreeButtonItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet);
+                this.mStandard = 'k.cc.a.1';
+                this.mType = 2;
+
+                var x = Math.floor((Math.random()*10)+1);
+		x = parseInt(x * 10);
+		x = parseInt(x-1);
+		var a = parseInt(x+1);
+                var b = 0;
+                var c = 0;
+
+                while (a == b || a == c || b == c || a < 0 || b < 0 || c < 0)
+                {
+                        b = Math.floor(Math.random()*7)-3;
+                        b = parseInt(a+b);
+                        c = Math.floor(Math.random()*7)-3;
+                        c = parseInt(a+c);
+                }
+
+                this.setQuestion('What comes after ' + x + '?');
+                this.setAnswer(parseInt(a),0);
+
+                this.mButtonA.setAnswer(a);
+                this.mButtonB.setAnswer(b);
+                this.mButtonC.setAnswer(c);
+                this.shuffle(10);
+        }
+});
+
