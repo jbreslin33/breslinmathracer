2d1e428ebba4f9854757028394ea64c0c1445ec4
jbreslin33 <jbreslin33@localhost>
2014-07-04 15:19:03 -0400

    getting correct question.

--- src/src/core/math/k_cc_a_2.php
+++ src/src/core/math/k_cc_a_2.php
@@ -47,34 +47,30 @@ Extends: Sheet,
               
 		this.setScoreNeeded(APPLICATION.mLevel); 
 
-		this.addItem(new i_k_cc_a_2_t_1(this));
-		this.addItem(new i_k_cc_a_2_t_2(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
-		this.addItem(new i_k_cc_a_2_t_3(this));
+		this.addItem(new i_k_cc_a_2_t_4(this));
+		this.addItem(new i_k_cc_a_2_t_4(this));
+		this.addItem(new i_k_cc_a_2_t_4(this));
+		this.addItem(new i_k_cc_a_2_t_4(this));
+		this.addItem(new i_k_cc_a_2_t_4(this));
+		this.addItem(new i_k_cc_a_2_t_4(this));
+		this.addItem(new i_k_cc_a_2_t_4(this));
+		this.addItem(new i_k_cc_a_2_t_4(this));
+		this.addItem(new i_k_cc_a_2_t_4(this));
+		this.addItem(new i_k_cc_a_2_t_4(this));
+		this.addItem(new i_k_cc_a_2_t_4(this));
+		this.addItem(new i_k_cc_a_2_t_4(this));
+		this.addItem(new i_k_cc_a_2_t_4(this));
+		this.addItem(new i_k_cc_a_2_t_4(this));
+		this.addItem(new i_k_cc_a_2_t_4(this));
+		this.addItem(new i_k_cc_a_2_t_4(this));
+		this.addItem(new i_k_cc_a_2_t_4(this));
+		this.addItem(new i_k_cc_a_2_t_4(this));
+		this.addItem(new i_k_cc_a_2_t_4(this));
+		this.addItem(new i_k_cc_a_2_t_4(this));
+		this.addItem(new i_k_cc_a_2_t_4(this));
+		this.addItem(new i_k_cc_a_2_t_4(this));
+		this.addItem(new i_k_cc_a_2_t_4(this));
+		this.addItem(new i_k_cc_a_2_t_4(this));
 		
 		this.randomize(10);
         }
@@ -197,3 +193,44 @@ Extends: ThreeButtonItem,
                 this.shuffle(10);
         }
 });
+
+/*****************
+i_k_cc_a_2_t_4: This type will ask what the missing number is. e.g. What is the missing number? 1,2,3,_,5,6,7. This will be done up to 100.
+****************/
+var i_k_cc_a_2_t_4 = new Class(
+{
+Extends: ThreeButtonItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet);
+                this.mStandard = 'k.cc.a.2';
+                this.mType = 4;
+
+                var x = Math.floor(Math.random()*98);
+                var a = parseInt(x+1);
+                var b = 0;
+                var c = 0;
+
+                while (a == b || a == c || b == c || a < 0 || b < 0 || c < 0)
+                {
+                        b = Math.floor(Math.random()*7)-3;
+                        b = parseInt(a+b);
+                        c = Math.floor(Math.random()*7)-3;
+                        c = parseInt(a+c);
+                }
+
+                a = a + ',' + parseInt(a+1) + ',' + parseInt(a+2);
+                b = b + ',' + parseInt(b+1) + ',' + parseInt(b+2);
+                c = c + ',' + parseInt(c+1) + ',' + parseInt(c+2);
+
+                this.setQuestion('What is the missing number? ' + parseInt(x-3) + ',' + parseInt(x-2) + ',' + parseInt(x-1) + ',_,' + parseInt(x+1) + ',' + parseInt(x+2) + ',' + parseInt(x+3));
+
+                this.setAnswer(a,0);
+
+                this.mButtonA.setAnswer(a);
+                this.mButtonB.setAnswer(b);
+                this.mButtonC.setAnswer(c);
+                this.shuffle(10);
+        }
+});
+
