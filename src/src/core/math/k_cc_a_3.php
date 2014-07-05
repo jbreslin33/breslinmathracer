e773fdf812daf2883a19ef44edbe646567bf1236
jbreslin33 <jbreslin33@localhost>
2014-07-05 11:33:30 -0400

    k.cc.a.3 works. next can i send an ajax insert for every question......

--- src/src/core/math/k_cc_a_3.php
+++ src/src/core/math/k_cc_a_3.php
@@ -53,15 +53,9 @@ Extends: Sheet,
                 }
 
 		this.addItem(new i_k_cc_a_3_t_1(this));
-		this.addItem(new i_k_cc_a_3_t_1(this));
-		this.addItem(new i_k_cc_a_3_t_1(this));
-		this.addItem(new i_k_cc_a_3_t_1(this));
-		this.addItem(new i_k_cc_a_3_t_1(this));
-		this.addItem(new i_k_cc_a_3_t_1(this));
-		this.addItem(new i_k_cc_a_3_t_1(this));
-		this.addItem(new i_k_cc_a_3_t_1(this));
-		this.addItem(new i_k_cc_a_3_t_1(this));
-		this.addItem(new i_k_cc_a_3_t_1(this));
+		this.addItem(new i_k_cc_a_3_t_2(this));
+		this.addItem(new i_k_cc_a_3_t_3(this));
+		this.addItem(new i_k_cc_a_3_t_4(this));
 		
 		this.randomize(10);
         }
@@ -121,3 +115,145 @@ Extends: ThreeButtonItem,
 		}
 	}
 });
+
+/* TYPE_DESCRIPTION: i_k_cc_a_3_t_2: Count the objects up to 20. Make answer zero. */
+
+var i_k_cc_a_3_t_2 = new Class(
+{
+Extends: ThreeButtonItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet);
+                this.mStandard = 'k.cc.a.3';
+                this.mType = 2;
+
+                var a = 0;
+                var b = 0;
+                var c = 0;
+
+                while (a == b || a == c || b == c || a < 0 || b < 0 || c < 0)
+                {
+                        b = Math.floor(Math.random()*7)-3;
+                        b = parseInt(a+b);
+                        c = Math.floor(Math.random()*7)-3;
+                        c = parseInt(a+c);
+                }
+
+                this.setQuestion('How many kids?');
+                this.setAnswer(a,0);
+
+                this.mButtonA.setAnswer(a);
+                this.mButtonB.setAnswer(b);
+                this.mButtonC.setAnswer(c);
+                this.shuffle(10);
+        }
+});
+/ * ITEMS: */
+
+/* TYPE_DESCRIPTION: i_k_cc_a_3_t_3: Count the objects up to 20. make sure answer is between 11-15 */
+
+var i_k_cc_a_3_t_3 = new Class(
+{
+Extends: ThreeButtonItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet);
+                this.mStandard = 'k.cc.a.3';
+                this.mType = 3;
+
+                var a = Math.floor(Math.random()*5)+11;
+                var b = 0;
+                var c = 0;
+
+                while (a == b || a == c || b == c || a < 0 || b < 0 || c < 0)
+                {
+                        b = Math.floor(Math.random()*7)-3;
+                        b = parseInt(a+b);
+                        c = Math.floor(Math.random()*7)-3;
+                        c = parseInt(a+c);
+                }
+
+                this.setQuestion('How many kids?');
+                this.setAnswer(a,0);
+
+                this.mButtonA.setAnswer(a);
+                this.mButtonB.setAnswer(b);
+                this.mButtonC.setAnswer(c);
+                this.shuffle(10);
+        },
+
+        createQuestionShapes: function()
+        {      
+                var x = 0;
+                var y = 300;
+
+                var answer = this.getAnswer();
+                answer = parseInt(answer);     
+
+                for (var i = 0; i < answer; i++)
+                {
+                        if (i == 10)
+                        {
+                                x = 0;
+                                y = 375;
+                        }
+                        x = parseInt(x + 70);  
+                        this.addQuestionShape(new Shape(50,50,x,y,this.mSheet.mGame,"/images/bus/kid.png","",""));
+                }
+        }
+});
+
+/* TYPE_DESCRIPTION: i_k_cc_a_3_t_4: Count the objects up to 20. make sure answer is between 16-20 */
+
+var i_k_cc_a_3_t_4 = new Class(
+{
+Extends: ThreeButtonItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet);
+                this.mStandard = 'k.cc.a.3';
+                this.mType = 4;
+
+                var a = Math.floor(Math.random()*5)+16;
+                var b = 0;
+                var c = 0;
+
+                while (a == b || a == c || b == c || a < 0 || b < 0 || c < 0)
+                {
+                        b = Math.floor(Math.random()*7)-3;
+                        b = parseInt(a+b);
+                        c = Math.floor(Math.random()*7)-3;
+                        c = parseInt(a+c);
+                }
+
+                this.setQuestion('How many kids?');
+                this.setAnswer(a,0);
+
+                this.mButtonA.setAnswer(a);
+                this.mButtonB.setAnswer(b);
+                this.mButtonC.setAnswer(c);
+                this.shuffle(10);
+        },
+
+        createQuestionShapes: function()
+        {
+                var x = 0;
+                var y = 300;
+
+                var answer = this.getAnswer();
+                answer = parseInt(answer);
+
+                for (var i = 0; i < answer; i++)
+                {
+                        if (i == 10)
+                        {
+                                x = 0;
+                                y = 375;
+                        }
+                        x = parseInt(x + 70); 
+                        this.addQuestionShape(new Shape(50,50,x,y,this.mSheet.mGame,"/images/bus/kid.png","",""));
+                }
+        }
+});
+
+
