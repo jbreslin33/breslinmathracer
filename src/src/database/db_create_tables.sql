868fe0544c7317fe5035f38dddcdc328e761fd0e
jbreslin33 <jbreslin33@localhost>
2014-07-05 18:55:06 -0400

    took out userid

--- src/src/database/db_create_tables.sql
+++ src/src/database/db_create_tables.sql
@@ -109,12 +109,10 @@ CREATE TABLE item_attempts (
         id SERIAL,
         start_time timestamp,
         end_time timestamp,
-        user_id integer NOT NULL,
 	levelattempts_id integer NOT NULL,
 	type_id integer DEFAULT 0 NOT NULL, -- 0 means no type identified
         transaction_code integer DEFAULT 0 NOT NULL, --were you correct?? 0 not answered yet   1 correct    2 incorrect
         PRIMARY KEY (id),
-	FOREIGN KEY (user_id) REFERENCES users(id),
 	FOREIGN KEY (levelattempts_id) REFERENCES levelattempts(id)
 );
 
