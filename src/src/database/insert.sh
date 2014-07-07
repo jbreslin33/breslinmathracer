a70b884c64974084caaaabcbdb5c61bab9ae41e2
jbreslin33 <jbreslin33@localhost>
2014-07-06 22:43:22 -0400

    edited scripts to point to new sql file

--- src/src/database/insert.sh
+++ src/src/database/insert.sh
@@ -1,1 +1,2 @@
-sudo -u postgres psql -d jamesanthonybreslin -f src/database/db_insert.sql
+echo sudo -u postgres psql -d jamesanthonybreslin -f src/database/insert.sql
+sudo -u postgres psql -d jamesanthonybreslin -f src/database/insert.sql
