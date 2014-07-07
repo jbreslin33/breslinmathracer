9261fe246627b73b333fa3ec4ffe04b5ce4edac0
jbreslin33 <jbreslin33@localhost>
2014-07-07 00:01:28 -0400

    added more inserts to db.

--- src/src/database/insert.sql
+++ src/src/database/insert.sql
@@ -99,30 +99,14 @@ insert into core_clusters(id,core_domains_subjects_grades_id,description) values
 insert into core_clusters(id,core_domains_subjects_grades_id,description) values (9,5,'Analyze, compare, create, and compose shapes.');
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---insert into core_clusters(id,descriptions,core_domain_id) values (1,'Know number names and the count sequence.',1); 
-
-
 --ELA
 --insert into core_standards (id,subject_id,description) values ('rl.k.1',2,'With prompting and support, ask and answer questions about key details in a text.');
 
 --MATH
---insert into core_standards (id,subject_id,description) values ('k.cc.a.1',1,'Count to 100 by ones and by tens.');
---insert into core_standards (id,subject_id,description) values ('k.cc.a.2',2,'Count forward beginning from a given number within the known sequence (instead of having to begin at 1).');
---insert into core_standards (id,subject_id,description) values ('k.cc.a.3',2,'Write numbers from 0 to 20. Represent a number of objects with a written numeral 0-20 (with 0 representing a count of no objects).');
+insert into core_standards (id,core_clusters_id,description) values ('k.cc.a.1',1,'Count to 100 by ones and by tens.');
+insert into core_standards (id,core_clusters_id,description) values ('k.cc.a.2',1,'Count forward beginning from a given number within the known sequence (instead of having to begin at 1).');
+insert into core_standards (id,core_clusters_id,description) values ('k.cc.a.3',1,'Write numbers from 0 to 20. Represent a number of objects with a written numeral 0-20 (with 0 representing a count of no objects).');
+
 --insert into core_standards (id,subject_id,description) values ('k.cc.b.4',2,'Understand the relationship between numbers and quantities; connect counting to cardinality.');
 --insert into core_standards (id,subject_id,description) values ('k.cc.b.4.a',2,'When counting objects, say the number names in the standard order, pairing each object with one and only one number name and each number name with one and only one object.');
 --insert into core_standards (id,subject_id,description) values ('g4.nbt.a.1',2,'Recognize that in a multi-digit whole number, a digit in one place represents ten times what it represents in the place to its right.');
