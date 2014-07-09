09d0fa2ddd2696bf4b627d167696d1d4f7b8c1b3
jbreslin33 <jbreslin33@localhost>
2014-07-09 13:57:33 -0400

    getting newLearningStandard.

--- src/web/login/check_login.php
+++ src/web/login/check_login.php
@@ -5,14 +5,14 @@ session_start();
 // If the user is not logged in send him/her to the login form
 if ($_SESSION["Login"] != "YES")
 {
-        if ($_SESSION["subject_id"] == 1)
-        {
+        //if ($_SESSION["subject_id"] == 1)
+        //{
                 header("Location: /web/login/login_form_math.php");
-        }
-        if ($_SESSION["subject_id"] == 2)
-        {
-                header("Location: /web/login/login_form_ela.php");
-        }
+        //}
+        //if ($_SESSION["subject_id"] == 2)
+        //{
+         //       header("Location: /web/login/login_form_ela.php");
+        //}
 }
 
 ?>
