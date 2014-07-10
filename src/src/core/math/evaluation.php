c05f8489ff19e25321e08eeb852faf37e85b6643
jbreslin33 <jbreslin33@localhost>
2014-07-10 17:29:02 -0400

    still works. took out some logs.

--- src/src/core/math/evaluation.php
+++ src/src/core/math/evaluation.php
@@ -38,35 +38,28 @@ Extends: Sheet,
         {
                 this.parent(game);
 		this.setScoreNeeded(APPLICATION.mItemTypeIDArray.length);
-		APPLICATION.log('scoreNeeded:' + APPLICATION.mItemTypeIDArray.length);
         },
 
         createItems: function()
         {
                 this.parent();
               
-		APPLICATION.log('length:' + APPLICATION.mItemTypeIDArray.length);
 		for (var i = 0; i < APPLICATION.mItemTypeIDArray.length; i++)
 		{	
-			APPLICATION.log('loop APPLICATION.mItemTypeIDArray[i]:' + APPLICATION.mItemTypeIDArray[i]);
 			if ( APPLICATION.mItemTypeIDArray[i] == "1")
 			{
-				APPLICATION.log('create item:1');
 				this.addItem(new i_k_cc_a_1_t_1(this));
 			}
 			if ( APPLICATION.mItemTypeIDArray[i] == "2")
 			{
-				APPLICATION.log('create item:2');
 				this.addItem(new i_k_cc_a_1_t_2(this));
 			}
 			if ( APPLICATION.mItemTypeIDArray[i] == "3")
 			{
-				APPLICATION.log('create item:3');
 				this.addItem(new i_k_cc_a_1_t_3(this));
 			}
 			if ( APPLICATION.mItemTypeIDArray[i] == "4")
 			{
-				APPLICATION.log('create item:4');
 				this.addItem(new i_k_cc_a_1_t_4(this));
 			}
 		}
