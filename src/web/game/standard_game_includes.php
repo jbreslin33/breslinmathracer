36c1f13563fcf26c5c06a2d4fc5395397df9bfee
jbreslin33 <jbreslin33@localhost>
2014-07-10 17:10:39 -0400

    bug is probably fixed. gonna run a few more times.

--- src/web/game/standard_game_includes.php
+++ src/web/game/standard_game_includes.php
@@ -9,6 +9,7 @@
 <script type="text/javascript" src="/src/application/states/application_states.php"></script>
 
 <script type="text/javascript" src="/src/game/game.php"></script>
+<script type="text/javascript" src="/src/core/math/game_sheet.php"></script>
 <script type="text/javascript" src="/src/game/quiz_game.php"></script>
 <script type="text/javascript" src="/src/game/states/states.php"></script>
 
