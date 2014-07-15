ca008d8ba8f8faacc1d4df6df3df44c9e4a18da0
jbreslin33 <jbreslin33@localhost>
2014-07-14 21:54:30 -0400

    adding login php class

--- src/web/login/login.php
+++ src/web/login/login.php
@@ -1,6 +1,7 @@
 <?php
 include(getenv("DOCUMENT_ROOT") . "/src/php/database_connection.php");
 include(getenv("DOCUMENT_ROOT") . "/src/php/sessions.php");
+include(getenv("DOCUMENT_ROOT") . "/src/php/login.php");
 
 //start new session
 session_start();
@@ -12,47 +13,9 @@ if ($_SESSION["subject_id"] == "")
         header("Location: /index.html");
 }
 
-//let's set a var that will be false if there was a problem..
-$problem = "";
-
 $_SESSION["password"] = $_POST["password"];
 $_SESSION["username"] = $_POST["username"];
 $_SESSION["school_id"] = "1";
-$_SESSION["school_name"] = "visitationbvm";
 
-$databaseConnection = new DatabaseConnection();
-$user_id = $databaseConnection->selectUserID($_SESSION["username"], $_SESSION["password"]);
-
-if ($user_id)
-{
-        //set sessions
-        $_SESSION["user_id"] = $user_id;
-        $_SESSION["Login"] = "YES";
-
-	//SESSION
-        $sessions = new Sessions();
-        $sessions->setSessionVariables();
-}
-else
-{
-        $_SESSION["Login"] = "NO";
-        $problem = "no_user";
-        $_SESSION["user_id"] = 0;
-}
-
-if ($problem == "")
-{
-        header("Location: /web/home/home.php");
-}
-else
-{
-        if ($_SESSION["subject_id"] == 1)
-        {
-                header("Location: login_form_math.php?message=$problem");
-        }
-        if ($_SESSION["subject_id"] == 2)
-        {
-                header("Location: login_form_ela.php?message=$problem");
-        }
-}
+$login = new Login();
 ?>
