b3fe34beb0ed31fa29382cae5aefb2aca87f0fa0
jbreslin33 <jbreslin33@localhost>
2014-07-14 23:10:46 -0400

    advance class works.

---
+++ src/src/database/advance.php
@@ -0,0 +1,27 @@
+<?php
+
+include(getenv("DOCUMENT_ROOT") . "/src/php/database_connection.php");
+include(getenv("DOCUMENT_ROOT") . "/src/php/advance.php");
+
+//start new session
+session_start();
+
+$advance = Advance();
+
+//fill php vars
+$returnString = "101,"; 
+$returnString .= $_SESSION["ref_id"];
+$returnString .= ",";
+$returnString .= $_SESSION["level"];
+$returnString .= ",";
+$returnString .= $_SESSION["standard"];
+$returnString .= ",";
+$returnString .= $_SESSION["progression"];
+$returnString .= ",";
+$returnString .= $_SESSION["levels"];
+$returnString .= ",";
+$returnString .= $_SESSION["item_type_id_raw_data"];
+echo $returnString;
+
+?>
+
