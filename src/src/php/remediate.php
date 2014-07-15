db8ee6009ba3b5b48244e029110fce5e7c53caf3
jbreslin33 <jbreslin33@localhost>
2014-07-15 17:37:53 -0400

    adding remediate

---
+++ src/src/php/remediate.php
@@ -0,0 +1,21 @@
+<?php
+
+class Remediate 
+{
+    private $mDatabaseConnection;
+
+function __construct()
+{
+	$this->mDatabaseConnection = new DatabaseConnection();
+	$this->process();
+}
+
+public function process()
+{
+
+}
+
+
+//end of class
+}
+?>
