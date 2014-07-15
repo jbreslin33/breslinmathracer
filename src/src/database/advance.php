02ef5c9b7f6934e277d54f6bd2cd401ae4fa1914
jbreslin33 <jbreslin33@localhost>
2014-07-15 19:11:39 -0400

    more debugging of php logs

--- src/src/database/advance.php
+++ src/src/database/advance.php
@@ -20,8 +20,8 @@ $returnString .= ",";
 $returnString .= $_SESSION["progression"];
 $returnString .= ",";
 $returnString .= $_SESSION["levels"];
-$returnString .= ",";
-$returnString .= $_SESSION["item_type_id_raw_data"];
+//$returnString .= ",";
+//$returnString .= $_SESSION["item_type_id_raw_data"];
 echo $returnString;
 
 ?>
