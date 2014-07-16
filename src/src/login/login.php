20af2f82b8e8af6ed703ab5168286ddcd744eaa0
jbreslin33 <jbreslin33@localhost>
2014-07-16 13:45:21 -0400

    adding lables.

--- src/src/login/login.php
+++ src/src/login/login.php
@@ -10,10 +10,18 @@ Extends: Game,
        		this.parent(application);
 
 		APPLICATION.log('login::constructor');	
+               
+		this.mUsernameLabel = new Shape(200,50,300,100,this,"","","");
+                this.mUsernameLabel.setText('Username:');
+                this.mShapeArray.push(this.mUsernameLabel);
 
                 this.mUsernameTextBox = new Shape(200,50,400,100,this,"INPUT","","");
                 this.mUsernameTextBox.mMesh.value = '';
                 this.mShapeArray.push(this.mUsernameTextBox);
+
+                this.mPasswordLabel = new Shape(200,50,300,165,this,"","","");
+                this.mPasswordLabel.setText('Password:');
+                this.mShapeArray.push(this.mPasswordLabel);
 
                 this.mPasswordTextBox = new Shape(200,50,400,165,this,"INPUT","","");
                 this.mPasswordTextBox.mMesh.value = '';
