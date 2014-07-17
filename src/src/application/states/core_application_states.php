d7bd322cb8f08b1da41f2f898917d55511a544bb
jbreslin33 <jbreslin33@localhost>
2014-07-17 14:23:24 -0400

    moved all middle men files to web/php and all php class files are in /src/php

--- src/src/application/states/core_application_states.php
+++ src/src/application/states/core_application_states.php
@@ -149,12 +149,6 @@ enter: function(application)
 execute: function(application)
 {
 	//you might not have a game if this your first time or you just went to a level that requires new game
-/*
-	if (application.mGame == 0)
-        {
-		application.mCoreStateMachine.changeState(application.mGET_LEVEL_DATA_APPLICATION);
-        }
-*/
 	if (application.mLevelCompleted)
 	{
 		application.mCoreStateMachine.changeState(application.mADVANCE_TO_NEXT_LEVEL_APPLICATION);
@@ -169,39 +163,6 @@ exit: function(application)
 {
 	application.mLevelCompleted = false;
 	application.mEvaluationFailed = false;
-}
-
-});
-
-var GET_LEVEL_DATA_APPLICATION = new Class(
-{
-Extends: State,
-
-initialize: function()
-{
-},
-
-enter: function(application)
-{
-	if (application.mStateLogs)
-	{
-		application.log('APPLICATION::GET_LEVEL_DATA_APPLICATION');
-	}
-	application.mWaitingOnLevelData = true;
-	application.getLevelData();
-},
-
-execute: function(application)
-{
-	if (!application.mWaitingOnLevelData)
-	{
-		application.mCoreStateMachine.changeState(application.mNORMAL_CORE_APPLICATION);
-	}
-},
-
-exit: function(application)
-{
-	application.mWaitingOnLevelData = false;
 }
 
 });
