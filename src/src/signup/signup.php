6d4eade082bf791bb10f52263f4d5e8c022a7112
jbreslin33 <jbreslin33@localhost>
2014-07-16 22:11:39 -0400

    you can now hit enter to tab to next textbox in signup.

--- src/src/signup/signup.php
+++ src/src/signup/signup.php
@@ -10,7 +10,8 @@ Extends: Game,
        		this.parent(application);
 
 		APPLICATION.log('signup::constructor');	
-		
+	
+		//username	
 		this.mUsernameLabel = new Shape(200,50,300,100,this,"","","");
                 this.mUsernameLabel.setText('Username:');
                 this.mShapeArray.push(this.mUsernameLabel);
@@ -19,7 +20,16 @@ Extends: Game,
                 this.mUsernameTextBox.mMesh.value = '';
                 this.mShapeArray.push(this.mUsernameTextBox);
 		this.mUsernameTextBox.mMesh.focus();	
-		
+                if (navigator.appName == "Microsoft Internet Explorer")
+                {
+                        this.mUsernameTextBox.mMesh.attachEvent('onkeypress',this.usernameTextBoxMicrosoftHit);
+                }
+                else
+                {
+                        this.mUsernameTextBox.mMesh.addEvent('keypress',this.usernameTextBoxFirefoxHit);
+                }
+
+		//password	
 		this.mPasswordLabel = new Shape(200,50,300,165,this,"","","");
                 this.mPasswordLabel.setText('Password:');
                 this.mShapeArray.push(this.mPasswordLabel);
@@ -27,7 +37,16 @@ Extends: Game,
                 this.mPasswordTextBox = new Shape(200,50,400,165,this,"INPUT","","");
                 this.mPasswordTextBox.mMesh.value = '';
                 this.mShapeArray.push(this.mPasswordTextBox);
-		
+                if (navigator.appName == "Microsoft Internet Explorer")
+                {
+                        this.mPasswordTextBox.mMesh.attachEvent('onkeypress',this.passwordTextBoxMicrosoftHit);
+                }
+                else
+                {
+                        this.mPasswordTextBox.mMesh.addEvent('keypress',this.passwordTextBoxFirefoxHit);
+                }
+
+		//firstName	
 		this.mFirstNameLabel = new Shape(200,50,300,230,this,"","","");
                 this.mFirstNameLabel.setText('First Name:');
                 this.mShapeArray.push(this.mFirstNameLabel);
@@ -35,7 +54,16 @@ Extends: Game,
   		this.mFirstNameTextBox = new Shape(200,50,400,230,this,"INPUT","","");
                 this.mFirstNameTextBox.mMesh.value = '';
                 this.mShapeArray.push(this.mFirstNameTextBox);
-		
+             	if (navigator.appName == "Microsoft Internet Explorer")
+                {
+                        this.mFirstNameTextBox.mMesh.attachEvent('onkeypress',this.firstNameTextBoxMicrosoftHit);
+                }
+                else
+                {
+                        this.mFirstNameTextBox.mMesh.addEvent('keypress',this.firstNameTextBoxFirefoxHit);
+                }
+	
+		//lastName	
 		this.mLastNameLabel = new Shape(200,50,300,295,this,"","","");
                 this.mLastNameLabel.setText('Last Name:');
                 this.mShapeArray.push(this.mLastNameLabel);
@@ -79,9 +107,59 @@ Extends: Game,
                 this.mShapeArray.push(this.mLoginButton);
                 this.mLoginButton.mMesh.innerHTML = 'Login';
 	},
+
+	//***tab to next
 	
+	//username
+	usernameTextBoxMicrosoftHit: function(e)
+        {
+                if (e.keyCode == 13)
+                {
+                        APPLICATION.mGame.mPasswordTextBox.mMesh.focus();
+                }
+        },
 
+        usernameTextBoxFirefoxHit: function(e)
+        {
+                if (e.key == 'enter')
+                {
+                        APPLICATION.mGame.mPasswordTextBox.mMesh.focus();
+                }
+        },
 
+	//password
+ 	passwordTextBoxMicrosoftHit: function(e)
+        {
+                if (e.keyCode == 13)
+                {
+                        APPLICATION.mGame.mFirstNameTextBox.mMesh.focus();
+                }
+        },
+
+        passwordTextBoxFirefoxHit: function(e)
+        {
+                if (e.key == 'enter')
+                {
+                        APPLICATION.mGame.mFirstNameTextBox.mMesh.focus();
+                }
+        },
+
+	//firstName
+        firstNameTextBoxMicrosoftHit: function(e)
+        {
+                if (e.keyCode == 13)
+                {
+                        APPLICATION.mGame.mLastNameTextBox.mMesh.focus();
+                }
+        },
+
+        firstNameTextBoxFirefoxHit: function(e)
+        {
+                if (e.key == 'enter')
+                {
+                        APPLICATION.mGame.mLastNameTextBox.mMesh.focus();
+                }
+        },
 
 	//sendSignup 
         lastNameTextBoxMicrosoftHit: function(e)
