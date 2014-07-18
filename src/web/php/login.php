f8b8720f57dc8592eeeb0b5b2887886d6cdd95ce
jbreslin33 <jbreslin33@localhost>
2014-07-18 16:02:49 -0400

    using full_return_string.php for everything except not logged in.

--- src/web/php/login.php
+++ src/web/php/login.php
@@ -9,27 +9,6 @@ $_SESSION["password"]   = $_GET["password"];
 
 $login = new Login();
 
-//fill php vars
-$returnString = "101,";
-$returnString .= $_SESSION["ref_id"];
-$returnString .= ",";
-$returnString .= $_SESSION["level"];
-$returnString .= ",";
-$returnString .= $_SESSION["standard"];
-$returnString .= ",";
-$returnString .= $_SESSION["progression"];
-$returnString .= ",";
-$returnString .= $_SESSION["levels"];
-$returnString .= ",";
-$returnString .= $_SESSION["LOGGED_IN"];
-$returnString .= ",";
-$returnString .= $_SESSION["username"];
-$returnString .= ",";
-$returnString .= $_SESSION["first_name"];
-$returnString .= ",";
-$returnString .= $_SESSION["last_name"];
-$returnString .= ",";
-$returnString .= $_SESSION["raw_data"];
-echo $returnString;
+include_once(getenv("DOCUMENT_ROOT") . "/web/php/full_return_string.php");
 
 ?>
--- src/web/php/signup.php
+++ src/web/php/signup.php
@@ -10,28 +10,5 @@ $_SESSION["first_name"] = $_GET["first_name"];
 $_SESSION["last_name"]  = $_GET["last_name"];
 
 $signup = new SignUp();
-
-//fill php vars
-$returnString = "101,";
-$returnString .= $_SESSION["ref_id"];
-$returnString .= ",";
-$returnString .= $_SESSION["level"];
-$returnString .= ",";
-$returnString .= $_SESSION["standard"];
-$returnString .= ",";
-$returnString .= $_SESSION["progression"];
-$returnString .= ",";
-$returnString .= $_SESSION["levels"];
-$returnString .= ",";
-$returnString .= $_SESSION["LOGGED_IN"];
-$returnString .= ",";
-$returnString .= $_SESSION["username"];
-$returnString .= ",";
-$returnString .= $_SESSION["first_name"];
-$returnString .= ",";
-$returnString .= $_SESSION["last_name"];
-$returnString .= ",";
-$returnString .= $_SESSION["raw_data"];
-echo $returnString;
-
+include_once(getenv("DOCUMENT_ROOT") . "/web/php/full_return_string.php");
 ?>
