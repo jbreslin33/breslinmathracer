e7bed4a6e6a47a66ce010880100943518472f7e2
jbreslin33 <jbreslin33@localhost>
2014-07-18 16:24:53 -0400

    using picker class for evaluation.

--- src/src/core/math/picker.php
+++ src/src/core/math/picker.php
@@ -5,9 +5,9 @@ this class will return an item if you  call getItem(1) and pass a typeid.
 var Picker = new Class(
 {
 
-initialize: function()
+initialize: function(sheet)
 {
-
+	this.mSheet = sheet;
 },
 
 getItem: function(id)
@@ -15,19 +15,19 @@ getItem: function(id)
 	id = parseInt(id);		
 	if (id == 1)
 	{
-		return new i_k_cc_a_1_t_1(this);
+		return new i_k_cc_a_1_t_1(this.mSheet);
 	}
 	if (id == 2)
 	{
-		return new i_k_cc_a_1_t_2(this);
+		return new i_k_cc_a_1_t_2(this.mSheet);
 	}
 	if (id == 3)
 	{
-		return new i_k_cc_a_1_t_3(this);
+		return new i_k_cc_a_1_t_3(this.mSheet);
 	}
 	if (id == 4)
 	{
-		return new i_k_cc_a_1_t_4(this);
+		return new i_k_cc_a_1_t_4(this.mSheet);
 	}
 }
 		
