2f8ea597892423aae93504f9039e5e58db052448
jbreslin33 <jbreslin33@localhost>
2014-07-18 16:02:49 -0400

    using full_return_string.php for everything except not logged in.

--- src/web/php/check_login.php
+++ src/web/php/check_login.php
@@ -6,27 +6,7 @@ session_start();
 //check if still LOGGED_IN on server
 if ( isset($_SESSION["LOGGED_IN"]) )
 {
-	$returnString = "101,";
-	$returnString .= $_SESSION["ref_id"];
-	$returnString .= ",";
-	$returnString .= $_SESSION["level"];
-	$returnString .= ",";
-	$returnString .= $_SESSION["standard"];
-	$returnString .= ",";
-	$returnString .= $_SESSION["progression"];
-	$returnString .= ",";
-	$returnString .= $_SESSION["levels"];
-	$returnString .= ",";
-	$returnString .= $_SESSION["LOGGED_IN"];
-	$returnString .= ",";
-	$returnString .= $_SESSION["username"];
-	$returnString .= ",";
-	$returnString .= $_SESSION["first_name"];
-	$returnString .= ",";
-	$returnString .= $_SESSION["last_name"];
-	$returnString .= ",";
-	$returnString .= $_SESSION["raw_data"];
-	echo $returnString;
+	include_once(getenv("DOCUMENT_ROOT") . "/web/php/full_return_string.php");
 }
 else
 {
---
+++ src/web/php/full_return_string.php
@@ -0,0 +1,24 @@
+<?php
+//fill php vars
+$returnString = "101,";
+$returnString .= $_SESSION["ref_id"];
+$returnString .= ",";
+$returnString .= $_SESSION["level"];
+$returnString .= ",";
+$returnString .= $_SESSION["standard"];
+$returnString .= ",";
+$returnString .= $_SESSION["progression"];
+$returnString .= ",";
+$returnString .= $_SESSION["levels"];
+$returnString .= ",";
+$returnString .= $_SESSION["LOGGED_IN"];
+$returnString .= ",";
+$returnString .= $_SESSION["username"];
+$returnString .= ",";
+$returnString .= $_SESSION["first_name"];
+$returnString .= ",";
+$returnString .= $_SESSION["last_name"];
+$returnString .= ",";
+$returnString .= $_SESSION["raw_data"];
+echo $returnString;
+?>
