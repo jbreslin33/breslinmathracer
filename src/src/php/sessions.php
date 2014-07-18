dc2b9e4b93d39bc649b161b3ff012723cf3b799e
jbreslin33 <jbreslin33@localhost>
2014-07-18 14:11:40 -0400

    using process function

--- src/src/php/sessions.php
+++ src/src/php/sessions.php
@@ -7,15 +7,11 @@ class Sessions
 function __construct()
 {
   	$this->mDatabaseConnection = new DatabaseConnection();
+	$this->process();
 }
 
-public function setSessionVariables()
+public function process()
 {
- 
-	$equery = "insert into error_log (error_time,error,username) values (CURRENT_TIMESTAMP,'setSessionVariables','');";
-        $eresult = pg_query($this->mDatabaseConnection->getConn(),$equery);
-
-
 	if (isset($_SESSION["subject_id"]) == false)
 	{
 		$_SESSION["subject_id"] = 1;
