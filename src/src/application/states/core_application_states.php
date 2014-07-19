d2c9fc35df5ff6b60406ba503fd1d3d47954e41d
jbreslin33 <jbreslin33@localhost>
2014-07-19 17:52:52 -0400

    remediate now just takes one parameter typeid

--- src/src/application/states/core_application_states.php
+++ src/src/application/states/core_application_states.php
@@ -244,8 +244,8 @@ enter: function(application)
         {
                 application.log('APPLICATION::REMEDIATE_APPLICATION');
         }
-        //tell db remediate student on standard they answered wrong
-        application.remediate(application.mGame.mSheet.getItem().mStandard,application.mGame.mSheet.getItem().mType);
+        //tell db remediate student on item type answered wrong
+        application.remediate(application.mGame.mSheet.getItem().mType);
 },
 
 execute: function(application)
