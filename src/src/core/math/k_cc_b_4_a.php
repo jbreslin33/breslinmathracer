579fadae2606cf8e0d69ce9665d309cb4c0acf7d
jbreslin33 <jbreslin33@localhost>
2014-07-21 22:06:06 -0400

    kid count is off on buttons.

--- src/src/core/math/k_cc_b_4_a.php
+++ src/src/core/math/k_cc_b_4_a.php
@@ -38,10 +38,10 @@ Extends: Sheet,
                         this.setScoreNeeded(APPLICATION.mLevel); 
                 }
 
-		this.addItem(new i_301(this));
-		this.addItem(new i_301(this));
-		this.addItem(new i_301(this));
-		this.addItem(new i_301(this));
+		this.addItem(new i_302(this));
+		this.addItem(new i_302(this));
+		this.addItem(new i_302(this));
+		this.addItem(new i_302(this));
 		
 		this.randomize(10);
         }
@@ -57,6 +57,7 @@ Extends: ThreeButtonItem,
         initialize: function(sheet)
         {
                 this.parent(sheet);
+
                 this.mType = 301;
 
 		var a = Math.floor(Math.random()*5)+5;
@@ -119,7 +120,7 @@ Extends: ThreeButtonItem,
 	}
 });
 
-/* TYPE_DESCRIPTION: Count the objects up to 20. Make answer zero. */
+/* TYPE_DESCRIPTION: When given counting words in order pick the group with that many items. */
 
 var i_302 = new Class(
 {
@@ -127,27 +128,135 @@ Extends: ThreeButtonItem,
         initialize: function(sheet)
         {
                 this.parent(sheet);
-                this.mType = 302;
+                
+		this.mType = 302;
+		
+		//button positions
 
-                var a = 0;
-                var b = 0;
-                var c = 0;
+                this.mButtonA.setPosition(100,200);
+                this.mButtonB.setPosition(380,200);
+                this.mButtonC.setPosition(675,200);
 
-                while (a == b || a == c || b == c || a < 0 || b < 0 || c < 0)
+                this.a = 0;
+                this.b = 0;
+                this.c = 0;
+                
+		while (this.a == this.b || this.a == this.c || this.b == this.c)
                 {
-                        b = Math.floor(Math.random()*7)-3;
-                        b = parseInt(a+b);
-                        c = Math.floor(Math.random()*7)-3;
-                        c = parseInt(a+c);
+                        this.a = Math.floor(Math.random()*11);
+                        this.b = Math.floor(Math.random()*11);
+                        this.c = Math.floor(Math.random()*11);
                 }
 
-                this.setQuestion('How many kids?');
-                this.setAnswer(a,0);
+		var mNumberNameArray = new Array();
+                mNumberNameArray.push('Zero');
+                mNumberNameArray.push('One');
+                mNumberNameArray.push('One, two');
+                mNumberNameArray.push('One, two, three');
+                mNumberNameArray.push('One, two, three, four');
+                mNumberNameArray.push('One, two, three, four, five');
+                mNumberNameArray.push('One, two, three, four, five, six');
+                mNumberNameArray.push('One, two, three, four, five, six, seven');
+                mNumberNameArray.push('One, two, three, four, five, six, seven, eight');
+                mNumberNameArray.push('One, two, three, four, five, six, seven, eight, nine');
+                mNumberNameArray.push('One, two, three, four, five, six, seven, eight, nine, ten');
+                
+		rightLetterIndex = Math.floor(Math.random()*3);
+		if (rightLetterIndex == 0)
+		{
+               		this.setQuestion('Lucy counted. ' + mNumberNameArray[this.a] + ' kids. Which group A,B or C shows the kids Lucy counted.');
+                	this.setAnswer('A',0);
+		}
+		if (rightLetterIndex == 1)
+		{
+               		this.setQuestion('Lucy counted. ' + mNumberNameArray[this.b] + ' kids. Which group A,B or C shows the kids Lucy counted.');
+                	this.setAnswer('B',0);
+		}
+		if (rightLetterIndex == 2)
+		{
+               		this.setQuestion('Lucy counted. ' + mNumberNameArray[this.c] + ' kids. Which group A,B or C shows the kids Lucy counted.');
+                	this.setAnswer('C',0);
+		}
+                this.mButtonA.setAnswer('A');
+                this.mButtonB.setAnswer('B');
+                this.mButtonC.setAnswer('C');
+        },
 
-                this.mButtonA.setAnswer(a);
-                this.mButtonB.setAnswer(b);
-                this.mButtonC.setAnswer(c);
-                this.shuffle(10);
+        createQuestionShapes: function()
+        {      
+                var x = 0;
+                var y = 250;
+
+		//a
+                for (var i = 0; i < this.a; i++)
+                {
+                        if (i == 4)
+                        {
+                                x = 0;
+                                y = 325;
+                        }
+                        if (i == 8)
+                        {
+                                x = 0;
+                                y = 325;
+                        }
+                        if (i == 12)
+                        {
+                                x = 0;
+                                y = 400;
+                        }
+                        x = parseInt(x + 50);  
+                        this.addQuestionShape(new Shape(50,50,x,y,this.mSheet.mGame,"/images/bus/kid.png","",""));
+                }
+                
+		var x = 280;
+                var y = 250;
+               
+		//b 
+                for (var i = 0; i < this.b; i++)
+                {
+                        if (i == 4)
+                        {
+                                x = 0;
+                                y = 325;
+                        }
+                        if (i == 8)
+                        {
+                                x = 0;
+                                y = 325;
+                        }
+                        if (i == 12)
+                        {
+                                x = 0;
+                                y = 400;
+                        }
+                        x = parseInt(x + 50); 
+                        this.addQuestionShape(new Shape(50,50,x,y,this.mSheet.mGame,"/images/bus/kid.png","",""));
+                }
+                var x = 575;
+                var y = 250;
+
+                //b
+                for (var i = 0; i < this.c; i++)
+                {
+                        if (i == 4)
+                        {
+                                x = 0;
+                                y = 325;
+                        }
+                        if (i == 8)
+                        {
+                                x = 0;
+                                y = 325;
+                        }
+                        if (i == 12)
+                        {
+                                x = 0;
+                                y = 400;
+                        }
+                        x = parseInt(x + 50);
+                        this.addQuestionShape(new Shape(50,50,x,y,this.mSheet.mGame,"/images/bus/kid.png","",""));
+                }
         }
 });
 / * ITEMS: */
