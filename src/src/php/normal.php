fe19700c05ae332fd47a02c0a759afefe9647eef
jbreslin33 <jbreslin33@localhost>
2014-07-23 17:19:19 -0400

    hardcoding raw_data in normal

--- src/src/php/normal.php
+++ src/src/php/normal.php
@@ -1,6 +1,5 @@
 <?php
 include_once(getenv("DOCUMENT_ROOT") . "/src/php/database_connection.php");
-include_once(getenv("DOCUMENT_ROOT") . "/src/php/advance.php");
 
 class Normal 
 {
@@ -9,134 +8,84 @@ class Normal
 function __construct()
 {
 	$this->mDatabaseConnection = new DatabaseConnection();
+
 	$this->process();
 }
 
 public function process()
 {
-        $nextID = 'normal';
+        $_SESSION["ref_id"] = 'normal';
 
+        //BEGIN NEW CODE
+        //right here you need to check the level of the ref_id you are about to send them to.
+        $selectLastLevelAttempt = "select level, transaction_code from levelattempts where user_id = ";
+        $selectLastLevelAttempt .= $_SESSION["user_id"];
+        $selectLastLevelAttempt .= " and learning_standards_id = '";
+        $selectLastLevelAttempt .= $_SESSION["ref_id"];
+        $selectLastLevelAttempt .= "' order by start_time desc limit 1;";
+
+       	$selectLastLevelAttemptResult = pg_query($this->mDatabaseConnection->getConn(),$selectLastLevelAttempt) or die('Could not connect: ' . pg_last_error());
+        $numLastLevelAttemptRows = pg_num_rows($selectLastLevelAttemptResult);
+
+        if ($numLastLevelAttemptRows > 0)
+        {
+        	$levelVar               = pg_Result($selectLastLevelAttemptResult, 0, 'level');
+        	$transaction_codeVar = pg_Result($selectLastLevelAttemptResult, 0, 'transaction_code');
+                $_SESSION["level"] = $levelVar;
+                
+		//passed
+                if ($transaction_codeVar == 1)
+                {
+                	$levelVar = (int) preg_replace('/[^0-9]/', '', $levelVar);
+               	        $levelVar++;
+                	$_SESSION["level"] = $level;
+                }
+                //failed
+                if ($transaction_codeVar == 2)
+                {
+                	if ($levelVar > 1)
+                        {
+                                $levelVar = (int) preg_replace('/[^0-9]/', '', $levelVar);
+                       	        $levelVar--;
+                		$_SESSION["level"] = $level;
+                        }
+                }
+        }
+        else
+        {
+        	$_SESSION["level"] = 1;
+        }
+        //END NEW CODE
+   
         //do the insert...
         $insert = "insert into levelattempts (start_time, user_id,level,learning_standards_id,transaction_code) VALUES (CURRENT_TIMESTAMP,";
         $insert .= $_SESSION["user_id"];
         $insert .= ",";
-        $insert .= 1;
-        $insert .= ",'";
-        $insert .= $nextID;
-        $insert .= "',3);";
+        $insert .= $_SESSION["level"];
+        $insert .= ",'normal',3);";
 
         $insertResult = pg_query($this->mDatabaseConnection->getConn(),$insert) or die('Could not connect: ' . pg_last_error());
+
         //update session vars with some hard coding
-        $_SESSION["level"] = 1;
-        $_SESSION["levels"] = 1;
-        $_SESSION["standard"] = 'normal';
-        $_SESSION["ref_id"] = 'normal';
+        $_SESSION["levels"] = 3; //normal is always 10 levels....
 
         $this->setRawData();
 }
-
+//you are not using user id in selects that is why it skipped eval....
 public function setRawData()
 {
-	//right here you need to query db to get rawdata for questions.
-	$progression_counter = 0;
-	
-	$itemArray = array();
-	$good = true;
-	while($good)	
-	{
-		$query = "select item_types_id, progression from item_attempts INNER JOIN item_types ON item_attempts.item_types_id=item_types.id JOIN levelattempts ON levelattempts.id=item_attempts.levelattempts_id where progression > ";
-		$query .= $progression_counter; 
-		$query .= " AND user_id = ";
-		$query .= $_SESSION["user_id"];
-		$query .= " order by progression asc limit 1;";
-	
-		$result = pg_query($this->mDatabaseConnection->getConn(),$query) or die('Could not connect: ' . pg_last_error());
-		
-		$num = pg_num_rows($result);
-        
-		if ($num > 0)
-        	{
-			//set the progression counter for the next loop thru
-               		$progression_counter = pg_Result($result, 0, 'progression');
-
-               		//get the id from user table to use in next query
-               		$item_types_id = pg_Result($result, 0, 'item_types_id');
-               
-			//now do another query with on just the item_type and user_id and just grab the last 10 by start_time	
-			$query2 = "select item_attempts.start_time, item_types_id, item_attempts.transaction_code, progression from item_attempts INNER JOIN item_types ON item_attempts.item_types_id=item_types.id JOIN levelattempts ON levelattempts.id=item_attempts.levelattempts_id where item_types_id = ";
-			$query2 .= $item_types_id;
-			$query2 .= " AND user_id = ";
-			$query2 .= $_SESSION["user_id"];
-			$query2 .= " order by start_time asc limit 10;";		
-			
-			$result2 = pg_query($this->mDatabaseConnection->getConn(),$query2) or die('Could not connect: ' . pg_last_error());
-	
-			$num2 = pg_num_rows($result2);
-
-			//if we have 10 lets check if we mastered all ten
-			if ($num2 == 10) 
-			{
-				$masteredAllTen = true;		
-				
-				for ($i = 0; $i < $num2; $i++)
-				{	
-               				$transaction_code = pg_Result($result2, 0, 'transaction_code');
-					if ($transaction_code != 1)
-					{
-						$masteredAllTen = false;		
-					} 	
-				}
-				if (!$masteredAllTen)
-				{
-					array_push($itemArray,"$item_types_id");
-				}
-			}
-			//less than 10 so we could not have mastered 10 so just add it to array.
-			else 
-			{
-				array_push($itemArray,"$item_types_id");
-			}	
-		}	
-		else
-		{
-			$good = false;
-		}
-	}
-	
 	$itemString = "";
-
-	if (count($itemArray) > 0)
-	{
-		$itemString .= $itemArray[0];
-		if (count($itemArray) > 1)
-		{		
-			$itemString .= ":";
-		}
-	}
-
-	$totalCount = count($itemArray);
-
-	for ($i = 1; $i < $totalCount; $i++)
-	{
-		$itemString .= $itemArray[$i];
-		if ($i < $totalCount - 1)
-		{ 
-			$itemString .= ":";
-		}
-	} 
+	$itemString .= $this->mTypeID;
+	$itemString .= ":";
 	
        	$_SESSION["raw_data"] = $itemString; 
        	$rawData = $_SESSION["raw_data"]; 
 
+	//temp hard code...
+       	$_SESSION["raw_data"] = "1:2:3:4:101:102:103:104:201:202"; 
+
   	$equery = "insert into error_log (error_time,error,username) values (CURRENT_TIMESTAMP,'$rawData','rawData');";
   	$eresult = pg_query($this->mDatabaseConnection->getConn(),$equery);
-  
-	//if you have no questions say that you did a normal and send back thru setLevelSessionVariablesAdvance
-	if (count($itemArray) < 1)
-	{
-       		$_SESSION["ref_id"] = 'normal'; 
-		$advance = new Advance();	
-	}
 }
 //end of class
 }
