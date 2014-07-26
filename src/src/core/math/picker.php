0fcc460d74f65c8b216c60ddbb25bb5fe1703989
jbreslin33 <jbreslin33@localhost>
2014-07-26 10:10:01 -0400

    adding new picking format. Each developer has their own picker file and the there is a generic picker one too. Also renamed developer item files and added a generic one as well.

--- src/src/core/math/picker.php
+++ src/src/core/math/picker.php
@@ -1,5 +1,5 @@
 /* PICKER: 
-this class will return an item if you  call getItem(1) and pass a typeid.
+this class will return an item or 0 if you  call getItem(1) and pass a typeid.
 */ 
 
 var Picker = new Class(
@@ -62,42 +62,7 @@ getItem: function(id)
 		return new i_k_cc_a_3__4(this.mSheet);
 	}
 
-	//k.cc.b.5
-	if (id == 'k.cc.b.5_1')
-	{
-		return new i_k_cc_b_5__1(this.mSheet);
-	}
-	if (id == 'k.cc.b.5_2')
-	{
-		return new i_k_cc_b_5__2(this.mSheet);
-	}
-	if (id == 'k.cc.b.5_3')
-	{
-		return new i_k_cc_b_5__3(this.mSheet);
-	}
-	if (id == 'k.cc.b.5_4')
-	{
-		return new i_k_cc_b_5__4(this.mSheet);
-	}
-
-	//k.cc.c.6
-	if (id == 'k.cc.c.6_1')
-	{
-		return new i_k_cc_c_6__1(this.mSheet);
-	}
-	
-	//k.cc.c.7
-	if (id == 'k.cc.c.7_1')
-	{
-		return new i_k_cc_c_7__1(this.mSheet);
-	}
-	
-	//4.nbt.a.2
-	if (id == '4.nbt.a.2_1')
-	{
-		return new i_4_nbt_a_2_1(this.mSheet);
-	}
-	
+	return 0;	
 }
 		
 });
