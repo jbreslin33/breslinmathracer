ae5fb03226947b0cd8a0f538f0df277cb58d549e
jbreslin33 <jbreslin33@localhost>
2014-07-27 22:23:54 -0400

    after succesful advancement in practice you now go back to previous learning_standard_attempt that was not practice.

--- src/web/php/practice.php
+++ src/web/php/practice.php
@@ -7,7 +7,7 @@ session_start();
 $typeid = $_GET["typeid"];
 
 //start new so pass 1 as first paremeter. pass the type id sent from client.
-$practice = new Practice(1,$typeid);
+$practice = new Practice(1,$typeid,0);
 
 include_once(getenv("DOCUMENT_ROOT") . "/web/php/full_return_string.php");
 ?>
