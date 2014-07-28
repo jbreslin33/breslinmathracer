e664d228a0061085c891a436e8d28aa042461873
jbreslin33 <jbreslin33@localhost>
2014-07-27 21:56:14 -0400

    getting to practice but its not adancing levels.

---
+++ src/web/php/practice.php
@@ -0,0 +1,13 @@
+<?php
+include(getenv("DOCUMENT_ROOT") . "/src/php/practice.php");
+
+//start new session
+session_start();
+
+$typeid = $_GET["typeid"];
+
+//start new so pass 1 as first paremeter. pass the type id sent from client.
+$practice = new Practice(1,$typeid);
+
+include_once(getenv("DOCUMENT_ROOT") . "/web/php/full_return_string.php");
+?>
