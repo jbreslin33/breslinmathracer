cdf12f7ecabd8b090faf8e4ed6b9340b7aae16c3
bbreslin99 <bbreslin99@localhost>
2014-07-29 07:06:05 -0400

    added insert_brian.sql

---
+++ src/src/database/insert_brian.sql
@@ -0,0 +1,4 @@
+insert into core_standards (id,core_clusters_id,description) values ('4.nbt.a.2',1,'Read and write multi-digit whole numbers using base-ten numerals, number names, and expanded form. Compare two multi-digit numbers based on meanings of the digits in each place, using >, =, and < symbols to record the results of comparisons.');
+
+--4.nbt.a.2
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.2_1',99991,'4.nbt.a.2','This type will give the name and ask for number');
