e3d07a54cecd4c47e8457948378db6d3d487c550
jbreslin33 <jbreslin33@localhost>
2014-07-29 21:49:44 -0400

    type8

--- src/src/core/math/items_jim.php
+++ src/src/core/math/items_jim.php
@@ -1,5 +1,45 @@
 /*  5.oa.a.1 */
 
+/* TYPE_DESCRIPTION: x - (x+y) */
+var i_5_oa_a_1__8 = new Class(
+{
+Extends: ThreeButtonItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet);
+
+                this.mType = '5.oa.a.1_8';
+
+                var a = '';
+                var b = '';
+                var c = '';
+
+                var x = 0;
+                var y = 0;
+                var z = 0;
+
+                while (a == b || a == c || b == c || a < 0 || b < 0 || c < 0)
+                {
+                        x = Math.floor(Math.random()*10);
+                        y = Math.floor(Math.random()*10);
+                        z = Math.floor(Math.random()*10);
+                        c = parseInt(x - ( y + z));
+
+                        a = Math.floor((Math.random()*5)+parseInt(c-3));
+                        b = Math.floor((Math.random()*5)+parseInt(c-3));
+                }
+
+                this.setQuestion( x + ' - (' + y + ' + ' + z + ')' );
+                this.setAnswer(c,0);
+
+                this.mButtonA.setAnswer(a);
+                this.mButtonB.setAnswer(b);
+                this.mButtonC.setAnswer(c);
+                this.shuffle(10);
+        }
+});
+
+
 /* TYPE_DESCRIPTION: (x+y) - z */
 var i_5_oa_a_1__7 = new Class(
 {
@@ -38,7 +78,6 @@ Extends: ThreeButtonItem,
                 this.shuffle(10);
         }
 });
-
 
 /* TYPE_DESCRIPTION: x/(y+z) */
 var i_5_oa_a_1__6 = new Class(
