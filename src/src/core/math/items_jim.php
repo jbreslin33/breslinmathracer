c0eb330400f9932e26093a9a3c9dbc1e9267a349
jbreslin33 <jbreslin33@localhost>
2014-07-29 22:07:05 -0400

    5.oa.a.2 rfm

--- src/src/core/math/items_jim.php
+++ src/src/core/math/items_jim.php
@@ -1,3 +1,45 @@
+/*  5.oa.a.2 */
+
+/* TYPE_DESCRIPTION: Match equations to number sentences  */
+var i_5_oa_a_2__1 = new Class(
+{
+Extends: ThreeButtonItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet);
+
+                this.mType = '5.oa.a.2_1';
+
+                var a = '';
+                var b = '';
+                var c = '';
+
+                var x = 0;
+                var y = 0;
+                var z = 0;
+
+                while (a == b || a == c || b == c || y + z > 10)
+                {
+                        x = Math.floor(Math.random()*10);
+                        y = Math.floor(Math.random()*10);
+                        z = Math.floor(Math.random()*10);
+                        c = parseInt( x * (y + z ));
+
+                        a = Math.floor((Math.random()*5)+parseInt(c-3));
+                        b = Math.floor((Math.random()*5)+parseInt(c-3));
+                }
+
+                this.setQuestion('hello ' + x + '(' + y + ' + ' + z + ')' );
+                this.setAnswer(c,0);
+
+                this.mButtonA.setAnswer(a);
+                this.mButtonB.setAnswer(b);
+                this.mButtonC.setAnswer(c);
+                this.shuffle(10);
+        }
+});
+
+
 /*  5.oa.a.1 */
 
 /* TYPE_DESCRIPTION: x - (x+y) */
--- src/src/database/insert_jim.sql
+++ src/src/database/insert_jim.sql
@@ -9,3 +9,16 @@ insert into item_types(id,progression,core_standards_id,description) values ('5.
 insert into item_types(id,progression,core_standards_id,description) values ('5.oa.a.1_8',5007,'5.oa.a.1','');
 insert into item_types(id,progression,core_standards_id,description) values ('5.oa.a.1_9',5008,'5.oa.a.1','');
 insert into item_types(id,progression,core_standards_id,description) values ('5.oa.a.1_10',5009,'5.oa.a.1','');
+
+insert into core_standards (id,core_clusters_id,description) values ('5.oa.a.2',1,'Write simple expressions that record calculations with numbers, and interpret numerical expressions without evaluating them. For example, express the calculation "add 8 and 7, then multiply by 2" as 2 × (8 + 7). Recognize that 3 × (18932 + 921) is three times as large as 18932 + 921, without having to calculate the indicated sum or product.');
+
+insert into item_types(id,progression,core_standards_id,description) values ('5.oa.a.2_1',5201,'5.oa.a.2','');
+insert into item_types(id,progression,core_standards_id,description) values ('5.oa.a.2_2',5202,'5.oa.a.2','');
+insert into item_types(id,progression,core_standards_id,description) values ('5.oa.a.2_3',5203,'5.oa.a.2','');
+insert into item_types(id,progression,core_standards_id,description) values ('5.oa.a.2_4',5204,'5.oa.a.2','');
+insert into item_types(id,progression,core_standards_id,description) values ('5.oa.a.2_5',5205,'5.oa.a.2','');
+insert into item_types(id,progression,core_standards_id,description) values ('5.oa.a.2_6',5206,'5.oa.a.2','');
+insert into item_types(id,progression,core_standards_id,description) values ('5.oa.a.2_7',5207,'5.oa.a.2','');
+insert into item_types(id,progression,core_standards_id,description) values ('5.oa.a.2_8',5208,'5.oa.a.2','');
+insert into item_types(id,progression,core_standards_id,description) values ('5.oa.a.2_9',5209,'5.oa.a.2','');
+insert into item_types(id,progression,core_standards_id,description) values ('5.oa.a.2_10',5210,'5.oa.a.2','');
