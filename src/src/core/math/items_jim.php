f82a0b8421a386167614aedf1b1cb2620b3cc1aa
jbreslin33 <jbreslin33@localhost>
2014-07-29 21:20:17 -0400

    type 5

--- src/src/core/math/items_jim.php
+++ src/src/core/math/items_jim.php
@@ -1,5 +1,48 @@
 /*  5.oa.a.1 */
 
+/* TYPE_DESCRIPTION: x/(y+z) */
+var i_5_oa_a_1__5 = new Class(
+{
+Extends: ThreeButtonItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet);
+
+                this.mType = '5.oa.a.1_5';
+
+                var a = '';
+                var b = '';
+                var c = '';
+
+                var x = 0;
+                var y = 0;
+                var z = 0;
+
+                while (a == b || a == c || b == c || c < 0)
+                {
+                        y = Math.floor(Math.random()*10);
+                        z = Math.floor(Math.random()*10);
+			
+			missingFactor = Math.floor(Math.random()*10); 
+			x = missingFactor * (y+z);
+
+                        c = parseInt( x / (y + z ));
+
+                        a = Math.floor((Math.random()*5)+parseInt(c-3));
+                        b = Math.floor((Math.random()*5)+parseInt(c-3));
+                }
+
+                this.setQuestion( x + '/(' + y + ' + ' + z + ')' );
+                this.setAnswer(c,0);
+
+                this.mButtonA.setAnswer(a);
+                this.mButtonB.setAnswer(b);
+                this.mButtonC.setAnswer(c);
+                this.shuffle(10);
+        }
+});
+
+
 /* TYPE_DESCRIPTION: x(y+z) */
 var i_5_oa_a_1__4 = new Class(
 {
