442b614de1bf6e6285da4843189beb79ef1de2cc
jbreslin33 <jbreslin33@localhost>
2014-07-29 20:23:06 -0400

    dumped some more stuff in insert.sql

--- src/src/database/insert.sql
+++ src/src/database/insert.sql
@@ -163,3 +163,10 @@ insert into item_types(id,progression,core_standards_id,description) values ('k.
 
 --k.cc.c.7
 insert into item_types(id,progression,core_standards_id,description) values ('k.cc.c.7_1',801,'k.cc.c.7','Compare 2 numbers from 1-10 with greater than, equal to or less than.');
+
+--k.oa.a.1
+insert into core_standards (id,core_clusters_id,description) values ('k.oa.a.1',1,'Represent addition and subtraction with objects, fingers, mental images, drawings1, sounds (e.g., claps), acting out situations, verbal explanations, expressions, or equations.');
+insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.1_1',10,'k.oa.a.1','Add within 5 with pictures to help.');
+insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.1_2',11,'k.oa.a.1','Subtract within 5 with pictures to help.');
+insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.1_3',12,'k.oa.a.1','Pick the number sentence that represents the addition word problem.');
+insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.1_4',13,'k.oa.a.1','Pick the number sentence that represents the subtraction word problem.');
