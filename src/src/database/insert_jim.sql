0d1fd79aafac4bf918741207d9f602649f5a4056
jbreslin33 <jbreslin33@localhost>
2014-07-29 20:23:06 -0400

    dumped some more stuff in insert.sql

--- src/src/database/insert_jim.sql
+++ src/src/database/insert_jim.sql
@@ -1,5 +1,2 @@
-insert into core_standards (id,core_clusters_id,description) values ('k.oa.a.1',1,'Represent addition and subtraction with objects, fingers, mental images, drawings1, sounds (e.g., claps), acting out situations, verbal explanations, expressions, or equations.');
-insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.1_1',10,'k.oa.a.1','Add within 5 with pictures to help.');
-insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.1_2',11,'k.oa.a.1','Subtract within 5 with pictures to help.');
-insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.1_3',12,'k.oa.a.1','Pick the number sentence that represents the addition word problem.');
-insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.1_4',13,'k.oa.a.1','Pick the number sentence that represents the subtraction word problem.');
+insert into core_standards (id,core_clusters_id,description) values ('5.oa.a.1',1,'Use parentheses, brackets, or braces in numerical expressions, and evaluate expressions with these symbols.');
+insert into item_types(id,progression,core_standards_id,description) values ('5.oa.a.1_1',5000,'5.oa.a.1','');
