130757a777b70024ae0c0ff6897c9dcda12928df
jbreslin33 <jbreslin33@localhost>
2014-07-30 23:42:24 -0400

    moving picker stuff

--- src/src/core/math/picker.php
+++ src/src/core/math/picker.php
@@ -63,6 +63,39 @@ getItem: function(id)
 		return new i_k_cc_a_3__4(this.mSheet);
 	}
 
+	//k.cc.b.4.a 
+	//MISSING
+
+	//k.cc.b.4.b 
+	//MISSING
+
+	//k.cc.b.4.c 
+	//MISSING
+ 
+	//k.cc.b.5
+        if (id == 'k.cc.b.5_1')
+        {
+                return new i_k_cc_b_5__1(this.mSheet);
+        }
+        if (id == 'k.cc.b.5_2')
+        {
+                return new i_k_cc_b_5__2(this.mSheet);
+        }
+        if (id == 'k.cc.b.5_3')
+        {
+                return new i_k_cc_b_5__3(this.mSheet);
+        }
+        if (id == 'k.cc.b.5_4')
+        {
+                return new i_k_cc_b_5__4(this.mSheet);
+        }
+
+        //k.cc.c.6
+        if (id == 'k.cc.c.6_1')
+        {
+                return new i_k_cc_c_6__1(this.mSheet);
+        }
+
 	return 0;	
 }
 		
