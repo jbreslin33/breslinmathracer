503416817fad7ee11041ef266dada6bc2023284f
jbreslin33 <jbreslin33@localhost>
2014-07-31 15:46:05 -0400

    javascript for k.cc.c.7 done.

--- src/src/core/math/k_cc_c_7.php
+++ src/src/core/math/k_cc_c_7.php
@@ -42,14 +42,14 @@ Extends: ThreeButtonItem,
 	}
 });
 
-/* TYPE_DESCRIPTION: Greater than for 2 numbers up to 10.*/
-var i_k_cc_c_7__4 = new Class(
+/* TYPE_DESCRIPTION: Equal to for 2 numbers up to 10.*/
+var i_k_cc_c_7__2 = new Class(
 {
 Extends: ThreeButtonItem,
         initialize: function(sheet)
         {
                 this.parent(sheet);
-                this.mType = 'k.cc.c.7_4';
+                this.mType = 'k.cc.c.7_2';
 
 		//BUTTON A
                 this.mButtonA.setPosition(380,100);
@@ -57,21 +57,54 @@ Extends: ThreeButtonItem,
                 this.mButtonC.setPosition(380,300);
 
 		this.a = Math.floor(Math.random()*10+1);
-		this.b = Math.floor(Math.random()*10+1);
+		this.b = this.a;		
 
 		this.setQuestion('Compare.');
-		if (this.a > this.b)
-		{
-                	this.setAnswer('Is greater than.',0);
+                this.setAnswer('Is equal to.',0);
+
+                this.mButtonA.setAnswer('Is greater than.');
+                this.mButtonB.setAnswer('Is equal to.');
+                this.mButtonC.setAnswer('Is less than.');
+        },
+    	
+	createQuestionShapes: function()
+	{	
+		var shapeA = new Shape(50,50,240,200,this.mSheet.mGame,"","","");
+		var shapeB = new Shape(50,50,530,200,this.mSheet.mGame,"","","");
+	
+		shapeA.setText(this.a);
+		shapeB.setText(this.b);
+
+		this.addQuestionShape(shapeA);	
+		this.addQuestionShape(shapeB);	
+	}
+});
+
+/* TYPE_DESCRIPTION: Less than for 2 numbers up to 10.*/
+var i_k_cc_c_7__3 = new Class(
+{
+Extends: ThreeButtonItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet);
+                this.mType = 'k.cc.c.7_3';
+
+		//BUTTON A
+                this.mButtonA.setPosition(380,100);
+                this.mButtonB.setPosition(380,200);
+                this.mButtonC.setPosition(380,300);
+
+		this.a = 0;		
+		this.b = 0;		
+
+		while (this.a >= this.b)
+		{	
+			this.a = Math.floor(Math.random()*10+1);
+			this.b = Math.floor(Math.random()*10+1);
 		}
-		if (this.a == this.b)
-		{
-                	this.setAnswer('Is equal to.',0);
- 		}
-		if (this.a < this.b)
-		{
-                	this.setAnswer('Is less than.',0);
-		}
+
+		this.setQuestion('Compare.');
+                this.setAnswer('Is less than.',0);
 
                 this.mButtonA.setAnswer('Is greater than.');
                 this.mButtonB.setAnswer('Is equal to.');
