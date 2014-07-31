fdba9124190f2eefa78caf95239578b5ae499873
jbreslin33 <jbreslin33@localhost>
2014-07-31 18:39:44 -0400

    beginning or word problem.

--- src/src/core/math/k_oa_a_2.php
+++ src/src/core/math/k_oa_a_2.php
@@ -1,4 +1,55 @@
 
+/* TYPE_DESCRIPTION: Addition word problems with sum within 10. */
+var i_k_oa_a_2__3 = new Class(
+{
+Extends: ThreeButtonItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet);
+
+                this.mType = 'k.oa.a.2_3';
+
+		this.mNameMachine = new NameMachine();
+		this.mNameOne = this.mNameMachine.getName();
+		this.mNameTwo = this.mNameMachine.getName();
+             
+		this.a = 0;
+		this.b = 0;
+		this.c = 1000;
+
+		this.x = 0;	 
+		this.y = 0;	 
+
+		while (this.a == this.b || this.a == this.c || this.b == this.c || this.c > 10 || this.a < 0 || this.b < 0)
+		{
+			//variables
+                	this.x = Math.floor(Math.random()*11);
+                	this.y = Math.floor(Math.random()*11);
+			this.c = parseInt(this.x + this.y);  
+	
+			//wrong answers 
+			this.a = Math.floor(Math.random()*5)+parseInt(this.c-3);
+			this.b = Math.floor(Math.random()*5)+parseInt(this.c-3);
+                }
+
+                this.setAnswer(parseInt(this.c),0);
+
+                this.mButtonA.setAnswer(this.a);
+                this.mButtonB.setAnswer(this.b);
+                this.mButtonC.setAnswer(this.c);
+
+
+                var roll = Math.floor(Math.random()*11);
+		roll = 0;
+		if (roll == 0)
+		{
+                	this.setQuestion(this.mNameOne + ' had ' + this.x + ' + ' + this.y + ' =');
+		}
+
+
+                this.shuffle(10);
+        }
+});
 
 /* TYPE_DESCRIPTION: Subtract with sum and minuend within 10. */
 var i_k_oa_a_2__2 = new Class(
