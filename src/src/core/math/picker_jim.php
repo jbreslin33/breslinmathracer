4f94fb6ec08a28611c922b2376b90fa4a97a3fe8
jbreslin33 <jbreslin33@localhost>
2014-08-01 00:04:53 -0400

    k.oa.a.5 rfm

--- src/src/core/math/picker_jim.php
+++ src/src/core/math/picker_jim.php
@@ -40,6 +40,12 @@ getItem: function(id)
         {
                 return new i_k_oa_a_4__1(this.mSheet);
         }
+	
+	//k.oa.a.5
+        if (id == 'k.oa.a.5_1')
+        {
+                return new i_k_oa_a_5__1(this.mSheet);
+        }
 
 
 	/*** GRADE 5 ***/
