db85f43c0aa3f5341c9f62208da983d3451167f0
bbreslin99 <bbreslin99@localhost>
2014-08-03 09:27:57 -0400

    fixed a few things

--- src/src/core/math/items_brian.php
+++ src/src/core/math/items_brian.php
@@ -2,9 +2,9 @@
 //include(getenv("DOCUMENT_ROOT") . "/web/php/check_login.php");
 include(getenv("DOCUMENT_ROOT") . "/src/core/math/g4_nbt_a_1.php");
 include(getenv("DOCUMENT_ROOT") . "/src/core/math/g4_nbt_a_2.php");
-include(getenv("DOCUMENT_ROOT") . "/src/core/math/4_nbt_b_4.php");
-include(getenv("DOCUMENT_ROOT") . "/src/core/math/4_nbt_b_5.php");
-include(getenv("DOCUMENT_ROOT") . "/src/core/math/4_md_a_3.php");
+include(getenv("DOCUMENT_ROOT") . "/src/core/math/g4_nbt_b_4.php");
+include(getenv("DOCUMENT_ROOT") . "/src/core/math/g4_nbt_b_5.php");
+include(getenv("DOCUMENT_ROOT") . "/src/core/math/g4_md_a_3.php");
 ?>
 
 
