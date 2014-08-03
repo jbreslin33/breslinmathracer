8534dbc191a03177a518b6e065a0052ebd260bab
jbreslin33 <jbreslin33@localhost>
2014-08-03 19:10:51 -0400

    type6 c(a)(b) done.

--- src/src/core/math/g5_oa_a_1.php
+++ src/src/core/math/g5_oa_a_1.php
@@ -42,6 +42,44 @@ this might be toughest i need to give maybe through in one with braces and brack
 
 
 ********/
+/* TYPE_DESCRIPTION: c(a)(b) */
+var i_5_oa_a_1__6 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+
+        this.mType = '5.oa.a.1_6';
+        this.mQuestionLabel.setSize(225,50);
+        this.mQuestionLabel.setPosition(250,95);
+
+        var x = 0;
+        var r = 1;
+
+        while (x < 1 || r != 0)
+        {
+                var a1 = Math.floor(Math.random()*8)+2;
+                var a2 = Math.floor(Math.random()*8)+2;
+
+                var b1 = Math.floor((Math.random()*5)+1);
+                var b2 = Math.floor((Math.random()*5)+1);
+
+                var c1 = Math.floor((Math.random()*5)+16);
+                var c2 = Math.floor((Math.random()*5)+10);
+
+                var a12 = parseInt(  a1 * a2 );
+                var b12 = parseInt(  b1 + b2 );
+                r = a12 % b12;
+
+                x = parseInt( c1 - c2 + (a1 * a2) / (  b1 + b2 )  );
+
+                this.setQuestion( c1 + ' - ' + c2 + ' + ' + '(' + a1 + ' x ' + a2 + ') / (' + b1 + ' + ' + b2 + ')' );
+                this.setAnswer(x,0);
+        }
+}
+});
 /* TYPE_DESCRIPTION: (a)(b) */
 var i_5_oa_a_1__5 = new Class(
 {
