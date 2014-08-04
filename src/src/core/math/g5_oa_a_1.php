eee82174c7124e0aa2304e3ec3dbce353397cc99
jbreslin33 <jbreslin33@localhost>
2014-08-04 12:39:52 -0400

    added to more types to db ofr 5.oa.a.1

--- src/src/core/math/g5_oa_a_1.php
+++ src/src/core/math/g5_oa_a_1.php
@@ -43,6 +43,47 @@ this might be toughest i need to give maybe through in one with braces and brack
 
 ********/
 /* TYPE_DESCRIPTION: c(a)d(b) */
+var i_5_oa_a_1__10 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+
+        this.mType = '5.oa.a.1_10';
+        this.mQuestionLabel.setSize(325,50);
+        this.mQuestionLabel.setPosition(200,95);
+	this.mAnswerTextBox.setPosition(525,100);
+
+        var x = 0;
+        var r = 1;
+
+        while (x < 1 || r != 0)
+        {
+                var a1 = Math.floor(Math.random()*8)+2;
+                var a2 = Math.floor(Math.random()*8)+2;
+
+                var b1 = Math.floor((Math.random()*5)+1);
+                var b2 = Math.floor((Math.random()*5)+1);
+
+                var c1 = Math.floor((Math.random()*10)+1);
+                var c2 = Math.floor((Math.random()*10)+1);
+                
+		var d1 = Math.floor((Math.random()*5)+1);
+                var d2 = Math.floor((Math.random()*5)+1);
+
+                r = c1 % c2;
+                
+                x = parseInt( c1 - c2 * (a1 + a2) - d1 + d2 * (b1 + b2) + c1 / c2 );
+
+                this.setQuestion('h' +  c1 + ' - ' + c2 + ' (' + a1 + ' + ' + a2 + ') - ' +  d1 + ' + ' + d2 + ' (' + b1 + ' + ' + b2 + ') + ' + c1 + ' / ' + c2  );
+                this.setAnswer(x,0);
+        }
+}
+});
+
+/* TYPE_DESCRIPTION: c(a)d(b) */
 var i_5_oa_a_1__9 = new Class(
 {
 Extends: TextItem,
