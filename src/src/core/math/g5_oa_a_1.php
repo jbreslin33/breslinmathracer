b15e760b78827c22758727e61828b287c4df60c5
jbreslin33 <jbreslin33@localhost>
2014-08-06 14:34:12 -0400

    type17 done.

--- src/src/core/math/g5_oa_a_1.php
+++ src/src/core/math/g5_oa_a_1.php
@@ -47,6 +47,42 @@ this might be toughest i need to give maybe through in one with braces and brack
 
 ********/
 /* TYPE_DESCRIPTION:  */
+	//17: d(b(a)c) 
+var i_5_oa_a_1__17 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+
+        this.mType = '5.oa.a.1_17';
+        this.mQuestionLabel.setSize(325,50);
+        this.mQuestionLabel.setPosition(200,95);
+	this.mAnswerTextBox.setPosition(525,100);
+
+        var x = 0;
+	while (x < 1)
+        {
+		//17: d(b(a)c) 
+                var a1 = Math.floor(Math.random()*5)+1;
+                var a2 = Math.floor(Math.random()*5)+1;
+
+                var b1 = Math.floor((Math.random()*8)+2);
+
+                var c1 = Math.floor((Math.random()*8)+2);
+                
+		var d1 = Math.floor((Math.random()*50)+50);
+
+                x = parseInt(   d1 - (b1 * (a1 - a2) + c1)   );
+
+                this.setQuestion( d1 + ' - ' + '(' + b1 + '(' + a1 + ' - ' + a2 + ') + ' + c1 + ')'   );
+                this.setAnswer(x,0);
+        }
+}
+});
+
+/* TYPE_DESCRIPTION:  */
 	//16: c(b(a)) 
 var i_5_oa_a_1__16 = new Class(
 {
