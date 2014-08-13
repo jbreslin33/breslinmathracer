babbdf34dc783435ef745b9f8c20b13c0cd419f0
jbreslin33 <jbreslin33@localhost>
2014-08-13 15:55:09 -0400

    in the middle of ajax function for item stats...but i am going to switch and send it with item ids

--- src/src/php/get_student_item_stats.php
+++ src/src/php/get_student_item_stats.php
@@ -9,6 +9,20 @@ function __construct()
 {
 	$this->mDatabaseConnection = new DatabaseConnection();
 
+        //this id is either going in array or not
+/*
+        $item_types_id = pg_Result($result, 0, 'id');
+        $progression_counter = pg_Result($result, 0, 'progression');
+
+        $query = "select item_attempts.item_types_id, item_attempts.transaction_code from levelattempts JOIN item_attempts ON levelattempts.id=item_attempts.levelattempts_id JOIN learning_standards_attempts ON learning_standards_attempts.id=levelattempts.learning_standards_attempts_id where item_attempts.item_types_id = '";
+        $query .= $item_types_id;
+        $query .= "' AND learning_standards_attempts.user_id = ";
+        $query .= $_SESSION["user_id"];
+        $query .= "order by item_attempts.start_time asc limit 10;";
+
+        $result = pg_query($this->mDatabaseConnection->getConn(),$query) or die('no connection: ' . pg_last_error());
+        $num = pg_num_rows($result);
+
      	$query = "select id from item_types order by progression;";
 
        	//get db result
@@ -49,6 +63,7 @@ function __construct()
         }
 
         $_SESSION["practice_description"] = $itemString;
+*/
 }
 //end of class
 }
