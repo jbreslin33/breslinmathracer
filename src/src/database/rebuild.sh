49868d432c53fcd9644a9b1a73395304801943d3
jbreslin33 <jbreslin33@localhost>
2014-08-15 15:53:28 -0400

    deleting contents of insert_types in script

--- src/src/database/rebuild.sh
+++ src/src/database/rebuild.sh
@@ -1,3 +1,5 @@
 sudo -u postgres psql -d jamesanthonybreslin -f src/database/build.sql
 sudo -u postgres psql -d jamesanthonybreslin -f src/database/insert.sql
+> src/database/insert_types.sql
 sudo -u postgres psql -d jamesanthonybreslin -f src/database/insert_types.sql
+
