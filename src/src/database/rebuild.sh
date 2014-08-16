21323f8dc57ba917b57b10c2a2520a9398eac7da
jbreslin33 <jbreslin33@localhost>
2014-08-15 22:22:34 -0400

    now we recreate sql file in script.

--- src/src/database/rebuild.sh
+++ src/src/database/rebuild.sh
@@ -1,5 +1,11 @@
 sudo -u postgres psql -d jamesanthonybreslin -f src/database/build.sql
 sudo -u postgres psql -d jamesanthonybreslin -f src/database/insert.sql
+if [ -e src/database/insert_types.sql ]; 
+then
+rm src/database/insert_types.sql
+else
+touch src/database/insert_types.sql
+fi
 > src/database/insert_types.sql
 grep -rhI --exclude-dir=database 'insert into item_types' ./ >> src/database/insert_types.sql
 sudo -u postgres psql -d jamesanthonybreslin -f src/database/insert_types.sql
