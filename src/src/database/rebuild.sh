86c3908f9ad62efbcde0413b60c01dc1c2eff05a
jbreslin33 <jbreslin33@localhost>
2014-08-15 20:25:43 -0400

    fixed build sql warnings.

--- src/src/database/rebuild.sh
+++ src/src/database/rebuild.sh
@@ -1,5 +1,5 @@
 sudo -u postgres psql -d jamesanthonybreslin -f src/database/build.sql
 sudo -u postgres psql -d jamesanthonybreslin -f src/database/insert.sql
 > src/database/insert_types.sql
-grep --exclude-from=/src/database/insert_types.sql -rhI 'insert into item_types' ./ >> src/database/insert_types.sql
+grep -rhI --exclude-dir=database 'insert into item_types' ./ >> src/database/insert_types.sql
 sudo -u postgres psql -d jamesanthonybreslin -f src/database/insert_types.sql
