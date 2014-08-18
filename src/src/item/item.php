799974799aa69b7f7dddffe7864e29ef954aa024
jbreslin33 <jbreslin33@localhost>
2014-08-18 16:06:03 -0400

    cosmetic

--- src/src/item/item.php
+++ src/src/item/item.php
@@ -323,24 +323,10 @@ var Item = new Class(
 
 	getAnswer: function(index)
 	{
-		if (index == '')
-		{
-			APPLICATION.log('a');
-			return this.mAnswerArray[0];	
-		}
 		if (index == null)
 		{
-			APPLICATION.log('b');
 			return this.mAnswerArray[0];	
 		}
-		if (index == 0)
-		{
-			APPLICATION.log('c');
-			return this.mAnswerArray[0];	
-		}
-
-		APPLICATION.log('d');
-			
 		return this.mAnswerArray[index];
 	},
 
