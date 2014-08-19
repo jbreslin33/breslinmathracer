4e277f1bd490899761246c6ccfd589606b6696c4
jbreslin33 <jbreslin33@localhost>
2014-08-19 19:55:29 -0400

    4.oa.a.2 mod

--- src/src/core/math/g4_oa_a_2.php
+++ src/src/core/math/g4_oa_a_2.php
@@ -11,13 +11,17 @@ Extends: TextItem,
 
                 this.mType = '4.oa.a.2_1';
 
+		//move gui
+		this.mUserAnswerLabel.setPosition(125,200);
+		this.mCorrectAnswerLabel.setPosition(425,200);
+
                 this.mNameMachine = new NameMachine();
-                this.mNameOne = this.mNameMachine.getName();
-                this.mNameTwo = this.mNameMachine.getName();
-                this.mThing = this.mNameMachine.getThing();
-                this.mOwned = this.mNameMachine.getOwned();
-                this.mAdded = this.mNameMachine.getAdded();
-                this.mSubtracted = this.mNameMachine.getSubtracted();
+                this.mNameOne     = this.mNameMachine.getName();
+                this.mNameTwo     = this.mNameMachine.getName();
+                this.mThing       = this.mNameMachine.getThing();
+                this.mOwned       = this.mNameMachine.getOwned();
+                this.mAdded       = this.mNameMachine.getAdded();
+                this.mSubtracted  = this.mNameMachine.getSubtracted();
 
                	//variables
                 this.a = Math.floor(Math.random()*9)+1;
@@ -25,9 +29,28 @@ Extends: TextItem,
                 this.b = Math.floor(Math.random()*8)+2;
                 this.c = parseInt(this.a * this.b);
 
-                this.setQuestion(this.mNameOne + ' ' + this.mOwned + ' ' + this.a + ' ' + this.mThing + '. ' + this.mNameTwo + ' had ' + this.b + ' times as many ' + this.mThing + ' as ' + this.mNameOne + '. How many ' + this.mThing + ' does ' + this.mNameTwo + ' have? Use + for addition, - for subtraction, * for multiplication and / for division.'); 
-                //this.setAnswer(this.c,0);
-		
-                this.setAnswer('' + this.x,0);
+                this.setQuestion(this.mNameOne + ' ' + this.mOwned + ' ' + this.a + ' ' + this.mThing + '. ' + this.mNameTwo + ' had ' + this.b + ' times as many ' + this.mThing + ' as ' + this.mNameOne + '. Write an equation that can be used to solve how many ' + this.mThing + ' ' + this.mNameTwo + ' has. Remember an equation has an equal sign. Use + for addition, - for subtraction, * for multiplication and / for division. Do not use spaces.'); 
+
+                this.setAnswer('' + this.a + '*' + this.b + '=' + this.c ,0);
+                this.setAnswer('' + this.b + '*' + this.a + '=' + this.c ,1);
+                this.setAnswer('' + this.c + '=' + this.a + '*' + this.b ,2);
+                this.setAnswer('' + this.c + '=' + this.b + '*' + this.a ,3);
         }
 });
+/*
+		//user Answer label
+                this.mUserAnswerLabel = new Shape(100,50,425,100,this.mSheet.mGame,"","","");
+                this.addShape(this.mUserAnswerLabel);
+                this.mUserAnswerLabel.mCollidable = false;
+                this.mUserAnswerLabel.mCollisionOn = false;
+                this.mUserAnswerLabel.setText(this.mQuestion);
+                this.mUserAnswerLabel.setVisibility(false);
+
+                //correctAnswer Label
+                this.mCorrectAnswerLabel = new Shape(100,50,425,200,this.mSheet.mGame,"","","");
+                this.addShape(this.mCorrectAnswerLabel);
+                this.mCorrectAnswerLabel.mCollidable = false;
+                this.mCorrectAnswerLabel.mCollisionOn = false;
+                this.mCorrectAnswerLabel.setText(this.mQuestion);
+                this.mCorrectAnswerLabel.setVisibility(false);
+*/
