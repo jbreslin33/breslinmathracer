2371c25751b3a3ae74dafbcfbc8a4df0afc32923
jbreslin33 <jbreslin33@localhost>
2014-08-19 22:04:29 -0400

    4.oa.a.2_3 rfm

--- src/src/core/math/g4_oa_a_2.php
+++ src/src/core/math/g4_oa_a_2.php
@@ -1,3 +1,51 @@
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.oa.a.2_3',4.0203,'4.oa.a.2','Multiplicative or Additive Comparison. Multiplicative.');
+*/
+
+var i_4_oa_a_2__3 = new Class(
+{
+Extends: ThreeButtonItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet);
+                this.mType = '4.oa.a.2_3';
+
+                //BUTTON A
+                this.mButtonA.setPosition(380,100);
+                this.mButtonB.setPosition(380,200);
+                this.mButtonC.setPosition(380,300);
+
+                this.a = 0;
+                this.b = 0;
+
+                while (this.a <= this.b)
+                {
+                        this.a = Math.floor(Math.random()*10+1);
+                        this.b = Math.floor(Math.random()*10+1);
+                }
+
+                this.setQuestion('Compare.');
+                this.setAnswer('Is greater than.',0);
+
+                this.mButtonA.setAnswer('Is greater than.');
+                this.mButtonB.setAnswer('Is equal to.');
+                this.mButtonC.setAnswer('Is less than.');
+        },
+
+        createQuestionShapes: function()
+        {
+                var shapeA = new Shape(50,50,240,200,this.mSheet.mGame,"","","");
+                var shapeB = new Shape(50,50,530,200,this.mSheet.mGame,"","","");
+
+                shapeA.setText(this.a);
+                shapeB.setText(this.b);
+
+                this.addQuestionShape(shapeA);
+                this.addQuestionShape(shapeB);
+        }
+});
+
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('4.oa.a.2_2',4.0202,'4.oa.a.2','Word problem. Answer in equation form. Multiplicative comparision. Division operation.' );
 */
