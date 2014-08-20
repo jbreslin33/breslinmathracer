5e58fc2d7e4cc52588ed2c2d2f9247c41525bfc9
jbreslin33 <jbreslin33@localhost>
2014-08-20 15:50:20 -0400

    3.oa.a.1 rfm

--- src/src/core/math/g3_oa_a_1.php
+++ src/src/core/math/g3_oa_a_1.php
@@ -1,1 +1,45 @@
-//add
+
+/*
+prerequisites:
+none finished
+*/
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('3.oa.a.1_1',3.0101,'3.oa.a.1','Word Problem. Multiplication. Interprete(not solve). Factors between 1-10.' );
+*/
+
+var i_3_oa_a_1__1 = new Class(
+{
+Extends: TextItem,
+        initialize: function(sheet)
+        {
+        	this.parent(sheet,600,50,330,75,100,50,685,80);
+
+                this.mType = '3.oa.a.1_1';
+
+		//move gui
+		this.mUserAnswerLabel.setPosition(125,200);
+		this.mCorrectAnswerLabel.setPosition(425,200);
+
+                this.mNameMachine = new NameMachine();
+                this.mNameOne     = this.mNameMachine.getName();
+                this.mNameTwo     = this.mNameMachine.getName();
+                this.mThing       = this.mNameMachine.getThing();
+                this.mOwned       = this.mNameMachine.getOwned();
+
+               	//variables
+                this.a = Math.floor(Math.random()*9)+1;
+		this.a = this.a * 10;
+                this.b = Math.floor(Math.random()*8)+2;
+                this.c = parseInt(this.a * this.b);
+		
+                this.setQuestion(this.mNameOne + ' ' + this.mOwned + ' ' + this.a + ' ' + this.mThing + '. ' + this.mNameTwo + ' had ' + this.b + ' times as many ' + this.mThing + ' as ' + this.mNameOne + '. Write an expression that can be used to solve how many ' + this.mThing + ' ' + this.mNameTwo + ' has. Remember an equation has an equal sign. Use + for addition, - for subtraction, x for multiplication and / for division. Do not use spaces. Example Answer: 1+2=3'); 
+
+                this.setAnswer('' + this.a + '*' + this.b + '=' + this.c ,0);
+                this.setAnswer('' + this.b + '*' + this.a + '=' + this.c ,1);
+                this.setAnswer('' + this.c + '=' + this.a + '*' + this.b ,2);
+                this.setAnswer('' + this.c + '=' + this.b + '*' + this.a ,3);
+        }
+});
+
+
