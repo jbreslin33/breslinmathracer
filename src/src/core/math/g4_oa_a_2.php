736e7db0b2069fb6468a1a4ff6657df7508bebff
jbreslin33 <jbreslin33@localhost>
2014-08-19 20:28:01 -0400

    4.oa.a.2_2

--- src/src/core/math/g4_oa_a_2.php
+++ src/src/core/math/g4_oa_a_2.php
@@ -1,5 +1,5 @@
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('4.oa.a.2_1',4.0201,'4.oa.a.2','twice as many');
+insert into item_types(id,progression,core_standards_id,description) values ('4.oa.a.2_1',4.0201,'4.oa.a.2','Word problem. Answer in equation form. Multiplicative comparision. Multiplication operation.' );
 */
 
 var i_4_oa_a_2__1 = new Class(
@@ -37,3 +37,44 @@ Extends: TextItem,
                 this.setAnswer('' + this.c + '=' + this.b + '*' + this.a ,3);
         }
 });
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.oa.a.2_2',4.0202,'4.oa.a.2','Word problem. Answer in equation form. Multiplicative comparision. Division operation.' );
+*/
+
+var i_4_oa_a_2__2 = new Class(
+{
+Extends: TextItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet,600,50,330,75,100,50,685,80);
+
+                this.mType = '4.oa.a.2_2';
+
+                //move gui
+                this.mUserAnswerLabel.setPosition(125,200);
+                this.mCorrectAnswerLabel.setPosition(425,200);
+
+                this.mNameMachine = new NameMachine();
+                this.mNameOne     = this.mNameMachine.getName();
+                this.mNameTwo     = this.mNameMachine.getName();
+                this.mThing       = this.mNameMachine.getThing();
+                this.mOwned       = this.mNameMachine.getOwned();
+                this.mAdded       = this.mNameMachine.getAdded();
+                this.mSubtracted  = this.mNameMachine.getSubtracted();
+
+                //variables
+                this.a = Math.floor(Math.random()*9)+1;
+                this.a = this.a * 10;
+                this.b = Math.floor(Math.random()*8)+2;
+                this.c = parseInt(this.a * this.b);
+
+                this.setQuestion(this.mNameOne + ' ' + this.mOwned + ' ' + this.a + ' ' + this.mThing + '. ' + this.mNameTwo + ' had ' + this.b + ' times as many ' + this.mThing + ' as ' + this.mNameOne + '. Write an equation that can be used to solve how many ' + this.mThing + ' ' + this.mNameTwo + ' has. Remember an equation has an equal sign. Use + for addition, - for subtraction, * for multiplication and / for division. Do not use spaces. Example Answer: 3+4=12');
+
+                this.setAnswer('' + this.a + '*' + this.b + '=' + this.c ,0);
+                this.setAnswer('' + this.b + '*' + this.a + '=' + this.c ,1);
+                this.setAnswer('' + this.c + '=' + this.a + '*' + this.b ,2);
+                this.setAnswer('' + this.c + '=' + this.b + '*' + this.a ,3);
+        }
+});
+
--- src/src/core/math/picker.php
+++ src/src/core/math/picker.php
@@ -140,6 +140,62 @@ getItem: function(id)
         {
                 return new i_4_oa_a_2__1(this.mSheet);
         }
+        if (id == '4.oa.a.2_2')
+        {
+                return new i_4_oa_a_2__2(this.mSheet);
+        }
+        if (id == '4.oa.a.2_3')
+        {
+                return new i_4_oa_a_2__3(this.mSheet);
+        }
+        if (id == '4.oa.a.2_4')
+        {
+                return new i_4_oa_a_2__4(this.mSheet);
+        }
+        if (id == '4.oa.a.2_5')
+        {
+                return new i_4_oa_a_2__5(this.mSheet);
+        }
+        if (id == '4.oa.a.2_6')
+        {
+                return new i_4_oa_a_2__6(this.mSheet);
+        }
+        if (id == '4.oa.a.2_7')
+        {
+                return new i_4_oa_a_2__7(this.mSheet);
+        }
+        if (id == '4.oa.a.2_8')
+        {
+                return new i_4_oa_a_2__8(this.mSheet);
+        }
+        if (id == '4.oa.a.2_9')
+        {
+                return new i_4_oa_a_2__9(this.mSheet);
+        }
+        if (id == '4.oa.a.2_10')
+        {
+                return new i_4_oa_a_2__10(this.mSheet);
+        }
+        if (id == '4.oa.a.2_11')
+        {
+                return new i_4_oa_a_2__11(this.mSheet);
+        }
+        if (id == '4.oa.a.2_12')
+        {
+                return new i_4_oa_a_2__12(this.mSheet);
+        }
+        if (id == '4.oa.a.2_13')
+        {
+                return new i_4_oa_a_2__13(this.mSheet);
+        }
+        if (id == '4.oa.a.2_14')
+        {
+                return new i_4_oa_a_2__14(this.mSheet);
+        }
+        if (id == '4.oa.a.2_15')
+        {
+                return new i_4_oa_a_2__15(this.mSheet);
+        }
 
 
 
