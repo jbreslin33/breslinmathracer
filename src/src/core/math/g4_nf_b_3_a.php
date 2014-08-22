6711377cedd21adc94e60c11bfddf28386eae37d
jbreslin33 <jbreslin33@localhost>
2014-08-22 14:10:08 -0400

    converted 4.nf.b.a_3

--- src/src/core/math/g4_nf_b_3_a.php
+++ src/src/core/math/g4_nf_b_3_a.php
@@ -28,7 +28,7 @@ initialize: function(sheet)
         var answer = new Fraction(n,c);
 
         this.setAnswer(answer.getString(),0);
-        this.setQuestion(ac.getString() + '+' + bc.getString());
+        this.setQuestion(ac.getString() + '+' + bc.getString() + '=');
 }
 });
 
@@ -55,7 +55,7 @@ initialize: function(sheet)
         var answer = new Fraction(n,c);
 
         this.setAnswer(answer.getString(),0);
-        this.setQuestion(ac.getString() + '-' + bc.getString());
+        this.setQuestion(ac.getString() + '-' + bc.getString() + '=');
 }
 });
 
@@ -67,49 +67,25 @@ insert into item_types(id,progression,core_standards_id,description) values ('4.
 
 var i_4_nf_b_3_a__3 = new Class(
 {
-Extends: TextItem,
-   initialize: function(sheet)
-   {
-      this.parent(sheet);
-      this.mType = '4.nf.b.3.a_3';
-       	
-		var varA = 0;
-		var varB = 0;
-		var varC = 0;
-		var varD = 0;
-    var varE = 0;
-		var varN = 0;
+Extends: TextItemFraction,
+initialize: function(sheet)
+{
+	this.parent(sheet,150,50,125,95,100,50,425,100,100,50,425,175);
+	this.mType = '4.nf.b.3.a_3';
 
+	var a = Math.floor((Math.random()*5)+1);
+        var b = Math.floor((Math.random()*5)+1);
+        var c = Math.floor((Math.random()*5)+1);
+        var d = Math.floor((Math.random()*10)+16);
 
-		var start = 0;
-		var end = 0;
-		var rand = 0;
+        var n = parseInt(a + b + c);
 
-		var question;
-		var answer;
+        var ad = new Fraction(a,d);
+        var bd = new Fraction(b,d);
+        var cd = new Fraction(c,d);
+        var answer = new Fraction(n,d);
 
-
-			// get bottom number
-			varB = 9 + Math.floor(Math.random()*20);
-
-			// get top number
-			max = Math.floor(varB/3);
-			varA = 1 + Math.floor((Math.random()*(max-1)));
-
-			varC = 1 + Math.floor((Math.random()*max));
-
-      varE = 1 + Math.floor((Math.random()*max));
-
-			varD = varB;
-
-			answer = varA + varC + varE;
-			
-			question = '' + varA + '/' +  varB + ' + ' + varC + '/' +  varD + ' + ' + varE + '/' +  varD + ' =' + ' ?/' + varD;
-
-			this.setQuestion(question);
-      this.setAnswer('' + answer,0);
-
-      this.mQuestionLabel.setSize(220,50);
-      this.mQuestionLabel.setPosition(255,145);
-   }
+        this.setAnswer(answer.getString(),0);
+        this.setQuestion(ad.getString() + '+' + bd.getString() + '+' + cd.getString() + '=');
+}
 });
