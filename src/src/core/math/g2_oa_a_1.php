2150766d891f2a5424fa41e4492174cc1cb2b571
jbreslin33 <jbreslin33@localhost>
2014-08-22 20:17:36 -0400

    working on 2.oa.a.1

--- src/src/core/math/g2_oa_a_1.php
+++ src/src/core/math/g2_oa_a_1.php
@@ -1,1 +1,45 @@
-//add
+
+/*
+prerequisites:
+none finished
+*/
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('2.oa.a.1_1',2.0101,'2.oa.a.1','How many more.' );
+*/
+
+var i_2_oa_a_1__1 = new Class(
+{
+Extends: TextItem,
+        initialize: function(sheet)
+        {
+        	this.parent(sheet,600,50,330,75,100,50,685,80);
+
+                this.mType = '2.oa.a.1_1';
+
+                this.mNameMachine = new NameMachine();
+                this.mNameOne     = this.mNameMachine.getName();
+                this.mNameTwo     = this.mNameMachine.getName();
+                this.mThings      = this.mNameMachine.getThing();
+
+               	//variables
+                this.a = Math.floor(Math.random()*50)+50;
+                this.b = Math.floor(Math.random()*28)+12;
+                this.c = parseInt(this.a - this.b);
+	
+                random = Math.floor(Math.random()*2);
+		random = 1;
+		
+		if (random == 0)
+		{
+			this.setQuestion(this.mNameOne + ' has ' + this.a + ' ' + this.mThings + '. ' + this.mNameTwo + ' has ' + this.b + ' ' + this.mThings + '. How many more ' + this.mThings + ' does ' + this.mNameOne + ' than ' + this.mNameTwo);    	
+		}
+
+		if (random == 0)
+		{
+			this.setQuestion(this.mNameOne + ' has ' + this.a + ' ' + this.mThings + '. ' + this.mNameTwo + ' has ' + this.b + ' ' + this.mThings + '. How many more ' + this.mThings + ' does ' + this.mNameOne + ' than ' + this.mNameTwo);    	
+		}
+
+                this.setAnswer(this.c,0);
+        }
+});
--- src/src/core/math/picker_jim.php
+++ src/src/core/math/picker_jim.php
@@ -58,6 +58,49 @@ getItem: function(id)
                 return new i_k_nbt_a_1__1(this.mSheet);
         }
 	
+	/*** GRADE 2 ***/
+	//2.oa.a.1
+	if (id == '2.oa.a.1_1')
+        {
+                return new i_2_oa_a_1__1(this.mSheet);
+        }
+	if (id == '2.oa.a.1_2')
+        {
+                return new i_2_oa_a_1__2(this.mSheet);
+        }
+	if (id == '2.oa.a.1_3')
+        {
+                return new i_2_oa_a_1__3(this.mSheet);
+        }
+	if (id == '2.oa.a.1_4')
+        {
+                return new i_2_oa_a_1__3(this.mSheet);
+        }
+	if (id == '2.oa.a.1_5')
+        {
+                return new i_2_oa_a_1__4(this.mSheet);
+        }
+	if (id == '2.oa.a.1_6')
+        {
+                return new i_2_oa_a_1__5(this.mSheet);
+        }
+	if (id == '2.oa.a.1_7')
+        {
+                return new i_2_oa_a_1__6(this.mSheet);
+        }
+	if (id == '2.oa.a.1_8')
+        {
+                return new i_2_oa_a_1__7(this.mSheet);
+        }
+	if (id == '2.oa.a.1_9')
+        {
+                return new i_2_oa_a_1__8(this.mSheet);
+        }
+	if (id == '2.oa.a.1_10')
+        {
+                return new i_2_oa_a_1__9(this.mSheet);
+        }
+	
 	/*** GRADE 3 ***/
 	//3.oa.a.1
 	if (id == '3.oa.a.1_3')
