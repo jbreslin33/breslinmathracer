c833db5a2bbff1e5cdce849f4e2123a465a0a2f3
jbreslin33 <jbreslin33@localhost>
2014-08-26 22:56:21 -0400

    2.oa.a.1_7 m5 ..still need to do 1-4 then 1.oa.a.1 needs sum types.

--- src/src/core/math/g2_oa_a_1.php
+++ src/src/core/math/g2_oa_a_1.php
@@ -4,7 +4,83 @@ prerequisites:
 none finished
 */
 
-//total, altogether, in all...
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('2.oa.a.1_7',2.0107,'2.oa.a.1','Two step. Addition.' );
+*/
+
+var i_2_oa_a_1__7 = new Class(
+{
+Extends: TextItem,
+        initialize: function(sheet)
+        {
+        	this.parent(sheet,600,50,330,75,100,50,685,80);
+
+                this.mType = '2.oa.a.1_7';
+
+                this.mNameMachine = new NameMachine();
+                this.mNameOne     = this.mNameMachine.getName();
+                this.mNameTwo     = this.mNameMachine.getName();
+                this.mThings      = this.mNameMachine.getThing();
+                
+		this.mSchoolOne      = this.mNameMachine.getSchool();
+		this.mSchoolTwo      = this.mNameMachine.getSchool();
+
+		this.mAdultOne = this.mNameMachine.getAdult();
+	
+		this.mVegetableOne = this.mNameMachine.getVegetable();
+		this.mVegetableTwo = this.mNameMachine.getVegetable();
+		
+		this.mFruitOne = this.mNameMachine.getFruit();
+		this.mFruitTwo = this.mNameMachine.getFruit();
+
+		this.mPlayedActivityOne = this.mNameMachine.getPlayedActivity();	
+		this.mPlayedActivityTwo = this.mNameMachine.getPlayedActivity();	
+		this.mTimeIncrement = this.mNameMachine.getTimeIncrement('seconds','hours');
+	
+		this.mDayOfWeekOne = this.mNameMachine.getDayOfWeek();
+		this.mDayOfWeekTwo = this.mNameMachine.getDayOfWeek();
+		this.mDayOfWeekThree = this.mNameMachine.getDayOfWeek();
+
+		this.mSum = this.mNameMachine.getSum();
+
+               	//variables
+                this.a = Math.floor(Math.random()*10)+24;
+                this.b = Math.floor(Math.random()*10)+24;
+                this.c = Math.floor(Math.random()*10)+24;
+                this.d = parseInt(this.a + this.b + this.c);
+	
+                random = Math.floor(Math.random()*5)+1;
+		random = 5;
+		
+		if (random == 5)
+		{
+			this.setQuestion(this.mNameOne + ' has a fruit stand. ' + this.mNameMachine.getPronoun(this.mNameOne,1,0) + ' sold ' + this.a + ' ' + this.mFruitOne + ' on ' + this.mDayOfWeekOne + ', ' + this.b + ' ' + this.mFruitOne + ' on ' + this.mDayOfWeekTwo + ' and ' + this.c + ' on ' + this.mDayOfWeekThree + '. How many ' + this.mFruitOne + ' did ' + this.mNameMachine.getPronoun(this.mNameOne,0,0) + ' sell ' + this.mSum + '?');        
+		}
+		
+		if (random == 4)
+		{
+			this.setQuestion(this.mNameOne + ' played ' + this.mPlayedActivityOne + ' for ' + this.b + ' ' + this.mTimeIncrement + '. ' + this.mNameMachine.getPronoun(this.mNameOne,1,0) + ' played ' + this.mPlayedActivityTwo + ' for ' + ' ' + this.a + ' ' + this.mTimeIncrement + '. How long did ' + this.mNameMachine.getPronoun(this.mNameOne,0,0) + ' play ' + this.mSum + '?');  	
+		}
+	
+		if (random == 3)
+		{
+			this.setQuestion(this.mAdultOne + ' planted ' + this.a + ' ' + this.mVegetableOne + ' and ' + this.b + ' ' + this.mVegetableTwo + '. How many vegetables did he plant ' + this.mSum + '?');  	
+		}
+		
+		if (random == 2)
+		{
+			this.setQuestion(this.mSchoolOne + ' has ' + this.b + ' girls and ' + this.a + ' boys. How many students does ' + this.mSchoolTwo + ' have ' + this.mSum + '?');    	
+		}
+
+		if (random == 1)
+		{
+			this.setQuestion(this.mNameOne + ' has ' + this.a + ' ' + this.mThings + '. ' + this.mNameTwo + ' has ' + this.b + ' ' + this.mThings + '. How many ' + this.mThings + ' do they have ' + this.mSum + '?');    	
+		}
+
+                this.setAnswer(this.d,0);
+        }
+});
+
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('2.oa.a.1_6',2.0106,'2.oa.a.1','One step. Addition.' );
 */
