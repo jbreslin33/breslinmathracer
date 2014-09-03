380b4f021e834923d4755149c9a684d8e45c9680
jbreslin33 <jbreslin33@localhost>
2014-09-02 21:49:42 -0400

    took out unused functions. also it does not look like setRawData in normal is getting me to next item.

--- src/src/php/normal.php
+++ src/src/php/normal.php
@@ -28,54 +28,11 @@ function __construct($startNew)
         
 		$item_attempt = new ItemAttempt();
         	$item_attempt->insert();
+	
 	}
-	else
-	{
-		$this->continueAttempt();
-	}
-}
-
-public function insertEvaluationsAttempts()
-{
-        $insert = "insert into evaluations_attempts (start_time,user_id,evaluations_id) VALUES (CURRENT_TIMESTAMP,";
-        $insert .= $_SESSION["user_id"];
-        $insert .= ",1);";
-
-        $insertResult = pg_query($this->mDatabaseConnection->getConn(),$insert) or die('Could not connect: ' . pg_last_error());
-
-        $query = "select id from evaluations_attempts where user_id = ";
-        $query .= $_SESSION["user_id"];
-        $query .= " order by start_time desc limit 1;";
-
-        $result = pg_query($this->mDatabaseConnection->getConn(),$query) or die('Could not connect: ' . pg_last_error());
-
-        $num = pg_num_rows($result);
-
-        if ($num > 0)
-        {
-                //get the attempt_id
-                $evaluations_attempts_id = pg_Result($result, 0, 'id');
-
-                //set level_id
-                $_SESSION["evaluations_attempts_id"] = $evaluations_attempts_id;
-        }
-
-        //set sessions for signup
-        $_SESSION["ref_id"] = 'normal';
-        $_SESSION["level"] = 1;
-        $_SESSION["levels"] = 10;
-        $_SESSION["subject_id"] = 1;
-
-        $this->setRawData();
-}
-
-public function continueAttempt()
-{
-        $_SESSION["subject_id"] = 1;
-        
 	$this->setRawData();
 }
-//you are not using user id in selects that is why it skipped eval....
+
 public function initializeProgressionCounter()
 {
         $this->progression_counter = 0;
@@ -94,6 +51,7 @@ public function initializeProgressionCounter()
                 $this->progression_counter = pg_Result($result, 0, 'progression');
 	}
 }
+
 //i am going to remember the last thing i asked and only ask 1 question at a time.
 public function setRawData()
 {
