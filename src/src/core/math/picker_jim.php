30aaa22a69797dc9e05d5c0dc918338df6adc248
jbreslin33 <jbreslin33@localhost>
2014-09-19 18:36:06 -0400

    6.rp.a.2 rfm

--- src/src/core/math/picker_jim.php
+++ src/src/core/math/picker_jim.php
@@ -458,6 +458,48 @@ getItem: function(id)
         {
                 return new i_6_rp_a_1__10(this.mSheet);
         }
+	
+	//6.rp.a.2
+	if (id == '6.rp.a.2_1')
+        {
+                return new i_6_rp_a_2__1(this.mSheet);
+        }
+	if (id == '6.rp.a.2_2')
+        {
+                return new i_6_rp_a_2__2(this.mSheet);
+        }
+	if (id == '6.rp.a.2_3')
+        {
+                return new i_6_rp_a_2__3(this.mSheet);
+        }
+	if (id == '6.rp.a.2_4')
+        {
+                return new i_6_rp_a_2__4(this.mSheet);
+        }
+	if (id == '6.rp.a.2_5')
+        {
+                return new i_6_rp_a_2__5(this.mSheet);
+        }
+	if (id == '6.rp.a.2_6')
+        {
+                return new i_6_rp_a_2__6(this.mSheet);
+        }
+	if (id == '6.rp.a.2_7')
+        {
+                return new i_6_rp_a_2__7(this.mSheet);
+        }
+	if (id == '6.rp.a.2_8')
+        {
+                return new i_6_rp_a_2__8(this.mSheet);
+        }
+	if (id == '6.rp.a.2_9')
+        {
+                return new i_6_rp_a_2__9(this.mSheet);
+        }
+	if (id == '6.rp.a.2_10')
+        {
+                return new i_6_rp_a_2__10(this.mSheet);
+        }
 
 	return 0;	
 }
