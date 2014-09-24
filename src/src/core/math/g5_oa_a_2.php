63d9d7705e1cfcd22ba668bb54e9bc4ebceedc63
jbreslin33 <jbreslin33@localhost>
2014-09-23 21:50:17 -0400

    not sure i like using the item_fraction_expression one class.

--- src/src/core/math/g5_oa_a_2.php
+++ src/src/core/math/g5_oa_a_2.php
@@ -6,10 +6,11 @@ insert into item_types(id,progression,core_standards_id,description) values ('5.
 
 var i_5_oa_a_2__11 = new Class(
 {
-Extends: TextItem,
+Extends: TextItemFractionExpressionOne,
         initialize: function(sheet)
         {
-		this.parent(sheet,150,50,125,95,100,50,425,100,100,50,425,175);
+		//this.parent(sheet,150,50,125,95,100,50,425,100,100,50,425,175);
+ 		this.parent(sheet,150,50,125,95, 100,50,425,100, 100,50,425,175, 100,50,325,100, 100,50,525,100);
 
                 this.mType = '5.oa.a.2_11';
                 this.mNameMachine = new NameMachine();
