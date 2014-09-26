cebae00327fa8e324dd2762fe58ca86f0bc61063
jbreslin33 <jbreslin33@localhost>
2014-09-26 09:19:12 -0400

    fixed backup db.

--- src/src/database/db_restore.sh
+++ src/src/database/db_restore.sh
@@ -1,2 +1,3 @@
+sudo -u postgres psql -d jamesanthonybreslin -f src/database/drop_db.sql
 sudo -u postgres dropdb jamesanthonybreslin
 sudo -u postgres pg_restore -C -d postgres src/database/jamesanthonybreslin.dump
