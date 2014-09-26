9a8d4a70a54f514d60fccb857d15416ab4ae12c5
jbreslin33 <jbreslin33@localhost>
2014-09-26 18:20:36 -0400

    type 52 rfm.

--- src/src/core/math/picker_jim.php
+++ src/src/core/math/picker_jim.php
@@ -228,6 +228,10 @@ getItem: function(id)
         {
                 return new i_5_oa_a_1__0_51(this.mSheet);
         }
+	if (id == '5.oa.a.1_0_52')
+        {
+                return new i_5_oa_a_1__0_52(this.mSheet);
+        }
 	if (id == '5.oa.a.1_1')
         {
                 return new i_5_oa_a_1__1(this.mSheet);
