7c93e2f060e8a1bba694e30aa0c82f226008d1f3
jbreslin33 <jbreslin33@localhost>
2014-09-29 21:15:11 -0400

    adding times tables

--- src/src/core/math/g3_oa_c_7.php
+++ src/src/core/math/g3_oa_c_7.php
@@ -1,8 +1,6 @@
-
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('3.oa.c.7_1',3.0701,'3.oa.c.7','Multiply.');
+insert into item_types(id,progression,core_standards_id,description) values ('3.oa.c.7_1',3.0701,'3.oa.c.7','2x2');
 */
-
 var i_3_oa_c_7__1 = new Class(
 {
 Extends: TextItem,
@@ -10,13 +8,104 @@ Extends: TextItem,
 initialize: function(sheet)
 {
         this.parent(sheet,375,50,220,75,150,50,525,100);
-
         this.mType = '3.oa.c.7_1';
-
-        this.mChopWhiteSpace = false;
-
-        this.setQuestion('What dddis the name for a mathematical phrase containing only numbers and one or more operation symbols?');
-        this.setAnswer('numerical expression',0);
+        this.setQuestion('2x2');
+        this.setAnswer('4',0);
 }
 });
 
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('3.oa.c.7_2',3.0702,'3.oa.c.7','2x3');
+*/
+var i_3_oa_c_7__2 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,375,50,220,75,150,50,525,100);
+        this.mType = '3.oa.c.7_2';
+        this.setQuestion('2x3');
+        this.setAnswer('6',0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('3.oa.c.7_3',3.0703,'3.oa.c.7','3x2');
+*/
+var i_3_oa_c_7__3 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,375,50,220,75,150,50,525,100);
+        this.mType = '3.oa.c.7_3';
+        this.setQuestion('3x2');
+        this.setAnswer('6',0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('3.oa.c.7_4',3.0704,'3.oa.c.7','2x4');
+*/
+var i_3_oa_c_7__4 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,375,50,220,75,150,50,525,100);
+        this.mType = '3.oa.c.7_4';
+        this.setQuestion('2x4');
+        this.setAnswer('8',0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('3.oa.c.7_5',3.0705,'3.oa.c.7','4x2');
+*/
+var i_3_oa_c_7__5 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,375,50,220,75,150,50,525,100);
+        this.mType = '3.oa.c.7_5';
+        this.setQuestion('4x2');
+        this.setAnswer('8',0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('3.oa.c.7_6',3.0706,'3.oa.c.7','2x5');
+*/
+var i_3_oa_c_7__6 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,375,50,220,75,150,50,525,100);
+        this.mType = '3.oa.c.7_6';
+        this.setQuestion('2x5');
+        this.setAnswer('10',0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('3.oa.c.7_7',3.0707,'3.oa.c.7','5x2');
+*/
+var i_3_oa_c_7__7 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,375,50,220,75,150,50,525,100);
+        this.mType = '3.oa.c.7_7';
+        this.setQuestion('5x2');
+        this.setAnswer('10',0);
+}
+});
