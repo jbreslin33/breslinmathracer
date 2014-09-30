d8b1f99d051261a43a37d942f05e66cb5f4885f3
bbreslin99 <bbreslin99@localhost>
2014-09-30 17:11:30 -0400

    filled in table info

--- src/src/core/math/g5_oa_b_3.php
+++ src/src/core/math/g5_oa_b_3.php
@@ -28,7 +28,7 @@ insert into item_types(id,progression,core_standards_id,description) values ('5.
 */
 var i_5_oa_b_3__1 = new Class(
 {
-Extends: TextItem,
+Extends: TextItem2,
 
 initialize: function(sheet)
 {
@@ -36,6 +36,7 @@ initialize: function(sheet)
 
         this.mType = '5.oa.b.3_1';
 /*
+
 var r = Raphael(10, 50, 640, 480);
  "symbol":"s", axis:"0 0 1 1"});
 r.linechart(0, 0, 99, 99, [1,2,3,4,5], [[1,2,3,4,5], [1,3,9,16,25], [100,50,25,12,6]], {smooth: true, colors: ['#F00', '#0F0', '#FF0'], symbol: 'circle'});
@@ -43,6 +44,23 @@ r.linechart(0, 0, 99, 99, [1,2,3,4,5], [[1,2,3,4,5], [1,3,9,16,25], [100,50,25,1
 
 //var r = Raphael("simpleExample");
 //var linechart = r.g.linechart(10,10,300,220,[1,2,3,4,5],[10,20,15,35,30], {"colors":["#444"],
+
+
+this.mAnswerTextBox.setPosition(375,130);
+this.mAnswerTextBox2.setPosition(435,130);
+this.mAnswerTextBox.setSize(50,50);
+this.mAnswerTextBox2.setSize(50,50);
+//this.setQuestion('' + varA + ' / ' +  varB + ' = ');
+this.setQuestion('');
+this.setAnswer('' + 4,0);
+this.setAnswer('' + 4,1);   
+this.mHeadingAnswerLabel.setText('X');
+this.mHeadingAnswerLabel2.setText('Y'); 
+this.mHeadingAnswerLabel.setPosition(380,75);
+this.mHeadingAnswerLabel2.setPosition(440,75); 
+this.mHeadingAnswerLabel.setSize(25,25);
+this.mHeadingAnswerLabel2.setSize(25,25); 
+
 
 // graph coords
 var startX = 10;
@@ -52,8 +70,6 @@ var endY = 310;
 var width = endX - startX;
 var height = endY - startY;
 var range = [0,10];
-var pointsX = [2, 4, 6];
-var pointsY = [2, 4, 6];
 
 //var r = Raphael('graph');
 var rX1 = 10;
@@ -63,17 +79,35 @@ var rY2 = 480;
 
 var r = Raphael(rX1, rY1, rX2, rY2);
 
+
+
+var pointsX = [];
+var pointsY = [];
+
+pointsX[0] = Math.floor(Math.random()*3);
+pointsY[0] = Math.floor(Math.random()*3);
+
+var slopeX = Math.floor(Math.random()*4)+1;
+var slopeY = Math.floor(Math.random()*4)+1;
+
+pointsX[1] = pointsX[0] + slopeX;
+pointsY[1] = pointsY[0] + slopeY;
+
+pointsX[2] = pointsX[1] + slopeX;
+pointsY[2] = pointsY[1] + slopeY;
+
+
+
 var chart = new LineChart (this.mSheet.mGame,this,r,startX, startY, endX, endY,pointsX,pointsY,range,rX1,rY1,"#000000",false);
 
 this.addQuestionShape(chart);
-
 
 
  //raphael.clear();
 // this.mFractionBar = new LineOne (this.mSheet.mGame,raphael,x,y,x+length,y,"#000000",false);
 
 
-
+/*
 
         var x = 0;
         while (x < 1)
@@ -94,5 +128,7 @@ this.addQuestionShape(chart);
                 this.setQuestion('');
                 this.setAnswer(x,0);
         }
+*/
+
 }
 });
