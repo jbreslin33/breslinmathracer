9b05956781988a550dab71a2e30ee81fed86cfa1
jbreslin33 <jbreslin33@localhost>
2014-10-02 19:18:43 -0400

    fixed rebuild file for prequisites

--- src/src/database/rebuild.sh
+++ src/src/database/rebuild.sh
@@ -1,5 +1,11 @@
+if [ "$1" = "skip" ];
+then
+echo skipping build and insert
+else
+echo running build and insert
 sudo -u postgres psql -d jamesanthonybreslin -f src/database/build.sql
 sudo -u postgres psql -d jamesanthonybreslin -f src/database/insert.sql
+fi
 
 if [ -e src/database/insert_types.sql ]; 
 then
@@ -17,7 +23,7 @@ rm src/database/prerequisites.sql
 else
 touch src/database/prerequisites.sql
 fi
-> src/database/insert_types.sql
+> src/database/prerequisites.sql
 grep -rhI --exclude="*\.orig" --exclude-dir=database 'insert into prerequisites' ./ >> src/database/prerequisites.sql
 sudo -u postgres psql -d jamesanthonybreslin -f src/database/prerequisites.sql
 
