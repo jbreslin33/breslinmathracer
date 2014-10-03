b697a4c9d09e561953e822de8c44ea9ffd3768ca
jbreslin33 <jbreslin33@localhost>
2014-10-03 18:48:19 -0400

    cosmetic redid order.

--- src/src/core/math/g5_nbt_a_1.php
+++ src/src/core/math/g5_nbt_a_1.php
@@ -1,7 +1,8 @@
+
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.1_1',5.0401,'5.nbt.a.1','hundred thousand');
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.1_9',5.0409,'5.nbt.a.1','');
 */
-var i_5_nbt_a_1__1 = new Class(
+var i_5_nbt_a_1__9 = new Class(
 {
 Extends: TextItem,
 
@@ -9,13 +10,127 @@ initialize: function(sheet)
 {
         this.parent(sheet,375,50,220,75,150,50,525,100);
 
-        this.mType = '5.nbt.a.1_1';
+        this.mType = '5.nbt.a.1_9';
 
-        this.setQuestion('In the number 123456.789 what is the place with the digit 1 in it called?');
-        this.setAnswer('hundred thousands',0);
-        this.setAnswer('hundred thousands place',1);
-        this.setAnswer('hundred thousand',2);
-        this.setAnswer('hundred thousand place',3);
+        this.setQuestion('In the number 123456.789 what is the place with the digit 9 in it called?');
+        this.setAnswer('thousandths',0);
+        this.setAnswer('thousandths place',1);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.1_8',5.0408,'5.nbt.a.1','');
+*/
+var i_5_nbt_a_1__8 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,375,50,220,75,150,50,525,100);
+
+        this.mType = '5.nbt.a.1_8';
+
+        this.setQuestion('In the number 123456.789 what is the place with the digit 8 in it called?');
+        this.setAnswer('hundreths',0);
+        this.setAnswer('hundreths place',1);
+}
+});
+
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.1_7',5.0407,'5.nbt.a.1','');
+*/
+var i_5_nbt_a_1__7 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,375,50,220,75,150,50,525,100);
+
+        this.mType = '5.nbt.a.1_7';
+
+        this.setQuestion('In the number 123456.789 what is the place with the digit 7 in it called?');
+        this.setAnswer('tenths',0);
+        this.setAnswer('tenths place',1);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.1_6',5.0406,'5.nbt.a.1','');
+*/
+var i_5_nbt_a_1__6 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,375,50,220,75,150,50,525,100);
+
+        this.mType = '5.nbt.a.1_6';
+
+        this.setQuestion('In the number 123456.789 what is the place with the digit 6 in it called?');
+        this.setAnswer('ones',0);
+        this.setAnswer('ones place',1);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.1_5',5.0405,'5.nbt.a.1','');
+*/
+var i_5_nbt_a_1__5 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,375,50,220,75,150,50,525,100);
+
+        this.mType = '5.nbt.a.1_5';
+
+        this.setQuestion('In the number 123456.789 what is the place with the digit 5 in it called?');
+        this.setAnswer('tens',0);
+        this.setAnswer('tens place',1);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.1_4',5.0404,'5.nbt.a.1','');
+*/
+var i_5_nbt_a_1__4 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,375,50,220,75,150,50,525,100);
+
+        this.mType = '5.nbt.a.1_4';
+
+        this.setQuestion('In the number 123456.789 what is the place with the digit 4 in it called?');
+        this.setAnswer('hundreds',0);
+        this.setAnswer('hundreds place',1);
+}
+});
+
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.1_3',5.0403,'5.nbt.a.1','');
+*/
+var i_5_nbt_a_1__3 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,375,50,220,75,150,50,525,100);
+
+        this.mType = '5.nbt.a.1_3';
+
+        this.setQuestion('In the number 123456.789 what is the place with the digit 3 in it called?');
+        this.setAnswer('thousands',0);
+        this.setAnswer('thousands place',1);
 }
 });
 
@@ -41,9 +156,9 @@ initialize: function(sheet)
 });
 
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.1_3',5.0403,'5.nbt.a.1','');
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.1_1',5.0401,'5.nbt.a.1','hundred thousand');
 */
-var i_5_nbt_a_1__3 = new Class(
+var i_5_nbt_a_1__1 = new Class(
 {
 Extends: TextItem,
 
@@ -51,125 +166,14 @@ initialize: function(sheet)
 {
         this.parent(sheet,375,50,220,75,150,50,525,100);
 
-        this.mType = '5.nbt.a.1_3';
+        this.mType = '5.nbt.a.1_1';
 
-        this.setQuestion('In the number 123456.789 what is the place with the digit 3 in it called?');
-        this.setAnswer('thousands',0);
-        this.setAnswer('thousands place',1);
+        this.setQuestion('In the number 123456.789 what is the place with the digit 1 in it called?');
+        this.setAnswer('hundred thousands',0);
+        this.setAnswer('hundred thousands place',1);
+        this.setAnswer('hundred thousand',2);
+        this.setAnswer('hundred thousand place',3);
 }
 });
 
-/*
-insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.1_4',5.0404,'5.nbt.a.1','');
-*/
-var i_5_nbt_a_1__4 = new Class(
-{
-Extends: TextItem,
-
-initialize: function(sheet)
-{
-        this.parent(sheet,375,50,220,75,150,50,525,100);
-
-        this.mType = '5.nbt.a.1_4';
-
-        this.setQuestion('In the number 123456.789 what is the place with the digit 4 in it called?');
-        this.setAnswer('hundreds',0);
-        this.setAnswer('hundreds place',1);
-}
-});
-
-/*
-insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.1_5',5.0405,'5.nbt.a.1','');
-*/
-var i_5_nbt_a_1__5 = new Class(
-{
-Extends: TextItem,
-
-initialize: function(sheet)
-{
-        this.parent(sheet,375,50,220,75,150,50,525,100);
-
-        this.mType = '5.nbt.a.1_5';
-
-        this.setQuestion('In the number 123456.789 what is the place with the digit 5 in it called?');
-        this.setAnswer('tens',0);
-        this.setAnswer('tens place',1);
-}
-});
-
-/*
-insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.1_6',5.0406,'5.nbt.a.1','');
-*/
-var i_5_nbt_a_1__6 = new Class(
-{
-Extends: TextItem,
-
-initialize: function(sheet)
-{
-        this.parent(sheet,375,50,220,75,150,50,525,100);
-
-        this.mType = '5.nbt.a.1_6';
-
-        this.setQuestion('In the number 123456.789 what is the place with the digit 6 in it called?');
-        this.setAnswer('ones',0);
-        this.setAnswer('ones place',1);
-}
-});
-
-/*
-insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.1_7',5.0407,'5.nbt.a.1','');
-*/
-var i_5_nbt_a_1__7 = new Class(
-{
-Extends: TextItem,
-
-initialize: function(sheet)
-{
-        this.parent(sheet,375,50,220,75,150,50,525,100);
-
-        this.mType = '5.nbt.a.1_7';
-
-        this.setQuestion('In the number 123456.789 what is the place with the digit 7 in it called?');
-        this.setAnswer('tenths',0);
-        this.setAnswer('tenths place',1);
-}
-});
-
-/*
-insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.1_8',5.0408,'5.nbt.a.1','');
-*/
-var i_5_nbt_a_1__8 = new Class(
-{
-Extends: TextItem,
-
-initialize: function(sheet)
-{
-        this.parent(sheet,375,50,220,75,150,50,525,100);
-
-        this.mType = '5.nbt.a.1_8';
-
-        this.setQuestion('In the number 123456.789 what is the place with the digit 8 in it called?');
-        this.setAnswer('hundreths',0);
-        this.setAnswer('hundreths place',1);
-}
-});
-
-/*
-insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.1_9',5.0409,'5.nbt.a.1','');
-*/
-var i_5_nbt_a_1__9 = new Class(
-{
-Extends: TextItem,
-
-initialize: function(sheet)
-{
-        this.parent(sheet,375,50,220,75,150,50,525,100);
-
-        this.mType = '5.nbt.a.1_9';
-
-        this.setQuestion('In the number 123456.789 what is the place with the digit 9 in it called?');
-        this.setAnswer('thousandths',0);
-        this.setAnswer('thousandths place',1);
-}
-});
 
