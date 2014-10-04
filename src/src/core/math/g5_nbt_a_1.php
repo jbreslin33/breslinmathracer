5adbb4d1d5dc9551b1a951874f5e65259604a202
jbreslin33 <jbreslin33@localhost>
2014-10-04 19:36:47 -0400

    ten thousands place is in.

--- src/src/core/math/g5_nbt_a_1.php
+++ src/src/core/math/g5_nbt_a_1.php
@@ -15,6 +15,27 @@ initialize: function(sheet)
 }
 });
 
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.1_11',5.0411,'5.nbt.a.1','');
+*/
+var i_5_nbt_a_1__11 = new Class(
+{
+Extends: PlaceValueWholeNumberColors,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+
+        this.mType = '5.nbt.a.1_11';
+
+        var answer = parseInt(10000 * this.tenthousands);
+
+        this.setQuestion('What is the value of the red digit in the number ' + this.hundredthousands + '<span style="color: #f00;">' + this.tenthousands + '</span>' + '' + '' + this.thousands + ',' + this.hundreds + '' + this.tens + '' + this.ones);
+
+        this.setAnswer('' + answer,0);
+}
+});
+
 
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.1_10',5.0410,'5.nbt.a.1','');
