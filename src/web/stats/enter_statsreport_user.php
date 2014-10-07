d99911bacd2e3e0e2a7d11f5ab781440d9083977
jbreslin33 <jbreslin33@localhost>
2014-10-07 16:17:02 -0400

    sending username to statsreport

--- src/web/stats/enter_statsreport_user.php
+++ src/web/stats/enter_statsreport_user.php
@@ -18,9 +18,9 @@ $conn = dbConnect();
 include(getenv("DOCUMENT_ROOT") . "/web/navigation/top_links_user.php");
 echo "<br>";
 ?>
-        <p><b> Type date in form of: 2014-10-06 15:00:00 </p></b>
-        <form method="get" action="/web/stats/playing.php">
-Homework report start time: <input type="text" name="timefrom"><br>
+        <p><b> Enter username: </p></b>
+        <form method="get" action="/web/stats/statsreport.php">
+Username: <input type="text" name="username"><br>
         <p><input type="submit" value="UPDATE" /></p>
         </form>
 </body>
