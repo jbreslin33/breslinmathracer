cd39832416341017c09a8e4e1e2d0f5c09d47c21
jbreslin33 <jbreslin33@localhost>
2014-10-06 20:48:21 -0400

    updated home work.

--- src/web/stats/playing.php
+++ src/web/stats/playing.php
@@ -10,6 +10,14 @@
 <body>
 <?php
 session_start();
+$timefrom = '';
+if (empty($_GET)) {
+}
+else
+{
+	$timefrom = $_GET["timefrom"];
+}
+
 //db connection
 include(getenv("DOCUMENT_ROOT") . "/src/database/db_connect.php");
 $conn = dbConnect();
@@ -17,39 +25,31 @@ $conn = dbConnect();
 include(getenv("DOCUMENT_ROOT") . "/web/navigation/top_links_user.php");
 echo "<br>";
 ?>
-
-<?php
-$query = "select id from core_standards order by core_clusters_id, id;";
-$result = pg_query($conn,$query);
-$numrows = pg_numrows($result);
-?>
-        <p><b> Select Standard: </p></b>
+        <p><b> Type date in form of: 2014-10-06 15:00:00 </p></b>
         
-        <form method="post" action="/web/update/updatestandardid.php">
+        <form method="post" action="/web/stats/playing.php">
 
-<select name="standardid">
-
-<?php
-        // Loop on rows in the result set.
-        for($ri = 0; $ri < $numrows; $ri++) 
-        {
-                $row = pg_fetch_array($result, $ri);
-                echo "<option value=\"$row[0]\">$row[0]</option>";
-        }
-pg_free_result($result);
-?>
-
-</select>
-
+Time From: <input type="text" name="timefrom"><br>
         <p><input type="submit" value="UPDATE" /></p>
-
         </form>
 
 <p><b> Common Core Standards: </p></b>
 
 <?php
-//$query = "select id, description from core_standards order by core_clusters_id, id;";
-$query = "select item_attempts.start_time, users.username, users.first_name, users.last_name, item_attempts.item_types_id, item_attempts.transaction_code from item_attempts JOIN evaluations_attempts ON item_attempts.evaluations_attempts_id=evaluations_attempts.id JOIN users ON evaluations_attempts.user_id=users.id order BY item_attempts.start_time desc;";
+$query = '';
+if ($timefrom == '')
+{
+	$query = "select item_attempts.start_time, users.username, users.first_name, users.last_name, item_attempts.item_types_id, item_attempts.transaction_code from item_attempts JOIN evaluations_attempts ON item_attempts.evaluations_attempts_id=evaluations_attempts.id JOIN users ON evaluations_attempts.user_id=users.id where item_attempts.start_time > '2014-10-06 15:00:00' order BY item_attempts.start_time desc;"; 
+}
+else
+{
+	$query = "select item_attempts.start_time, users.username, users.first_name, users.last_name, item_attempts.item_types_id, item_attempts.transaction_code from item_attempts JOIN evaluations_attempts ON item_attempts.evaluations_attempts_id=evaluations_attempts.id JOIN users ON evaluations_attempts.user_id=users.id where item_attempts.start_time > '";
+$query .= $timefrom;
+$query .= "' order BY item_attempts.start_time desc;";
+}
+
+$equery = "insert into error_log (error_time,error,username) values (CURRENT_TIMESTAMP,'$timefrom','');";
+$eresult = pg_query($conn,$equery);
 
 $result = pg_query($conn,$query);
 $numrows = pg_numrows($result);
