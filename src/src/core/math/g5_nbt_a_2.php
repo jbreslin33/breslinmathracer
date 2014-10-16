f7c160dee12c173b9addc3383bf095d702becf5d
jbreslin33 <jbreslin33@localhost>
2014-10-15 21:37:30 -0400

    type12 5.nbt.a.1

--- src/src/core/math/g5_nbt_a_2.php
+++ src/src/core/math/g5_nbt_a_2.php
@@ -1,1 +1,22 @@
 //add
+//just throwing this here for now...
+/*
+var i_5_nbt_a_2__11 = new Class(
+{
+Extends: PlaceValueWholeNumberColors,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+
+        this.mType = '5.nbt.a.2_11';
+
+        var answer = parseInt(10000 * this.tenthousands);
+
+        this.setQuestion('What is the value of the red digit in the number ' + this.hundredthousands + '<span style="color: #f00;">' + this.tenthousands + '</span>' + '' + '' + this.thousands + ',' + this.hundreds + '' + this.tens + '' + this.ones);
+
+        this.setAnswer('' + answer,0);
+}
+});
+*/
+
