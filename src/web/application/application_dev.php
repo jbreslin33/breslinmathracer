20f3f022d561aa39951b34f582425ce30c8e65c9
jbreslin33 <jbreslin33@localhost>
2014-10-21 11:31:34 -0400

    adding min mootools.

--- src/web/application/application_dev.php
+++ src/web/application/application_dev.php
@@ -8,7 +8,7 @@
 <script type="text/javascript" src="/src/raphael/g.raphael-min.js"></script>
 <script type="text/javascript" src="/src/raphael/g.bar-min.js"></script>
 <script type="text/javascript" src="/src/raphael/g.line-min.js"></script>
-<script type="text/javascript" src="/src/mootools/mootools-core-1.4.5-full-compat.js"></script>
+<script type="text/javascript" src="/src/mootools/mootools-core-1.5.1-full-compat-yc.js"></script>
 
 <style type="text/css"> 
 .chartWrapper {
