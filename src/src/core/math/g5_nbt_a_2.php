23364c45e8e9e913d94c77fea9e656edb56f1e0c
jbreslin33 <jbreslin33@localhost>
2014-10-27 18:59:57 -0400

    2nd power

--- src/src/core/math/g5_nbt_a_2.php
+++ src/src/core/math/g5_nbt_a_2.php
@@ -18,6 +18,32 @@ function toFixed(x) {
 }
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.2_13',5.0513,'5.nbt.a.2','.001-.1');
+*/
+var i_5_nbt_a_2__13 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,600,50,320,95, 50,50,712,100);
+
+        this.mType = '5.nbt.a.2_13';
+
+        this.number = Math.floor(Math.random()*9)+1;
+
+        this.mBase = 10;
+        this.mExponent = 2;
+
+        this.setQuestion('What power of 10 will make this true: ' + '0.00' + this.number + ' &times ' + '__' + ' = 0.' + this.number + ' Sample Answer: 10^4');
+
+	var answer = '' + this.mBase + '^' + this.mExponent; 
+
+        this.setAnswer('' + answer,0);
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.2_12',5.0512,'5.nbt.a.2','.001-.01');
 */
 var i_5_nbt_a_2__12 = new Class(
@@ -30,13 +56,12 @@ initialize: function(sheet)
 
         this.mType = '5.nbt.a.2_12';
 
-        this.thousandths = Math.floor(Math.random()*9)+1;
+        this.number = Math.floor(Math.random()*9)+1;
 
         this.mBase = 10;
-        //this.mExponent = Math.floor(Math.random()*9)+1;
         this.mExponent = 1;
 
-        this.setQuestion('What power of 10 will make this true: ' + '0.00' + this.thousandths + ' &times ' + '__' + ' = 0.0' + this.thousandths + ' Sample Answer: 10^4');
+        this.setQuestion('What power of 10 will make this true: ' + '0.00' + this.number + ' &times ' + '__' + ' = 0.0' + this.number + ' Sample Answer: 10^4');
 
 	var answer = '' + this.mBase + '^' + this.mExponent; 
 
