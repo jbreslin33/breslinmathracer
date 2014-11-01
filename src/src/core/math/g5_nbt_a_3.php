faa73f2ff8baf412f023b7e807a87b5739855fe9
jbreslin33 <jbreslin33@localhost>
2014-11-01 17:08:30 -0400

    5.nbt.a.3_1 rfm.

--- src/src/core/math/g5_nbt_a_3.php
+++ src/src/core/math/g5_nbt_a_3.php
@@ -1,1 +1,32 @@
-//add
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.3_1',5.0601,'5.nbt.a.3','');
+*/
+var i_5_nbt_a_3__1 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,375,50,220,75,150,50,525,100);
+
+        this.mType = '5.nbt.a.3_1';
+
+        this.mNameMachine = new NameMachine();
+        this.ns = new NameSampler();
+
+        this.thousands = Math.floor((Math.random()*8)+1);
+        this.a = parseInt(this.thousands * 1000);
+        this.hundreds = Math.floor((Math.random()*8)+1);
+        this.b = parseInt(this.hundreds * 100);
+        this.tens = Math.floor((Math.random()*8)+1);
+        this.c = parseInt(this.tens * 10);
+        this.ones = Math.floor((Math.random()*8)+1);
+        this.d = this.ones;
+
+        this.setQuestion('Evaluate: ' + this.thousands + ' &times 1,000 + ' + this.hundreds + ' &times 100 + ' + this.tens + ' &times 10 + ' + this.ones);
+
+        this.setAnswer(parseInt(this.a + this.b + this.c + this.d),0);
+}
+});
+
--- src/src/core/math/picker_jim.php
+++ src/src/core/math/picker_jim.php
@@ -1311,6 +1311,48 @@ getItem: function(id)
                 return new i_5_nbt_a_2__48(this.mSheet);
         }
 
+	//5.nbt.a.3	
+	if (id == '5.nbt.a.3_1')
+        {
+                return new i_5_nbt_a_3__1(this.mSheet);
+        }
+	if (id == '5.nbt.a.3_2')
+        {
+                return new i_5_nbt_a_3__2(this.mSheet);
+        }
+	if (id == '5.nbt.a.3_3')
+        {
+                return new i_5_nbt_a_3__3(this.mSheet);
+        }
+	if (id == '5.nbt.a.3_4')
+        {
+                return new i_5_nbt_a_3__4(this.mSheet);
+        }
+	if (id == '5.nbt.a.3_5')
+        {
+                return new i_5_nbt_a_3__5(this.mSheet);
+        }
+	if (id == '5.nbt.a.3_6')
+        {
+                return new i_5_nbt_a_3__6(this.mSheet);
+        }
+	if (id == '5.nbt.a.3_7')
+        {
+                return new i_5_nbt_a_3__7(this.mSheet);
+        }
+	if (id == '5.nbt.a.3_8')
+        {
+                return new i_5_nbt_a_3__8(this.mSheet);
+        }
+	if (id == '5.nbt.a.3_9')
+        {
+                return new i_5_nbt_a_3__9(this.mSheet);
+        }
+	if (id == '5.nbt.a.3_10')
+        {
+                return new i_5_nbt_a_3__10(this.mSheet);
+        }
+
 	
 	/*** GRADE 6 ***/
 	//6.rp.a.1
