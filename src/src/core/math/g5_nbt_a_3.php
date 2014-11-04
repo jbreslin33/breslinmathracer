596d61893df8575a0d5db1c5c404f782183462fc
jbreslin33 <jbreslin33@localhost>
2014-11-03 19:51:19 -0500

    type25p61n18

--- src/src/core/math/g5_nbt_a_3.php
+++ src/src/core/math/g5_nbt_a_3.php
@@ -1,4 +1,34 @@
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.3_25',5.0625,'5.nbt.a.3','');
+*/
+var i_5_nbt_a_3__25 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '5.nbt.a.3_25';
+
+        this.ns = new NameMachine();
+
+        this.tens = Math.floor((Math.random()*9)+1);
+        this.ones = Math.floor((Math.random()*9)+1);
+        this.tenths = Math.floor((Math.random()*9)+1);
+        this.hundreths = Math.floor((Math.random()*9)+1);
+        this.thousandths = Math.floor((Math.random()*9)+1);
+
+        this.tens_ones = parseInt(this.tens * 10 + this.ones);
+	this.hundreths_thousandths = parseInt(this.hundreths * 10 + this.thousandths);
+
+        this.setQuestion('Write the decimal as you would say it in words: ' + this.tens + this.ones + '.' + this.tenths + this.hundreths + this.thousandths,0);
+
+        this.setAnswer('' + this.ns.getNumberName(this.tens_ones) + ' and ' + this.ns.getNumberName(this.tenths) + ' hundred ' + this.ns.getNumberName(this.hundreths_thousandths) + ' thousandths',0);
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.3_24',5.0624,'5.nbt.a.3','');
 */
 var i_5_nbt_a_3__24 = new Class(
