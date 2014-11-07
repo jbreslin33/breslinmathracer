c7e261a192b160d85a35f1239397b996437170e4
jbreslin33 <jbreslin33@localhost>
2014-11-06 20:50:53 -0500

    t11

--- src/src/core/math/g5_nbt_a_3_b.php
+++ src/src/core/math/g5_nbt_a_3_b.php
@@ -1,4 +1,62 @@
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.3.b_11',5.0711,'5.nbt.a.3.b','');
+*/
+var i_5_nbt_a_3_b__11 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '5.nbt.a.3.b_11';
+
+        this.ns = new NameSampler();
+
+        this.tenths      = Math.floor((Math.random()*3)+1);
+        this.hundreths   = Math.floor((Math.random()*9)+1);
+        this.thousandths_a = Math.floor((Math.random()*5)+5);
+        this.thousandths_b = Math.floor((Math.random()*4)+1);
+        this.tenths_hundreths_thousandths_a = parseInt(this.tenths * 100 + this.hundreths * 10 + this.thousandths_a);
+        this.tenths_hundreths_thousandths_b = parseInt(this.tenths * 100 + this.hundreths * 10 + this.thousandths_b);
+
+        this.setQuestion('' + this.ns.mNameOne  + ' makes a basketball hoop that can fit a ball in it that is 0.' + this.tenths_hundreths_thousandths_a + ' feet in diameter. ' + this.ns.mNameTwo + ' has a ball that is 0.' + this.tenths_hundreths_thousandths_b + ' inches thick in diameter. Will the ball ' + this.ns.mNameTwo + ' has fit in the hoop? Answer yes or no.',0);
+
+        this.setAnswer('yes',0);
+}
+});
+
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.3.b_10',5.0710,'5.nbt.a.3.b','');
+*/
+var i_5_nbt_a_3_b__10 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '5.nbt.a.3.b_10';
+
+        this.ns = new NameSampler();
+
+        this.tenths      = Math.floor((Math.random()*3)+1);
+        this.hundreths   = Math.floor((Math.random()*9)+1);
+        this.thousandths_a = Math.floor((Math.random()*5)+5);
+        this.thousandths_b = Math.floor((Math.random()*4)+1);
+        this.tenths_hundreths_thousandths_a = parseInt(this.tenths * 100 + this.hundreths * 10 + this.thousandths_a);
+        this.tenths_hundreths_thousandths_b = parseInt(this.tenths * 100 + this.hundreths * 10 + this.thousandths_b);
+
+        this.setQuestion('' + this.ns.mNameOne  + ' has a phone that is ' + '0.' + this.tenths_hundreths_thousandths_a + ' inches thick and ' + this.ns.mNameTwo + ' has a phone that is 0.' + this.tenths_hundreths_thousandths_b + ' inches thick. Who has the thinner phone?',0);
+
+        this.setAnswer('' + this.ns.mNameOne,0);
+        this.setAnswer('0.' + this.tenths_hundreths_thousandths_b,1);
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.3.b_9',5.0709,'5.nbt.a.3.b','');
 */
 var i_5_nbt_a_3_b__9 = new Class(
