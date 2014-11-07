850d3fec045b6b73f7696d9254675dd9c73161b1
jbreslin33 <jbreslin33@localhost>
2014-11-06 20:05:43 -0500

    t8p69

--- src/src/core/math/g5_nbt_a_3_b.php
+++ src/src/core/math/g5_nbt_a_3_b.php
@@ -1,3 +1,58 @@
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.3.b_8',5.0708,'5.nbt.a.3.b','');
+*/
+var i_5_nbt_a_3_b__8 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '5.nbt.a.3.b_8';
+
+        this.ns = new NameSampler();
+
+        this.tenths      = Math.floor((Math.random()*9)+1);
+        this.hundreths   = Math.floor((Math.random()*9)+1);
+        this.thousandths_a = Math.floor((Math.random()*5)+5);
+        this.thousandths_b = Math.floor((Math.random()*4)+1);
+        this.tenths_hundreths_thousandths_a = parseInt(this.tenths * 100 + this.hundreths * 10 + this.thousandths_a);
+        this.tenths_hundreths_thousandths_b = parseInt(this.tenths * 100 + this.hundreths * 10 + this.thousandths_b);
+
+        this.setQuestion('Would the number 0.' + this.tenths_hundreths_thousandths_b + ' go to the right or left of 0.' + this.tenths_hundreths_thousandths_a + ' on a number line?',0);
+
+        this.setAnswer('left',0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.3.b_7',5.0707,'5.nbt.a.3.b','');
+*/
+var i_5_nbt_a_3_b__7 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '5.nbt.a.3.b_7';
+
+        this.ns = new NameSampler();
+
+        this.tenths      = Math.floor((Math.random()*9)+1);
+        this.hundreths   = Math.floor((Math.random()*9)+1);
+        this.thousandths_a = Math.floor((Math.random()*5)+5);
+        this.thousandths_b = Math.floor((Math.random()*4)+1);
+        this.tenths_hundreths_thousandths_a = parseInt(this.tenths * 100 + this.hundreths * 10 + this.thousandths_a);
+        this.tenths_hundreths_thousandths_b = parseInt(this.tenths * 100 + this.hundreths * 10 + this.thousandths_b);
+
+        this.setQuestion('Would the number 0.' + this.tenths_hundreths_thousandths_a + ' go to the right or left of 0.' + this.tenths_hundreths_thousandths_b + ' on a number line?',0);
+
+        this.setAnswer('right',0);
+}
+});
 
 
 /*
