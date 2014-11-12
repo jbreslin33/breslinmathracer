ae8d4f1d125d61e4afa470576665f75a3e171b63
jbreslin33 <jbreslin33@localhost>
2014-11-12 12:53:12 -0500

    5.nbt.a.4 question works. needs answer.

--- src/src/core/math/g5_nbt_a_4.php
+++ src/src/core/math/g5_nbt_a_4.php
@@ -1,6 +1,6 @@
 
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.4_1',5.0801,'5.nbt.a.4','');
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.a.4_1',5.0801,'5.nbt.a.4','no nines');
 */
 var i_5_nbt_a_4__1 = new Class(
 {
@@ -14,14 +14,14 @@ initialize: function(sheet)
 
         this.ns = new NameSampler();
 
-        this.tenths      = Math.floor((Math.random()*3)+1);
+        this.tens      = Math.floor((Math.random()*9)+1);
+        this.ones      = Math.floor((Math.random()*8)+1);
+        this.tenths      = Math.floor((Math.random()*9)+1);
         this.hundreths   = Math.floor((Math.random()*9)+1);
-        this.thousandths_a = Math.floor((Math.random()*5)+5);
-        this.thousandths_b = Math.floor((Math.random()*4)+1);
-        this.tenths_hundreths_thousandths_a = parseInt(this.tenths * 100 + this.hundreths * 10 + this.thousandths_a);
-        this.tenths_hundreths_thousandths_b = parseInt(this.tenths * 100 + this.hundreths * 10 + this.thousandths_b);
+        this.thousandths = Math.floor((Math.random()*5)+5);
+        this.tenths_hundreths_thousandths = parseInt(this.tenths * 100 + this.hundreths * 10 + this.thousandths);
 
-        this.setQuestion('' + this.ns.mNameOne  + ' makes a basketball hoop that can fit a ball in it that is 0.' + this.tenths_hundreths_thousandths_b + ' feet in diameter. ' + this.ns.mNameTwo + ' has a ball that is 0.' + this.tenths_hundreths_thousandths_a + ' inches thick in diameter. Will the ball ' + this.ns.mNameTwo + ' has fit in the hoop? Answer yes or no.',0);
+        this.setQuestion('' + this.ns.mNameOne + ' weighed some ' + this.ns.mThingOne  + '. The scale read ' + this.tens + this.ones + '.' + this.tenths_hundreths_thousandths + ' grams. Round the total to the nearest gram.',0);
 
         this.setAnswer('no',0);
 }
