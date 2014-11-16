6c1fee2dfa6b51b0d422737fe62bb7fb6163e578
jbreslin33 <jbreslin33@localhost>
2014-11-16 13:27:36 -0500

    adding field for speed

--- src/src/database/patches.sql
+++ src/src/database/patches.sql
@@ -1,3 +1,3 @@
 update item_types SET active_code = 2 WHERE core_standards_id = '5.nbt.a.3';
 update item_types SET type_mastery = 4 WHERE core_standards_id = '3.oa.c.7';
-
+alter table item_types add speed integer NOT NULL DEFAULT 0;
