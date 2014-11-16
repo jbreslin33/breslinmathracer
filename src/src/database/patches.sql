3405099d02d1d39823f7ec3dad9424d122c6831b
jbreslin33 <jbreslin33@localhost>
2014-11-16 13:31:04 -0500

    times tables has speed of 5

--- src/src/database/patches.sql
+++ src/src/database/patches.sql
@@ -1,3 +1,4 @@
 update item_types SET active_code = 2 WHERE core_standards_id = '5.nbt.a.3';
 update item_types SET type_mastery = 4 WHERE core_standards_id = '3.oa.c.7';
 alter table item_types add speed integer NOT NULL DEFAULT 0;
+update item_types set speed = 5 where core_standards_id = '3.oa.c.7';
