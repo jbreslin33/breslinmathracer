c4f4f42eff78516f88a43495954ebc13ba4a2570
jbreslin33 <jbreslin33@localhost>
2014-11-16 10:59:47 -0500

    getting a random 2 times table on first go. but then it repeats. prob going to practice instead of timestable

--- src/web/php/timestables.php
+++ src/web/php/timestables.php
@@ -4,11 +4,11 @@ include(getenv("DOCUMENT_ROOT") . "/src/php/timestables.php");
 //start new session
 session_start();
 
-$typeid = $_GET["typeid"];
+$tablenumber = $_GET["tablenumber"];
 $start_new = $_GET["start_new"];
 
 //start new so pass 1 as first paremeter. pass the type id sent from client.
-$timestables = new TimesTables($typeid,$start_new,0);
+$timestables = new TimesTables($tablenumber,$start_new,0);
 
 include_once(getenv("DOCUMENT_ROOT") . "/web/php/full_return_string.php");
 ?>
