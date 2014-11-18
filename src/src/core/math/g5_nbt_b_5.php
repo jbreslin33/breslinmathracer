2cafb7dab008a874a48517fb4ea255546d0c3100
jbreslin33 <jbreslin33@localhost>
2014-11-18 16:45:54 -0500

    t1-5

--- src/src/core/math/g5_nbt_b_5.php
+++ src/src/core/math/g5_nbt_b_5.php
@@ -1,9 +1,9 @@
 //2x1,2x2,3x1,3x2,3x3,
 
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.5_3',5.0903,'5.nbt.b.5','2 by 3');
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.5_5',5.0905,'5.nbt.b.5','2 by 3');
 */
-var i_5_nbt_b_5__3 = new Class(
+var i_5_nbt_b_5__5 = new Class(
 {
 Extends: TextItem,
 
@@ -11,7 +11,7 @@ initialize: function(sheet)
 {
         this.parent(sheet,575,50,320,75,720,50,380,150);
 
-        this.mType = '5.nbt.b.5_3';
+        this.mType = '5.nbt.b.5_5';
 
         this.ns = new NameSampler();
 
@@ -27,9 +27,9 @@ initialize: function(sheet)
 
 
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.5_2',5.0902,'5.nbt.b.5','3 by 2');
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.5_4',5.0904,'5.nbt.b.5','3 by 2');
 */
-var i_5_nbt_b_5__2 = new Class(
+var i_5_nbt_b_5__4 = new Class(
 {
 Extends: TextItem,
 
@@ -37,7 +37,7 @@ initialize: function(sheet)
 {
         this.parent(sheet,575,50,320,75,720,50,380,150);
 
-        this.mType = '5.nbt.b.5_2';
+        this.mType = '5.nbt.b.5_4';
 
         this.ns = new NameSampler();
 
@@ -52,9 +52,9 @@ initialize: function(sheet)
 });
 
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.5_1',5.0901,'5.nbt.b.5','2 by 2');
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.5_3',5.0903,'5.nbt.b.5','2 by 2');
 */
-var i_5_nbt_b_5__1 = new Class(
+var i_5_nbt_b_5__3 = new Class(
 {
 Extends: TextItem,
 
@@ -62,7 +62,7 @@ initialize: function(sheet)
 {
         this.parent(sheet,575,50,320,75,720,50,380,150);
 
-        this.mType = '5.nbt.b.5_1';
+        this.mType = '5.nbt.b.5_3';
 
         this.ns = new NameSampler();
 
@@ -76,4 +76,53 @@ initialize: function(sheet)
 }
 });
 
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.5_2',5.0902,'5.nbt.b.5','1 by 3');
+*/
+var i_5_nbt_b_5__2 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '5.nbt.b.5_2';
+
+        this.ns = new NameSampler();
+
+        this.x        = Math.floor((Math.random()*900)+100);
+        this.y        = Math.floor((Math.random()*8)+2);
+
+        this.setQuestion('Find the Product: ' + this.y + ' &times ' + this.x + '');
+        this.answer = parseInt(this.x * this.y);
+
+        this.setAnswer('' + this.answer,0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.5_1',5.0901,'5.nbt.b.5','3 by 1');
+*/
+var i_5_nbt_b_5__1 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '5.nbt.b.5_1';
+
+        this.ns = new NameSampler();
+
+        this.x        = Math.floor((Math.random()*900)+100);
+        this.y        = Math.floor((Math.random()*8)+2);
+
+        this.setQuestion('Find the Product: ' + this.x + ' &times ' + this.y + '');
+        this.answer = parseInt(this.x * this.y);
+
+        this.setAnswer('' + this.answer,0);
+}
+});
 
