dde9dc82dbc851086af4a942561c943a9c713a10
jbreslin33 <jbreslin33@localhost>
2014-11-21 15:51:36 -0500

    fixed login glitch

---
+++ src/web/php/login_return_string.php
@@ -0,0 +1,12 @@
+<?php
+//fill php vars
+$returnString = "112,";
+$returnString .= $_SESSION["LOGGED_IN"];
+$returnString .= ",";
+$returnString .= $_SESSION["username"];
+$returnString .= ",";
+$returnString .= $_SESSION["first_name"];
+$returnString .= ",";
+$returnString .= $_SESSION["last_name"];
+echo $returnString;
+?>
