53e653c40d7d99008564345feda460bc9ed11758
jbreslin33 <jbreslin33@localhost>
2014-11-25 21:53:37 -0500

    4/2 no remainders

--- src/src/core/math/g5_nbt_b_6.php
+++ src/src/core/math/g5_nbt_b_6.php
@@ -1,7 +1,8 @@
+
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.6_1',5.1001,'5.nbt.b.6','3 d 2 no remainder');
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.6_3',5.1003,'5.nbt.b.6','4 d 2 no remainder');
 */
-var i_5_nbt_b_6__1 = new Class(
+var i_5_nbt_b_6__3 = new Class(
 {
 Extends: TextItem,
 
@@ -9,17 +10,17 @@ initialize: function(sheet)
 {
         this.parent(sheet,575,50,320,75,720,50,380,150);
 
-        this.mType = '5.nbt.b.6_1';
+        this.mType = '5.nbt.b.6_3';
 
         this.ns = new NameSampler();
 
 	this.x = 0;
 	this.y = 0;
-	this.z = parseInt(1111); 
-	while(this.z > 999 || this.z < 100 )
+	this.z = parseInt(11111); 
+	while(this.z > 9999 || this.z < 1000 )
 	{ 
         	this.x        = Math.floor((Math.random()*90)+10);
-        	this.y        = Math.floor((Math.random()*90)+10);
+        	this.y        = Math.floor((Math.random()*900)+100);
         	this.z        = parseInt(this.x * this.y);
 	}
 
@@ -62,4 +63,36 @@ initialize: function(sheet)
 }
 });
 
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.6_1',5.1001,'5.nbt.b.6','3 d 2 no remainder');
+*/
+var i_5_nbt_b_6__1 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '5.nbt.b.6_1';
+
+        this.ns = new NameSampler();
+
+	this.x = 0;
+	this.y = 0;
+	this.z = parseInt(1111); 
+	while(this.z > 999 || this.z < 100 )
+	{ 
+        	this.x        = Math.floor((Math.random()*90)+10);
+        	this.y        = Math.floor((Math.random()*90)+10);
+        	this.z        = parseInt(this.x * this.y);
+	}
+
+        this.setQuestion('Find the Quotient: ' + this.z + ' &divide ' + this.x + '');
+        this.answer = parseInt(this.y);
+
+        this.setAnswer('' + this.answer,0);
+}
+});
 
