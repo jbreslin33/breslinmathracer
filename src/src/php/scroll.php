7543a765f34ec53bcf86f4f504d53d83e0717eda
jbreslin33 <jbreslin33@localhost>
2014-11-28 18:10:17 -0500

    scroll.

--- src/src/php/scroll.php
+++ src/src/php/scroll.php
@@ -11,56 +11,26 @@ function __construct()
 }
 public function setScroll()
 {
-
-	$itemString = "hello i love you"; 
-        $_SESSION["scroll"] = $itemString;
-}
-/*
-public function fillTypesArray()
-{
-	$query = "select id, progression, type_mastery, core_standards_id from item_types where progression > "; 
-	$query .= $this->progression_counter; 
-	$query .= " AND progression < "; //stay in grade 
-	$query .= $this->progression_counter_limit; 
-	$query .= " AND active_code = 1 AND speed = 0"; //skip unactive and speed standards
-	$query .= " order by progression asc;";
-
+	$query = "select first_name, last_name from users order by score desc";
+	
 	$result = pg_query($this->mDatabaseConnection->getConn(),$query) or die('no connection: ' . pg_last_error());
        	$numberOfResults = pg_num_rows($result);
-                
+
+	$itemString = "Leader Board-- "; 
+
+	$place = 1;
 	for($i=0; $i < $numberOfResults; $i++)
         {
-		$this->id_array[]              = pg_Result($result, $i, 'id');	
-		$this->progression_array[]     = pg_Result($result, $i, 'progression');	
-		$this->core_standards_id_array = pg_Result($result, $i, 'core_standards_id');
-		$this->type_mastery_array      = pg_Result($result, $i, 'type_mastery');
+		$itemString .= "$place-- ";
+		$itemString .= pg_Result($result, $i, 'first_name');	
+		$itemString .= pg_Result($result, $i, 'last_name');	
+		$place++;
+		
+		$itemString .= "--";
 	}
+
+        $_SESSION["scroll"] = $itemString;
 }
-*/	
-/*	
-	//pink
-        $itemString =  $this->item_types_id_to_ask; //ask this one
-
-	//blue
-        $itemString .= ":";
-        $itemString .= "L=";
-        $itemString .= "$high_progression";
-        $itemString .= " U=";
-	$itemString .= count($unmastered_array);
-
-	//yellow	
-        $itemString .= ":";
-        $itemString .= "$high_standard";
-
-	//green
-        $itemString .= ":";
-        $itemString .= intval(count($score_array)); 
-
-        $_SESSION["raw_data"] = $itemString;
-        $_SESSION["item_types_id"] = $this->item_types_id_to_ask;
-        $_SESSION["item_types_id_progressed"] = $this->item_types_id_to_ask;
-}
-*/
 //end of class
 }
 ?>
