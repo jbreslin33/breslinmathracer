7167f3af90607c234b267663ad28494e5c063825
jbreslin33 <jbreslin33@localhost>
2014-12-02 09:23:25 -0500

    t2

--- src/src/core/math/g5_nbt_b_7.php
+++ src/src/core/math/g5_nbt_b_7.php
@@ -16,10 +16,11 @@ xx.xx
 x.xx
 
 */ 
+
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.7_1',5.1101,'5.nbt.b.7','0.xx+0.x');
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.7_2',5.1102,'5.nbt.b.7','x.xx+x.x');
 */
-var i_5_nbt_b_7__1 = new Class(
+var i_5_nbt_b_7__2 = new Class(
 {
 Extends: TextItem,
 
@@ -27,15 +28,17 @@ initialize: function(sheet)
 {
         this.parent(sheet,575,50,320,75,720,50,380,150);
 
-        this.mType = '5.nbt.b.7_1';
+        this.mType = '5.nbt.b.7_2';
 
         this.ns = new NameSampler();
 
         this.a = Math.floor(Math.random()*10);
         this.b = Math.floor(Math.random()*10);
         this.c = Math.floor(Math.random()*10);
+        this.d = Math.floor(Math.random()*10);
+        this.e = Math.floor(Math.random()*10);
         
-	this.decimalPart = parseInt(this.a * 10 + this.b + this.c * 10);
+	this.decimalPart = parseInt(this.b * 10 + this.c + this.e * 10);
 
 	this.answer = 0; 
 	if (this.decimalPart > 99)
@@ -62,10 +65,61 @@ initialize: function(sheet)
 		}	
 	}
 
-        this.setQuestion('Find the sum: 0.' + this.a + this.b + ' + 0.' + this.c + '');
+        this.setQuestion('Find the sum: ' + this.a + '.' + this.b + this.c + ' + ' + this.d + '.' + this.e);
 
         this.setAnswer('' + this.answer,0);
 }
 });
 
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.7_1',5.1101,'5.nbt.b.7','0.xx+0.x');
+*/
+var i_5_nbt_b_7__1 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '5.nbt.b.7_1';
+
+        this.ns = new NameSampler();
+
+        this.a = Math.floor(Math.random()*10);
+        this.b = Math.floor(Math.random()*10);
+        this.c = Math.floor(Math.random()*10);
+
+        this.decimalPart = parseInt(this.a * 10 + this.b + this.c * 10);
+
+        this.answer = 0;
+        if (this.decimalPart > 99)
+        {
+                this.decimalPart = parseInt(this.decimalPart - 100);
+                if (this.decimalPart < 10)
+                {
+                        this.answer = '1.0' + this.decimalPart;
+                }
+                else
+                {
+                        this.answer = '1.' + this.decimalPart;
+                }
+        }
+        else
+        {
+                if (this.decimalPart < 10)
+                {
+                        this.answer = '0.0' + this.decimalPart;
+                }
+                else
+                {
+                        this.answer = '0.' + this.decimalPart;
+                }
+        }
+
+        this.setQuestion('Find the sum: 0.' + this.a + this.b + ' + 0.' + this.c + '');
+
+        this.setAnswer('' + this.answer,0);
+}
+});
 
