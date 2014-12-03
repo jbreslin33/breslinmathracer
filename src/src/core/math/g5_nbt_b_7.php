7b97b5f9b04d2c8722c8868e884ae62cbc526fa8
jbreslin33 <jbreslin33@localhost>
2014-12-03 15:25:07 -0500

    t4

--- src/src/core/math/g5_nbt_b_7.php
+++ src/src/core/math/g5_nbt_b_7.php
@@ -18,6 +18,85 @@ x.xx
 */ 
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.7_4',5.1104,'5.nbt.b.7','x.xx+x.x');
+*/
+var i_5_nbt_b_7__4 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '5.nbt.b.7_4';
+
+        this.ns = new NameSampler();
+
+        this.a = Math.floor(Math.random()*10);
+        this.b = Math.floor(Math.random()*10);
+        this.c = Math.floor(Math.random()*10);
+        this.d = Math.floor(Math.random()*10);
+        this.e = Math.floor(Math.random()*10);
+        
+	this.decimalPart = parseInt(this.b * 10 + this.c + this.e * 10);
+	this.wholePart = parseInt(this.a + this.d);
+
+	this.answer = 0; 
+	if (this.decimalPart > 99)
+	{
+		this.decimalPart = parseInt(this.decimalPart - 100);
+		this.wholePart++; //add one for carry 
+
+		if (this.decimalPart < 10)
+		{
+			if (parseInt(this.decimalPart) == 0)
+			{
+				this.answer = '' + this.wholePart; 	 
+			}
+			else
+			{
+				this.answer = '' + this.wholePart + '.0' + this.decimalPart; //add a zero in tenths cause its less than 10	 
+			}
+		}
+		else
+		{
+			if (this.decimalPart % 10 == 0)	
+			{
+				this.decimalPart = parseInt(this.decimalPart / 10);
+			}
+			this.answer = '' + this.wholePart + '.' + this.decimalPart; 	 
+		}	
+	}
+	else
+	{
+                if (this.decimalPart < 10)
+                {
+                        if (parseInt(this.decimalPart) == 0)
+                        {
+                                this.answer = '' + this.wholePart;                                              
+                        }
+                        else
+                        {
+                                this.answer = '' + this.wholePart + '.0' + this.decimalPart; //add a zero in tenths cause its less than 10
+                        }
+                }
+                else
+                {
+			if (this.decimalPart % 10 == 0)	
+                        {
+				this.decimalPart = parseInt(this.decimalPart / 10);
+                        }
+                        this.answer = '' + this.wholePart + '.' + this.decimalPart;
+                }
+	}
+
+        this.setQuestion('Find the sum: ' + this.a + '.' + this.b + this.c + ' + ' + this.d + '.' + this.e);
+
+        this.setAnswer('' + this.answer,0);
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.7_3',5.1103,'5.nbt.b.7','x.x+0.xx');
 */
 var i_5_nbt_b_7__3 = new Class(
