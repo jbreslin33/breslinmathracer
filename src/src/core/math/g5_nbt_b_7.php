b5dd0395879ddd3a47f70a1f91fa95e575ccb7bd
jbreslin33 <jbreslin33@localhost>
2014-12-03 15:16:05 -0500

    t3

--- src/src/core/math/g5_nbt_b_7.php
+++ src/src/core/math/g5_nbt_b_7.php
@@ -18,6 +18,84 @@ x.xx
 */ 
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.7_3',5.1103,'5.nbt.b.7','x.x+0.xx');
+*/
+var i_5_nbt_b_7__3 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '5.nbt.b.7_3';
+
+        this.ns = new NameSampler();
+
+        this.a = Math.floor(Math.random()*10);
+        this.b = Math.floor(Math.random()*10);
+        this.c = Math.floor(Math.random()*10);
+        this.d = Math.floor(Math.random()*10);
+        
+	this.decimalPart = parseInt(this.b * 10 + this.c * 10 + this.d);
+	this.wholePart = parseInt(this.a);
+
+	this.answer = 0; 
+	if (this.decimalPart > 99)
+	{
+		this.decimalPart = parseInt(this.decimalPart - 100);
+		this.wholePart++; //add one for carry 
+
+		if (this.decimalPart < 10)
+		{
+			if (parseInt(this.decimalPart) == 0)
+			{
+				this.answer = '' + this.wholePart; 	 
+			}
+			else
+			{
+				this.answer = '' + this.wholePart + '.0' + this.decimalPart; //add a zero in tenths cause its less than 10	 
+			}
+		}
+		else
+		{
+			if (this.decimalPart % 10 == 0)	
+			{
+				this.decimalPart = parseInt(this.decimalPart / 10);
+			}
+			this.answer = '' + this.wholePart + '.' + this.decimalPart; 	 
+		}	
+	}
+	else
+	{
+                if (this.decimalPart < 10)
+                {
+                        if (parseInt(this.decimalPart) == 0)
+                        {
+                                this.answer = '' + this.wholePart;                                              
+                        }
+                        else
+                        {
+                                this.answer = '' + this.wholePart + '.0' + this.decimalPart; //add a zero in tenths cause its less than 10
+                        }
+                }
+                else
+                {
+			if (this.decimalPart % 10 == 0)	
+                        {
+				this.decimalPart = parseInt(this.decimalPart / 10);
+                        }
+                        this.answer = '' + this.wholePart + '.' + this.decimalPart;
+                }
+	}
+
+        this.setQuestion('Find the sum: ' + this.a + '.' + this.b + ' + 0.' + this.c + this.d);
+
+        this.setAnswer('' + this.answer,0);
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.7_2',5.1102,'5.nbt.b.7','x.xx+x.x');
 */
 var i_5_nbt_b_7__2 = new Class(
@@ -44,30 +122,24 @@ initialize: function(sheet)
 	this.answer = 0; 
 	if (this.decimalPart > 99)
 	{
-		APPLICATION.log('1');
 		this.decimalPart = parseInt(this.decimalPart - 100);
 		this.wholePart++; //add one for carry 
 
 		if (this.decimalPart < 10)
 		{
-			APPLICATION.log('2');
 			if (parseInt(this.decimalPart) == 0)
 			{
-				APPLICATION.log('3');
 				this.answer = '' + this.wholePart; 	 
 			}
 			else
 			{
-				APPLICATION.log('4');
 				this.answer = '' + this.wholePart + '.0' + this.decimalPart; //add a zero in tenths cause its less than 10	 
 			}
 		}
 		else
 		{
-			APPLICATION.log('5');
 			if (this.decimalPart % 10 == 0)	
 			{
-				APPLICATION.log('6');
 				this.decimalPart = parseInt(this.decimalPart / 10);
 			}
 			this.answer = '' + this.wholePart + '.' + this.decimalPart; 	 
@@ -75,27 +147,21 @@ initialize: function(sheet)
 	}
 	else
 	{
-		APPLICATION.log('7');
                 if (this.decimalPart < 10)
                 {
-			APPLICATION.log('8');
                         if (parseInt(this.decimalPart) == 0)
                         {
-				APPLICATION.log('9');
                                 this.answer = '' + this.wholePart;                                              
                         }
                         else
                         {
-				APPLICATION.log('10');
                                 this.answer = '' + this.wholePart + '.0' + this.decimalPart; //add a zero in tenths cause its less than 10
                         }
                 }
                 else
                 {
-			APPLICATION.log('11');
 			if (this.decimalPart % 10 == 0)	
                         {
-				APPLICATION.log('12');
 				this.decimalPart = parseInt(this.decimalPart / 10);
                         }
                         this.answer = '' + this.wholePart + '.' + this.decimalPart;
