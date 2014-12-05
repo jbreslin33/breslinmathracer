2f668ddef278594adc52b851607c810da3934a62
jbreslin33 <jbreslin33@localhost>
2014-12-04 20:16:45 -0500

    subtraction decimals done.

--- src/src/core/math/g5_nbt_b_7.php
+++ src/src/core/math/g5_nbt_b_7.php
@@ -1,3 +1,88 @@
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.7_10',5.1110,'5.nbt.b.7','x.xx-x.x');
+*/
+var i_5_nbt_b_7__10 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '5.nbt.b.7_10';
+
+        this.ns = new NameSampler();
+
+        this.a = 0;
+        this.b = 0;
+        this.c = 0;
+        this.d = 0;
+        this.e = 0;
+        this.f = 0;
+
+        this.partA = 0;
+        this.partB = 1;
+        this.part = 0;
+
+        while (this.partA < this.partB)
+        {
+                this.a = Math.floor(Math.random()*10);
+                this.b = Math.floor(Math.random()*10);
+                this.c = Math.floor(Math.random()*10);
+                this.d = Math.floor(Math.random()*10);
+                this.e = Math.floor(Math.random()*10);
+                this.f = 0;
+
+                this.partA = parseInt(this.a * 100 + this.b * 10 + this.c);
+                this.partB = parseInt(this.d * 100 + this.e * 10 + this.f);
+                this.part =  parseInt(this.partA - this.partB);
+        }
+
+	if (this.part > 99) // we are 3 digits 
+	{
+        	if (this.part % 100 == 0) // we have a whole number
+        	{
+                	this.answer = '' + this.part;
+        	}
+        	else if (this.part % 10 == 0) // we have a multiple of 10  
+        	{
+			var tensAndOnes = parseInt(this.part / 10); 
+			var ones = tensAndOnes / 10;
+			var tenths = tensAndOnes % 10;
+		
+                	this.answer = '' + ones + '.' + tenths;
+        	}
+        	else // we have pure 3 digit number  
+		{
+			var hundreds = parseInt(this.part / 100); 
+			var tensAndOnes = this.part % 100; 
+			this.answer = '' + hundreds + '.' + tensAndOnes;
+		}
+	}
+        else if (this.part <= 99) // we are 3 digits
+        {
+                if (this.part % 10 == 0) // we have a multiple of 10
+                {
+                        var tensAndOnes = parseInt(this.part / 10);
+                        var ones = parseInt(tensAndOnes / 10);
+                        var tenths = tensAndOnes % 10;
+                        this.answer = '' + ones + '.' + tenths;
+                }
+        	else // we have pure 3 digit number  
+		{
+			var hundreds = parseInt(this.part / 100); 
+			var tensAndOnes = this.part % 100; 
+			this.answer = '' + hundreds + '.' + tensAndOnes;
+		}
+        }
+
+        this.setQuestion('Find the difference: ' + this.a + '.' + this.b + this.c + ' - ' + this.d + '.' + this.e);
+
+        this.setAnswer('' + this.answer,0);
+}
+});
+
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.7_9',5.1109,'5.nbt.b.7','x.x-0.xx');
 */
@@ -81,8 +166,9 @@ initialize: function(sheet)
         this.setAnswer('' + this.answer,0);
 }
 });
+
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.7_8',5.1108,'5.nbt.b.7','x.xx-x.x');
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.7_8',5.1108,'5.nbt.b.7','xx.xx-x.x');
 */
 var i_5_nbt_b_7__8 = new Class(
 {
@@ -113,12 +199,40 @@ initialize: function(sheet)
                 this.b = Math.floor(Math.random()*10);
                 this.c = Math.floor(Math.random()*10);
                 this.d = Math.floor(Math.random()*10);
-                this.e = Math.floor(Math.random()*10);
-                this.f = 0;
 
-                this.partA = parseInt(this.a * 100 + this.b * 10 + this.c);
-                this.partB = parseInt(this.d * 100 + this.e * 10 + this.f);
+                this.e = 0;
+                this.f = Math.floor(Math.random()*10);
+                this.g = Math.floor(Math.random()*10);
+                this.h = 0;
+
+                this.partA = parseInt(this.a * 1000 + this.b * 100 + this.c * 10 + this.d);
+                this.partB = parseInt(this.e * 1000 + this.f * 100 + this.g * 10 + this.h);
                 this.part =  parseInt(this.partA - this.partB);
+        }
+        if (this.part > 999) // we are 3 digits
+        {
+                if (this.part % 1000 == 0) // we have a whole number
+                {
+                        this.answer = '' + this.part;
+                }
+                if (this.part % 100 == 0) // we have a whole number
+                {
+                        this.answer = '' + this.part;
+                }
+                else if (this.part % 10 == 0) // we have a multiple of 10
+                {
+                        var tensAndOnes = parseInt(this.part / 10);
+                        var ones = tensAndOnes / 10;
+                        var tenths = tensAndOnes % 10;
+
+                        this.answer = '' + ones + '.' + tenths;
+                }
+                else // we have pure 3 digit number
+                {
+                        var hundreds = parseInt(this.part / 100);
+                        var tensAndOnes = this.part % 100;
+                        this.answer = '' + hundreds + '.' + tensAndOnes;
+                }
         }
 
 	if (this.part > 99) // we are 3 digits 
@@ -159,7 +273,7 @@ initialize: function(sheet)
 		}
         }
 
-        this.setQuestion('Find the difference: ' + this.a + '.' + this.b + this.c + ' - ' + this.d + '.' + this.e);
+        this.setQuestion('Find the difference: ' + this.a + this.b + '.' + this.c + this.d + ' - ' + this.f + '.' + this.g);
 
         this.setAnswer('' + this.answer,0);
 }
