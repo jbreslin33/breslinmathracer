7f86edd6accfe4127a0442e3449e49ba7e51ff2b
jbreslin33 <jbreslin33@localhost>
2014-12-14 00:01:01 -0500

    t18 done with divisoin

--- src/src/core/math/g5_nbt_b_7.php
+++ src/src/core/math/g5_nbt_b_7.php
@@ -43,6 +43,76 @@ process:  function()
 });
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.7_18',5.1118,'5.nbt.b.7','5.55/5.55');
+*/
+var i_5_nbt_b_7__18 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '5.nbt.b.7_18';
+	
+	this.mUtility = new Utility();
+
+	this.answer = 'setme';
+
+        this.a = 0;
+        this.b = 0;
+        this.c = 0;
+        this.d = 0;
+        this.e = 0;
+        this.f = 0;
+
+        this.divisor  = 0;
+        this.quotient = 0;
+        this.dividend = 0;
+	this.remainder = 1;
+
+	while (this.divisor == 0 || this.dividend == 0 || this.remainder != 0  )
+	{
+        	this.a = Math.floor(Math.random()*10);
+        	this.b = Math.floor(Math.random()*10);
+        	this.c = Math.floor(Math.random()*10);
+        	this.d = Math.floor(Math.random()*10);
+        	this.e = Math.floor(Math.random()*10);
+        	this.f = Math.floor(Math.random()*10);
+
+        	this.dividend  = parseInt(this.a * 100 + this.b * 10 + this.c);
+        	this.divisor   = parseInt(this.d * 100 + this.e * 10 + this.f);
+		this.quotient  = parseInt(this.dividend / this.divisor);
+		this.remainder = this.dividend % this.divisor;
+	}
+	
+	//answer will slide all the way over to right of dividend so if its 3 spaces it will be equal to div etc
+	var q = '' + this.quotient;
+
+	if (q.length == 0)
+	{
+		//	
+	}	
+	if (q.length == 1)
+	{
+		this.answer = '' + this.quotient;	
+	}	
+	if (q.length == 2)
+	{
+		this.answer = '' + this.quotient;	
+	}	
+	if (q.length == 3)
+	{
+		this.answer = '' + this.quotient;	
+	}	
+	
+        this.setQuestion('Find the quotient: ' + this.a + '.' + this.b + this.c + ' &divide ' + this.d + '.' + this.e + this.f);
+        this.setAnswer('' + this.answer,0);
+}
+});
+
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.7_17',5.1117,'5.nbt.b.7','5.5/0.55');
 */
 var i_5_nbt_b_7__17 = new Class(
@@ -83,16 +153,10 @@ initialize: function(sheet)
         	this.divisor   = parseInt(               this.d * 10 + this.e);
 		this.quotient  = parseInt(this.dividend / this.divisor);
 		this.remainder = this.dividend % this.divisor;
-		
-		APPLICATION.log('dividend:' + this.dividend);
-		APPLICATION.log('divisor:'  + this.divisor);
-		APPLICATION.log('quotient:'  + this.quotient);
-		APPLICATION.log('remainder:'  + this.remainder);
 	}
 	
 	//answer will slide all the way over to right of dividend so if its 3 spaces it will be equal to div etc
 	var q = '' + this.quotient;
-	APPLICATION.log('q:'  + q);
 	//q = q * 10  
 
 	if (q.length == 0)
