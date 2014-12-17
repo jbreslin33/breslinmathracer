3e55c0df0c65b6f5866dafeccbf5f423806031a4
jbreslin33 <jbreslin33@localhost>
2014-12-16 22:36:40 -0500

    t4 works but got 0 in numerator

--- src/src/core/math/g5_nf_a_1.php
+++ src/src/core/math/g5_nf_a_1.php
@@ -1,4 +1,40 @@
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nf.a.1_4',5.1204,'5.nf.a.1','add mixed number to mixed number');
+*/
+var i_5_nf_a_1__4 = new Class(
+{
+Extends: TextItemFraction,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,350,50,200,95, 100,50,425,100, 100,50,425,175,true);
+
+        this.mType = '5.nf.a.1_4';
+
+        var an = 1;
+        var ad = 1;
+        var bn = 0;
+        var bd = 0;
+        while (an < ad || an % ad == 0 || bn < bd || bn % bd)
+        {
+                an = Math.floor((Math.random()*9)+10);
+                ad = Math.floor((Math.random()*9)+1);
+                bn = Math.floor((Math.random()*9)+1);
+                bd = Math.floor((Math.random()*9)+1);
+        }
+
+        var fractionA = new Fraction(an,ad,false);
+        var fractionB = new Fraction(bn,bd,false);
+
+        //fraction class will simplyfy
+        var answer = fractionA.add(fractionB);
+
+        this.setAnswer(answer.getString(),0);
+        this.setQuestion('Evaluate: ' + fractionA.getMixedNumber() + ' + ' + fractionB.getMixedNumber());
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('5.nf.a.1_3',5.1203,'5.nf.a.1','add mixed number to fraction');
 */
 var i_5_nf_a_1__3 = new Class(
