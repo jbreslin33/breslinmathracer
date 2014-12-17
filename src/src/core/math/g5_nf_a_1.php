8a87dd49f90f5eb27c27ad26829cad7102cb8eca
jbreslin33 <jbreslin33@localhost>
2014-12-16 22:11:03 -0500

    rfm.

--- src/src/core/math/g5_nf_a_1.php
+++ src/src/core/math/g5_nf_a_1.php
@@ -1,4 +1,41 @@
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nf.a.1_3',5.1203,'5.nf.a.1','add mixed number');
+*/
+var i_5_nf_a_1__3 = new Class(
+{
+Extends: TextItemFraction,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,350,50,200,95, 100,50,425,100, 100,50,425,175,true);
+
+        this.mType = '5.nf.a.1_3';
+
+        var an = 0;
+        var ad = 0;
+        var bn = 0;
+        var bd = 0;
+        while (bn == bd)
+        {
+                an = Math.floor((Math.random()*9)+1);
+                ad = 1;
+                bn = Math.floor((Math.random()*9)+1);
+                bd = Math.floor((Math.random()*9)+1);
+        }
+
+        var fractionA = new Fraction(an,ad);
+        var fractionB = new Fraction(bn,bd,false);
+
+        //fraction class will simplyfy
+        var answer = fractionA.add(fractionB);
+
+        this.setAnswer(answer.getString(),0);
+        this.setQuestion('Evaluate: ' + fractionA.getString() + ' + ' + fractionB.getString());
+}
+});
+
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('5.nf.a.1_2',5.1202,'5.nf.a.1','');
 */
 var i_5_nf_a_1__2 = new Class(
