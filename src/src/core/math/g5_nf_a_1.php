14173360aa1a1bfd603a147603420bdfec53e55e
jbreslin33 <jbreslin33@localhost>
2014-12-17 08:44:05 -0500

    t6

--- src/src/core/math/g5_nf_a_1.php
+++ src/src/core/math/g5_nf_a_1.php
@@ -1,4 +1,39 @@
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nf.a.1_6',5.1206,'5.nf.a.1','');
+*/
+var i_5_nf_a_1__6 = new Class(
+{
+Extends: TextItemFraction,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,350,50,200,95, 100,50,425,100, 100,50,425,175,true);
+
+        this.mType = '5.nf.a.1_6';
+
+        var an = 0;
+        var ad = 0;
+        var bn = 0;
+        var bd = 0;
+        while (bn == bd)
+        {
+                an = Math.floor((Math.random()*9)+1);
+                ad = 1;
+                bn = Math.floor((Math.random()*9)+1);
+                bd = Math.floor((Math.random()*9)+1);
+        }
+
+        var fractionA = new Fraction(an,ad);
+        var fractionB = new Fraction(bn,bd,false);
+
+        var answer = fractionA.subtract(fractionB);
+
+        this.setAnswer(answer.getString(),0);
+        this.setQuestion('Evaluate: ' + fractionA.getString() + ' - ' + fractionB.getString());
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('5.nf.a.1_5',5.1205,'5.nf.a.1','subtract no whole numbers');
 */
 var i_5_nf_a_1__5 = new Class(
