9f58cdf31277e970b21f12b775793c1b9a88da52
jbreslin33 <jbreslin33@localhost>
2014-12-26 14:23:12 -0500

    the izzy works

--- src/src/database/patches.sql
+++ src/src/database/patches.sql
@@ -1,3 +1,3 @@
 update item_types SET type_mastery = 10 where progression > 3.07 and progression < 3.08;
-insert into evaluations (description) values ('izzy');
+insert into evaluations (description) values ('The Izzy');
 alter table users add alltimeizzy integer default 0;
