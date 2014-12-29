b7470fc9f8bec8c0382a57d01605c71dc525c111
jbreslin33 <jbreslin33@localhost>
2014-12-29 14:34:04 -0500

    done

--- src/src/core/math/g5_nf_b_3.php
+++ src/src/core/math/g5_nf_b_3.php
@@ -1,5 +1,33 @@
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('5.nf.b.3_1',5.1401,'5.nf.b.3','');
+insert into item_types(id,progression,core_standards_id,description) values ('5.nf.b.3_2',5.1402,'5.nf.b.3','improper fraction');
+*/
+var i_5_nf_b_3__2 = new Class(
+{
+Extends: TextItemFraction,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,350,50,200,95, 100,50,425,100, 100,50,425,175,true);
+
+        this.mType = '5.nf.b.3_2';
+
+        var n = 0;
+        var d = 1; 
+        while (d > n)
+        {
+                n = Math.floor((Math.random()*19)+1);
+                d = Math.floor((Math.random()*19)+1);
+        }
+
+        var answer = new Fraction(n,d);
+
+        this.setAnswer(answer.getString(),0);
+        this.setQuestion('Write the quotient as a fraction: ' + n + ' &divide ' + d);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nf.b.3_1',5.1401,'5.nf.b.3','regular fraction');
 */
 var i_5_nf_b_3__1 = new Class(
 {
@@ -11,25 +39,18 @@ initialize: function(sheet)
 
         this.mType = '5.nf.b.3_1';
 
-        var an = 0;
-        var ad = 0;
-        var bn = 0;
-        var bd = 0;
-        while (an == ad || bn == bd || ad == bd)
+        var n = 1;
+        var d = 0;
+        while (n > d)
         {
-                an = Math.floor((Math.random()*9)+1);
-                ad = Math.floor((Math.random()*9)+1);
-                bn = Math.floor((Math.random()*9)+1);
-                bd = Math.floor((Math.random()*9)+1);
+                n = Math.floor((Math.random()*19)+1);
+                d = Math.floor((Math.random()*19)+1);
         }
 
-        var fractionA = new Fraction(an,ad);
-        var fractionB = new Fraction(bn,bd);
-
-        var answer = fractionA.add(fractionB);
+        var answer = new Fraction(n,d);
 
         this.setAnswer(answer.getString(),0);
-        this.setQuestion('Evaluate: ' + fractionA.getString() + ' + ' + fractionB.getString());
+        this.setQuestion('Write the quotient as a fraction: ' + n + ' &divide ' + d);
 }
 });
 
