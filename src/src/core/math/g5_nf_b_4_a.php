485cced9983bbc2cf0809fb3c28b7bcc29000b85
jbreslin33 <jbreslin33@localhost>
2014-12-31 19:04:51 -0500

    t1 rfm

--- src/src/core/math/g5_nf_b_4_a.php
+++ src/src/core/math/g5_nf_b_4_a.php
@@ -3,13 +3,14 @@ insert into item_types(id,progression,core_standards_id,description) values ('5.
 */
 var i_5_nf_b_4_a__1 = new Class(
 {
-Extends: TextItemFraction,
+Extends: TextItem,
 
 initialize: function(sheet)
 {
-        this.parent(sheet,350,50,200,95, 100,50,425,100, 100,50,425,175,true);
+	this.parent(sheet,575,50,320,75,720,50,380,150);
 
         this.mType = '5.nf.b.4.a_1';
+ 	this.ns = new NameSampler();
 
         a = Math.floor((Math.random()*8)+2);
         nb = Math.floor((Math.random()*8)+2);
@@ -19,7 +20,7 @@ initialize: function(sheet)
         var answer = new Fraction(parseInt(a*nb),db,true);
 
         this.setAnswer(answer.getString(),0);
-        this.setQuestion('Find the product: ' + a + ' &times ' + fractionb.getString());
+        this.setQuestion('' + this.ns.mNameOne + ' has ' + a + ' ' + this.ns.mThingOne + '. ' + fractionb.getString() + ' of them are ' + this.ns.mColorOne + '. How many ' + this.ns.mThingOne + ' are ' + this.ns.mColorOne);
 }
 });
 
