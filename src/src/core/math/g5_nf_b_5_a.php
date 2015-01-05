51c7dd97e855e0bd0d61d85f083ec77f63988b0b
jbreslin33 <jbreslin33@localhost>
2015-01-05 17:21:45 -0500

    t1

--- src/src/core/math/g5_nf_b_5_a.php
+++ src/src/core/math/g5_nf_b_5_a.php
@@ -1,5 +1,5 @@
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('5.nf.b.5.a_1',5.1801,'5.nf.b.5.a','scaling');
+insert into item_types(id,progression,core_standards_id,description) values ('5.nf.b.5.a_1',5.1801,'5.nf.b.5.a','scaling. compare. whole number by fraction by fraction');
 */
 var i_5_nf_b_5_a__1 = new Class(
 {
@@ -13,15 +13,21 @@ Extends: ThreeButtonItem,
                 this.mButtonA.setPosition(380,100);
                 this.mButtonB.setPosition(380,200);
                 this.mButtonC.setPosition(380,300);
+	
+		var na = 0;	
+		var da = 0;	
+		this.b = 0;
+		this.c = 0;
 
-                this.a = 0;
-                this.b = 0;
-
-                while (this.a <= this.b)
+                while (na <= da)
                 {
-                        this.a = Math.floor(Math.random()*10+1);
-                        this.b = Math.floor(Math.random()*10+1);
+                        na = Math.floor(Math.random()*18+2);
+                        da = Math.floor(Math.random()*18+2);
+                        this.b = Math.floor(Math.random()*18+2);
+                        this.c = this.b;
                 }
+                
+		this.mFractionA = new Fraction(na,da,false);
 
                 this.setQuestion('Compare.');
                 this.setAnswer('Is greater than.',0);
@@ -33,11 +39,11 @@ Extends: ThreeButtonItem,
 
         createQuestionShapes: function()
         {
-                var shapeA = new Shape(50,50,240,200,this.mSheet.mGame,"","","");
-                var shapeB = new Shape(50,50,530,200,this.mSheet.mGame,"","","");
+                var shapeA = new Shape(100,100,240,200,this.mSheet.mGame,"","","");
+                var shapeB = new Shape(100,100,530,200,this.mSheet.mGame,"","","");
 
-                shapeA.setText(this.a);
-                shapeB.setText(this.b);
+                shapeA.setText(this.b + ' &times ' + this.mFractionA.getString());
+                shapeB.setText(this.c);
 
                 this.addQuestionShape(shapeA);
                 this.addQuestionShape(shapeB);
