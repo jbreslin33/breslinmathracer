a4c13b60c88df2fb2d85fcf8d86da95ba6551d0d
jbreslin33 <jbreslin33@localhost>
2015-01-06 18:16:37 -0500

    t4 rfm

--- src/src/core/math/g5_nf_b_5_a.php
+++ src/src/core/math/g5_nf_b_5_a.php
@@ -1,5 +1,57 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nf.b.5.a_4',5.1804,'5.nf.b.5.a','scaling. compare. fraction by fraction by fraction. >');
+*/
+var i_5_nf_b_5_a__4 = new Class(
+{
+Extends: ThreeButtonItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet);
+                this.mType = '5.nf.b.5.a_4';
+
+                //BUTTON A
+                this.mButtonA.setPosition(380,100);
+                this.mButtonB.setPosition(380,200);
+                this.mButtonC.setPosition(380,300);
+
+                var na = 0;
+                var da = 0;
+                this.b = 0;
+                this.c = 0;
+
+                while (na >= da)
+                {
+                        na = Math.floor(Math.random()*18+2);
+                        da = Math.floor(Math.random()*18+2);
+                        this.b = Math.floor(Math.random()*18+2);
+                        this.c = this.b;
+                }
+
+                this.mFractionA = new Fraction(na,da,false);
+
+                this.setQuestion('Compare.');
+                this.setAnswer('&lt;',0);
+
+                this.mButtonA.setAnswer('&gt;');
+                this.mButtonB.setAnswer('=');
+                this.mButtonC.setAnswer('&lt;');
+        },
+
+        createQuestionShapes: function()
+        {
+                var shapeA = new Shape(100,100,240,200,this.mSheet.mGame,"","","");
+                var shapeB = new Shape(100,100,530,200,this.mSheet.mGame,"","","");
+
+                shapeA.setText(this.b + ' &times ' + this.mFractionA.getString());
+                shapeB.setText(this.c);
+
+                this.addQuestionShape(shapeA);
+                this.addQuestionShape(shapeB);
+        }
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('5.nf.b.5.a_3',5.1803,'5.nf.b.5.a','scaling. compare. whole number by fraction by fraction. <');
 */
 var i_5_nf_b_5_a__3 = new Class(
