5c4aa2df1583944f633d9944a4a999a5cbdc303c
jbreslin33 <jbreslin33@localhost>
2015-01-07 21:32:33 -0500

    u

--- src/web/navigation/top_links_user.php
+++ src/web/navigation/top_links_user.php
@@ -13,6 +13,7 @@
 <li><a href="/web/update/update_password_form.php">Update Password</a></li>
 <li><a href="/web/update/update_first_name_form.php">Update First Name</a></li>
 <li><a href="/web/update/update_last_name_form.php">Update Last Name</a></li>
+<li><a href="/web/update/update_room_form.php">Update Room</a></li>
 <li><a href="/web/update/update_teacher_form.php">Update Teacher</a></li>
 <li><a href="/web/insert/upload.php">Upload School From Linux</a></li>
 </ul>
