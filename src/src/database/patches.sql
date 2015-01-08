9dee0ab8c241c3f4a5ce0fa01981ed1ca96f389b
jbreslin33 <jbreslin33@localhost>
2015-01-07 21:28:39 -0500

    patches

--- src/src/database/patches.sql
+++
@@ -1,4 +0,0 @@
-update item_types SET type_mastery = 10 where progression > 3.07 and progression < 3.08;
-insert into evaluations (description) values ('The Izzy');
-alter table users add alltimeizzy integer default 0;
-alter table users add room_id  integer default 0;
