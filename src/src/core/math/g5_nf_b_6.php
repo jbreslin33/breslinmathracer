a209e3b92f9d4179ad6a9684978d88b6d287447c
jbreslin33 <jbreslin33@localhost>
2015-01-12 20:23:56 -0500

    change numbersmake sure denom not same as whole number

--- src/src/core/math/g5_nf_b_6.php
+++ src/src/core/math/g5_nf_b_6.php
@@ -1,3 +1,4 @@
+
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('5.nf.b.6_1',5.2001,'5.nf.b.6','');
 */
@@ -6,7 +7,7 @@ var i_5_nf_b_6__1 = new Class(
 Extends: TextItem,
         initialize: function(sheet)
         {
-                this.parent(sheet);
+ 		this.parent(sheet,300,50,175,95,100,50,425,100);
                 this.mType = '5.nf.b.6_1';
    		this.ns = new NameSampler();
 
@@ -21,7 +22,6 @@ Extends: TextItem,
                         fractionB.mNumerator = Math.floor(Math.random()*8+2);
 			answer = fractionA.multiply(fractionB);
                 }
-
 
                 this.setQuestion('' + this.ns.mNameOne + ' sold ' + fractionB.mNumerator + ' pieces of fruit today. ' + fractionA.getString() + ' were ' + this.ns.mFruitOne + '. How many ' + this.ns.mFruitOne + ' did ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' sell today?');
 
