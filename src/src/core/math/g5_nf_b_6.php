c5770d33b3cf2e73d37eaf154d90e2318c76669c
jbreslin33 <jbreslin33@localhost>
2015-01-12 20:01:08 -0500

    needs to be whole number answer

--- src/src/core/math/g5_nf_b_6.php
+++ src/src/core/math/g5_nf_b_6.php
@@ -3,50 +3,31 @@ insert into item_types(id,progression,core_standards_id,description) values ('5.
 */
 var i_5_nf_b_6__1 = new Class(
 {
-Extends: ThreeButtonItem,
+Extends: TextItem,
         initialize: function(sheet)
         {
                 this.parent(sheet);
                 this.mType = '5.nf.b.6_1';
-
-                //BUTTON A
-                this.mButtonA.setPosition(380,100);
-                this.mButtonB.setPosition(380,200);
-                this.mButtonC.setPosition(380,300);
+   		this.ns = new NameSampler();
 
                 var na = 0;
                 var da = 0;
-                this.b = 0;
-                this.c = 0;
+                var b = 0;
 
-                while (na <= da)
+                while (na >= da)
                 {
-                        na = Math.floor(Math.random()*18+2);
-                        da = Math.floor(Math.random()*18+2);
-                        this.b = Math.floor(Math.random()*18+2);
-                        this.c = this.b;
+                        na = Math.floor(Math.random()*8+2);
+                        da = Math.floor(Math.random()*8+2);
+                        b = Math.floor(Math.random()*8+2);
                 }
 
-                this.mFractionA = new Fraction(na,da,false);
+                var fractionA = new Fraction(na,da,false);
+                var fractionB = new Fraction(b,1,false);
+		var answer = fractionA.multiply(fractionB);
 
-                this.setQuestion('Compare.');
-                this.setAnswer('&gt;',0);
+                this.setQuestion('' + this.ns.mNameOne + ' sold ' + b + ' pieces of fruit today. ' + fractionA.getString() + ' were ' + this.ns.mFruitOne + '. How many ' + this.ns.mFruitOne + ' did ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' sell today?');
 
-                this.mButtonA.setAnswer('&gt;');
-                this.mButtonB.setAnswer('=');
-                this.mButtonC.setAnswer('&lt;');
-        },
-
-        createQuestionShapes: function()
-        {
-                var shapeA = new Shape(100,100,240,200,this.mSheet.mGame,"","","");
-                var shapeB = new Shape(100,100,530,200,this.mSheet.mGame,"","","");
-
-                shapeA.setText(this.b + ' &times ' + this.mFractionA.getString());
-                shapeB.setText(this.c);
-
-                this.addQuestionShape(shapeA);
-                this.addQuestionShape(shapeB);
+                this.setAnswer('' + answer.getString(),0);
         }
 });
 
