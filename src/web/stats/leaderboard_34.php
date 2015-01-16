783ef647a42e494218e5717e002b97a4619e25e7
jbreslin33 <jbreslin33@localhost>
2015-01-16 15:07:37 -0500

    jefferson

--- src/web/stats/leaderboard_34.php
+++ src/web/stats/leaderboard_34.php
@@ -43,7 +43,7 @@ echo '<table border=\"1\">';
 	$score = '';
 	$unmastered = '';
 
-	$query = "select last_activity, first_name, last_name, score, unmastered from users where room_id = 34 order by score desc;";
+	$query = "select last_activity, first_name, last_name, score, unmastered from users where room_id = 34 and banned_id = 0 order by score desc;";
 	$result = pg_query($conn,$query);
 	$numrows = pg_numrows($result);
 
