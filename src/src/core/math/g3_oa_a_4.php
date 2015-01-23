2192be47ef81f58ad9a4255e0e3a0bccac2dc9b1
jbreslin33 <jbreslin33@localhost>
2015-01-22 22:55:36 -0500

    t2

--- src/src/core/math/g3_oa_a_4.php
+++ src/src/core/math/g3_oa_a_4.php
@@ -1,3 +1,28 @@
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('3.oa.a.4_2',3.0402,'3.oa.a.4','');
+*/
+var i_3_oa_a_4__2 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '3.oa.a.4_2';
+
+        this.ns = new NameSampler();
+
+        var a = Math.floor((Math.random()*4)+6);
+        var b = Math.floor((Math.random()*4)+6);
+        var c = parseInt(a*b);
+
+        this.setQuestion('Which number makes the equation true? ' + c + ' &divide __ = ' + b);
+        this.setAnswer('' + a,0);
+}
+});
+
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('3.oa.a.4_1',3.0401,'3.oa.a.4','');
 */
