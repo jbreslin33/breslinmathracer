f51d48b8af82274fab089b12609bb24ec5ce1aa4
jbreslin33 <jbreslin33@localhost>
2015-01-22 23:55:52 -0500

    c

--- src/src/core/math/g4_nbt_b_6.php
+++ src/src/core/math/g4_nbt_b_6.php
@@ -1,57 +1,5 @@
 
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.b.6_1',4.1101,'4.nbt.b.6','divide number up to 4 digit by one digit number');
-*/
-
-var i_4_nbt_b_6__1 = new Class(
-{
-Extends: TextItem2,
-   initialize: function(sheet)
-   {
-      this.parent(sheet);
-      this.mType = '4.nbt.b.6_1';
-       	
-		  var varA = 0;
-		  var varB = 0;
-		  var varC = 0;
-		  var varD = 0;
-
-		  var start = 0;
-		  var end = 0;
-		  var rand = 0;
-
-		
-			// pick number of digits (1 - 4)
-			rand = 2 + Math.floor((Math.random()*3));
-
-			// get start number based on digits
-			start = Math.pow(10, rand-1);
-
-			// get end number based on digits
-			end = Math.pow(10, rand);
-
-			// pick number from start to end range
-			varA = start + Math.floor(Math.random()*(end-start));	
-
-			// pick one digit number
-			varB = 2 + Math.floor(Math.random()*8);
-
-			//varC = Math.floor(varA / varB);	
-				
-			varC = parseInt(Math.floor(varA / varB));
-
-			varD =  parseInt(varA % varB);
-								                       
-				this.setQuestion('' + varA + ' / ' +  varB + ' = ');
-        this.setAnswer('' + varC,0);
-        this.setAnswer('' + varD,1);   
-
-        this.mHeadingAnswerLabel.setText('Quotient');
-        this.mHeadingAnswerLabel2.setText('Remainder');        
-   }
-});
-
-/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.b.6_2',4.1102,'4.nbt.b.6','divide number up to 4 digit by one digit number');
 */
 
@@ -102,7 +50,6 @@ Extends: ThreeButtonItem,
       this.shuffle(1);
    }
 });
-
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.b.6_3',4.1103,'4.nbt.b.6','divide number up to 4 digit by one digit number');
 */
@@ -163,3 +110,57 @@ Extends: ThreeButtonItem,
        this.shuffle(1);this.shuffle(1);
    }
 });
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.b.6_1',4.1101,'4.nbt.b.6','divide number up to 4 digit by one digit number');
+*/
+
+var i_4_nbt_b_6__1 = new Class(
+{
+Extends: TextItem2,
+   initialize: function(sheet)
+   {
+      this.parent(sheet);
+      this.mType = '4.nbt.b.6_1';
+       	
+		  var varA = 0;
+		  var varB = 0;
+		  var varC = 0;
+		  var varD = 0;
+
+		  var start = 0;
+		  var end = 0;
+		  var rand = 0;
+
+		
+			// pick number of digits (1 - 4)
+			rand = 2 + Math.floor((Math.random()*3));
+
+			// get start number based on digits
+			start = Math.pow(10, rand-1);
+
+			// get end number based on digits
+			end = Math.pow(10, rand);
+
+			// pick number from start to end range
+			varA = start + Math.floor(Math.random()*(end-start));	
+
+			// pick one digit number
+			varB = 2 + Math.floor(Math.random()*8);
+
+			//varC = Math.floor(varA / varB);	
+				
+			varC = parseInt(Math.floor(varA / varB));
+
+			varD =  parseInt(varA % varB);
+								                       
+				this.setQuestion('' + varA + ' / ' +  varB + ' = ');
+        this.setAnswer('' + varC,0);
+        this.setAnswer('' + varD,1);   
+
+        this.mHeadingAnswerLabel.setText('Quotient');
+        this.mHeadingAnswerLabel2.setText('Remainder');        
+   }
+});
+
+
