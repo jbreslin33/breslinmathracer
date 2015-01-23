9bc4884b6558542e3387f43b3a53fc3f85fdb2ce
jbreslin33 <jbreslin33@localhost>
2015-01-22 21:01:27 -0500

    remediate

--- src/src/database/patches.sql
+++ src/src/database/patches.sql
@@ -1,1 +1,9 @@
-alter table users add work_it_id text;
+CREATE TABLE remediate (
+        id SERIAL,
+        item_types_id text NOT NULL,
+        user_id integer NOT NULL,
+        PRIMARY KEY (id),
+        FOREIGN KEY (item_types_id) REFERENCES item_types(id),
+        FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
