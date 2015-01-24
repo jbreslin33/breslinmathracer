046a58e70da288bccfad82304ab6c26c80845853
jbreslin33 <jbreslin33@localhost>
2015-01-23 19:12:02 -0500

    t2 rfm.

--- src/src/core/math/g5_nf_b_7_a.php
+++ src/src/core/math/g5_nf_b_7_a.php
@@ -1,5 +1,35 @@
+
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('5.nf.b.7.a_1',5.2101,'5.nf.b.7.a','1/3');
+insert into item_types(id,progression,core_standards_id,description) values ('5.nf.b.7.a_2',5.2102,'5.nf.b.7.a','');
+*/
+var i_5_nf_b_7_a__2 = new Class(
+{
+Extends: TextItemMixedNumber,
+        initialize: function(sheet)
+        {
+                this.parent(sheet, 320,100,200,95, 100,50,510,137, 100,50,625,100, 100,50,625,175,true);
+
+                this.mType = '5.nf.b.7.a_2';
+                this.ns = new NameSampler();
+
+                var whole = new Fraction(1,1,true);
+                var fractionA = new Fraction(1,1,true);
+                fractionA.mDenominator = Math.floor(Math.random()*8+2);
+
+                var fractionB = new Fraction(3,1,false);
+
+                fractionC = whole.subtract(fractionA);
+
+                answer = fractionA.divide(fractionB);
+                answer.reduce();
+
+		this.setQuestion('' + this.ns.mNameOne + ' had some ' + this.ns.mRopeOne + ' that was ' + fractionA.getString() + ' ' + this.ns.mDistanceIncrementSmall + ' long.');    
+                this.setAnswer('' + answer.getString(),0);
+        }
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nf.b.7.a_1',5.2101,'5.nf.b.7.a','');
 */
 var i_5_nf_b_7_a__1 = new Class(
 {
@@ -26,6 +56,3 @@ Extends: TextItemMixedNumber,
                 this.setAnswer('' + answer.getString(),0);
         }
 });
-
-                //this.setQuestion('' + this.ns.mNameOne + ' has his own bedroom. ' +  + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,1) + ' need for ' + fractionB.mNumerator + ' servings?');
-
