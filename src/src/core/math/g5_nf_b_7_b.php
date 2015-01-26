786358611e6d5972c3be500373dbda8db175305f
jbreslin33 <jbreslin33@localhost>
2015-01-26 16:20:10 -0500

    t1 almost done

--- src/src/core/math/g5_nf_b_7_b.php
+++ src/src/core/math/g5_nf_b_7_b.php
@@ -18,10 +18,10 @@ Extends: TextItemFraction,
                 var fractionB = new Fraction(1,1,false);
                 fractionB.mDenominator = Math.floor(Math.random()*8+2);
 
-                answer = fractionA.multiply(fractionB);
+                answer = fractionA.divide(fractionB);
                 answer.reduce();
 
-		this.setQuestion('' + this.ns.mNameOne + ' has ' + fractionA.mNumerator + ' ' this.ns.mVegetableOne + '. Each ' + this.ns.mVegetableOne + ' is cut into ' + this.ns.mNameMachine.getDenominatorName(fractionA.mDenominator) + '. How many pieces ' + this.ns.mVegetableOne + ' does ' + this.ns.mNameOne + ' have now?');    
+		this.setQuestion('' + this.ns.mNameOne + ' has ' + fractionA.mNumerator + ' ' + this.ns.mVegetableOne + '. Each ' + this.ns.mVegetableOne + ' is cut into ' + this.ns.mNameMachine.getDenominatorName(fractionB.mDenominator) + '. How many pieces of ' + this.ns.mVegetableOne + ' does ' + this.ns.mNameOne + ' have now?');    
                 this.setAnswer('' + answer.getString(),0);
         }
 });
