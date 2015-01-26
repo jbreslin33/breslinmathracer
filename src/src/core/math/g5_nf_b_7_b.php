1feeb4e10421138a6bbc4d1093bee4056c9a029b
jbreslin33 <jbreslin33@localhost>
2015-01-26 17:03:49 -0500

    t2 rfm

--- src/src/core/math/g5_nf_b_7_b.php
+++ src/src/core/math/g5_nf_b_7_b.php
@@ -1,5 +1,32 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nf.b.7.b_2',5.2202,'5.nf.b.7.b','');
+*/
+var i_5_nf_b_7_b__2 = new Class(
+{
+Extends: TextItemFraction,
+        initialize: function(sheet)
+        {
+                this.parent(sheet,340,50,190,95, 100,50,425,100, 100,50,425,175,true);
+
+                this.mType = '5.nf.b.7.b_2';
+                this.ns = new NameSampler();
+
+                var fractionA = new Fraction(1,1,true);
+                fractionA.mNumerator = Math.floor(Math.random()*8+2);
+
+                var fractionB = new Fraction(1,1,false);
+                fractionB.mDenominator = Math.floor(Math.random()*8+2);
+
+                answer = fractionA.divide(fractionB);
+                answer.reduce();
+
+                this.setQuestion('' + this.ns.mNameOne + ' has ' + fractionA.mNumerator + ' ' + this.ns.mVegetableOne + '. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' cuts each of the ' + this.ns.mVegetableOne + ' into ' + this.ns.mNameMachine.getDenominatorName(fractionB.mDenominator) + '. How many pieces of ' + this.ns.mVegetableOne + ' does ' + this.ns.mNameOne + ' have now?');
+                this.setAnswer('' + answer.getString(),0);
+        }
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('5.nf.b.7.b_1',5.2201,'5.nf.b.7.b','');
 */
 var i_5_nf_b_7_b__1 = new Class(
