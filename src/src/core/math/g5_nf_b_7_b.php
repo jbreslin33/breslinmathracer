6fa867af64f3089283eedbcc5531fe8552c54ea9
jbreslin33 <jbreslin33@localhost>
2015-01-27 18:31:12 -0500

    t8

--- src/src/core/math/g5_nf_b_7_b.php
+++ src/src/core/math/g5_nf_b_7_b.php
@@ -1,5 +1,32 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nf.b.7.b_8',5.2208,'5.nf.b.7.b','');
+*/
+var i_5_nf_b_7_b__8 = new Class(
+{
+Extends: TextItemFraction,
+        initialize: function(sheet)
+        {
+                this.parent(sheet,340,50,190,95, 100,50,425,100, 100,50,425,175,true);
+
+                this.mType = '5.nf.b.7.b_8';
+                this.ns = new NameSampler();
+
+                var fractionA = new Fraction(1,1,true);
+                fractionA.mDenominator = Math.floor(Math.random()*8+2);
+
+                var fractionB = new Fraction(1,1,false);
+                fractionB.mNumerator = Math.floor(Math.random()*8+2);
+
+                answer = fractionB.divide(fractionA);
+                answer.reduce();
+
+                this.setQuestion('' + this.ns.mNameOne + ' is making a pizza. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' uses ' + fractionA.getString() + ' block of cheese for each pie. How many pies can ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' make with ' + fractionB.mNumerator + ' blocks of cheese?');
+                this.setAnswer('' + answer.getString(),0);
+        }
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('5.nf.b.7.b_7',5.2207,'5.nf.b.7.b','');
 */
 var i_5_nf_b_7_b__7 = new Class(
