2ae75bad7b8783c91e0e9395ac207e27f2e58219
jbreslin33 <jbreslin33@localhost>
2015-01-27 21:29:49 -0500

    t3

--- src/src/core/math/g4_md_a_1.php
+++ src/src/core/math/g4_md_a_1.php
@@ -1,5 +1,41 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.md.a.1_3',4.2403,'4.md.a.1','');
+*/
+var i_4_md_a_1__3 = new Class(
+{
+Extends: TextItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet,300,50,175,95,100,50,425,100);
+
+
+                this.mType = '4.md.a.1_3';
+                this.ns = new NameSampler();
+
+		var fractionA = new Fraction(1,1,true);
+                var fractionB = new Fraction(1,1,false);
+		var r = 1;
+		while (r != 0)
+               	{ 
+                	fractionA.mNumerator = Math.floor(Math.random()*8+2);
+
+                	fractionB.mNumerator = Math.floor(Math.random()*8+2);
+
+			var seconds = parseInt(fractionB.mNumerator * 60);	
+			r = parseInt(seconds % fractionA.mNumerator);
+		}
+               	
+		answer = parseInt(seconds / fractionA.mNumerator);
+
+                this.setQuestion('In a video game ' + this.ns.mNameOne + ' can stack one block every ' + fractionA.mNumerator + ' seconds. How many blocks can ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' stack in ' + fractionB.mNumerator + ' minutes?');
+                this.setAnswer('' + answer,0);
+                this.setAnswer('' + answer + ' blocks',1);
+        }
+});
+
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.md.a.1_2',4.2402,'4.md.a.1','');
 */
 var i_4_md_a_1__2 = new Class(
