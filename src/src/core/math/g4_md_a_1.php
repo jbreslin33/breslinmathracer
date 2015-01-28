4045705d1e388f6f431066b060869dceac381d78
jbreslin33 <jbreslin33@localhost>
2015-01-27 21:13:59 -0500

    t2

--- src/src/core/math/g4_md_a_1.php
+++ src/src/core/math/g4_md_a_1.php
@@ -1,3 +1,32 @@
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.md.a.1_2',4.2402,'4.md.a.1','');
+*/
+var i_4_md_a_1__2 = new Class(
+{
+Extends: TextItem,
+        initialize: function(sheet)
+        {
+        	this.parent(sheet,300,50,175,95,100,50,425,100);
+
+
+                this.mType = '4.md.a.1_2';
+                this.ns = new NameSampler();
+
+                var fractionA = new Fraction(1,1,true);
+                fractionA.mNumerator = Math.floor(Math.random()*8+2);
+
+                var fractionB = new Fraction(16,1,false);
+
+                answer = fractionA.multiply(fractionB);
+                answer.reduce();
+
+                this.setQuestion('' + this.ns.mNameOne + ' has ' + fractionA.mNumerator + ' gallons of ' + this.ns.mDrinkOne + '. How many cups of ' + this.ns.mDrinkOne + ' can ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' pour for ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,1)  + ' friends?');
+                this.setAnswer('' + answer.getString(),0);
+                this.setAnswer('' + answer.getString() + ' cups',1);
+        }
+});
+
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('4.md.a.1_1',4.2401,'4.md.a.1','');
 */
