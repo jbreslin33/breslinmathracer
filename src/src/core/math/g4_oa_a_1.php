e76d60d6960b9e33c0063b8945c3d11a27fbfebb
jbreslin33 <jbreslin33@localhost>
2015-01-28 22:47:34 -0500

    t11

--- src/src/core/math/g4_oa_a_1.php
+++ src/src/core/math/g4_oa_a_1.php
@@ -62,6 +62,25 @@ Extends: i_4_oa_a_1__word,
 });
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.oa.a.1_11',4.0111,'4.oa.a.1','Word Problem. Multiplication. Put in equation form. ' );
+*/
+
+var i_4_oa_a_1__11 = new Class(
+{
+Extends: i_4_oa_a_1__word_equation,
+        initialize: function(sheet)
+        {
+                this.parent(sheet,600,50,330,75,100,50,685,80);
+
+                this.mType = '4.oa.a.1_11';
+                this.ns = new NameSampler();
+
+                this.setQuestion(this.mNameOne + ' scored ' + this.a + ' points playing ' + this.ns.mPointActivityOne + ' on ' + this.ns.mDayOfWeekOne + '. ' + this.mNameMachine.getPronoun(this.mNameOne,1,0) + ' scored ' + this.b + ' times as many points playing ' + this.ns.mPointActivityOne + ' on ' + this.ns.mDayOfWeekTwo + '. Write a multiplication equation to represent this.');
+        }
+});
+
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.oa.a.1_10',4.0110,'4.oa.a.1','Word Problem. Multiplication. Put in equation form. ' );
 */
 
