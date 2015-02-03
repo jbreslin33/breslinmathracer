08859e9cb91324627f272aa1b3c6536762a82790
jbreslin33 <jbreslin33@localhost>
2015-02-02 21:44:45 -0500

    standards report

--- src/web/stats/standards_report.php
+++ src/web/stats/standards_report.php
@@ -23,11 +23,11 @@ echo "<br>";
 
 <?php
 
+//item types for 5th
 $q = "select id from item_types where progression > 4.99 AND progression < 6 order by progression asc;";
 $r = pg_query($conn,$q);
 $n = pg_numrows($r);
 
-//loopthru item types for user
 for($a = 0; $a < $n; $a++)
 {
 	$row = pg_fetch_array($result, $j);
@@ -35,58 +35,59 @@ for($a = 0; $a < $n; $a++)
         $last_name_array[] = $row[1];
 }
 
+//students for 5th
 $user_id_array = array();
 $first_name_array = array();
 $last_name_array = array();
+	
+$query = "select id, first_name, last_name from users where core_standards_id = '";
+$query .= "5.oa.a.1";
+$query .= "' AND score > 50 order by last_name asc;";
 
-	echo '<table border=\"1\">';
-        echo '<tr>';
-        echo '<td> Type';
-        echo '</td>';
-        echo '<td> UserName';
-        echo '</td>';
-        echo '<td> First Name';
-        echo '</td>';
-        echo '<td> Last Name';
-        echo '</td>';
-        echo '</tr>';
+$result = pg_query($conn,$query);
+$numStudents = pg_numrows($result);
 
-	$query = "select id, first_name, last_name from users where core_standards_id = '";
-	$query .= "5.oa.a.1";
-	$query .= "' AND score > 50 order by last_name asc;";
+for($i = 0; $i < $numStudents; $i++)
+{
+       	$row = pg_fetch_array($result, $i);
+	$user_id_array[] = $row[0];
+	$last_name_array[] = $row[1];
+	$first_name_array[] = $row[2];
+}
 
-	$result = pg_query($conn,$query);
-	$numrows = pg_numrows($result);
+//html for table
+echo '<table border=\"1\">';
+echo '<tr>';
+echo '<td> UserName';
+echo '</td>';
+echo '<td> First Name';
+echo '</td>';
+echo '<td> Last Name';
+echo '</td>';
+echo '<td> Type ID';
+echo '</td>';
+echo '</tr>';
 
-	for($i = 0; $i < $numrows; $i++)
-	{
-        	$row = pg_fetch_array($result, $i);
-		$user_id_array[] = $row[0];
-		$last_name_array[] = $row[1];
-		$first_name_array[] = $row[2];
-	}
 
-	for($i = 0; $i < $numrows; $i++)
+	for($i = 0; $i < $numStudents; $i++)
 	{ 
 
-		//lets interject right here....
- 		$query = "select id from item_types where progression > 4.99 AND progression < 6 order by progression asc;";
+		$query = "select item_types_id from item_attempts JOIN item_types ON item_types.id=item_attempts.item_types_id JOIN evaluations_attempts ON item_attempts.evaluations_attempts_id=evaluations_attempts.id where evaluations_attempts.user_id = "; 
+		$query .= $user_id_array[$i]; 	
+		$query .= " order by progression desc LIMIT 1;";
+
 		$result = pg_query($conn,$query);
 		$nrows = pg_numrows($result);
+		$typeid = 0;	
 
-		//loopthru item types for user	
-		for($j = 0; $j < $nrows; $j++)
+		if ($nrows > 0)
 		{
-        		$row = pg_fetch_array($result, $j);
-			$user_id_array[] = $row[0];
-			$last_name_array[] = $row[1];
+        		$row = pg_fetch_array($result, 0);
+			$typer_id_array[] = $row[0];
+			$typeid = $row[0];
 		}
 
-
        		echo '<tr>';
-        	echo '<td>';
-        	echo $user_id_array[$i];
-        	echo '</td>';
         	echo '<td>';
         	echo $user_id_array[$i];
         	echo '</td>';
@@ -95,6 +96,9 @@ $last_name_array = array();
         	echo '</td>';
         	echo '<td>';
         	echo $first_name_array[$i];
+        	echo '</td>';
+        	echo '<td>';
+        	echo $typeid;
         	echo '</td>';
         	echo '</tr>';
 	}
