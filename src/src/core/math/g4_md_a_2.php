e05221a780a51e94a16672024748f6865d0aba8d
jbreslin33 <jbreslin33@localhost>
2015-02-03 14:54:35 -0500

    t2 rfm

--- src/src/core/math/g4_md_a_2.php
+++ src/src/core/math/g4_md_a_2.php
@@ -1,3 +1,29 @@
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.md.a.2_15',4.2515,'4.md.a.2','');
+*/
+var i_4_md_a_2__15 = new Class(
+{
+Extends: TextItemFraction,
+        initialize: function(sheet)
+        {
+                this.parent(sheet,340,50,190,95, 100,50,425,100, 100,50,425,175,true);
+
+                this.mType = '4.md.a.2_15';
+                this.ns = new NameSampler();
+
+                var fractionA = new Fraction(1,1,true);
+                fractionA.mDenominator = Math.floor(Math.random()*8+2);
+
+                var fractionB = new Fraction(1,1,false);
+                fractionB.mNumerator = Math.floor(Math.random()*8+2);
+
+                answer = fractionA.divide(fractionB);
+                answer.reduce();
+
+                this.setQuestion('' + this.ns.mNameOne + ' had some ' + this.ns.mRopeOne + ' that was ' + fractionA.getString() + ' ' + this.ns.mDistanceIncrementMedium + ' long. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' cut it into ' + fractionB.mNumerator + ' pieces for an art project. How many ' + this.ns.mDistanceIncrementMedium + ' long is each piece?');
+                this.setAnswer('' + answer.getString(),0);
+        }
+});
 
 
 /*
