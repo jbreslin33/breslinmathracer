e4ed9aeb86ade26a488fd9933cd69197069cd0bc
jbreslin33 <jbreslin33@localhost>
2015-02-07 21:00:44 -0500

    t15 d

--- src/src/core/math/g4_oa_a_2.php
+++ src/src/core/math/g4_oa_a_2.php
@@ -1,5 +1,27 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.oa.a.2_15',4.0215,'4.oa.a.2','');
+*/
+var i_4_oa_a_2__15 = new Class(
+{
+Extends: TextItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet,700,200,365,145,100,50,580,130);
+
+                this.mType = '4.oa.a.2_15';
+                this.ns = new NameSampler();
+		this.mChopWhiteSpace = true;
+
+                var a = Math.floor(Math.random()*8+2);
+                var b = Math.floor(Math.random()*8+2);
+                var c = parseInt(a * b);
+
+                this.setQuestion('Write an equation to solve for the unknown number: a &times ' + b + ' = ' + c);
+                this.setAnswer('' + c + '/' + b + '=' + a,0);
+        }
+});
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.oa.a.2_14',4.0214,'4.oa.a.2','');
 */
 var i_4_oa_a_2__14 = new Class(
