7e13c41d29b515081cce0ed40c100f956959b5a6
jbreslin33 <jbreslin33@localhost>
2015-02-07 20:40:38 -0500

    t13 d

--- src/src/core/math/g4_oa_a_2.php
+++ src/src/core/math/g4_oa_a_2.php
@@ -1,5 +1,27 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.oa.a.2_13',4.0213,'4.oa.a.2','');
+*/
+var i_4_oa_a_2__13 = new Class(
+{
+Extends: TextItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet,450,200,255,145,100,50,580,100);
+
+                this.mType = '4.oa.a.2_13';
+                this.ns = new NameSampler();
+
+                var a = Math.floor(Math.random()*8+2);
+                var b = Math.floor(Math.random()*8+2);
+                var c = parseInt(a * b);
+
+                this.setQuestion('Angry Baby weighs ' + a + ' pounds. Her older brother is ' + b + ' times heavier. This makes Angry Baby so mad! How many pounds does her older brother weigh?');
+                this.setAnswer('' + c,0);
+        }
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.oa.a.2_12',4.0212,'4.oa.a.2','');
 */
 var i_4_oa_a_2__12 = new Class(
