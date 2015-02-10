8350bdc2aafe12c38cf998eab36c0a11e4bb75b8
jbreslin33 <jbreslin33@localhost>
2015-02-09 20:19:56 -0500

    t3 d

--- src/src/core/math/g4_oa_a_3.php
+++ src/src/core/math/g4_oa_a_3.php
@@ -1,5 +1,28 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.oa.a.3_3',4.0303,'4.oa.a.3','');
+*/
+var i_4_oa_a_3__3 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+	this.parent(sheet,450,200,255,145,100,50,580,100);
+
+       	this.mType = '4.oa.a.3_3';
+        this.ns = new NameSampler();
+
+        var a = Math.floor(Math.random()*50+350);
+        var b = Math.floor(Math.random()*10+15);
+        var c = parseInt(a - b);
+	var d = parseInt(a + c);
+
+        this.setQuestion('' + this.ns.mSchoolOne + ' had a talent show on Friday and Saturday night. On Friday they sold ' + a + ' tickets. On Saturday they sold ' + b + ' fewer tickets than on Friday. How many tickets did they sell ' + ' ' + this.ns.mSum + '?');
+        this.setAnswer('' + d,0);
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.oa.a.3_2',4.0302,'4.oa.a.3','subtract then divide evenly');
 */
 var i_4_oa_a_3__2 = new Class(
