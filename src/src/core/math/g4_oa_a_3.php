e537c9f90cc9569b194ea37382c6fac8a3524576
jbreslin33 <jbreslin33@localhost>
2015-02-09 19:21:17 -0500

    t1 d

--- src/src/core/math/g4_oa_a_3.php
+++ src/src/core/math/g4_oa_a_3.php
@@ -1,23 +1,35 @@
+
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('4.oa.a.3_1',4.0301,'4.oa.a.3','');
 */
 var i_4_oa_a_3__1 = new Class(
 {
 Extends: TextItem,
-        initialize: function(sheet)
-        {
-                this.parent(sheet,450,200,255,145,100,50,580,100);
+initialize: function(sheet)
+{
+	this.parent(sheet,450,200,255,145,100,50,580,100);
 
-                this.mType = '4.oa.a.3_1';
-                this.ns = new NameSampler();
+       	this.mType = '4.oa.a.3_1';
+        this.ns = new NameSampler();
 
-                var a = Math.floor(Math.random()*50+25);
-                var b = Math.floor(Math.random()*50+25);
-                var c = parseInt(a + b);
-                var d = parseInt(c * 16);
+	var a = 0;
+	var b = 0;
+	var c = 0;
+	var d = 0;
+	var e = 13;
+	var f = 13;
 
+	while(e % 8 != 0)
+	{		
+                a = Math.floor(Math.random()*10+20);
+                b = Math.floor(Math.random()*10+20);
+                c = Math.floor(Math.random()*2+2);
+                d = Math.floor(Math.random()*2+2);
+		e = parseInt(a + b + c + d);
+		f = parseInt(e / 8);
+	}
                 this.setQuestion('The class that ' + this.ns.mNameOne + ' is in is having a pizza party. The are ' + a + ' girls and ' + b + ' boys in the class. There will also be ' + c + ' parents and ' + d + ' teachers at the party. Each pizza has 8 slices. How many pizzas should they order so that everyone will have 1 slice.');
-                this.setAnswer('' + d,0);
+                this.setAnswer('' + f,0);
         }
 });
 
