625e9538d5d9eaa5e58112bc103860554641d568
jbreslin33 <jbreslin33@localhost>
2015-02-14 18:41:54 -0500

    root page

--- src/web/navigation/top_links_root.php
+++ src/web/navigation/top_links_root.php
@@ -3,20 +3,6 @@
 <li><a href="/web/select/select.php">Selects</a></li>
 <li><a href="/web/php/logout.php">Logout</a></li>
 
-<li><a href="/web/stats/leaderboard.php">School Leader Board</a></li>
-<li><a href="/web/stats/leaderboard_33.php">Room 33 Leader Board</a></li>
-<li><a href="/web/stats/leaderboard_34.php">Room 34 Leader Board</a></li>
-<li><a href="/web/stats/leaderboard_tt_2.php">XTABLE 2 Leader Board</a></li>
-<li><a href="/web/stats/leaderboard_tt_3.php">XTABLE 3 Leader Board</a></li>
-<li><a href="/web/stats/leaderboard_tt_4.php">XTABLE 4 Leader Board</a></li>
-<li><a href="/web/stats/leaderboard_tt_5.php">XTABLE 5 Leader Board</a></li>
-<li><a href="/web/stats/leaderboard_tt_6.php">XTABLE 6 Leader Board</a></li>
-<li><a href="/web/stats/leaderboard_tt_7.php">XTABLE 7 Leader Board</a></li>
-<li><a href="/web/stats/leaderboard_tt_8.php">XTABLE 8 Leader Board</a></li>
-<li><a href="/web/stats/leaderboard_tt_9.php">XTABLE 9 Leader Board</a></li>
-<li><a href="/web/stats/leaderboard_tt_10.php">XTABLE 10 Leader Board</a></li>
-<li><a href="/web/stats/leaderboard_timestables.php">XTABLES Leader Board</a></li>
-<li><a href="/web/stats/leaderboard_theizzy.php">THE IZZY Leader Board</a></li>
 <li><a href="/web/stats/enter_homework_date.php">Homework Report</a></li>
 <li><a href="/web/stats/enter_statsreport_user.php">Stats Report</a></li>
 <li><a href="/web/stats/realtime.php">RealTimeHelp</a></li>
