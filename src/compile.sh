0b109e52a2ddeca0b5fbc16896e19ec588701995
jbreslin33 <jbreslin33@localhost>
2015-02-14 18:06:46 -0500

    now if you pass in null to compile it wont create user

--- src/compile.sh
+++ src/compile.sh
@@ -2,20 +2,24 @@
 ./src/database/rebuild.sh
 echo ./src/database/db_restore.sh
 
-echo create dev user
-if [ -e src/database/dev_user.sql ];
+if [ "$1" = "" ];
 then
-rm src/database/dev_user.sql
+	echo no user created
 else
-touch src/database/dev_user.sql
+	echo create dev user
+	if [ -e src/database/dev_user.sql ];
+	then
+		rm src/database/dev_user.sql
+	else
+		touch src/database/dev_user.sql
+	fi
+	echo "insert into users (username, password, first_name, last_name, school_id, core_standards_id) VALUES ('j','j','dev','user',1,'5.oa.a.1');" >> src/database/dev_user.sql
+
+	echo "insert into evaluations_attempts (start_time,user_id,evaluations_id) VALUES (CURRENT_TIMESTAMP,1,2);" >> src/database/dev_user.sql
+
+	echo "insert into item_attempts (start_time,evaluations_attempts_id,transaction_code,item_types_id) VALUES (CURRENT_TIMESTAMP,1,0,'$1');" >> src/database/dev_user.sql
+
+	sudo -u postgres psql -d jamesanthonybreslin -f src/database/dev_user.sql
 fi
-
-echo "insert into users (username, password, first_name, last_name, school_id, core_standards_id) VALUES ('j','j','dev','user',1,'5.oa.a.1');" >> src/database/dev_user.sql
-
-echo "insert into evaluations_attempts (start_time,user_id,evaluations_id) VALUES (CURRENT_TIMESTAMP,1,2);" >> src/database/dev_user.sql
-
-echo "insert into item_attempts (start_time,evaluations_attempts_id,transaction_code,item_types_id) VALUES (CURRENT_TIMESTAMP,1,0,'$1');" >> src/database/dev_user.sql
-
-sudo -u postgres psql -d jamesanthonybreslin -f src/database/dev_user.sql
 
 
