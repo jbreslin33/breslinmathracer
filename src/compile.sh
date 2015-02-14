0d467943edc3ebc59a8fc2e8425a889e622c1a1a
jbreslin33 <jbreslin33@localhost>
2015-02-14 12:37:39 -0500

    insert evaluations_attempts works.

--- src/compile.sh
+++ src/compile.sh
@@ -12,6 +12,8 @@ fi
 
 echo "insert into users (username, password, first_name, last_name, school_id, core_standards_id) VALUES ('j','j','dev','user',1,'5.oa.a.1');" >> src/database/dev_user.sql
 
+echo "insert into evaluations_attempts (start_time,user_id,evaluations_id) VALUES (CURRENT_TIMESTAMP,1,1);" >> src/database/dev_user.sql
+
 sudo -u postgres psql -d jamesanthonybreslin -f src/database/dev_user.sql
 
 
