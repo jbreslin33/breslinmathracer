316680c25a5467017ab860282422156e39e24943
jbreslin33 <jbreslin33@localhost>
2015-02-14 17:29:17 -0500

    compile ready for param pass

--- src/compile.sh
+++ src/compile.sh
@@ -12,7 +12,9 @@ fi
 
 echo "insert into users (username, password, first_name, last_name, school_id, core_standards_id) VALUES ('j','j','dev','user',1,'5.oa.a.1');" >> src/database/dev_user.sql
 
-echo "insert into evaluations_attempts (start_time,user_id,evaluations_id) VALUES (CURRENT_TIMESTAMP,1,1);" >> src/database/dev_user.sql
+echo "insert into evaluations_attempts (start_time,user_id,evaluations_id) VALUES (CURRENT_TIMESTAMP,1,2);" >> src/database/dev_user.sql
+
+echo "insert into item_attempts (start_time,evaluations_attempts_id,transaction_code,item_types_id) VALUES (CURRENT_TIMESTAMP,1,0,'5.nf.b.7.b_10');" >> src/database/dev_user.sql
 
 sudo -u postgres psql -d jamesanthonybreslin -f src/database/dev_user.sql
 
