5dd4d616ab079d36e2b3c938516264de293ce741
jbreslin33 <jbreslin33@localhost>
2015-02-14 12:34:08 -0500

    insert user works.

--- src/compile.sh
+++ src/compile.sh
@@ -10,5 +10,8 @@ else
 touch src/database/dev_user.sql
 fi
 
-echo "wake up!" >> src/database/dev_user.sql
+echo "insert into users (username, password, first_name, last_name, school_id, core_standards_id) VALUES ('j','j','dev','user',1,'5.oa.a.1');" >> src/database/dev_user.sql
+
+sudo -u postgres psql -d jamesanthonybreslin -f src/database/dev_user.sql
+
 
