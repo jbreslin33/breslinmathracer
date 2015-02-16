157a3eedbf0484d96007547b207324a3cd008768
jbreslin33 <jbreslin33@localhost>
2015-02-16 10:51:25 -0500

    working on school admin

--- src/web/navigation/top_links_school.php
+++ src/web/navigation/top_links_school.php
@@ -5,5 +5,6 @@
 <li><a href="/web/stats/leaderboards.php">School Leader Board</a></li>
 <li><a href="/web/insert/upload.php">Upload School From Linux</a></li>
 <li><a href="/web/stats/remediate.php">Remediate</a></li>
+<li><a href="/web/stats/statsreports.php">statsreports</a></li>
 
 </ul>
