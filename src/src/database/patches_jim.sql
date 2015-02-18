eda837b3a93a67caf31000ab417cc4ce5e48b219
jbreslin33 <jbreslin33@localhost>
2015-02-18 15:01:00 -0500

    patches

---
+++ src/src/database/patches_jim.sql
@@ -0,0 +1,4 @@
+alter table users ALTER school_id SET DEFAULT NULL;
+alter table users ALTER teacher_id SET DEFAULT NULL;
+alter table users ALTER room_id SET DEFAULT NULL;
+alter table users ALTER team_id SET DEFAULT NULL;
