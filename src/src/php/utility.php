7168c660a906bab3a79fa7e1f890cc63ba2118ea
jbreslin33 <jbreslin33@localhost>
2015-02-17 20:38:36 -0500

    user_answer is converted

--- src/src/php/utility.php
+++ src/src/php/utility.php
@@ -12,6 +12,11 @@ function convertToWeb($string)
         $replace = '>';
         $string = preg_replace($pattern, $replace, $string);
 	
+	// <
+        $pattern = '/frasl;/';
+        $replace = '&frasl;';
+        $string = preg_replace($pattern, $replace, $string);
+	
 	return $string;
 }
 
