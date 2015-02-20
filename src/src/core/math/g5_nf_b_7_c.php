515c9e320c4c53cad0e56c06c693c0cce6379300
jbreslin33 <jbreslin33@localhost>
2015-02-19 20:50:12 -0500

    t1 works.

--- src/src/core/math/g5_nf_b_7_c.php
+++ src/src/core/math/g5_nf_b_7_c.php
@@ -14,29 +14,31 @@ Extends: TextItemFraction,
 
                 var fractionA = new Fraction(1,1,true);
                 fractionA.mDenominator = Math.floor(Math.random()*8+2);
-
-                var fractionB = new Fraction(1,1,false);
-                fractionB.mNumerator = Math.floor(Math.random()*8+2);
                 
+		var fractionB = new Fraction(1,1,true);
+                fractionB.mNumerator= Math.floor(Math.random()*8+2);
+
 		var c = Math.floor(Math.random()*8+2);
-		var d = Math.floor(Math.random()*90+10);
-		var decimal = new Decimal(c,d,1);  
-		
-		var e = Math.floor(Math.random()*8+2);
+		var m = Math.floor(Math.random()*90+10);
+		var decimalPerBatch = new Decimal(c,m,1);  
 
-                answer = fractionB.divide(fractionA);
-                answer.reduce();
+		fractionC = fractionB.divide(fractionA);
 
+		var decimalBatches = new Decimal(fractionC.mNumerator,0,1);
 
+		var answer = decimalBatches.multiply(decimalPerBatch);		
+	
+		/*	
 		var decimalA = new Decimal(2,2,1);  
 		var decimalB = new Decimal(2,2,1);  
 
 		decimalC = decimalA.multiply(decimalB);
 		APPLICATION.log('' + decimalA.getString() + '*' + decimalB.getString());
 		APPLICATION.log('decimalC:' + decimalC.getString());
+		*/
 
 
-                this.setQuestion('' + this.ns.mNameOne + ' is making smoothies for a fund raiser for ' + this.ns.mSchoolOne + '. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' uses ' + fractionA.getString() + ' of a bag of ' + this.ns.mFruitOne + ' for each batch of smoothies. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' makes $' +  decimal.getString() + ' for each batch of smoothies ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' sells. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' used ' + e + ' bags of ' + this.ns.mFruitOne + '. How much money did ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' raise so far?');
+                this.setQuestion('' + this.ns.mNameOne + ' is making smoothies for a fund raiser for ' + this.ns.mSchoolOne + '. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' uses ' + fractionA.getString() + ' of a bag of ' + this.ns.mFruitOne + ' for each batch of smoothies. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' makes $' +  decimalPerBatch.getString() + ' for each batch of smoothies ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' sells. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' used ' + fractionB.mNumerator + ' bags of ' + this.ns.mFruitOne + '. How much money did ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' raise so far?');
 
                 this.setAnswer('' + answer.getString(),0);
         }
