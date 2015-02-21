72c3ae1bd5b10464ff61905a50543c132d8c7cd7
jbreslin33 <jbreslin33@localhost>
2015-02-21 15:25:05 -0500

    t7

--- src/src/core/math/g5_nbt_b_7.php
+++ src/src/core/math/g5_nbt_b_7.php
@@ -758,109 +758,23 @@ initialize: function(sheet)
 
         this.mType = '5.nbt.b.7_7';
 
-        this.ns = new NameSampler();
-
-        this.a = 0;
-        this.b = 0;
-        this.c = 0;
-        this.d = 0;
-        this.e = 0;
-        this.f = 0;
-
-        this.partA = 0;
-        this.partB = 1;
-        this.part = 0;
-
-        while (this.partA < this.partB)
+        var decimalA = new Decimal(1);
+        var decimalB = new Decimal(2);
+        while (parseFloat(decimalA.mDecimal) <= parseFloat(decimalB.mDecimal))
         {
-                this.a = Math.floor(Math.random()*9+1);
-                this.b = Math.floor(Math.random()*9+1);
-                this.c = Math.floor(Math.random()*9+1);
-                this.d = Math.floor(Math.random()*9+1);
+                var a = Math.floor(Math.random()*8999+1000);
+                a = parseFloat(a / 100);
+                decimalA = new Decimal(a);
 
-                this.e = 0;
-                this.f = Math.floor(Math.random()*9+1);
-                this.g = Math.floor(Math.random()*9+1);
-                this.h = 0;
-
-                this.partA = parseInt(this.a * 1000 + this.b * 100 + this.c * 10 + this.d);
-                this.partB = parseInt(this.e * 1000 + this.f * 100 + this.g * 10 + this.h);
-                this.part =  parseInt(this.partA - this.partB);
-        }
-        if (this.part > 999) // we are 4 digits
-        {
-                if (this.part % 1000 == 0) // we have a whole number
-                {
-                        this.answer = '' + this.part;
-                }
-                if (this.part % 100 == 0) // we have a whole number
-                {
-                        this.answer = '' + this.part;
-                }
-                else if (this.part % 10 == 0) // we have a multiple of 10
-                {
-                        var tensAndOnes = parseInt(this.part / 10);
-                        var ones = parseInt(tensAndOnes / 10);
-                        var tenths = tensAndOnes % 10;
-
-                        this.answer = '' + ones + '.' + tenths;
-                }
-                else // we have pure 3 digit number
-                {
-                        var hundreds = parseInt(this.part / 100);
-                        var tensAndOnes = this.part % 100;
-			if (tensAndOnes < 10)
-			{
-                        	this.answer = '' + hundreds + '.0' + tensAndOnes;
-			}
-			else
-			{
-                        	this.answer = '' + hundreds + '.' + tensAndOnes;
-			}
-                }
+                var b = Math.floor(Math.random()*89+10);
+                b = parseFloat(b / 10);
+                decimalB = new Decimal(b);
         }
 
-	else if (this.part > 99) // we are 3 digits 
-	{
-        	if (this.part % 100 == 0) // we have a whole number
-        	{
-                	this.answer = '' + this.part;
-        	}
-        	else if (this.part % 10 == 0) // we have a multiple of 10  
-        	{
-			var tensAndOnes = parseInt(this.part / 10); 
-			var ones = parseInt(tensAndOnes / 10);
-			var tenths = tensAndOnes % 10;
-		
-                	this.answer = '' + ones + '.' + tenths;
-        	}
-        	else // we have pure 3 digit number  
-		{
-			var hundreds = parseInt(this.part / 100); 
-			var tensAndOnes = this.part % 100; 
-			this.answer = '' + hundreds + '.' + tensAndOnes;
-		}
-	}
-        else if (this.part <= 99) // we are 3 digits
-        {
-                if (this.part % 10 == 0) // we have a multiple of 10
-                {
-                        var tensAndOnes = parseInt(this.part / 10);
-                        var ones = parseInt(tensAndOnes / 10);
-                        var tenths = tensAndOnes % 10;
-                        this.answer = '' + ones + '.' + tenths;
-                }
-        	else // we have pure 3 digit number  
-		{
-			var hundreds = parseInt(this.part / 100); 
-			var tensAndOnes = this.part % 100; 
-			this.answer = '' + hundreds + '.' + tensAndOnes;
-		}
-        }
+        var answer = decimalA.subtract(decimalB);
 
-        this.setQuestion('Find the difference: ' + this.a + this.b + '.' + this.c + this.d + ' - ' + this.f + '.' + this.g);
-
-        this.setAnswer('' + this.answer,0);
+        this.setQuestion('Find the difference: ' + decimalA.getString() + ' - ' + decimalB.getString() + '');
+        this.setAnswer('' + answer.getString(),0);
 }
 });
 
@@ -888,51 +802,12 @@ initialize: function(sheet)
         	var b = Math.floor(Math.random()*9+1);
         	b = parseFloat(b / 10);
         	decimalB = new Decimal(b);
-		
-		APPLICATION.log('a:' + decimalA.mDecimal);	
-		APPLICATION.log('b:' + decimalB.mDecimal);	
 	}
 
         var answer = decimalA.subtract(decimalB);
 
         this.setQuestion('Find the difference: ' + decimalA.getString() + ' - ' + decimalB.getString() + '');
         this.setAnswer('' + answer.getString(),0);
-/*
-        this.ns = new NameSampler();
-
-        this.a = 0;
-        this.b = 0;
-        this.c = 0;
-        this.d = 0;
-
-        this.decimalPartA = 0;
-        this.decimalPartB = 1;
-	
-	while (this.decimalPartA < this.decimalPartB)
-	{
-        	this.a = Math.floor(Math.random()*9+1);
-        	this.b = Math.floor(Math.random()*9+1);
-        	this.c = Math.floor(Math.random()*9+1);
-        	this.d = 0;
-        
-		this.decimalPartA = parseInt(this.a * 10 + this.b);
-        	this.decimalPartB = parseInt(this.c * 10);
-		this.decimalPart =  parseInt(this.decimalPartA - this.decimalPartB);
-	}
-
-        if (this.decimalPart < 10)
-        {
-                this.answer = '0.0' + this.decimalPart;
-        }
-        else
-        {
-              	this.answer = '0.' + this.decimalPart;
-        }
-
-        this.setQuestion('Find the difference: 0.' + this.a + this.b + ' - 0.' + this.c + '');
-
-        this.setAnswer('' + this.answer,0);
-*/
 }
 });
 
