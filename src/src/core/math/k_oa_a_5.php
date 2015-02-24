1f5e51b45eb6acfd5f7689d8d050d7f7d5405146
jbreslin33 <jbreslin33@localhost>
2015-02-24 15:29:38 -0500

    working on adding

--- src/src/core/math/k_oa_a_5.php
+++ src/src/core/math/k_oa_a_5.php
@@ -1,85 +1,37 @@
 
-/*
-insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.5_2',0.1502,'k.oa.a.5','Subtract within 5.');
-*/
 
-var i_k_oa_a_5__2 = new Class(
-{
-Extends: ThreeButtonItem,
-        initialize: function(sheet)
-        {
-                this.parent(sheet);
-
-                this.mType = 'k.oa.a.5_2';
-             
-		this.a = 0;
-		this.b = 0;
-		this.c = 1000;
-
-		this.x = 0;	 
-		this.y = 0;	 
-
-		while (this.a == this.b || this.a == this.c || this.b == this.c || this.c < 0 || this.c > 5 || this.x > 10 || this.a < 0 || this.b < 0)
-		{
-			//variables
-                	this.x = Math.floor(Math.random()*6);
-                	this.y = Math.floor(Math.random()*6);
-			this.c = parseInt(this.x - this.y);  
-	
-			//wrong answers 
-			this.a = Math.floor(Math.random()*6);
-			this.b = Math.floor(Math.random()*6);
-                }
-
-                this.setQuestion(this.x + ' - ' + this.y + ' =');
-                this.setAnswer(parseInt(this.c),0);
-
-                this.mButtonA.setAnswer(this.a);
-                this.mButtonB.setAnswer(this.b);
-                this.mButtonC.setAnswer(this.c);
-                this.shuffle(10);
-        }
-});
 
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.5_1',0.1501,'k.oa.a.5','Add within 5.');
+insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.5_1',0.1501,'k.oa.a.5','Add Subtract within 5.');
 */
-
+/*
 var i_k_oa_a_5__1 = new Class(
 {
-Extends: ThreeButtonItem,
-        initialize: function(sheet)
-        {
-                this.parent(sheet);
+Extends: TimesTables,
 
-                this.mType = 'k.oa.a.5_1';
-             
-		this.a = 0;
-		this.b = 0;
-		this.c = 1000;
-
-		this.x = 0;	 
-		this.y = 0;	 
-
-		while (this.a == this.b || this.a == this.c || this.b == this.c || this.c > 5 || this.a < 0 || this.b < 0)
-		{
-			//variables
-                	this.x = Math.floor(Math.random()*6);
-                	this.y = Math.floor(Math.random()*6);
-			this.c = parseInt(this.x + this.y);  
-	
-			//wrong answers 
-			this.a = Math.floor(Math.random()*6);
-			this.b = Math.floor(Math.random()*6);
-                }
-
-                this.setQuestion(this.x + ' + ' + this.y + ' =');
-                this.setAnswer(parseInt(this.c),0);
-
-                this.mButtonA.setAnswer(this.a);
-                this.mButtonB.setAnswer(this.b);
-                this.mButtonC.setAnswer(this.c);
-                this.shuffle(10);
-        }
+initialize: function(sheet)
+{
+        this.parent(sheet);
+        this.mType = 'k.oa.a.5_1';
+        this.setQuestion('1 + 0');
+        this.setAnswer('1',0);
+}
 });
+*/
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.5_2',0.1502,'k.oa.a.5','Add Subtract within 5.');
+*/
+/*
+var i_k_oa_a_5__2 = new Class(
+{
+Extends: TimesTables,
 
+initialize: function(sheet)
+{
+        this.parent(sheet);
+        this.mType = 'k.oa.a.5_2';
+        this.setQuestion('0 + 1');
+        this.setAnswer('1',0);
+}
+});
+*/
