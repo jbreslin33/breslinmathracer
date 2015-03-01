af3ace782a77690aafecc9afaa3d5883a21739b4
jbreslin33 <jbreslin33@localhost>
2015-03-01 16:43:22 -0500

    t1 4.oa.b.4 rfm

--- src/src/core/math/g4_oa_b_4.php
+++ src/src/core/math/g4_oa_b_4.php
@@ -1,1 +1,35 @@
-//add
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.oa.b.4_1',4.0401,'4.oa.b.4','');
+*/
+var i_4_oa_b_4__1 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,450,200,255,145,100,50,580,100);
+
+        this.mType = '4.oa.b.4_1';
+        this.ns = new NameSampler();
+
+        var a = 0;
+        var b = 0;
+        var c = 0;
+        var d = 0;
+        var e = 13;
+        var f = 13;
+
+        while(e % 8 != 0)
+        {
+                a = Math.floor(Math.random()*10+20);
+                b = Math.floor(Math.random()*10+20);
+                c = Math.floor(Math.random()*2+2);
+                d = Math.floor(Math.random()*2+2);
+                e = parseInt(a + b + c + d);
+                f = parseInt(e / 8);
+        }
+        this.setQuestion('The class that ' + this.ns.mNameOne + ' is in is having a pizza party. The are ' + a + ' girls and ' + b + ' boys in the class. There will also be ' + c + ' parents and ' + d + ' teachers at the party. Each pizza has 8 slices. How many pizzas should they order so that everyone will have 1 slice.');
+        this.setAnswer('' + f,0);
+}
+});
+
