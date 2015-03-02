58df398e50a51f26d8c297bd704aa92e9471ad7c
jbreslin33 <jbreslin33@localhost>
2015-03-02 13:55:14 -0500

    t11 m

--- src/src/core/math/g4_oa_b_4.php
+++ src/src/core/math/g4_oa_b_4.php
@@ -1,3 +1,39 @@
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.oa.b.4_11',4.0411,'4.oa.b.4','');
+*/
+var i_4_oa_b_4__11 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,450,200,255,145,100,50,580,100);
+
+        this.mType = '4.oa.b.4_11';
+        this.ns = new NameSampler();
+
+        var a = Math.floor(Math.random()*8+3);
+        var b = Math.floor(Math.random()*8+3);
+        var multiples  = '';
+
+        for (var i = 1; i < 7; i++)
+        {
+                if (multiples.length == 0)  //first one no comma
+                {
+                        multiples = multiples + '' + a;
+                }
+                else
+                {
+                        var nextMultiple = parseInt(a * i);
+                        multiples = multiples + ',' + nextMultiple;
+                }
+        }
+
+        this.setQuestion('' + this.ns.mNameOne + ' is watching some little kids. To keep their attention he starts a new game of ' + this.ns.mPlayedActivityOne + ' every ' + a + ' minutes and a new game of ' + this.ns.mPlayedActivityTwo + ' every ' + b + ' minutes.');
+        this.setAnswer('' + multiples,0);
+}
+});
+
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('4.oa.b.4_10',4.0410,'4.oa.b.4','');
 */
@@ -9,7 +45,6 @@ initialize: function(sheet)
         this.parent(sheet,450,200,255,145,100,50,580,100);
 
         this.mType = '4.oa.b.4_10';
-        this.ns = new NameSampler();
 
         var a = Math.floor(Math.random()*8+3);
         var multiples  = '';
