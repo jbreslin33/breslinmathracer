ec7a58f7ee25848a925083214703549a3dbc22b0
jbreslin33 <jbreslin33@localhost>
2015-03-01 20:34:55 -0500

    t1

--- src/src/core/math/g4_oa_b_4.php
+++ src/src/core/math/g4_oa_b_4.php
@@ -12,24 +12,19 @@ initialize: function(sheet)
         this.mType = '4.oa.b.4_1';
         this.ns = new NameSampler();
 
-        var a = 0;
-        var b = 0;
-        var c = 0;
-        var d = 0;
-        var e = 13;
-        var f = 13;
+        var a = Math.floor(Math.random()*10+5);
+	var count = 0; 
 
-        while(e % 8 != 0)
+	for (var i = 0; i <= a; i++)
         {
-                a = Math.floor(Math.random()*10+20);
-                b = Math.floor(Math.random()*10+20);
-                c = Math.floor(Math.random()*2+2);
-                d = Math.floor(Math.random()*2+2);
-                e = parseInt(a + b + c + d);
-                f = parseInt(e / 8);
+		if (parseInt(a % i) == 0)
+		{
+			count++;
+		}
         }
+
         this.setQuestion('' + this.ns.mNameOne + ' has ' + a + ' ' + this.ns.mThingOne + '. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' wants to put them in rows. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' wants each row to have the same amount of ' + this.ns.mThingOne + '. How many different ways can ' + this.ns.mNameOne + ' arrange ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,1) + ' ' + this.ns.mThingOne + ' while still making sure that all the rows have an equal amount of ' + this.ns.mThingOne + '?');
-        this.setAnswer('' + f,0);
+        this.setAnswer('' + count,0);
 }
 });
 
