8ad5e7a999a3aee25e52f81d7f9f7dcd1047f081
jbreslin33 <jbreslin33@localhost>
2015-03-02 20:23:08 -0500

    t1

--- src/src/core/math/g4_oa_c_5.php
+++ src/src/core/math/g4_oa_c_5.php
@@ -13,23 +13,24 @@ initialize: function(sheet)
 
         var a = Math.floor(Math.random()*8+3);
         var b = Math.floor(Math.random()*8+3);
-        var multiples  = '';
+        var answer  = '';
+        var total = a;
 
         for (var i = 1; i < 7; i++)
         {
-                if (multiples.length == 0)  //first one no comma
+                if (answer.length == 0)  //first one no comma
                 {
-                        multiples = multiples + '' + a;
+			total = parseInt(b + total);
+			answer = '' + total; 
                 }
                 else
                 {
-                        var nextMultiple = parseInt(a * i);
-                        multiples = multiples + ',' + nextMultiple;
+			total = parseInt(total + b);
+			answer = '' + answer + ',' + total;
                 }
         }
 
-        this.setQuestion('' + this.ns.mNameOne + ' wants to save money for ' + this.ns.mPurchaseOne + '. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' already has $' + a + '. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' plans to add $' + b + ' per week to that total. Write ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,1) + ' weekly totals for the first 6 weeks seperated by commas.');
-        this.setAnswer('' + multiples,0);
+        this.setQuestion('' + this.ns.mNameOne + ' wants to save money for ' + this.ns.mPurchaseOne + '. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' already has $' + a + '. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' plans to add $' + b + ' per week to that total. Write ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,1) + ' weekly totals for the first 6 weeks seperated by commas. For example: 2,4,6,8,10,12');
+        this.setAnswer('' + answer,0);
 }
 });
-
