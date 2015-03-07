88e2e566edbf68d22ef708fe7a05e81062f45690
jbreslin33 <jbreslin33@localhost>
2015-03-07 09:20:46 -0500

    normal uses just one evaluations_attempts

--- src/src/php/normal.php
+++ src/src/php/normal.php
@@ -29,6 +29,7 @@ function __construct($startNew)
 
 	if ($startNew == 1)
 	{
+		error_log('startNew');
 		//close old evaluation_attempts.......
 		$evaluations_attempts = new EvaluationsAttempts();
 		$evaluations_attempts->update();
@@ -43,6 +44,37 @@ function __construct($startNew)
 	}
 	else
 	{
+		error_log('old');
+
+		//check for last evaluation that was a normal type
+      		$query = "select id from evaluations_attempts where user_id = ";
+        	$query .= $_SESSION["user_id"];
+        	$query .= " AND evaluations_id = 1 order by start_time desc limit 1;";
+
+       	 	$result = pg_query($this->mDatabaseConnection->getConn(),$query) or die('Could not connect: ' . pg_last_error());
+
+        	$num = pg_num_rows($result);
+
+        	if ($num > 0)
+        	{
+                	//get the attempt_id
+                	$evaluations_attempts_id = pg_Result($result, 0, 'id');
+
+                	//set id
+                	$_SESSION["evaluations_attempts_id"] = $evaluations_attempts_id;
+		}
+		else
+		{
+			//this is first visit or you closed the old one
+        		$insert = "insert into evaluations_attempts (start_time,user_id,evaluations_id) VALUES (CURRENT_TIMESTAMP,";
+        		$insert .= $_SESSION["user_id"];
+        		$insert .= ",";
+        		$insert .= $_SESSION["evaluations_id"];
+        		$insert .= ");";
+
+        		$insertResult = pg_query($this->mDatabaseConnection->getConn(),$insert) or die('Could not connect: ' . pg_last_error());
+		}
+
 		$this->setRawData();
 		
 		$item_attempt = new ItemAttempt();
--- src/web/php/normal.php
+++ src/web/php/normal.php
@@ -5,7 +5,7 @@ include(getenv("DOCUMENT_ROOT") . "/src/php/normal.php");
 session_start();
 
 //start new so pass 1 as first paremeter.
-$normal = new Normal(1);
+$normal = new Normal(0);
 
 include_once(getenv("DOCUMENT_ROOT") . "/web/php/full_return_string.php");
 ?>
