0223eb886b7a24155170a9b2786b249e2ab8020c
jbreslin33 <jbreslin33@localhost>
2015-03-12 22:26:43 -0400

    t1-5

--- src/src/core/math/g4_md_b_4.php
+++ src/src/core/math/g4_md_b_4.php
@@ -1,4 +1,77 @@
-//add 4.27
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.md.b.4_5',4.2705,'4.md.b.4','');
+*/
+var i_4_md_b_4__5 = new Class(
+{
+Extends: TextItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet,300,50,175,95,100,50,425,100);
+
+                this.mType = '4.md.b.4_5';
+
+                this.setQuestion('A point where two or more straight lines meet. A corner.');
+                this.setAnswer('' + 'vertex',0);
+                this.setAnswer('' + 'a vertex',1);
+        }
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.md.b.4_4',4.2704,'4.md.b.4','');
+*/
+var i_4_md_b_4__4 = new Class(
+{
+Extends: TextItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet,300,50,175,95,100,50,425,100);
+
+                this.mType = '4.md.b.4_4';
+
+                this.setQuestion('The amount of turn between two straight lines that have a common end point.');
+                this.setAnswer('' + 'angle',0);
+                this.setAnswer('' + 'an angle',1);
+        }
+});
+
+
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.md.b.4_3',4.2703,'4.md.b.4','');
+*/
+var i_4_md_b_4__3 = new Class(
+{
+Extends: TextItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet,300,50,175,95,100,50,425,100);
+
+                this.mType = '4.md.b.4_3';
+
+                this.setQuestion('A point or value that marks the end of a ray or one of the ends of a line segment.');
+                this.setAnswer('' + 'endpoint',0);
+                this.setAnswer('' + 'an endpoint',1);
+        }
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.md.b.4_2',4.2702,'4.md.b.4','');
+*/
+var i_4_md_b_4__2 = new Class(
+{
+Extends: TextItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet,300,50,175,95,100,50,425,100);
+
+                this.mType = '4.md.b.4_2';
+
+                this.setQuestion('A precise location or place on a plane. Usually represented by a dot.');
+                this.setAnswer('' + 'point',0);
+                this.setAnswer('' + 'a point',1);
+        }
+});
 
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('4.md.b.4_1',4.2701,'4.md.b.4','');
@@ -11,11 +84,10 @@ Extends: TextItem,
                 this.parent(sheet,300,50,175,95,100,50,425,100);
 
                 this.mType = '4.md.b.4_1';
-                var a = Math.floor((Math.random()*8)+2);
-                var answer = parseInt(a * 1000);
 
-                this.setQuestion('How many grams in ' + a + ' kilograms?');
-                this.setAnswer('' + answer,0);
+                this.setQuestion('A line with a start point but no end point. It goes to infinity.');
+                this.setAnswer('' + 'ray',0);
+                this.setAnswer('' + 'a ray',1);
         }
 });
 
