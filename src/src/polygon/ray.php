cfe77ee5160165688bd9ea2235fadf1d73a4db7b
jbreslin33 <jbreslin33@localhost>
2015-03-13 21:55:05 -0400

    rotates triangle in ray.

--- src/src/polygon/ray.php
+++ src/src/polygon/ray.php
@@ -30,7 +30,6 @@ Extends: RaphaelPolygon,
 		this.mItem.addQuestionShape(this.mEndPoint);
 
 		//triangle at end of ray
-						//game,raphael,x1,y1,x2,y2,x3,y3,r,g,b,s,op,d)
 		this.mTriangle = new Triangle (this.mItem.mSheet.mGame,this.mItem.mRaphael,parseInt(x2-10),y2, parseInt(x2-20),parseInt(y2+20), parseInt(x2-20),parseInt(y2-20)   ,0,1,1,"none",.5,false)
 		this.mItem.addQuestionShape(this.mTriangle);
 
@@ -38,5 +37,8 @@ Extends: RaphaelPolygon,
 		//lets rotate according to passed in value
 		var rotateAmount = '' + 'r' + r + ',' + x1 + ',' + y1;   
 		this.mPolygon.transform(rotateAmount);
+	
+		//rotate triangle ??	
+		this.mTriangle.mPolygon.transform(rotateAmount);
 	}
 });
