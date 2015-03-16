97c4a0f28270311e3497b1db8a86b11b84b07532
jbreslin33 <jbreslin33@localhost>
2015-03-16 18:58:27 -0400

    arc

--- src/src/core/math/g4_md_b_4.php
+++ src/src/core/math/g4_md_b_4.php
@@ -126,8 +126,19 @@ createShapes: function()
 
 	//need an arc
 //game,raphael,x,y,radius,start,end,r,g,b,s,op,d
-	this.mArc = new Arc (this.mSheet.mGame,this.mRaphael,100,100,25,90,45,0,1,1,"none",.5,false);
- 	this.addQuestionShape(this.mArc);
+	//this.mArc = new Arc (this.mSheet.mGame,this.mRaphael,100,100,25,90,45,0,1,1,"none",.5,false);
+ 	//this.addQuestionShape(this.mArc);
+
+	var fifty = this.sector(100,100,50,0,180,{"fill":"red"});
+},
+sector: function(cx, cy, r, startAngle, endAngle, params)
+{
+	var rad = Math.PI / 180;
+        var x1 = cx + r * Math.cos(-startAngle * rad),
+        x2 = cx + r * Math.cos(-endAngle * rad),
+        y1 = cy + r * Math.sin(-startAngle * rad),
+        y2 = cy + r * Math.sin(-endAngle * rad);
+    	return this.mRaphael.path(["M", cx, cy, "L", x1, y1, "A", r, r, 0, +(endAngle - startAngle > 180), 0, x2, y2, "z"]).attr(params);
 }
 
 });
