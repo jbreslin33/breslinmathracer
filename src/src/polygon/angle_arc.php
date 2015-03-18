da77fc09ed8a9fc83fe90ee6e830de2c08614843
jbreslin33 <jbreslin33@localhost>
2015-03-18 15:07:17 -0400

    home

--- src/src/polygon/angle_arc.php
+++ src/src/polygon/angle_arc.php
@@ -1,46 +1,44 @@
 var AngleArc = new Class(
 {
 Extends: RaphaelPolygon,
-        initialize: function (item,x,y,radius,start,end,r,g,b,s,op,d)
-        {
-		this.parent(0,0,x,y,item.mSheet.mGame,item.mRaphael,r,g,b,s,op,d);
+initialize: function (item,x,y,radius,start,end,r,g,b,s,op,d)
+{
+	this.parent(0,0,x,y,item.mSheet.mGame,item.mRaphael,r,g,b,s,op,d);
 
-        	var rad = Math.PI / 180;
-        	var x1 = x + radius * Math.cos(-start * rad),
-        	x2 = x + radius * Math.cos(-end * rad),
-        	y1 = y + radius * Math.sin(-start * rad),
-        	y2 = y + radius * Math.sin(-end * rad);
+       	var rad = Math.PI / 180;
+       	var x1 = x + radius * Math.cos(-start * rad),
+       	x2 = x + radius * Math.cos(-end * rad),
+       	y1 = y + radius * Math.sin(-start * rad),
+       	y2 = y + radius * Math.sin(-end * rad);
         
-		this.mPolygon = this.mRaphael.path(["M", x, y, "L", x1, y1, "A", radius, radius, 0, +(end - start > 180), 0, x2, y2, "z"]).attr({fill: "hsb(" + this.mRed + "," + this.mGreen + "," + this.mBlue + ")", stroke: this.mStroke, opacity: this.mOpacity});
+	this.mPolygon = this.mRaphael.path(["M", x, y, "L", x1, y1, "A", radius, radius, 0, +(end - start > 180), 0, x2, y2, "z"]).attr({fill: "hsb(" + this.mRed + "," + this.mGreen + "," + this.mBlue + ")", stroke: this.mStroke, opacity: this.mOpacity});
+	//this.mText = this.mRaphael.text(190, 100, "ellipse").attr({fill: '#ff0000'});
+	//this.mText = this.mRaphael.text(x, parseFloat(y + radius), "ellipse").attr({fill: '#ff0000'});
 
-		this.mPolygon.mPolygon = this;
+	var c     = Math.sin(start);  
+	var textX = parseFloat(c * radius); 
+	
+	var s     = Math.cos(end);  
+	var textY = parseFloat(s * radius); 
+	APPLICATION.log('textX:' + textX);
+	APPLICATION.log('textY:' + textY);
+	
+	this.mText = this.mRaphael.text(textX, textY, "ellipse").attr({fill: '#ff0000'});
 
-		if (this.mDrag)
-		{
- 			this.mPolygon.drag(this.move, this.start, this.up);
-		}
-	},
-
-	dragMove: function(dx,dy)
-	{
-   		var deltaX = dx - this.mLastX;
-                var deltaY = dy - this.mLastY;
-
-		//update mPosition
-		this.mPosition.mX += deltaX; 
-		this.mPosition.mY += deltaY; 
-
-                this.x1 += deltaX;
-                this.y1 += deltaY;
-                this.x2 += deltaX;
-                this.y2 += deltaY;
-                this.x3 += deltaX;
-                this.y3 += deltaY;
-
-                this.mPathString = "M" + this.x1 + "," + this.y1 + " L" + this.x2 + "," + this.y2 + " L" + this.x3 + "," + this.y3 + " z";
-                this.mPolygon.attr({path:"" + this.mPathString});
-
-                this.mLastX = dx;
-                this.mLastY = dy;
-	}
+	this.mPolygon.mPolygon = this;
+}
 });
+/*
+The simple equations you've linked to give the X and Y coordinates of the point on the circle relative to the center of the circle.
+
+X = r * cosine(angle)
+Y = r * sine(angle)
+
+This tells you how far the point is offset from the center of the circle. Since you have the coordinates of the center (xcircle, ycircle), simply add the calculated offset.
+
+The coordinates of the point on the circle are:
+
+X = xcircle + (r * sine(angle))
+Y = ycircle + (r * cosine(angle))
+
+*/
