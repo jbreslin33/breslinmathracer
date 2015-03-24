2d72fd3d4c5ba28d2435222136c8259fde8a7eb6
bbreslin99 <bbreslin99@localhost>
2015-03-24 08:35:09 -0400

    started 5mdc5

--- src/src/core/math/g5_md_c_3_a.php
+++ src/src/core/math/g5_md_c_3_a.php
@@ -1,4 +1,141 @@
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('5.md.c.3.a_11',5.26011,'5.md.c.3.a','');
+*/
+var i_5_md_c_3_a__11 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,450,200,255,145,100,50,580,100);
+
+        this.mType = '5.md.c.3.a_11';
+        this.ns = new NameSampler();
+        var rx = 10;
+        var ry = 120;
+        this.mRaphael = Raphael(rx,ry,400,600);
+
+// position of rubix cube
+var x = 35;
+var y = 220;
+
+// dimensions of a single cube
+var w1 = 40;
+var h1 = 40;
+var d1 = 40;
+
+// dimensions of rubix cube
+var w2 = Math.floor(Math.random()*3+2);
+var h2 = Math.floor(Math.random()*3+2);
+var d2 = Math.floor(Math.random()*3+2);
+
+var rCube = new RubixCube(this,this.mSheet.mGame,this.mRaphael,rx,ry,x,y,w1,h1,d1,w2,h2,d2,.5,.5,.5,"#000",1,false);
+
+        answer = w2*h2*d2;
+
+        this.setQuestion('The right rectangular prism is made of centimeter cubes. What is the volume of the prism in cubic centimeters?');
+        this.setAnswer('' + answer,0);
+
+this.mUserAnswerLabel.setPosition(625,150);
+this.mCorrectAnswerLabel.setPosition(625,250);
+
+}
+
+             
+});
+
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('5.md.c.3.a_10',5.26010,'5.md.c.3.a','');
+*/
+var i_5_md_c_3_a__10 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,450,200,255,145,100,50,580,100);
+
+        this.mType = '5.md.c.3.a_10';
+        this.ns = new NameSampler();
+        var rx = 10;
+        var ry = 120;
+        this.mRaphael = Raphael(rx,ry,400,600);
+
+// position of rubix cube
+var x = 35;
+var y = 220;
+
+// dimensions of a single cube
+var w1 = 40;
+var h1 = 40;
+var d1 = 40;
+
+// dimensions of rubix cube
+var w2 = Math.floor(Math.random()*3+2);
+var h2 = Math.floor(Math.random()*3+2);
+var d2 = Math.floor(Math.random()*3+2);
+
+var rCube = new RubixCube(this,this.mSheet.mGame,this.mRaphael,rx,ry,x,y,w1,h1,d1,w2,h2,d2,.5,.5,.5,"#000",1,false);
+
+        answer = w2*h2*d2;
+
+        this.setQuestion('The right rectangular prism is made of inch cubes. What is the volume of the prism in cubic inches?');
+        this.setAnswer('' + answer,0);
+
+this.mUserAnswerLabel.setPosition(625,150);
+this.mCorrectAnswerLabel.setPosition(625,250);
+
+}
+           
+});
+
+
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('5.md.c.3.a_9',5.2609,'5.md.c.3.a','');
+*/
+var i_5_md_c_3_a__9 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,450,200,255,145,100,50,580,100);
+
+        this.mType = '5.md.c.3.a_9';
+        this.ns = new NameSampler();
+        var rx = 10;
+        var ry = 120;
+        this.mRaphael = Raphael(rx,ry,400,600);
+
+// position of rubix cube
+var x = 35;
+var y = 220;
+
+// dimensions of a single cube
+var w1 = 40;
+var h1 = 40;
+var d1 = 40;
+
+// dimensions of rubix cube
+var w2 = Math.floor(Math.random()*3+2);
+var h2 = Math.floor(Math.random()*3+2);
+var d2 = Math.floor(Math.random()*3+2);
+
+var rCube = new RubixCube(this,this.mSheet.mGame,this.mRaphael,rx,ry,x,y,w1,h1,d1,w2,h2,d2,.5,.5,.5,"#000",1,false);
+
+        answer = w2*h2*d2;
+
+        this.setQuestion('The right rectangular prism is made of inch cubes. What is the volume of the prism in cubic inches?');
+        this.setAnswer('' + answer,0);
+
+this.mUserAnswerLabel.setPosition(625,150);
+this.mCorrectAnswerLabel.setPosition(625,250);
+
+}
+        
+});
+
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('5.md.c.3.a_8',5.2608,'5.md.c.3.a','');
 */
 var i_5_md_c_3_a__8 = new Class(
@@ -25,11 +162,8 @@ this.mUserAnswerLabel.setPosition(625,150);
 this.mCorrectAnswerLabel.setPosition(625,250);
 
 }
-
-             
+           
 });
-
-
 
 
 /*
@@ -219,8 +353,12 @@ this.mCorrectAnswerLabel.setPosition(625,250);
 });
 
 
+
+
+
+
 /*
-insert into item_types(id,progression,core_standards_id,description) valuescleveland steamers ('5.md.c.3.a_2',5.2602,'5.md.c.3.a','');
+insert into item_types(id,progression,core_standards_id,description) values ('5.md.c.3.a_2',5.2602,'5.md.c.3.a','');
 */
 var i_5_md_c_3_a__2 = new Class(
 {
@@ -237,31 +375,23 @@ initialize: function(sheet)
 
 // position of rubix cube
 var x = 35;
-var y = 90;
+var y = 220;
 
 // dimensions of a single cube
-var w1 = Math.floor(Math.random()*4)+1;
-var h1 = Math.floor(Math.random()*4)+1;
-var d1 = Math.floor(Math.random()*4)+1;
+var w1 = 40;
+var h1 = 40;
+var d1 = 40;
 
-answer = w1*h1*d1; //Math.floor(Math.random()*3+1);
+// dimensions of rubix cube
+var w2 = Math.floor(Math.random()*3+2);
+var h2 = Math.floor(Math.random()*3+2);
+var d2 = Math.floor(Math.random()*3+2);
 
-//var w1 = 6;
-//var h1 = 4;
-//var d1 = 4;
+var rCube = new RubixCube(this,this.mSheet.mGame,this.mRaphael,rx,ry,x,y,w1,h1,d1,w2,h2,d2,.5,.5,.5,"#000",1,false);
 
+        answer = w2*h2*d2;
 
-console.log(w1);
-console.log(h1);
-console.log(d1);
-
-w1 = w1*40;
-h1 = h1*40;
-d1 = d1*40;
-
-var cube = new Cube(this,this.mSheet.mGame,this.mRaphael,rx,ry,x,y,w1,h1,d1,.5,.5,.5,"#000",1,false,'units');
-
-        this.setQuestion('What is the volume of the right rectangular prism in cubic units?');
+        this.setQuestion('The right rectangular prism is made of meter cubes. What is the volume of the prism in cubic meters?');
         this.setAnswer('' + answer,0);
 
 this.mUserAnswerLabel.setPosition(625,150);
@@ -271,6 +401,9 @@ this.mCorrectAnswerLabel.setPosition(625,250);
 
              
 });
+
+
+
 
 
 /*
@@ -305,9 +438,9 @@ var d2 = Math.floor(Math.random()*3+2);
 
 var rCube = new RubixCube(this,this.mSheet.mGame,this.mRaphael,rx,ry,x,y,w1,h1,d1,w2,h2,d2,.5,.5,.5,"#000",1,false);
 
-        answer = w2*h2*d2; //Math.floor(Math.random()*3+1);
+        answer = w2*h2*d2;
 
-        this.setQuestion('What is the volume of the right rectangular prism in cubic units?');
+        this.setQuestion('The right rectangular prism is made of unit cubes. What is the volume of the prism in cubic units?');
         this.setAnswer('' + answer,0);
 
 this.mUserAnswerLabel.setPosition(625,150);
--- src/src/core/math/g5_md_c_5_a.php
+++ src/src/core/math/g5_md_c_5_a.php
@@ -1,1 +1,157 @@
-//add
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('5.md.c.5.a_1',5.2801,'5.md.c.5','');
+*/
+var i_5_md_c_5_a__1 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,450,200,255,145,100,50,580,100);
+
+        this.mType = '5.md.c.5.a_1';
+        this.ns = new NameSampler();
+        var rx = 10;
+        var ry = 120;
+        this.mRaphael = Raphael(rx,ry,400,600);
+
+// position of rubix cube
+var x = 35;
+var y = 90;
+
+// dimensions of a single cube
+var w1 = Math.floor(Math.random()*4)+1;
+var h1 = Math.floor(Math.random()*4)+1;
+var d1 = Math.floor(Math.random()*4)+1;
+
+answer = w1*h1*d1; //Math.floor(Math.random()*3+1);
+
+//var w1 = 6;
+//var h1 = 4;
+//var d1 = 4;
+
+w1 = w1*40;
+h1 = h1*40;
+d1 = d1*40;
+
+var cube = new Cube(this,this.mSheet.mGame,this.mRaphael,rx,ry,x,y,w1,h1,d1,.5,.5,.5,"#000",1,false,'inches');
+
+        this.setQuestion('What is the volume of the right rectangular prism in cubic inches?');
+        this.setAnswer('' + answer,0);
+
+this.mUserAnswerLabel.setPosition(625,150);
+this.mCorrectAnswerLabel.setPosition(625,250);
+
+}
+
+             
+});
+
+
+
+
+
+
+
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('5.md.c.5.a_2',5.2802,'5.md.c.5','');
+*/
+var i_5_md_c_5_a__2 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,450,200,255,145,100,50,580,100);
+
+        this.mType = '5.md.c.5.a_2';
+        this.ns = new NameSampler();
+        var rx = 10;
+        var ry = 120;
+        this.mRaphael = Raphael(rx,ry,400,600);
+
+// position of rubix cube
+var x = 35;
+var y = 90;
+
+// dimensions of a single cube
+var w1 = Math.floor(Math.random()*4)+1;
+var h1 = Math.floor(Math.random()*4)+1;
+var d1 = Math.floor(Math.random()*4)+1;
+
+answer = w1*h1*d1; //Math.floor(Math.random()*3+1);
+
+//var w1 = 6;
+//var h1 = 4;
+//var d1 = 4;
+
+w1 = w1*40;
+h1 = h1*40;
+d1 = d1*40;
+
+var cube = new Cube(this,this.mSheet.mGame,this.mRaphael,rx,ry,x,y,w1,h1,d1,.5,.5,.5,"#000",1,false,'feet');
+
+        this.setQuestion('What is the volume of the right rectangular prism in cubic feet?');
+        this.setAnswer('' + answer,0);
+
+this.mUserAnswerLabel.setPosition(625,150);
+this.mCorrectAnswerLabel.setPosition(625,250);
+
+}
+
+             
+});
+
+
+
+
+
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('5.md.c.5.a_3',5.2803,'5.md.c.5','');
+*/
+var i_5_md_c_5_a__3 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,450,200,255,145,100,50,580,100);
+
+        this.mType = '5.md.c.5.a_3';
+        this.ns = new NameSampler();
+        var rx = 10;
+        var ry = 120;
+        this.mRaphael = Raphael(rx,ry,400,600);
+
+// position of rubix cube
+var x = 35;
+var y = 90;
+
+// dimensions of a single cube
+var w1 = Math.floor(Math.random()*4)+1;
+var h1 = Math.floor(Math.random()*4)+1;
+var d1 = Math.floor(Math.random()*4)+1;
+
+answer = w1*h1*d1; //Math.floor(Math.random()*3+1);
+
+//var w1 = 6;
+//var h1 = 4;
+//var d1 = 4;
+
+w1 = w1*40;
+h1 = h1*40;
+d1 = d1*40;
+
+var cube = new Cube(this,this.mSheet.mGame,this.mRaphael,rx,ry,x,y,w1,h1,d1,.5,.5,.5,"#000",1,false,'units');
+
+        this.setQuestion('What is the volume of the right rectangular prism in cubic units?');
+        this.setAnswer('' + answer,0);
+
+this.mUserAnswerLabel.setPosition(625,150);
+this.mCorrectAnswerLabel.setPosition(625,250);
+
+}
+
+             
+});
+
+
