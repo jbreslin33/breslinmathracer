20b4168c7a02549def66974e656e894ebd628056
jbreslin33 <jbreslin33@localhost>
2015-03-28 10:43:38 -0400

    t11

--- src/src/core/math/g4_nbt_a_2.php
+++ src/src/core/math/g4_nbt_a_2.php
@@ -1,5 +1,35 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.2_11',4.0711,'4.nbt.a.2','');
+*/
+var i_4_nbt_a_2__11 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '4.nbt.a.2_11';
+        this.ns = new NameMachine();
+        this.mChopWhiteSpace = false;
+
+        this.tenthousands = Math.floor((Math.random()*8)+2);
+        this.thousands     = Math.floor((Math.random()*8)+2);
+        this.hundreds      = Math.floor((Math.random()*8)+2);
+        this.tens          = Math.floor((Math.random()*8)+2);
+        this.ones          = Math.floor((Math.random()*8)+2);
+
+        this.tenthousands_thousands = parseInt(this.tenthousands * 10000 + this.thousands * 1000);
+        this.tens_ones = parseInt(this.tens * 10 + this.ones);
+        this.number    = parseInt(this.tenthousands_thousands + this.hundreds * 100 + this.tens_ones);
+
+        this.setQuestion('Write the number as you would say it in words: ' + this.number,0);
+        this.setAnswer('' + this.ns.getNumberName( parseInt(this.tenthousands * 10 + this.thousands)) + ' thousand ' + this.ns.getNumberName(this.hundreds) + ' hundred ' + this.ns.getNumberName(this.tens_ones) + '',0);
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.2_10',4.0710,'4.nbt.a.2','');
 */
 var i_4_nbt_a_2__10 = new Class(
