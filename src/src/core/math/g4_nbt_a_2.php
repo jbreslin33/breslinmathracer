2662f090075ce1c7616f12c8b6e178cd3566d37d
jbreslin33 <jbreslin33@localhost>
2015-03-28 10:33:30 -0400

    t10

--- src/src/core/math/g4_nbt_a_2.php
+++ src/src/core/math/g4_nbt_a_2.php
@@ -1,8 +1,8 @@
 
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.2_8',4.0708,'4.nbt.a.2','');
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.2_10',4.0710,'4.nbt.a.2','');
 */
-var i_4_nbt_a_2__8 = new Class(
+var i_4_nbt_a_2__10 = new Class(
 {
 Extends: TextItem,
 
@@ -10,13 +10,42 @@ initialize: function(sheet)
 {
         this.parent(sheet,575,50,320,75,720,50,380,150);
 
-        this.mType = '4.nbt.a.2_8';
+        this.mType = '4.nbt.a.2_10';
+        this.ns = new NameMachine();
+        this.mChopWhiteSpace = false;
+
+        this.thousands = Math.floor((Math.random()*8)+2);
+        this.hundreds  = 0;
+        this.tens      = Math.floor((Math.random()*8)+2);
+        this.ones      = Math.floor((Math.random()*8)+2);
+
+        this.tens_ones = parseInt(this.tens * 10 + this.ones);
+        this.number    = parseInt(this.thousands * 1000 + this.hundreds * 100 + this.tens_ones);
+
+        this.setQuestion('Write the number as you would say it in words: ' + this.number,0);
+        this.setAnswer('' + this.ns.getNumberName(this.thousands) + ' thousand ' + this.ns.getNumberName(this.tens_ones) + '',0);
+}
+});
+
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.2_9',4.0709,'4.nbt.a.2','');
+*/
+var i_4_nbt_a_2__9 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '4.nbt.a.2_9';
         this.ns = new NameMachine();
         this.mChopWhiteSpace = false;
 
         this.thousands = Math.floor((Math.random()*8)+2);
         this.hundreds  = Math.floor((Math.random()*8)+2);
-        this.tens      = Math.floor((Math.random()*8)+2);
+        this.tens      = 0;
         this.ones      = Math.floor((Math.random()*8)+2);
         
 	this.tens_ones = parseInt(this.tens * 10 + this.ones);
