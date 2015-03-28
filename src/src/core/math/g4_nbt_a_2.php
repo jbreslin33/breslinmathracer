3507d14578080229e0ec684dc43bf65509513d7e
jbreslin33 <jbreslin33@localhost>
2015-03-28 11:45:45 -0400

    t18

--- src/src/core/math/g4_nbt_a_2.php
+++ src/src/core/math/g4_nbt_a_2.php
@@ -1,5 +1,36 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.2_18',4.0718,'4.nbt.a.2','');
+*/
+var i_4_nbt_a_2__18 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '4.nbt.a.2_18';
+        this.ns = new NameMachine();
+        this.mChopWhiteSpace = false;
+
+        this.hundredthousands = Math.floor((Math.random()*8)+1);
+        this.tenthousands     = Math.floor((Math.random()*8)+1);
+        this.thousands        = Math.floor((Math.random()*8)+1);
+        this.hundreds         = Math.floor((Math.random()*8)+1);
+        this.tens             = 0;
+        this.ones             = Math.floor((Math.random()*8)+1);
+
+        this.tenthousands_thousands = parseInt(this.tenthousands * 10000 + this.thousands * 1000);
+        this.tens_ones = parseInt(this.tens * 10 + this.ones);
+        this.number    = parseInt(this.hundredthousands * 100000 + this.tenthousands_thousands + this.hundreds * 100 + this.tens_ones);
+
+        this.setQuestion('Write the number as you would say it in words: ' + this.number,0);
+        this.setAnswer('' + this.ns.getNumberName(this.hundredthousands) + ' hundred ' + this.ns.getNumberName( parseInt(this.tenthousands * 10 + this.thousands)) + ' thousand ' + this.ns.getNumberName(this.hundreds) + ' hundred ' + this.ns.getNumberName(this.tens_ones) + '',0);
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.2_17',4.0717,'4.nbt.a.2','');
 */
 var i_4_nbt_a_2__17 = new Class(
@@ -48,7 +79,7 @@ initialize: function(sheet)
         this.hundredthousands = Math.floor((Math.random()*8)+1);
         this.tenthousands     = Math.floor((Math.random()*8)+1);
         this.thousands        = Math.floor((Math.random()*8)+1);
-        this.hundreds         = Math.floor((Math.random()*8)+1);
+        this.hundreds         = 0;
         this.tens             = Math.floor((Math.random()*8)+1);
         this.ones             = Math.floor((Math.random()*8)+1);
 
@@ -57,7 +88,7 @@ initialize: function(sheet)
         this.number    = parseInt(this.hundredthousands * 100000 + this.tenthousands_thousands + this.hundreds * 100 + this.tens_ones);
 
         this.setQuestion('Write the number as you would say it in words: ' + this.number,0);
-        this.setAnswer('' + this.ns.getNumberName(this.hundredthousands) + ' hundred ' + this.ns.getNumberName( parseInt(this.tenthousands * 10 + this.thousands)) + ' thousand ' + this.ns.getNumberName(this.hundreds) + ' hundred ' + this.ns.getNumberName(this.tens_ones) + '',0);
+        this.setAnswer('' + this.ns.getNumberName(this.hundredthousands) + ' hundred ' + this.ns.getNumberName( parseInt(this.tenthousands * 10 + this.thousands)) + ' thousand ' + this.ns.getNumberName(this.tens_ones) + '',0);
 }
 });
 
