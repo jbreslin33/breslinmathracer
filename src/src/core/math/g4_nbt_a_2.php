a25628b32276617e6096741841b3d9800a68bb1f
jbreslin33 <jbreslin33@localhost>
2015-03-31 17:56:42 -0400

    t40

--- src/src/core/math/g4_nbt_a_2.php
+++ src/src/core/math/g4_nbt_a_2.php
@@ -253,6 +253,37 @@ initialize: function(sheet)
 });
 
 //40,41
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.2_40',4.0740,'4.nbt.a.2','');
+*/
+var i_4_nbt_a_2__40 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '4.nbt.a.2_40';
+        this.ns = new NameMachine();
+        this.mStripCommas = true;
+
+        this.hundredthousands = Math.floor((Math.random()*8)+1);
+        this.tenthousands     = Math.floor((Math.random()*8)+1);
+        this.thousands        = Math.floor((Math.random()*8)+1);
+        this.hundreds         = Math.floor((Math.random()*8)+1);
+        this.tens             = Math.floor((Math.random()*8)+1);
+        this.ones             = Math.floor((Math.random()*8)+1);
+
+        this.tenthousands_thousands = parseInt(this.tenthousands * 10000 + this.thousands * 1000);
+        this.tens_ones = parseInt(this.tens * 10 + this.ones);
+        this.number    = parseInt(this.hundredthousands * 100000 + this.tenthousands_thousands + this.hundreds * 100 + this.tens_ones);
+
+        this.setQuestion('Write the number in standard form: ' + parseInt(this.hundredthousands * 100000) + '+' + parseInt(this.tenthousands * 10000) + '+' + parseInt(this.thousands * 1000) + '+' +  parseInt(this.hundreds * 100) + '+' + parseInt(this.tens * 10) + '+' + this.ones,0);
+        this.setAnswer('' + this.number,0);
+}
+});
+
 
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.2_39',4.0739,'4.nbt.a.2','');
