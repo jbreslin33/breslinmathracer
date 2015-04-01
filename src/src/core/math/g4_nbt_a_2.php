3c35549d2007d2f1ff90d777d6f9e234d56d9541
jbreslin33 <jbreslin33@localhost>
2015-04-01 09:27:32 -0400

    t56

--- src/src/core/math/g4_nbt_a_2.php
+++ src/src/core/math/g4_nbt_a_2.php
@@ -1,3 +1,35 @@
+//56,57
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.2_56',4.0756,'4.nbt.a.2','');
+*/
+var i_4_nbt_a_2__56 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '4.nbt.a.2_56';
+        this.ns = new NameMachine();
+        this.mChopWhiteSpace = false;
+
+        this.hundredthousands = Math.floor((Math.random()*8)+1);
+        this.tenthousands     = 0;
+        this.thousands        = 0;
+        this.hundreds         = 0;
+        this.tens             = Math.floor((Math.random()*8)+1);
+        this.ones             = Math.floor((Math.random()*8)+1);
+
+        this.tenthousands_thousands = parseInt(this.tenthousands * 10000 + this.thousands * 1000);
+        this.tens_ones = parseInt(this.tens * 10 + this.ones);
+        this.number    = parseInt(this.hundredthousands * 100000 + this.tenthousands_thousands + this.hundreds * 100 + this.tens_ones);
+
+        this.setQuestion('Write the number in standard form: ' + parseInt(this.hundredthousands * 100000) + '+' + parseInt(this.tens * 10) + '+' + this.ones,0);
+        this.setAnswer('' + this.number,0);
+}
+});
 
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.2_55',4.0755,'4.nbt.a.2','');
