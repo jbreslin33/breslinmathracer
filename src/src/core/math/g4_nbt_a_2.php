915720a6b6d8442a481eceb462cf4aabbaffe4dc
jbreslin33 <jbreslin33@localhost>
2015-04-01 09:33:41 -0400

    t57

--- src/src/core/math/g4_nbt_a_2.php
+++ src/src/core/math/g4_nbt_a_2.php
@@ -1,4 +1,36 @@
 //56,57
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.2_57',4.0757,'4.nbt.a.2','');
+*/
+var i_4_nbt_a_2__57 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '4.nbt.a.2_57';
+        this.ns = new NameMachine();
+        this.mStripCommas = true;
+
+        this.hundredthousands = Math.floor((Math.random()*8)+1);
+        this.tenthousands     = 0;
+        this.thousands        = 0;
+        this.hundreds         = 0;
+        this.tens             = Math.floor((Math.random()*8)+1);
+        this.ones             = Math.floor((Math.random()*8)+1);
+
+        this.tenthousands_thousands = parseInt(this.tenthousands * 10000 + this.thousands * 1000);
+        this.tens_ones = parseInt(this.tens * 10 + this.ones);
+        this.number    = parseInt(this.hundredthousands * 100000 + this.tenthousands_thousands + this.hundreds * 100 + this.tens_ones);
+
+        this.setQuestion('Write the number in expanded form: ' + this.number,0);
+        this.setAnswer('' + parseInt(this.hundredthousands * 100000) + '+' + parseInt(this.tens * 10) + '+' + this.ones,0);
+}
+});
+
+
 
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.2_56',4.0756,'4.nbt.a.2','');
@@ -13,7 +45,7 @@ initialize: function(sheet)
 
         this.mType = '4.nbt.a.2_56';
         this.ns = new NameMachine();
-        this.mChopWhiteSpace = false;
+        this.mStripCommas = true;
 
         this.hundredthousands = Math.floor((Math.random()*8)+1);
         this.tenthousands     = 0;
