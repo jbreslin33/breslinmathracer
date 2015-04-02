116e1fab9f8881c3735acc333aa267ceb85162fe
jbreslin33 <jbreslin33@localhost>
2015-04-02 10:00:53 -0400

    word prob

--- src/src/core/math/g4_nbt_a_2.php
+++ src/src/core/math/g4_nbt_a_2.php
@@ -425,7 +425,7 @@ initialize: function(sheet)
         this.parent(sheet,575,50,320,75,720,50,380,150);
 
         this.mType = '4.nbt.a.2_54';
-        this.ns = new NameMachine();
+        this.ns = new NameSampler();
         this.mStripCommas = true;
 
         this.hundredthousands = Math.floor((Math.random()*8)+1);
@@ -439,7 +439,7 @@ initialize: function(sheet)
         this.tens_ones = parseInt(this.tens * 10 + this.ones);
         this.number    = parseInt(this.hundredthousands * 100000 + this.tenthousands_thousands + this.hundreds * 100 + this.tens_ones);
 
-        this.setQuestion('Write the number: ' + this.ns.getNumberName(this.hundredthousands) + ' hundred ' + 'thousand ' + this.ns.getNumberName(this.tens_ones) + '',0);
+        this.setQuestion('' + this.ns.mNameOne + ' says ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' scored ' + this.ns.mNameMachine.getNumberName(this.hundredthousands) + ' hundred ' + 'thousand ' + this.ns.mNameMachine.getNumberName(this.tens_ones) + ' points in ' + this.ns.mVideoGameOne + '. Write that number in standard form.',0);
         this.setAnswer('' + this.number,0);
 }
 });
