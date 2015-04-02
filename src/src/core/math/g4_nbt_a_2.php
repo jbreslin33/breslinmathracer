30fc3364c06cf5d44555cec0d4689e9818148840
jbreslin33 <jbreslin33@localhost>
2015-04-02 13:48:20 -0400

    t46

--- src/src/core/math/g4_nbt_a_2.php
+++ src/src/core/math/g4_nbt_a_2.php
@@ -677,7 +677,7 @@ initialize: function(sheet)
         this.parent(sheet,575,50,320,75,720,50,380,150);
 
         this.mType = '4.nbt.a.2_46';
-        this.ns = new NameMachine();
+        this.ns = new NameSampler();
         this.mStripCommas = true;
 
         this.hundredthousands = Math.floor((Math.random()*8)+1);
@@ -691,7 +691,7 @@ initialize: function(sheet)
         this.tens_ones = parseInt(this.tens * 10 + this.ones);
         this.number    = parseInt(this.hundredthousands * 100000 + this.tenthousands_thousands + this.hundreds * 100 + this.tens_ones);
 
-        this.setQuestion('Write the number: ' + this.ns.getNumberName(this.hundredthousands) + ' hundred ' + this.ns.getNumberName( parseInt(this.tenthousands * 10 + this.thousands)) + ' thousand ' + this.ns.getNumberName(this.hundreds) + ' hundred ' + this.ns.getNumberName(this.tens_ones) + '',0);
+        this.setQuestion('' + 'The area of the state ' + this.ns.mNameOne + ' lives in is ' + this.ns.mNameMachine.getNumberName(this.hundredthousands) + ' hundred ' + this.ns.mNameMachine.getNumberName( parseInt(this.tenthousands * 10 + this.thousands)) + ' thousand ' + this.ns.mNameMachine.getNumberName(this.hundreds) + ' hundred ' + this.ns.mNameMachine.getNumberName(this.tens_ones) + ' square miles. Write the amount of square miles in standard form.',0);
         this.setAnswer('' + this.number,0);
 }
 });
