ba1bfbf6056d6db9312e07e62094463a6d25a411
jbreslin33 <jbreslin33@localhost>
2015-04-03 14:07:22 -0400

    comments

--- src/src/core/math/g4_nbt_a_3.php
+++ src/src/core/math/g4_nbt_a_3.php
@@ -1,6 +1,50 @@
 
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.3_5',4.0805,'4.nbt.a.3','');
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.3_6',4.0806,'4.nbt.a.3','round to 10,000 place');
+*/
+var i_4_nbt_a_3__6 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '4.nbt.a.3_6';
+        this.ns = new NameSampler();
+        this.mStripCommas = true;
+
+        this.hundredthousands = Math.floor(Math.random()*10);
+        this.tenthousandsA   = Math.floor(Math.random()*10);
+        this.tenthousandsB   = this.tenthousandsA;
+        this.thousands       = Math.floor(Math.random()*9);
+        this.hundreds        = Math.floor(Math.random()*10);
+        this.tens            = Math.floor(Math.random()*10);
+        this.ones            = Math.floor(Math.random()*10);
+
+        if (this.hundreds > 4)
+        {
+                this.thousandsB = parseInt(this.thousandsB + 1);
+        }
+
+        //a
+        this.tenthousands_thousands = parseInt(this.tenthousandsA * 10000 + this.thousands * 1000);
+        this.tens_ones = parseInt(this.tens * 10 + this.ones);
+        this.numberA    = parseInt(this.hundredthousands * 100000 + this.tenthousands_thousands + this.hundreds * 100 + this.tens_ones);
+
+        //b
+        this.numberB = parseInt(this.hundredthousands * 100000 + this.tenthousandsB * 10000 + this.thousands * 1000);
+
+        this.setQuestion('' + this.ns.mNameOne + ' is planning a trip across the country. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' totals up the miles ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' will travel and it comes to ' + this.numberA + '. Round that number to the nearest ten thousands place.',0);
+        this.setAnswer('' + this.numberB,0);
+
+
+}
+});
+
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.3_5',4.0805,'4.nbt.a.3','middle number');
 */
 var i_4_nbt_a_3__5 = new Class(
 {
@@ -44,7 +88,7 @@ initialize: function(sheet)
 });
 
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.3_4',4.0804,'4.nbt.a.3','');
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.3_4',4.0804,'4.nbt.a.3','round to 1000 with a 9.');
 */
 var i_4_nbt_a_3__4 = new Class(
 {
@@ -79,7 +123,7 @@ initialize: function(sheet)
 });
 
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.3_3',4.0803,'4.nbt.a.3','');
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.3_3',4.0803,'4.nbt.a.3','word problem. round to 1000 place.');
 */
 var i_4_nbt_a_3__3 = new Class(
 {
@@ -94,7 +138,7 @@ initialize: function(sheet)
         this.mStripCommas = true;
 
         this.tenthousands = Math.floor(Math.random()*10);
-        this.thousandsA   = Math.floor(Math.random()*10);
+        this.thousandsA   = Math.floor(Math.random()*9);
         this.thousandsB   = this.thousandsA;
         this.hundreds     = Math.floor(Math.random()*10);
         this.tens         = Math.floor(Math.random()*10);
@@ -119,7 +163,7 @@ initialize: function(sheet)
 });
 
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.3_2',4.0802,'4.nbt.a.3','');
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.3_2',4.0802,'4.nbt.a.3','estimate word problem. 10,000');
 */
 var i_4_nbt_a_3__2 = new Class(
 {
@@ -160,7 +204,7 @@ initialize: function(sheet)
 });
 
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.3_1',4.0801,'4.nbt.a.3','');
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.3_1',4.0801,'4.nbt.a.3','estimate word problem. 100,000');
 */
 var i_4_nbt_a_3__1 = new Class(
 {
