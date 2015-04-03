62fec9255e4d49a589bedec8feb02f7e4057d22f
jbreslin33 <jbreslin33@localhost>
2015-04-02 20:16:34 -0400

    t3

--- src/src/core/math/g4_nbt_a_3.php
+++ src/src/core/math/g4_nbt_a_3.php
@@ -1,5 +1,45 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.3_3',4.0803,'4.nbt.a.3','');
+*/
+var i_4_nbt_a_3__3 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '4.nbt.a.3_3';
+        this.ns = new NameSampler();
+        this.mStripCommas = true;
+
+        this.tenthousands = Math.floor(Math.random()*10);
+        this.thousandsA   = Math.floor(Math.random()*10);
+        this.thousandsB   = this.thousandsA;
+        this.hundreds     = Math.floor(Math.random()*10);
+        this.tens         = Math.floor(Math.random()*10);
+        this.ones         = Math.floor(Math.random()*10);
+
+        if (this.hundreds > 4)
+        {
+                this.thousandsB = parseInt(this.thousandsB + 1);
+        }
+
+        //a
+        this.tenthousands_thousands = parseInt(this.tenthousands * 10000 + this.thousandsA * 1000);
+        this.tens_ones = parseInt(this.tens * 10 + this.ones);
+        this.numberA    = parseInt(this.tenthousands_thousands + this.hundreds * 100 + this.tens_ones);
+
+        //b
+        this.numberB = parseInt(this.tenthousands * 10000 + this.thousandsB * 1000);
+
+        this.setQuestion('' + 'Round ' + this.numberA + ' to the thousands place.',0);
+        this.setAnswer('' + this.numberB,0);
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.3_2',4.0802,'4.nbt.a.3','');
 */
 var i_4_nbt_a_3__2 = new Class(
