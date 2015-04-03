3f0bd7731e64d9389750ad0dc418de8bbec56e1e
jbreslin33 <jbreslin33@localhost>
2015-04-03 14:51:52 -0400

    t2

--- src/src/core/math/g4_nbt_a_3.php
+++ src/src/core/math/g4_nbt_a_3.php
@@ -201,9 +201,9 @@ initialize: function(sheet)
 });
 
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.3_2',4.0802,'4.nbt.a.3','estimate word problem. 10,000');
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.3_12',4.0812,'4.nbt.a.3','estimate word problem. 10,000');
 */
-var i_4_nbt_a_3__2 = new Class(
+var i_4_nbt_a_3__12 = new Class(
 {
 Extends: TextItem,
 
@@ -211,7 +211,7 @@ initialize: function(sheet)
 {
         this.parent(sheet,575,50,320,75,720,50,380,150);
 
-        this.mType = '4.nbt.a.3_2';
+        this.mType = '4.nbt.a.3_12';
         this.ns = new NameSampler();
         this.mStripCommas = true;
 
@@ -240,6 +240,50 @@ initialize: function(sheet)
         this.setAnswer('' + this.numberB,0);
 }
 });
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.3_2',4.0802,'4.nbt.a.3','round to 100,000 place');
+*/
+var i_4_nbt_a_3__2 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '4.nbt.a.3_2';
+        this.ns = new NameSampler();
+        this.mStripCommas = true;
+
+        this.hundredthousandsA = Math.floor(Math.random()*9);
+        this.hundredthousandsB = this.hundredthousandsA;
+        this.tenthousands      = Math.floor(Math.random()*10);
+        this.thousands         = Math.floor(Math.random()*10);
+        this.hundreds          = Math.floor(Math.random()*10);
+        this.tens              = Math.floor(Math.random()*10);
+        this.ones              = Math.floor(Math.random()*10);
+
+        if (this.tenthousands > 4)
+        {
+                this.hundredthousandsB = parseInt(this.hundredthousandsB + 1);
+        }
+
+        //a
+        this.tenthousands_thousands = parseInt(this.tenthousands * 10000 + this.thousands * 1000);
+        this.tens_ones = parseInt(this.tens * 10 + this.ones);
+        this.numberA    = parseInt(this.hundredthousandsA * 100000 + this.tenthousands_thousands + this.hundreds * 100 + this.tens_ones);
+
+        //b
+        this.numberB = parseInt(this.hundredthousandsB * 100000);
+
+        this.setQuestion('' + this.ns.mNameOne + ' is planning a trip across the country. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' totals up the miles ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' will travel and it comes to ' + this.numberA + '. Round that number to the nearest hundred thousands place.',0);
+        this.setAnswer('' + this.numberB,0);
+
+
+}
+});
+
 
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.3_1',4.0801,'4.nbt.a.3','estimate word problem. 100,000');
