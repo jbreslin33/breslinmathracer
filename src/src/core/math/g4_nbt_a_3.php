c18c50b0515de33f861538d93dd2509b341e230c
jbreslin33 <jbreslin33@localhost>
2015-04-03 23:03:59 -0400

    t8

--- src/src/core/math/g4_nbt_a_3.php
+++ src/src/core/math/g4_nbt_a_3.php
@@ -1,3 +1,37 @@
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.3_8',4.0808,'4.nbt.a.3','10,000');
+*/
+var i_4_nbt_a_3__8 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '4.nbt.a.3_8';
+        this.ns = new NameSampler();
+        this.mStripCommas = true;
+
+        var hundredthousands = Math.floor(Math.random()*9)+1;
+        var tenthousands     = 0;
+        var thousands        = Math.floor(Math.random()*5);
+        var hundreds         = Math.floor(Math.random()*10);
+        var tens             = Math.floor(Math.random()*10);
+        var ones             = Math.floor(Math.random()*10);
+
+        //a
+        var tenthousands_thousands = parseInt(tenthousands * 10000 + thousands * 1000);
+        var tens_ones              = parseInt(tens * 10 + ones);
+        var numberA                = parseInt(hundredthousands * 100000 + tenthousands_thousands + hundreds * 100 + tens_ones);
+
+        //b
+        var numberB                = parseInt(hundredthousands * 100000 + tenthousands * 10000);
+
+        this.setQuestion('' + 'Round ' + numberA + ' to the nearest ten thousand.',0);
+        this.setAnswer('' + numberB,0);
+}
+});
 
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.a.3_7',4.0807,'4.nbt.a.3','10,000');
