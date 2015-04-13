cc00c882fca1d7010957843e463699051a6fc3a3
jbreslin33 <jbreslin33@localhost>
2015-04-12 20:57:43 -0400

    4.nbt.b.4 done

--- src/src/core/math/g4_nbt_b_4.php
+++ src/src/core/math/g4_nbt_b_4.php
@@ -1,5 +1,132 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.b.4_14',4.0914,'4.nbt.b.4','100000-100000');
+*/
+
+var i_4_nbt_b_4__14 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '4.nbt.b.4_14';
+
+        this.mStripCommas = true;
+
+        var a = 0;
+        var b = 1;
+
+        while (a < b)
+        {
+                a = Math.floor(Math.random()*899999)+100000;
+                b = Math.floor(Math.random()*899999)+100000;
+        }
+
+        var answer = parseInt(a - b);
+
+        this.setQuestion('' + a + ' - ' + b + ' = ',0);
+        this.setAnswer('' + answer,0);
+}
+});
+
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.b.4_13',4.0913,'4.nbt.b.4','100000-10000');
+*/
+
+var i_4_nbt_b_4__13 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '4.nbt.b.4_13';
+
+        this.mStripCommas = true;
+
+        var a = 0;
+        var b = 1;
+
+        while (a < b)
+        {
+                a = Math.floor(Math.random()*899999)+100000;
+                b = Math.floor(Math.random()*89999)+10000;
+        }
+
+        var answer = parseInt(a - b);
+
+        this.setQuestion('' + a + ' - ' + b + ' = ',0);
+        this.setAnswer('' + answer,0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.b.4_12',4.0912,'4.nbt.b.4','100000-1000');
+*/
+
+var i_4_nbt_b_4__12 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '4.nbt.b.4_12';
+
+        this.mStripCommas = true;
+
+        var a = 0;
+        var b = 1;
+
+        while (a < b)
+        {
+                a = Math.floor(Math.random()*899999)+100000;
+                b = Math.floor(Math.random()*8999)+1000;
+        }
+
+        var answer = parseInt(a - b);
+
+        this.setQuestion('' + a + ' - ' + b + ' = ',0);
+        this.setAnswer('' + answer,0);
+}
+});
+
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.b.4_11',4.0911,'4.nbt.b.4','10000-10000');
+*/
+
+var i_4_nbt_b_4__11 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '4.nbt.b.4_11';
+
+        this.mStripCommas = true;
+
+        var a = 0;
+        var b = 1;
+
+        while (a < b)
+        {
+                a = Math.floor(Math.random()*89999)+10000;
+                b = Math.floor(Math.random()*89999)+10000;
+        }
+
+        var answer = parseInt(a - b);
+
+        this.setQuestion('' + a + ' - ' + b + ' = ',0);
+        this.setAnswer('' + answer,0);
+}
+});
+
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.b.4_10',4.0910,'4.nbt.b.4','10000-1000');
 */
 
