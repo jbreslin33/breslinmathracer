b39c0027bf54559b6e316e7331b244c9e6ae00d3
jbreslin33 <jbreslin33@localhost>
2015-04-12 20:39:45 -0400

    t3

--- src/src/core/math/g4_nbt_b_4.php
+++ src/src/core/math/g4_nbt_b_4.php
@@ -1,3 +1,52 @@
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.b.4_3',4.0903,'4.nbt.b.4','10000+1000');
+*/
+
+var i_4_nbt_b_4__3 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '4.nbt.b.4_3';
+
+        this.mStripCommas = true;
+
+        var a = Math.floor(Math.random()*89999)+10000;
+        var b = Math.floor(Math.random()*8999)+1000;
+
+        var answer = parseInt(a + b);
+
+        this.setQuestion('' + a + ' + ' + b + ' = ',0);
+        this.setAnswer('' + answer,0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.b.4_2',4.0902,'4.nbt.b.4','1000+1000');
+*/
+
+var i_4_nbt_b_4__2 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '4.nbt.b.4_2';
+
+        this.mStripCommas = true;
+
+        var a = Math.floor(Math.random()*8999)+1000;
+        var b = Math.floor(Math.random()*8999)+1000;
+
+        var answer = parseInt(a + b);
+
+        this.setQuestion('' + a + ' + ' + b + ' = ',0);
+        this.setAnswer('' + answer,0);
+}
+});
 
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.b.4_1',4.0901,'4.nbt.b.4','1000+100');
@@ -12,8 +61,6 @@ initialize: function(sheet)
 
         this.mType = '4.nbt.b.4_1';          	
 
-	this.mQuestionL
-	
 	this.mStripCommas = true;
 
 	var a = Math.floor(Math.random()*8999)+1000;
