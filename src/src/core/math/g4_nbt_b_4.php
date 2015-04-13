0e1f5db36c5c3125588788c9ec6381315bcfdacf
jbreslin33 <jbreslin33@localhost>
2015-04-12 20:41:30 -0400

    t4

--- src/src/core/math/g4_nbt_b_4.php
+++ src/src/core/math/g4_nbt_b_4.php
@@ -1,4 +1,29 @@
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.b.4_4',4.0904,'4.nbt.b.4','10000+10000');
+*/
+
+var i_4_nbt_b_4__4 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '4.nbt.b.4_4';
+
+        this.mStripCommas = true;
+
+        var a = Math.floor(Math.random()*89999)+10000;
+        var b = Math.floor(Math.random()*89999)+10000;
+
+        var answer = parseInt(a + b);
+
+        this.setQuestion('' + a + ' + ' + b + ' = ',0);
+        this.setAnswer('' + answer,0);
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nbt.b.4_3',4.0903,'4.nbt.b.4','10000+1000');
 */
 
