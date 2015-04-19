ccf7ccc788e2f75b6dc27623e22bd80ebaa52489
jbreslin33 <jbreslin33@localhost>
2015-04-19 19:07:01 -0400

    t3

--- src/src/core/math/g4_nf_a_1.php
+++ src/src/core/math/g4_nf_a_1.php
@@ -1,215 +1,96 @@
 
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('4.nf.a.1_4',4.1204,'4.nf.a.1','put fraction in lower form by finding missing numerator');
-*/
-
-var i_4_nf_a_1__4 = new Class(
-{
-Extends: TextItem,
-   initialize: function(sheet)
-   {
-      this.parent(sheet);
-      this.mType = '4.nf.a.1_4';
-       	
-		var varA = 0;
-		var varB = 0;
-		var varC = 0;
-		var varD = 0;
-		var varN = 0;
-
-
-		var start = 0;
-		var end = 0;
-		var rand = 0;
-
-		var question = '';
-
-			// pick random number (1 - 9)
-			varA = 1 + Math.floor((Math.random()*9));
-
-			// pick random number from 2-12 (greater than varA)
-			varB = varA + (1 + Math.floor((Math.random()*(12-varA))));
-
-			
-			// pick random number (1 - 9) as a multiplier
-			varN = 2 + Math.floor((Math.random()*9));
-
-			varC = varA * varN;
-			varD = varB * varN;
-					
-		  question = '' + varC + ' / ' +  varD + ' = ' + '' + '?' + ' / ' +  varB;
-								                       
-			this.setQuestion(question);
-      this.setAnswer('' + varA,0);
-
-      this.mQuestionLabel.setSize(200,50);
-      this.mQuestionLabel.setPosition(275,95);  
-   }
-});
-
-/*
-insert into item_types(id,progression,core_standards_id,description) values ('4.nf.a.1_3',4.1203,'4.nf.a.1','put fraction in lower form by finding missing denominator');
+insert into item_types(id,progression,core_standards_id,description) values ('4.nf.a.1_3',4.1203,'4.nf.a.1','1h');
 */
 
 var i_4_nf_a_1__3 = new Class(
-{
-Extends: TextItem,
-   initialize: function(sheet)
-   {
-      this.parent(sheet);
-      this.mType = '4.nf.a.1_3';
-       	
-		var varA = 0;
-		var varB = 0;
-		var varC = 0;
-		var varD = 0;
-		var varN = 0;
-
-
-		var start = 0;
-		var end = 0;
-		var rand = 0;
-
-		var question = '';
-
-			// pick random number (1 - 9)
-			varA = 1 + Math.floor((Math.random()*9));
-
-			// pick random number from 2-12 (greater than varA)
-			varB = varA + (1 + Math.floor((Math.random()*(12-varA))));
-
-			
-			// pick random number (1 - 9) as a multiplier
-			varN = 2 + Math.floor((Math.random()*9));
-
-			varC = varA * varN;
-			varD = varB * varN;
-					
-		  question = '' + varC + ' / ' +  varD + ' = ' + '' + varA + ' / ' +  '?';
-								                       
-			this.setQuestion(question);
-      this.setAnswer('' + varB,0);
-
-      this.mQuestionLabel.setSize(200,50);
-      this.mQuestionLabel.setPosition(275,95);
-   }
-});
-
-
-/*
-insert into item_types(id,progression,core_standards_id,description) values ('4.nf.a.1_2',4.1202,'4.nf.a.1','put fraction in higher form by finding missing numerator');
-*/
-
-var i_4_nf_a_1__2 = new Class(
-{
-Extends: TextItem,
-   initialize: function(sheet)
-   {
-      this.parent(sheet);
-      this.mType = '4.nf.a.1_2';
-       	
-		var varA = 0;
-		var varB = 0;
-		var varC = 0;
-		var varD = 0;
-		var varN = 0;
-
-
-		var start = 0;
-		var end = 0;
-		var rand = 0;
-
-		var question = '';
-
-			// pick random number (1 - 9)
-			varA = 1 + Math.floor((Math.random()*9));
-
-			// pick random number from 2-12 (greater than varA)
-			varB = varA + (1 + Math.floor((Math.random()*(12-varA))));
-
-			
-			// pick random number (1 - 9) as a multiplier
-			varN = 2 + Math.floor((Math.random()*9));
-
-			varC = varA * varN;
-			varD = varB * varN;
-					
-		  question = '' + varA + ' / ' +  varB + ' = ' + '' + '?' + ' / ' +  varD;
-								                       
-			this.setQuestion(question);
-      this.setAnswer('' + varC,0);
-
-      this.mQuestionLabel.setSize(200,50);
-      this.mQuestionLabel.setPosition(275,95);  
-   }
-});
-
-
-/*
-insert into item_types(id,progression,core_standards_id,description) values ('4.nf.a.1_1',4.1201,'4.nf.a.1','put fraction in higher form by finding missing denominator');
-*/
-
-var i_4_nf_a_1__1 = new Class(
-{
-Extends: TextItem,
-   initialize: function(sheet)
-   {
-      this.parent(sheet);
-      this.mType = '4.nf.a.1_1';
-       	
-		var varA = 0;
-		var varB = 0;
-		var varC = 0;
-		var varD = 0;
-		var varN = 0;
-
-
-		var start = 0;
-		var end = 0;
-		var rand = 0;
-
-		var question = '';
-
-			// pick random number (1 - 9)
-			varA = 1 + Math.floor((Math.random()*9));
-
-			// pick random number from 2-12 (greater than varA)
-			varB = varA + (1 + Math.floor((Math.random()*(12-varA))));
-
-			
-			// pick random number (1 - 9) as a multiplier
-			varN = 2 + Math.floor((Math.random()*9));
-
-			varC = varA * varN;
-			varD = varB * varN;
-
-			question = '' + varD + ' / ' +  varB + ' = ' + '' + varC + ' / ' +  '?';
-
-			this.setQuestion(question);
-      this.setAnswer('' + varA,0);
-
-      this.mQuestionLabel.setSize(200,50);
-      this.mQuestionLabel.setPosition(275,95);
-   }
-});
-
-
-/*
-insert into item_types(id,progression,core_standards_id,description) values ('4.nf.a.1_0_10',4.120010,'4.nf.a.1','compare');
-*/
-
-var i_4_nf_a_1__0_10 = new Class(
 {
 Extends: TextItem,
 initialize: function(sheet)
 {
         this.parent(sheet,600,200,325,145,100,50,580,130);
 
-      	this.mType = '4.nf.a.1_0_10';
+        this.mType = '4.nf.a.1_3';
+        this.mChopWhiteSpace = false;
+
+        var a = 0;
+        var b = 0;
+
+        while (a == b)
+        {
+                a = Math.floor(Math.random()*9)+1;
+                b = Math.floor(Math.random()*9)+1;
+        }
+
+        fractionA = new Fraction(a,b,false);
+
+        var c = parseInt(a * 2);
+        var d = parseInt(b * 2);
+
+        fractionB = new Fraction(c,d,false);
+
+        this.setQuestion('Compare ' + fractionB.getString() + ' and ' + fractionA.getString() + '. Write equal or not equal.');
+        this.setAnswer('equal',0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nf.a.1_2',4.1202,'4.nf.a.1','2x numerator 1x denominator');
+*/
+
+var i_4_nf_a_1__2 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,600,200,325,145,100,50,580,130);
+
+      	this.mType = '4.nf.a.1_2';
 	this.mChopWhiteSpace = false;
 
-	var a = Math.floor(Math.random()*9)+1;
-	var b = Math.floor(Math.random()*9)+1;
+	var a = 0;
+	var b = 0;
+
+	while (a == b)
+	{
+		a = Math.floor(Math.random()*9)+1;
+		b = Math.floor(Math.random()*9)+1;
+	}
+
+	fractionA = new Fraction(a,b,false);
+
+	var c = parseInt(a * 2);
+	var d = parseInt(b * 1);
+	
+	fractionB = new Fraction(c,d,false);
+
+        this.setQuestion('Compare ' + fractionA.getString() + ' and ' + fractionB.getString() + '. Write equal or not equal.');
+        this.setAnswer('not equal',0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nf.a.1_1',4.1201,'4.nf.a.1','2x');
+*/
+
+var i_4_nf_a_1__1 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,600,200,325,145,100,50,580,130);
+
+      	this.mType = '4.nf.a.1_1';
+	this.mChopWhiteSpace = false;
+
+	var a = 0;
+	var b = 0;
+
+	while (a == b)
+	{
+		a = Math.floor(Math.random()*9)+1;
+		b = Math.floor(Math.random()*9)+1;
+	}
 
 	fractionA = new Fraction(a,b,false);
 
@@ -220,35 +101,6 @@ initialize: function(sheet)
 
         this.setQuestion('Compare ' + fractionA.getString() + ' and ' + fractionB.getString() + '. Write equal or not equal.');
         this.setAnswer('equal',0);
-}
-});
-
-/*
-insert into item_types(id,progression,core_standards_id,description) values ('4.nf.a.1_0_11',4.120011,'4.nf.a.1','compare');
-*/
-
-var i_4_nf_a_1__0_11 = new Class(
-{
-Extends: TextItem,
-initialize: function(sheet)
-{
-        this.parent(sheet,600,200,325,145,100,50,580,130);
-
-        this.mType = '4.nf.a.1_0_11';
-        this.mChopWhiteSpace = false;
-
-        var a = Math.floor(Math.random()*9)+1;
-        var b = Math.floor(Math.random()*9)+1;
-
-        fractionA = new Fraction(a,b,false);
-
-        var c = parseInt(a * 2);
-        var d = parseInt(b * 1);
-
-        fractionB = new Fraction(c,d,false);
-
-        this.setQuestion('Compare ' + fractionB.getString() + ' and ' + fractionA.getString() + '. Write equal or not equal.');
-        this.setAnswer('not equal',0);
 }
 });
 
