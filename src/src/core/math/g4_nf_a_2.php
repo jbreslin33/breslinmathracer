6f9514892a6c5358e9130957c45e2004a0455abf
jbreslin33 <jbreslin33@localhost>
2015-04-20 19:05:46 -0400

    t1

--- src/src/core/math/g4_nf_a_2.php
+++ src/src/core/math/g4_nf_a_2.php
@@ -1,89 +1,51 @@
-
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('4.nf.a.2_1',4.1301,'4.nf.a.2','Compare two fractions with different numerators and denominators');
+insert into item_types(id,progression,core_standards_id,description) values ('4.nf.a.2_1',4.1301,'4.nf.a.2','');
 */
-
 var i_4_nf_a_2__1 = new Class(
 {
 Extends: ThreeButtonItem,
-   initialize: function(sheet)
-   {
-      this.parent(sheet);
-      this.mType = '4.nf.a.2_1';
-	this.mChopWhiteSpace = false;
-       	
-		var varA = 0;
-		var varB = 0;
-		var varC = 0;
-		var varD = 0;
-		var varN = 0;
+        initialize: function(sheet)
+        {
+                this.parent(sheet);
+                this.mType = '4.nf.a.2_1';
 
+                //BUTTON A
+                this.mButtonA.setPosition(380,100);
+                this.mButtonB.setPosition(380,200);
+                this.mButtonC.setPosition(380,300);
 
-		var start = 0;
-		var end = 0;
-		var rand = 0;
+                var na = 0;
+                var da = 0;
+                this.b = 0;
+                this.c = 0;
 
-		var question;
-		var answer;
-		var diff = 0;
-		
-	  if(rand == 0)
-		{
+                while (na <= da)
+                {
+                        na = Math.floor(Math.random()*18+2);
+                        da = Math.floor(Math.random()*18+2);
+                        this.b = Math.floor(Math.random()*18+2);
+                        this.c = this.b;
+                }
 
-			// pick random number (1 - 9)
-			varA = 1 + Math.floor((Math.random()*9));
+                this.mFractionA = new Fraction(na,da,false);
 
-			// pick random number from 2-12 (greater than varA)
-			varB = varA + (1 + Math.floor((Math.random()*(12-varA))));
+                this.setQuestion('Compare.');
+                this.setAnswer('&gt;',0);
 
-			varC = 1 + Math.floor((Math.random()*9));
-			varD = varC + (1 + Math.floor((Math.random()*(12-varA))));
+                this.mButtonA.setAnswer('&gt;');
+                this.mButtonB.setAnswer('=');
+                this.mButtonC.setAnswer('&lt;');
+        },
 
-			if(varA == varC && varB == varD)
-			   varA = varA + 1;
+        createQuestionShapes: function()
+        {
+                var shapeA = new Shape(100,100,240,200,this.mSheet.mGame,"","","");
+                var shapeB = new Shape(100,100,530,200,this.mSheet.mGame,"","","");
 
-		 }
-		 else
-		 {
+                shapeA.setText(this.b + ' &times ' + this.mFractionA.getString());
+                shapeB.setText(this.c);
 
-			// pick random number (1 - 5)
-			varA = 1 + Math.floor((Math.random()*5));
-
-			// pick random number from 2-10 (greater than varA)
-			varB = varA + (1 + Math.floor((Math.random()*(10-varA))));
-
-			// pick random number (2 - 4) as a multiplier
-			varN = 2 + Math.floor((Math.random()*3));
-
-			varC = varA * varN;
-			varD = varB * varN;
-
-		 }
-
-			diff = varA/varB - varC/varD;
-
-			if(diff > 0)
-			   answer = 'greater than';
-			else if(diff < 0)
-			   answer = 'less than';
-			else
-			   answer = 'equal to';
-			
-			question = '' + varA + ' / ' +  varB + ' is ? ' + '' + varC + ' / ' +  varD;
-
-			this.setQuestion(question);
-      this.setAnswer('' + answer,0);
-
-      this.mButtonA.setAnswer('greater than');
-      this.mButtonB.setAnswer('less than');
-      this.mButtonC.setAnswer('equal to');
-      //this.shuffle(1);
-
-      //this.mQuestionLabel.setSize(200,50);
-      //this.mQuestionLabel.setPosition(275,95);
-   }
+                this.addQuestionShape(shapeA);
+                this.addQuestionShape(shapeB);
+        }
 });
-
-
-
-//add
