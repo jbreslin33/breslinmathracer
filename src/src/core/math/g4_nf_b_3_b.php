c6bf77ee154bc63e3779419efa472afb38883789
jbreslin33 <jbreslin33@localhost>
2015-04-21 21:04:09 -0400

    t3

--- src/src/core/math/g4_nf_b_3_b.php
+++ src/src/core/math/g4_nf_b_3_b.php
@@ -1,3 +1,39 @@
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.3.b_3',4.1503,'4.nf.b.3.b','find missing numerator');
+*/
+
+var i_4_nf_b_3_b__3 = new Class(
+{
+Extends: TextItemFraction,
+initialize: function(sheet)
+{
+
+        this.parent(sheet,150,50,125,95,100,50,425,100,100,50,425,175);
+        this.mType = '4.nf.b.3_b_3';
+
+        var a = 0;
+        var b = 0;
+        var c = 0;
+        var d = 0;
+        var e = 0;
+
+        a = Math.floor(Math.random()*4)+1;
+        b = Math.floor(Math.random()*4)+1;
+        c = Math.floor(Math.random()*4)+1;
+        d = parseInt(a + b + c);
+        e = Math.floor(Math.random()*10)+5;
+
+        var fractionA = new Fraction(a,e,false);
+        var fractionB = new Fraction(c,e);
+        var fractionC = new Fraction(b,e,false);
+        var fractionD = new Fraction(d,e,false);
+
+        this.setQuestion('' + fractionA.getString() + ' + ? ' + fractionC.getString() + ' = ' + fractionD.getString());
+        this.setAnswer('' + fractionB.getString(),0);
+}
+});
+
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.3.b_2',4.1502,'4.nf.b.3.b','find missing numerator');
 */
