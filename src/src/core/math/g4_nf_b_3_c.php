0b2e9dfb4ee578d77a1c8819519e39cb9af17f10
jbreslin33 <jbreslin33@localhost>
2015-04-22 19:04:06 -0400

    t9

--- src/src/core/math/g4_nf_b_3_c.php
+++ src/src/core/math/g4_nf_b_3_c.php
@@ -1,5 +1,74 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.3.c_9',4.1609,'4.nf.b.3.c','');
+*/
+
+var i_4_nf_b_3_c__9 = new Class(
+{
+Extends: TextItemMixedNumber,
+
+initialize: function(sheet)
+{
+        this.parent(sheet, 320,100,200,95, 100,50,510,137, 100,50,625,100, 100,50,625,175,true);
+
+        this.mType = '4.nf.b.3.c_9';
+        this.ns = new NameSampler();
+
+        var a = 0;
+        var b = 0;
+        var d = 1;
+
+        while (b < d || a % d != 0 || b % d == 0 || a <= b)
+        {
+                a = Math.floor(Math.random()*10)+10;
+                b = Math.floor(Math.random()*10)+10;
+                d = Math.floor(Math.random()*3)+2;
+        }
+
+        var fractionA = new Fraction(a,d,true);
+        var fractionB = new Fraction(b,d,false);
+        var fractionC = fractionA.subtract(fractionB);
+
+        this.setQuestion('' + this.ns.mNameOne + ' ran ' + fractionA.getMixedNumber() + ' ' + this.ns.mDistanceIncrementLarge + ' and ' + this.ns.mNameTwo + ' ran ' + fractionB.getMixedNumber() + ' ' + this.ns.mDistanceIncrementLarge + '. How many more ' + this.ns.mDistanceIncrementLarge + ' did ' + this.ns.mNameOne + ' run then ' + this.ns.mNameTwo + '?');
+        this.setAnswer('' + fractionC.getString(),0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.3.c_8',4.1608,'4.nf.b.3.c','');
+*/
+
+var i_4_nf_b_3_c__8 = new Class(
+{
+Extends: TextItemMixedNumber,
+
+initialize: function(sheet)
+{
+        this.parent(sheet, 320,100,200,95, 100,50,510,137, 100,50,625,100, 100,50,625,175,true);
+
+        this.mType = '4.nf.b.3.c_8';
+
+        var a = 0;
+        var b = 0;
+        var d = 1;
+
+        while (b < d || a % d == 0 || b % d == 0 || a <= b)
+        {
+                a = Math.floor(Math.random()*10)+10;
+                b = Math.floor(Math.random()*10)+10;
+                d = Math.floor(Math.random()*3)+2;
+        }
+
+        var fractionA = new Fraction(a,d,false);
+        var fractionB = new Fraction(b,d,false);
+        var fractionC = fractionA.subtract(fractionB);
+
+        this.setQuestion('' + fractionA.getMixedNumber() + ' - ' + fractionB.getMixedNumber() + ' = ');
+        this.setAnswer('' + fractionC.getString(),0);
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.3.c_7',4.1607,'4.nf.b.3.c','');
 */
 
@@ -17,7 +86,7 @@ initialize: function(sheet)
         var b = 0;
         var d = 1;
 
-        while (b < d || a % d != 0 || b % d == 0 || a < b)
+        while (b < d || a % d != 0 || b % d == 0 || a <= b)
         {
                 a = Math.floor(Math.random()*10)+10;
                 b = Math.floor(Math.random()*10)+10;
@@ -51,7 +120,7 @@ initialize: function(sheet)
         var b = 0;
         var d = 1;
 
-        while (b < d || a % d != 0 || b % d == 0 || a < b)
+        while (b < d || a % d != 0 || b % d == 0 || a <= b)
         {
                 a = Math.floor(Math.random()*10)+10;
                 b = Math.floor(Math.random()*10)+10;
