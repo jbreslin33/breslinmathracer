41364c4878b82f3a674df0cd9181e653c5ca4a74
jbreslin33 <jbreslin33@localhost>
2015-04-24 18:32:00 -0400

    t2

--- src/src/core/math/g4_nf_b_3_d.php
+++ src/src/core/math/g4_nf_b_3_d.php
@@ -1,4 +1,32 @@
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.3.d_2',4.1702,'4.nf.b.3.d','word problems - subtract 2 fractions with like denominators');
+*/
+
+var i_4_nf_b_3_d__2 = new Class(
+{
+Extends: TextItemFraction,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,95,100,50,425,100,100,50,425,175);
+        this.mType = '4.nf.b.3.d_2';
+        this.ns = new NameSampler();
+
+        var a = Math.floor((Math.random()*5)+1);
+        var b = Math.floor((Math.random()*5)+1);
+        var c = Math.floor((Math.random()*10)+10);
+
+        var n = parseInt(a + b);
+
+        var ac = new Fraction(a,c,false);
+        var bc = new Fraction(b,c,false);
+        var answer = new Fraction(n,c);
+
+	this.setQuestion('' + this.ns.mNameOne + ' drove ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,1) + ' car ' + ac.getString() + ' of a ' + this.ns.mDistanceIncrementLarge + ' and stopped at a gas station. Then ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' drove ' + bc.getString() + ' ' + this.ns.mDistanceIncrementLarge + ' to ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,1) + ' house. How many miles did ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' drive ' + this.ns.mSum + '?');
+        this.setAnswer('' + answer.getString(),0);
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.3.d_1',4.1701,'4.nf.b.3.d','word problems - add 2 fractions with like denominators');
 */
 
@@ -26,45 +54,8 @@ initialize: function(sheet)
 }
 });
 
-
-/*
-insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.3.d_2',4.1702,'4.nf.b.3.d','word problems - subtract 2 fractions with like denominators');
-*/
-
-var i_4_nf_b_3_d__2 = new Class(
-{
-Extends: TextItemFraction,
-initialize: function(sheet)
-{
-        this.parent(sheet,300,50,175,95,100,50,425,100,100,50,425,175);
-        this.mType = '4.nf.b.3.d_2';
-        this.ns = new NameSampler();
-
-        var a = Math.floor((Math.random()*5)+1);
-        var b = Math.floor((Math.random()*5)+1);
-        var c = Math.floor((Math.random()*10)+10);
-
-        var n = parseInt(a + b);
-
-        var ac = new Fraction(a,c,false);
-        var bc = new Fraction(b,c,false);
-        var answer = new Fraction(n,c);
-
-        this.setQuestion('' + this.ns.mNameOne + ' filled a bucket with ' + ac.getString() + ' ' + this.ns.mLiquidVolumeOne + ' of ' + this.ns.mDrinkOne + '. Later ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' poured ' + bc.getString() + ' ' + this.ns.mLiquidVolumeOne + ' of ' + this.ns.mDrinkTwo + ' into the bucket. How many ' + this.ns.mLiquidVolumeOne + ' of liquid are in the bucket now?');
-        this.setAnswer('' + answer.getString(),0);
-}
-});
-
 //addition
 /*
-			if(rand == 0)
-			{			
-			question = 'Tammy filled a bucket with ' + varA + '/' +  varB + ' of a gallon of water. Later, she poured ' + varC + '/' +  varD + ' of a gallon of water into the bucket. How many gallons of water are in the bucket?';
-
-			//question.mTipArray[0] = 'xxxxxxxxxxxxxxxxxxxx';
-			}
-			if(rand == 1)
-			{			
 			question ='John drove his car ' + varA + '/' +  varB + ' of a mile and stopped at a gas station. Then, he drove ' + varC + '/' +  varD + ' of a mile to his house. How many miles did he drive altogether?';
 			}
 			if(rand == 2)
