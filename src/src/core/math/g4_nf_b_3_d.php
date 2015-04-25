19cfc01e12b3b5939dcb4fdd427eb083fa9a8222
jbreslin33 <jbreslin33@localhost>
2015-04-25 11:04:29 -0400

    t6

--- src/src/core/math/g4_nf_b_3_d.php
+++ src/src/core/math/g4_nf_b_3_d.php
@@ -1,5 +1,35 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.3.d_6',4.1706,'4.nf.b.3.d','');
+*/
+
+var i_4_nf_b_3_d__6 = new Class(
+{
+Extends: TextItemFraction,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,95,100,50,425,100,100,50,425,175);
+        this.mType = '4.nf.b.3.d_6';
+        this.ns = new NameSampler();
+
+        var a = Math.floor((Math.random()*5)+1);
+        var b = Math.floor((Math.random()*5)+1);
+        var c = Math.floor((Math.random()*10)+10);
+
+        var n = parseInt(a + b);
+
+        var ac = new Fraction(a,c,false);
+        var bc = new Fraction(b,c,false);
+        var answer = new Fraction(n,c);
+			//question = 'Of the pies that Mom and Pops Pie Shop sold last month, ' + varA + '/' +  varB + ' were blueberry pies and ' + varC + '/' +  varD + ' were blackberry pies. What fraction of the pies sold were either blueberry or blackberry?';
+
+        this.setQuestion('' + this.ns.mNameOne + ' and ' + this.ns.mNameTwo + ' operate a pie bakery. Of the the pies they sold last month ' + ac.getString() + ' were made with ' + this.ns.mFruitOne + ' and ' + bc.getString() + ' of the pies were made with ' + this.ns.mFruitTwo + '. What fraction of the pies sold last month were made with either ' + this.ns.mFruitOne + ' or ' + this.ns.mFruitTwo + '?');
+        this.setAnswer('' + answer.getString(),0);
+}
+});
+
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.3.d_5',4.1705,'4.nf.b.3.d','');
 */
 
@@ -27,7 +57,6 @@ initialize: function(sheet)
         var ac = new Fraction(a,c,false);
         var bc = new Fraction(b,c,false);
         var answer = new Fraction(n,c);
-			//question = 'At the market, Vicky bought ' + varA + '/' +  varB + ' of a pound of red apples and ' + varC + '/' +  varD + ' of a pound of green apples. How many more pounds of red apples did Vicky purchase?';
 
 	this.setQuestion('' + 'At the market ' + this.ns.mNameOne + ' bought ' + ac.getString() + ' pounds ' + ' of ' + this.ns.mFruitOne + ' and ' + bc.getString() + ' pounds of ' + this.ns.mFruitTwo + '. How many more pounds of ' + this.ns.mFruitOne + ' than ' + this.ns.mFruitTwo + ' did ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' purchase?');
         this.setAnswer('' + answer.getString(),0);
@@ -56,8 +85,6 @@ initialize: function(sheet)
         var ac = new Fraction(a,c,false);
         var bc = new Fraction(b,c,false);
         var answer = new Fraction(n,c);
-
-			//'Katie went to the salon and had ' + varA + '/' +  varB + ' of an inch of hair cut off. The next day she went back and asked for another ' + varC + '/' +  varD + ' of an inch to be cut off. How many inches of hair did she have cut off in all?';
 
         this.setQuestion('' + this.ns.mNameOne + ' went to the salon and had ' + ac.getString() + ' ' + this.ns.mDistanceIncrementSmall + ' of hair cut off. The next day ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' went back and asked for another ' + bc.getString() + ' ' + this.ns.mDistanceIncrementSmall + ' of hair to be cut off. How many ' + this.ns.mDistanceIncrementSmall + ' did ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' have cut off ' + this.ns.mSum + '?');
         this.setAnswer('' + answer.getString(),0);
@@ -127,7 +154,8 @@ initialize: function(sheet)
         var answer = new Fraction(n,c);
 
 	this.setQuestion('' + this.ns.mNameOne + ' drove ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,1) + ' car ' + ac.getString() + ' of a ' + this.ns.mDistanceIncrementLarge + ' and stopped at a gas station. Then ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' drove ' + bc.getString() + ' ' + this.ns.mDistanceIncrementLarge + ' to ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,1) + ' house. How many miles did ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' drive ' + this.ns.mSum + '?');
-        this.setAnswer('' + answer.getString(),0);
+        
+	this.setAnswer('' + answer.getString(),0);
 }
 });
 
@@ -155,6 +183,7 @@ initialize: function(sheet)
         var answer = new Fraction(n,c);
 
 	this.setQuestion('' + this.ns.mNameOne + ' filled a bucket with ' + ac.getString() + ' ' + this.ns.mLiquidVolumeOne + ' of ' + this.ns.mDrinkOne + '. Later ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' poured ' + bc.getString() + ' ' + this.ns.mLiquidVolumeOne + ' of ' + this.ns.mDrinkTwo + ' into the bucket. How many ' + this.ns.mLiquidVolumeOne + ' of liquid are in the bucket now?');
+
 	this.setAnswer('' + answer.getString(),0);
 }
 });
@@ -164,7 +193,6 @@ initialize: function(sheet)
 			}
 			if(rand == 3)
 			{			
-			question = 'Of the pies that Mom and Pops Pie Shop sold last month, ' + varA + '/' +  varB + ' were blueberry pies and ' + varC + '/' +  varD + ' were blackberry pies. What fraction of the pies sold were either blueberry or blackberry?';
 			}
 */
 
