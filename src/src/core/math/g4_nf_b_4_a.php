fa3b24888857670247959044e7dcbed567a1754e
jbreslin33 <jbreslin33@localhost>
2015-04-25 21:29:10 -0400

    t1

--- src/src/core/math/g4_nf_b_4_a.php
+++ src/src/core/math/g4_nf_b_4_a.php
@@ -1,15 +1,15 @@
 
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.4.a_2',4.1602,'4.nf.b.4.a','Multiply unit fractions by whole numbers');
+insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.4.a_3',4.1603,'4.nf.b.4.a','Multiply unit fractions by whole numbers');
 */
 
-var i_4_nf_b_4_a__2 = new Class(
+var i_4_nf_b_4_a__3 = new Class(
 {
 Extends: TextItemFraction,
 initialize: function(sheet)
 {
         this.parent(sheet,300,50,175,95,100,50,425,100,100,50,425,175);
-        this.mType = '4.nf.b.4.a_2';
+        this.mType = '4.nf.b.4.a_3';
         this.ns = new NameSampler();
 
 	var a = 1;
@@ -37,6 +37,42 @@ initialize: function(sheet)
 });
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.4.a_2',4.1602,'4.nf.b.4.a','Multiply unit fractions by whole numbers');
+*/
+
+var i_4_nf_b_4_a__2 = new Class(
+{
+Extends: TextItemFraction,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,95,100,50,425,100,100,50,425,175);
+        this.mType = '4.nf.b.4.a_2';
+        this.ns = new NameSampler();
+
+        var a = 1;
+        var b = 0;
+        var c = 0;
+        var d = 1;
+
+        while (b == c)
+        {
+                var b = Math.floor((Math.random()*8)+2);
+                var c = Math.floor((Math.random()*8)+2);
+        }
+
+        var ab = new Fraction(a,b,false);
+        var cd = new Fraction(c,d,false);
+
+        var answer = ab.multiply(cd);
+
+        this.setAnswer('' + answer.getString(),0);
+        this.setQuestion('' + '' + c + ' &times ' + ab.getString() + ' = ');
+}
+
+});
+
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.4.a_1',4.1601,'4.nf.b.4.a','Multiply unit fractions by whole numbers');
 */
 
@@ -49,10 +85,17 @@ initialize: function(sheet)
 	this.mType = '4.nf.b.4.a_1';
         this.ns = new NameSampler();
 
-        var a = 1;
-	var b = Math.floor((Math.random()*8)+2);
-	var c = Math.floor((Math.random()*8)+2);
-        var d = 1;
+	var a = 1;
+	var b = 0;
+	var c = 0;
+	var d = 1;
+	
+	while (b == c)
+	{
+		var b = Math.floor((Math.random()*8)+2);
+		var c = Math.floor((Math.random()*8)+2);
+	}
+
 
 	var ab = new Fraction(a,b,false);
 	var cd = new Fraction(c,d,false);
