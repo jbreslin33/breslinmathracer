479bb097a10b5b6b3ea1627e89641e97ec348a2c
jbreslin33 <jbreslin33@localhost>
2015-04-25 21:15:47 -0400

    t2

--- src/src/core/math/g4_nf_b_4_a.php
+++ src/src/core/math/g4_nf_b_4_a.php
@@ -1,3 +1,41 @@
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.4.a_2',4.1602,'4.nf.b.4.a','Multiply unit fractions by whole numbers');
+*/
+
+var i_4_nf_b_4_a__2 = new Class(
+{
+Extends: TextItemFraction,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,95,100,50,425,100,100,50,425,175);
+        this.mType = '4.nf.b.4.a_2';
+        this.ns = new NameSampler();
+
+	var a = 1;
+	var b = 0;
+
+	while(a >=b)
+	{
+		a = Math.floor((Math.random()*8)+2);
+		b = Math.floor((Math.random()*8)+2);
+	}
+	
+	var c = a;
+        var d = 1;
+	
+	var e = 1;
+        var f = b;
+
+        var ab = new Fraction(a,b,false);
+        var cd = new Fraction(c,d,false);
+        var ef = new Fraction(e,f,false);
+
+        this.setAnswer('' + ef.getString(),0);
+        this.setQuestion('' + '' + ab.getString() + ' = ' + c + ' &times ? ');
+}
+});
+
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.4.a_1',4.1601,'4.nf.b.4.a','Multiply unit fractions by whole numbers');
 */
@@ -12,8 +50,8 @@ initialize: function(sheet)
         this.ns = new NameSampler();
 
         var a = 1;
-        var b = 2;
-        var c = 0;
+	var b = Math.floor((Math.random()*8)+2);
+	var c = Math.floor((Math.random()*8)+2);
         var d = 1;
 
 	var ab = new Fraction(a,b,false);
