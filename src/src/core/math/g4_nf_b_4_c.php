8ba26b1549ff1b9ba9bc952692cb5c9778c42ee6
jbreslin33 <jbreslin33@localhost>
2015-04-27 19:34:02 -0400

    t5

--- src/src/core/math/g4_nf_b_4_c.php
+++ src/src/core/math/g4_nf_b_4_c.php
@@ -1,5 +1,43 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.4.c_5',4.1805,'4.nf.b.4.c','improper fraction');
+*/
+
+var i_4_nf_b_4_c__5 = new Class(
+{
+Extends: TextItemFraction,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,95,100,50,425,100,100,50,425,175);
+        this.mType = '4.nf.b.4.c_5';
+        this.ns = new NameSampler();
+
+        var a = 1;
+        var b = 0;
+        var c = 0;
+        var d = 1;
+
+        while (b == c || a <= b)
+        {
+                var a = Math.floor((Math.random()*8)+2);
+                var b = Math.floor((Math.random()*8)+2);
+                var c = Math.floor((Math.random()*8)+2);
+        }
+
+
+        var ab = new Fraction(a,b,false);
+        var cd = new Fraction(c,d,false);
+
+        var answer = ab.multiply(cd);
+	//		question = 'Pedro has a lemon cookie recipe that calls for ' + varA + '/' +  varB + ' of a cup of sugar. How much sugar would Pedro use to make ' + varC + ' batches of cookies?';
+
+        this.setAnswer('' + answer.getString(),0);
+        this.setQuestion('' + this.ns.mNameOne + ' has a lemon cookie recipe that calls for ' + ab.getString() + ' of a cup of sugar. How many cups of sugar would ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' use to make ' + c + ' batches of cookies?');
+}
+
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.4.c_4',4.1804,'4.nf.b.4.c','fraction');
 */
 
@@ -105,7 +143,6 @@ initialize: function(sheet)
         var cd = new Fraction(c,d,false);
 
         var answer = ab.multiply(cd);
-			//question = 'Carey uses ' + varA + '/' +  varB + ' of a cup of vinegar in her salad dressing recipe. How many cups of vinegar would Carey use to make ' + varC + ' recipes?';
 
         this.setAnswer('' + answer.getString(),0);
         this.setQuestion('' + this.ns.mNameOne + ' uses ' + ab.getString() + ' of a cup of vinegar in ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,1) + ' salad dressing recipe. How many cups of vinegar would ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' use to make ' + c + ' cups?');
@@ -151,36 +188,11 @@ initialize: function(sheet)
 
 });
 /*
-			if(rand == 0)
-			{			
-			}
-			if(rand == 1)
-			{			
 			question = 'Jenny had ' + varC + ' pounds of strawberries. Jenny let her friend Doris eat ' + varA + '/' + varB + ' of the strawberries. How many pounds of strawberries did Doris eat?';
-			}
-			if(rand == 2)
-			{			
-			question = 'Kwan operates an orange juice stand. On Monday he used ' + varC + ' bags of oranges. On Tuesday he used ' + varA + '/' + varB + ' as many oranges as on Monday. How many bags of oranges did Kwan use on Tuesday?';
-			}
-			if(rand == 3)
-			{			
-			question = 'Pedro has a lemon cookie recipe that calls for ' + varA + '/' +  varB + ' of a cup of sugar. How much sugar would Pedro use to make ' + varC + ' batches of cookies?';
-			}
-			if(rand == 4)
-			{			
-			}
-			if(rand == 5)
-			{			
-			}
-			if(rand == 6)
-			{			
+	
+	6		question = 'Kwan operates an orange juice stand. On Monday he used ' + varC + ' bags of oranges. On Tuesday he used ' + varA + '/' + varB + ' as many oranges as on Monday. How many bags of oranges did Kwan use on Tuesday?';
+			
 			question = 'Yesterday, a doughnut shop sold ' + varC + ' times as many chocolate doughnuts as cinnamon doughnuts. If they sold ' + varA + '/' + varB + ' of a tray of cinnamon doughnuts, how many trays of chocolate doughnuts did they sell?';
-			}
-			if(rand == 7)
-			{			
+			
 			question = 'Tracy made strawberry jam and raspberry jam. She made enough strawberry jam to fill ' + varA + '/' +  varB + ' of a jar. If she made ' + varC + ' times as much raspberry jam as strawberry jam, how many jars will the raspberry jam fill?';
-			}
-			if(rand == 8)
-			{			
-			}
 */
