1acc3fc6f8d190a57ddea5c4823fc0ebce54f5d1
jbreslin33 <jbreslin33@localhost>
2015-04-27 18:11:14 -0400

    t9

--- src/src/core/math/g4_nf_b_4_a.php
+++ src/src/core/math/g4_nf_b_4_a.php
@@ -1,5 +1,39 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.4.a_9',4.1609,'4.nf.b.4.a','');
+*/
+
+var i_4_nf_b_4_a__9 = new Class(
+{
+Extends: TextItemFraction,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,95,100,50,425,100,100,50,425,175);
+        this.mType = '4.nf.b.4.a_9';
+        this.ns = new NameSampler();
+
+        var a = 1;
+        var b = 0;
+        var c = 0;
+        var d = 1;
+
+        while (b == c)
+        {
+                var b = Math.floor((Math.random()*8)+2);
+                var c = Math.floor((Math.random()*15)+2);
+        }
+
+        var ab = new Fraction(a,b,false);
+        var cd = new Fraction(c,d,false);
+
+        var answer = ab.multiply(cd);
+
+        this.setAnswer('' + answer.getString(),0);
+        this.setQuestion('' + '' + this.ns.mNameOne + ' practices times tables for ' + ab.getString() + ' hours every day. If ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' does this for ' + c + ' days than how many hours will ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' have practiced times tables for?');
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.4.a_8',4.1608,'4.nf.b.4.a','');
 */
 
