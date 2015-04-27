58e1c996bb5f33aca5f069fff381fcc6865f8f6b
jbreslin33 <jbreslin33@localhost>
2015-04-27 19:39:28 -0400

    t6

--- src/src/core/math/g4_nf_b_4_c.php
+++ src/src/core/math/g4_nf_b_4_c.php
@@ -1,5 +1,40 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.4.c_6',4.1806,'4.nf.b.4.c','mixed number');
+*/
+
+var i_4_nf_b_4_c__6 = new Class(
+{
+Extends: TextItemFraction,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,95,100,50,425,100,100,50,425,175);
+        this.mType = '4.nf.b.4.c_6';
+        this.ns = new NameSampler();
+
+        var a = 1;
+        var b = 0;
+        var c = 0;
+        var d = 1;
+
+        while (b == c || a <= b)
+        {
+                var a = Math.floor((Math.random()*8)+2);
+                var b = Math.floor((Math.random()*8)+2);
+                var c = Math.floor((Math.random()*8)+2);
+        }
+
+        var ab = new Fraction(a,b,false);
+        var cd = new Fraction(c,d,false);
+
+        var answer = ab.multiply(cd);
+
+        this.setAnswer('' + answer.getString(),0);
+        this.setQuestion('' + '' + this.ns.mNameOne + ' operates an orange juice stand. On Monday ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' used ' + c + ' oranges. On Tuesday ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' used ' + ab.getMixedNumber() + ' times as many oranges as on Monday. How many bags of oranges did ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' use on Tuesday?');
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.4.c_5',4.1805,'4.nf.b.4.c','improper fraction');
 */
 
@@ -29,7 +64,6 @@ initialize: function(sheet)
         var cd = new Fraction(c,d,false);
 
         var answer = ab.multiply(cd);
-	//		question = 'Pedro has a lemon cookie recipe that calls for ' + varA + '/' +  varB + ' of a cup of sugar. How much sugar would Pedro use to make ' + varC + ' batches of cookies?';
 
         this.setAnswer('' + answer.getString(),0);
         this.setQuestion('' + this.ns.mNameOne + ' has a lemon cookie recipe that calls for ' + ab.getString() + ' of a cup of sugar. How many cups of sugar would ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' use to make ' + c + ' batches of cookies?');
@@ -68,7 +102,6 @@ initialize: function(sheet)
         var answer = ab.multiply(cd);
 
         this.setAnswer('' + answer.getString(),0);
-//	question = 'Tina is making calzones to sell at her restaurant. She starts with ' + varC + ' cans of tomato sauce and then uses ' + varA + '/' + varB + ' of the cans for the first batch of calzones. How many cans of tomato sauce does Tina use for the first batch of calzones?';
 
         this.setQuestion('' + '' + this.ns.mNameOne + ' is making calzones to sell at ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,1) + ' restaurant. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' starts with ' + c + ' cans of tomato sauce and then uses ' + ab.getString() + ' of the cans for the first batch of calzones. How many cans of tomato sauce does ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' use for the first batch of calzones?');
 }
@@ -100,17 +133,14 @@ initialize: function(sheet)
                 var c = Math.floor((Math.random()*8)+2);
         }
 
-
         var ab = new Fraction(a,b,false);
         var cd = new Fraction(c,d,false);
 
         var answer = ab.multiply(cd);
-			//question = 'A factory makes sheets of metal that are ' + varA + '/' +  varB + ' of an inch thick. If a worker at the factory makes a stack of ' + varC + ' of the sheets, how many inches thick will the stack be?';
 
         this.setAnswer('' + answer.getString(),0);
         this.setQuestion('' + '' + this.ns.mNameOne + ' works in a factory where ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' makes sheets of metal that are ' + ab.getMixedNumber() + ' of an inch thick. If ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' makes ' + c + ' sheets than how many inches thick will the stack be?');
 }
-
 });
 
 /*
@@ -182,7 +212,6 @@ initialize: function(sheet)
         var answer = ab.multiply(cd);
 
         this.setAnswer('' + answer.getString(),0);
-			//question = 'Peter owns ' + varC + ' acres of farmland. He grows corn on ' + varA + '/' + varB + ' of the land. On how many acres of land does Peter grow corn?';
         this.setQuestion('' + '' + this.ns.mNameOne + ' owns ' + c + ' acres of farmland. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' grows '  + this.ns.mVegetableOne + ' on ' + ab.getString() + ' of the land. On how many acres of land does ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' grow ' + this.ns.mVegetableOne + '?');
 }
 
@@ -190,7 +219,6 @@ initialize: function(sheet)
 /*
 			question = 'Jenny had ' + varC + ' pounds of strawberries. Jenny let her friend Doris eat ' + varA + '/' + varB + ' of the strawberries. How many pounds of strawberries did Doris eat?';
 	
-	6		question = 'Kwan operates an orange juice stand. On Monday he used ' + varC + ' bags of oranges. On Tuesday he used ' + varA + '/' + varB + ' as many oranges as on Monday. How many bags of oranges did Kwan use on Tuesday?';
 			
 			question = 'Yesterday, a doughnut shop sold ' + varC + ' times as many chocolate doughnuts as cinnamon doughnuts. If they sold ' + varA + '/' + varB + ' of a tray of cinnamon doughnuts, how many trays of chocolate doughnuts did they sell?';
 			
