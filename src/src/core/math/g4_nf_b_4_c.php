6e1d11a07f4e02f21aec7d64c2d1155a548b8c2e
jbreslin33 <jbreslin33@localhost>
2015-04-27 19:20:46 -0400

    t4

--- src/src/core/math/g4_nf_b_4_c.php
+++ src/src/core/math/g4_nf_b_4_c.php
@@ -1,5 +1,43 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.4.c_4',4.1804,'4.nf.b.4.c','fraction');
+*/
+
+var i_4_nf_b_4_c__4 = new Class(
+{
+Extends: TextItemFraction,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,95,100,50,425,100,100,50,425,175);
+        this.mType = '4.nf.b.4.c_4';
+        this.ns = new NameSampler();
+
+        var a = 1;
+        var b = 0;
+        var c = 0;
+        var d = 1;
+
+        while (b == c || a >= b)
+        {
+                var a = Math.floor((Math.random()*8)+2);
+                var b = Math.floor((Math.random()*8)+2);
+                var c = Math.floor((Math.random()*8)+2);
+        }
+
+        var ab = new Fraction(a,b,false);
+        var cd = new Fraction(c,d,false);
+
+        var answer = ab.multiply(cd);
+
+        this.setAnswer('' + answer.getString(),0);
+//	question = 'Tina is making calzones to sell at her restaurant. She starts with ' + varC + ' cans of tomato sauce and then uses ' + varA + '/' + varB + ' of the cans for the first batch of calzones. How many cans of tomato sauce does Tina use for the first batch of calzones?';
+
+        this.setQuestion('' + '' + this.ns.mNameOne + ' is making calzones to sell at ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,1) + ' restaurant. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' starts with ' + c + ' cans of tomato sauce and then uses ' + ab.getString() + ' of the cans for the first batch of calzones. How many cans of tomato sauce does ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' use for the first batch of calzones?');
+}
+
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.4.c_3',4.1803,'4.nf.b.4.c','mixed number');
 */
 
@@ -130,7 +168,6 @@ initialize: function(sheet)
 			}
 			if(rand == 4)
 			{			
-			question = 'Tina is making calzones to sell at her restaurant. She starts with ' + varC + ' cans of tomato sauce and then uses ' + varA + '/' + varB + ' of the cans for the first batch of calzones. How many cans of tomato sauce does Tina use for the first batch of calzones?';
 			}
 			if(rand == 5)
 			{			
