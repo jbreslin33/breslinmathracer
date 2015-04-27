ab0a139997699a60a4a29e7d5a7ad4266ff257f2
jbreslin33 <jbreslin33@localhost>
2015-04-27 14:52:11 -0400

    t8

--- src/src/core/math/g4_nf_b_4_a.php
+++ src/src/core/math/g4_nf_b_4_a.php
@@ -1,5 +1,39 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.4.a_8',4.1608,'4.nf.b.4.a','');
+*/
+
+var i_4_nf_b_4_a__8 = new Class(
+{
+Extends: TextItemFraction,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,95,100,50,425,100,100,50,425,175);
+        this.mType = '4.nf.b.4.a_8';
+        this.ns = new NameSampler();
+
+        var a = 1;
+        var b = 0;
+        var c = 0;
+        var d = 1;
+
+        while (b == c)
+        {
+                var b = Math.floor((Math.random()*8)+2);
+                var c = Math.floor((Math.random()*15)+2);
+        }
+
+        var ab = new Fraction(a,b,false);
+        var cd = new Fraction(c,d,false);
+
+        var answer = ab.multiply(cd);
+
+        this.setAnswer('' + answer.getString(),0);
+        this.setQuestion('' + '' + this.ns.mNameOne + ' has a pizzeria. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' makes ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' pepperoni pizzas with ' + ab.getString() + ' pounds of pepperoni. If ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' makes ' + c + ' pepperoni pies than how many pounds of pepperoni will ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' use?');
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.4.a_7',4.1607,'4.nf.b.4.a','');
 */
 
@@ -328,7 +362,7 @@ initialize: function(sheet)
 
 
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.4.a_1',4.1601,'4.nf.b.4.a','Multiply unit fractions by whole numbers');
+insert into item_types(id,progression,core_standards_id,description) values ('4.nf.b.4.a_1',4.1601,'4.nf.b.4.a','');
 */
 
 var i_4_nf_b_4_a__1 = new Class(
