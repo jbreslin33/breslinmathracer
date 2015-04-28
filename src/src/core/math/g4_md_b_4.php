215307722d550d78e84e30949e6a2ac62c87d635
jbreslin33 <jbreslin33@localhost>
2015-04-28 15:09:56 -0400

    c g4_md_b_b.php

--- src/src/core/math/g4_md_b_4.php
+++ src/src/core/math/g4_md_b_4.php
@@ -8,80 +8,74 @@ Extends: TextItemMixedNumber,
 
 initialize: function(sheet)
 {
-    this.parent(sheet,320,100,200,95,100,50,510,137,100,50,625,100, 100,50,625,175,true);
-
-
+	this.parent(sheet,320,100,200,95,100,50,510,137,100,50,625,100, 100,50,625,175,true);
         this.mType = '4.md.b.4_1';
+      	this.ns = new NameSampler();
 
-      this.ns = new NameSampler();
+	// graph coords
+	var startX = 10;
+	var endX = 310;
+	var startY = 10;
+	var endY = 310;
+	var width = endX - startX;
+	var height = endY - startY;
+	var range = [0,10];
 
-// graph coords
-var startX = 10;
-var endX = 310;
-var startY = 10;
-var endY = 310;
-var width = endX - startX;
-var height = endY - startY;
-var range = [0,10];
+	var rX1 = 10;
+	var rY1 = 50;
+	var rX2 = 330;
+	var rY2 = 350;
 
-var rX1 = 10;
-var rY1 = 50;
-var rX2 = 330;
-var rY2 = 350;
+	this.raphael = Raphael(rX1, rY1, rX2, rY2);
 
-this.raphael = Raphael(rX1, rY1, rX2, rY2);
+	this.raphaelSizeX = rX2;
+	this.raphaelSizeY = rY2;
 
-this.raphaelSizeX = rX2;
-this.raphaelSizeY = rY2;
+	var pointsX = [];
+	var pointsY = [];
+	var r = 0;
+	var sum = 71;
+	
+	while (sum > 30 || sum == 23 || sum == 15 || sum == 7)
+	{
 
-var pointsX = [];
-var pointsY = [];
-var r = 0;
-var sum = 71;
-//pick starting x for plot
-//var a = Math.floor(Math.random()*3 + 1);
+  		//keep track of how many dots at each x
+  		var plotX = [0,0,0,0,0,0,0,0,0,0,0];
 
-while (sum > 30 || sum == 23 || sum == 15 || sum == 7) {
-
-  //keep track of how many dots at each x
-  var plotX = [0,0,0,0,0,0,0,0,0,0,0];
-
-  //pick random points to make plot
-  for (var i = 0; i < 7; i++) {
-
-    r = (Math.floor(Math.random()*7) + 1);
-    pointsX[i] = r;
-    pointsY[i] = plotX[r] + 1;
-    plotX[r] = pointsY[i];
-  }
+  		//pick random points to make plot
+  		for (var i = 0; i < 7; i++)
+ 		{
+    			r = (Math.floor(Math.random()*7) + 1);
+    			pointsX[i] = r;
+    			pointsY[i] = plotX[r] + 1;
+    			plotX[r] = pointsY[i];
+  		}
   
-  sum = 0;
+  		sum = 0;
 
-  for (var j = 0; j < 7; j++) {
-    sum = sum + pointsX[j];
-  }
-}
+  		for (var j = 0; j < 7; j++)
+ 		{
+    			sum = sum + pointsX[j];
+  		}
+	}
 
-var fractionA = new Fraction(sum,8);
-var fractionB = new Fraction(31,8);
-var answer = fractionB.subtract(fractionA);
+	var fractionA = new Fraction(sum,8);
+	var fractionB = new Fraction(31,8);
+	var answer = fractionB.subtract(fractionA);
 
 	//var answer = fractionA;
 	answer.reduce();
 
-  this.setAnswer('' + answer.getString(),0);
+  	this.setAnswer('' + answer.getString(),0);
 
- this.setQuestion('' + this.ns.mNameOne + ' has 7 patches of fabric which are shown in the line plot below. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' wants to make a patchwork sweater, which requires ' + fractionB.getMixedNumber() + ' yards of fabric. How much more fabric will ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' need?');
+ 	this.setQuestion('' + this.ns.mNameOne + ' has 7 patches of fabric which are shown in the line plot below. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' wants to make a patchwork sweater, which requires ' + fractionB.getMixedNumber() + ' yards of fabric. How much more fabric will ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' need?');
 
-//create line plot
-var chart = new LineChartThree (this.mSheet.mGame,this,this.raphael,startX, startY, endX, endY,pointsX,pointsY,range,rX1,rY1,"#000000",false);
+	//create line plot
+	var chart = new LineChartThree (this.mSheet.mGame,this,this.raphael,startX, startY, endX, endY,pointsX,pointsY,range,rX1,rY1,"#000000",false);
 
-this.addQuestionShape(chart);
+	this.addQuestionShape(chart);
 
-this.mQuestionLabel.setSize(300,100);
-this.mQuestionLabel.setPosition(180,80);
-
+	this.mQuestionLabel.setSize(300,100);
+	this.mQuestionLabel.setPosition(180,80);
 }
-
 });
-
