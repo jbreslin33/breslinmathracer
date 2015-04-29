3d0629119cfd0e89c19e0db43b59e64713005471
jbreslin33 <jbreslin33@localhost>
2015-04-28 21:23:12 -0400

    degrees

--- src/src/core/math/g4_md_c_5_b.php
+++ src/src/core/math/g4_md_c_5_b.php
@@ -1,5 +1,44 @@
+
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('4.md.c.5.b_1',4.2908,'4.md.c.5.b','');
+insert into item_types(id,progression,core_standards_id,description) values ('4.md.c.5.b_2',4.2902,'4.md.c.5.b','');
+*/
+var i_4_md_c_5_b__2 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.mRaphael = Raphael(20,20,380,380);
+        this.parent(sheet,300,50,575,95,200,50,625,200);
+        this.mType = '4.md.c.5.b_2';
+        this.mChopWhiteSpace = false;
+
+        var f = new Fraction(180,360,false);
+
+        this.setQuestion('What is the turn of this angle in degrees?');
+        this.setAnswer('' + '180',0);
+        this.setAnswer('' + '180 degrees',1);
+},
+
+createShapes: function()
+{
+        this.parent();
+
+        var angleA = 180;
+        var angleB = 360;
+
+        this.mRayA = new Ray (parseInt(this.mRaphael.width/2),parseInt(this.mRaphael.height/2),100,angleA,this,"#000000",.5,false);
+        this.addQuestionShape(this.mRayA);
+
+        this.mRayB = new Ray (parseInt(this.mRaphael.width/2),parseInt(this.mRaphael.height/2),100,angleB,this,"#000000",.5,false);
+        this.addQuestionShape(this.mRayB);
+
+        this.mAngleArc = new AngleArc(parseInt(this.mRaphael.width/2),parseInt(this.mRaphael.height/2),50, parseFloat(angleB),parseFloat(angleA),false,this,0,0,1,"none",.5,false);;
+        this.addQuestionShape(this.mAngleArc);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.md.c.5.b_1',4.2902,'4.md.c.5.b','');
 */
 var i_4_md_c_5_b__1 = new Class(
 {
--- src/src/polygon/angle_arc.php
+++ src/src/polygon/angle_arc.php
@@ -1,7 +1,7 @@
 var AngleArc = new Class(
 {
 Extends: RaphaelPolygon,
-initialize: function (x,y,radius,start,end,item,r,g,b,s,op,d)
+initialize: function (x,y,radius,start,end,show_degrees,item,r,g,b,s,op,d)
 {
 	this.parent(0,0,x,y,item.mSheet.mGame,item.mRaphael,r,g,b,s,op,d);
 
@@ -24,13 +24,19 @@ initialize: function (x,y,radius,start,end,item,r,g,b,s,op,d)
         var degrees =  parseInt(end - start); 
 
 	//angleDegree text
- 	this.mAngleDegree = new AngleDegree(item,parseInt(this.mRaphael.width/2),parseInt(this.mRaphael.height/2),50, parseFloat(end),parseFloat(start),0,0,1,"none",.5,false);;
-	item.addQuestionShape(this.mAngleDegree);
+	if (show_degrees)
+	{
+ 		this.mAngleDegree = new AngleDegree(item,parseInt(this.mRaphael.width/2),parseInt(this.mRaphael.height/2),50, parseFloat(end),parseFloat(start),0,0,1,"none",.5,false);;
+		item.addQuestionShape(this.mAngleDegree);
+	}
 	
 	var halfway = parseFloat(degrees / 2);
 	//because its backards going in
 	var rotateTo = parseFloat(originalEnd + halfway);
 	var rotateAmount = '' + 'r' + rotateTo + ',' + x + ',' + y;
-        this.mAngleDegree.mPolygon.transform(rotateAmount);
+	if (show_degrees)
+	{
+        	this.mAngleDegree.mPolygon.transform(rotateAmount);
+	}
 }
 });
