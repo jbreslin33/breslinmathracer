f2e14c49e328f39c7deb621a1172a8d1eb181336
jbreslin33 <jbreslin33@localhost>
2015-04-29 14:13:07 -0400

    t1

--- src/src/core/math/g4_nf_c_5.php
+++ src/src/core/math/g4_nf_c_5.php
@@ -4,32 +4,24 @@ insert into item_types(id,progression,core_standards_id,description) values ('4.
 */
 var i_4_nf_c_5__1 = new Class(
 {
-Extends: TextItemFraction,
+Extends: TextItem,
 initialize: function(sheet)
 {
-        this.parent(sheet,300,50,175,95,100,50,425,100,100,50,425,175);
+        this.parent(sheet,300,50,175,95,100,50,425,100);
         this.mType = '4.nf.c.5_1';
         this.ns = new NameSampler();
 
-        var a = 1;
-        var b = 0;
-        var c = 0;
-        var d = 1;
-
-        while (b == c || a >= b)
-        {
-                var a = Math.floor((Math.random()*8)+2);
-                var b = Math.floor((Math.random()*8)+2);
-                var c = Math.floor((Math.random()*8)+2);
-        }
+        var a = Math.floor((Math.random()*8)+2);
+	var b = 10;
+	var c = parseInt(a * 10);
+	var d = 100;
 
         var ab = new Fraction(a,b,false);
         var cd = new Fraction(c,d,false);
 
-        var answer = ab.multiply(cd);
-        this.setAnswer('' + answer.getString(),0);
+        this.setAnswer('' + c,0);
 
-        this.setQuestion('' + this.ns.mNameOne + ' made strawberry jam and raspberry jam. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' made enough strawberry jam to fill ' + ab.getString() + ' of a jar. If ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' made ' + c + ' times as much raspberry jam as strawberry jam than how many jars will the raspberry jam fill?');
+        this.setQuestion('' + 'If you want to convert ' + ab.getString() + ' to an equivalent fraction with a denominator of 100 than what would you make the numerator?');
 }
 });
 
