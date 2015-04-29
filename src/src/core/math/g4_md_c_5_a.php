62c42807ce055ed356bff9915b4eb67e3d092a66
jbreslin33 <jbreslin33@localhost>
2015-04-29 10:02:40 -0400

    u

--- src/src/core/math/g4_md_c_5_a.php
+++ src/src/core/math/g4_md_c_5_a.php
@@ -167,7 +167,7 @@ initialize: function(sheet)
 {
 	this.mRaphael = Raphael(20,20,380,380);
 	this.parent(sheet,300,50,575,95,200,50,625,200);
-        this.mType = '4.md.c.5_a_7';
+        this.mType = '4.md.c.5.a_7';
 	this.mChopWhiteSpace = false;
 
 	var f = new Fraction(1,360);
