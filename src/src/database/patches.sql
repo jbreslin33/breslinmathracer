9af29aaa4165e3759e6f35d3462d986ee3cc08c7
jbreslin33 <jbreslin33@localhost>
2015-04-29 11:35:08 -0400

    patches

--- src/src/database/patches.sql
+++ src/src/database/patches.sql
@@ -1,2 +1,3 @@
 update item_types SET active_code = 1 where id = '4.nf.b.3.c_7';
 update item_types SET active_code = 1 where id = '4.md.a.3_3';
+update item_types SET active_code = 1 where id = '4.md.c.5.b_2';
