1eaef750cc06492bde336620f45ba851b9ab4f39
jbreslin33 <jbreslin33@localhost>
2015-05-02 21:16:32 -0400

    t2

--- src/src/core/math/g4_nf_c_6.php
+++ src/src/core/math/g4_nf_c_6.php
@@ -1,5 +1,32 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nf.c.6_2',4.2002,'4.nf.c.6','');
+*/
+var i_4_nf_c_6__2 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+  	this.parent(sheet,300,50,175,95,100,50,425,100);
+
+        this.mType = '4.nf.c.6_2';
+        this.ns = new NameSampler();
+
+        var d = Math.floor((Math.random()*9)+1);
+        var a = d;
+        var b = 10;
+
+	var decimal = new Decimal('0.' + a);
+
+        var ab = new Fraction(a,b);
+
+        this.setAnswer('' + decimal.getString(),0);
+
+        this.setQuestion('' + 'Write ' + ab.getString() + ' as a decimal.');
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nf.c.6_1',4.2001,'4.nf.c.6','');
 */
 var i_4_nf_c_6__1 = new Class(
@@ -11,11 +38,11 @@ initialize: function(sheet)
         this.mType = '4.nf.c.6_1';
         this.ns = new NameSampler();
 
-        var d = Math.floor((Math.random()*8)+2);
+        var d = Math.floor((Math.random()*9)+1);
         var a = d;
         var b = 10;
 
-	var decimal = new Decimal('0.' + a);
+        var decimal = new Decimal('0.' + a);
 
         var ab = new Fraction(a,b);
 
@@ -24,4 +51,6 @@ initialize: function(sheet)
         this.setQuestion('' + 'Write ' + decimal.getString() + ' as a fraction.');
 }
 });
+
+
 
