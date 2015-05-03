591b4d86249f0fc63aa2c1e8994b54e85ceb3b20
jbreslin33 <jbreslin33@localhost>
2015-05-02 21:55:40 -0400

    t5

--- src/src/core/math/g4_nf_c_6.php
+++ src/src/core/math/g4_nf_c_6.php
@@ -1,5 +1,31 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.nf.c.6_5',4.2005,'4.nf.c.6','');
+*/
+var i_4_nf_c_6__5 = new Class(
+{
+Extends: TextItemFraction,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,95,100,50,425,100,100,50,425,175);
+        this.mType = '4.nf.c.6_5';
+        this.ns = new NameSampler();
+
+        var d = Math.floor((Math.random()*9)+1);
+        var a = d;
+        var b = 100;
+
+        var decimal = new Decimal('0.0' + a);
+
+        var ab = new Fraction(a,b);
+
+        this.setAnswer('' + ab.getString(),0);
+
+        this.setQuestion('' + 'Write ' + decimal.getString() + ' as a fraction.');
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.nf.c.6_4',4.2004,'4.nf.c.6','');
 */
 var i_4_nf_c_6__4 = new Class(
