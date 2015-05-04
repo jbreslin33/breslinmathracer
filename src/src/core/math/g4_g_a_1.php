91cb9e48f3cfa4d46366a30a9882988584e03a1d
jbreslin33 <jbreslin33@localhost>
2015-05-04 10:41:24 -0400

    t1 d

--- src/src/core/math/g4_g_a_1.php
+++ src/src/core/math/g4_g_a_1.php
@@ -8,15 +8,11 @@ Extends: TextItem,
 initialize: function(sheet)
 {
         this.parent(sheet,300,50,175,95,100,50,425,100);
-	this.ns = new NameSampler();
         this.mRaphael = Raphael(10,150,500,350);
         this.mType = '4.g.a.1_1';
         
-        var a = Math.floor((Math.random()*8)+2);
-        var answer = parseInt(a * 1000);
-
-        this.setQuestion('How many grams in ' + a + ' kilograms?');
-       	this.setAnswer('' + answer,0);
+        this.setQuestion('How many angles are inside the drawing below?');
+       	this.setAnswer('' + '7',0);
 },
 
 createQuestionShapes: function()
