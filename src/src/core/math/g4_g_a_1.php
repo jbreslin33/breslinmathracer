66c30ca3121609caa84080b09c4c20e15a413b5a
jbreslin33 <jbreslin33@localhost>
2015-05-04 11:05:12 -0400

    t2

--- src/src/core/math/g4_g_a_1.php
+++ src/src/core/math/g4_g_a_1.php
@@ -11,8 +11,8 @@ initialize: function(sheet)
         this.mRaphael = Raphael(10,150,500,350);
         this.mType = '4.g.a.1_2';
 
-        this.setQuestion('How many angles are inside the drawing below?');
-        this.setAnswer('' + '7',0);
+        this.setQuestion('How many obtuse angles appear to be inside the drawing below?');
+        this.setAnswer('' + '18',0);
 },
 
 createQuestionShapes: function()
@@ -21,7 +21,7 @@ createQuestionShapes: function()
         //this.addQuestionShape(rectangle);
         //var triangle = new Triangle (125,125,100,150,150,150,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.5,false);
         //this.addQuestionShape(triangle);
-        var hexagonA = new Hexagon (this.mSheet.mGame,this.mRaphael,140,140, 125,175, 140,210, 185,210, 200,175, 185,140,.5,.5,.5,"#000",.5,false);
+        var hexagonA = new Hexagon (this.mSheet.mGame,this.mRaphael,140,90, 125,125, 140,160, 185,160, 200,125, 185,90,.5,.5,.5,"#000",.5,false);
         this.addQuestionShape(hexagonA);
 
 	 //initialize: function (game,raphael,x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,r,g,b,s,op,d)
