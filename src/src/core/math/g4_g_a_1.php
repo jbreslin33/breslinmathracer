608b12a37a51bc7f98ca38f1ad645bd4ac7be848
jbreslin33 <jbreslin33@localhost>
2015-05-04 10:42:32 -0400

    t2 rfm

--- src/src/core/math/g4_g_a_1.php
+++ src/src/core/math/g4_g_a_1.php
@@ -1,5 +1,30 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.g.a.1_2',4.3202,'4.g.a.1','');
+*/
+var i_4_g_a_1__2 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,95,100,50,425,100);
+        this.mRaphael = Raphael(10,150,500,350);
+        this.mType = '4.g.a.1_2';
+
+        this.setQuestion('How many angles are inside the drawing below?');
+        this.setAnswer('' + '7',0);
+},
+
+createQuestionShapes: function()
+{
+        var rectangle = new Rectangle(50,50,100,150,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.5,false);
+        this.addQuestionShape(rectangle);
+        var triangle = new Triangle (125,125,100,150,150,150,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.5,false);
+        this.addQuestionShape(triangle);
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.g.a.1_1',4.3201,'4.g.a.1','');
 */
 var i_4_g_a_1__1 = new Class(
