7807588b59b4cdbd23d3e648c8b0c32898378700
jbreslin33 <jbreslin33@localhost>
2015-05-04 10:55:01 -0400

    t2

--- src/src/core/math/g4_g_a_1.php
+++ src/src/core/math/g4_g_a_1.php
@@ -17,10 +17,15 @@ initialize: function(sheet)
 
 createQuestionShapes: function()
 {
-        var rectangle = new Rectangle(50,50,100,150,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.5,false);
-        this.addQuestionShape(rectangle);
-        var triangle = new Triangle (125,125,100,150,150,150,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.5,false);
-        this.addQuestionShape(triangle);
+       // var rectangle = new Rectangle(50,50,100,150,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.5,false);
+        //this.addQuestionShape(rectangle);
+        //var triangle = new Triangle (125,125,100,150,150,150,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.5,false);
+        //this.addQuestionShape(triangle);
+        var hexagonA = new Hexagon (this.mSheet.mGame,this.mRaphael,150,150,125,175,150,200,175,200,200,175,175,150,.5,.5,.5,"#000",.5,false);
+        this.addQuestionShape(hexagonA);
+
+	 //initialize: function (game,raphael,x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,r,g,b,s,op,d)
+
 }
 });
 
