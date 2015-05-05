0702a88e5d764e5dde36db608512e258283a1ad3
jbreslin33 <jbreslin33@localhost>
2015-05-05 11:11:42 -0400

    t2

--- src/src/core/math/g4_g_a_1.php
+++ src/src/core/math/g4_g_a_1.php
@@ -140,6 +140,33 @@ createQuestionShapes: function()
 });
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.g.a.1_2',4.3202,'4.g.a.1','');
+*/
+var i_4_g_a_1__2 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,95,100,50,425,100);
+        this.mRaphael = Raphael(10,150,500,350);
+        this.mType = '4.g.a.1_2';
+
+        this.setQuestion('What kind of lines are these parallel or perpendicular?');
+        this.setAnswer('' + 'perpendicular',0);
+},
+
+createQuestionShapes: function()
+{
+        var lineA = new LineOne (150,50,150,200,this.mGame,this.mRaphael,"#000",.5,false);
+        this.addQuestionShape(lineA);
+
+        var lineB = new LineOne (200,100,350,100,this.mGame,this.mRaphael,"#000",.5,false);
+        this.addQuestionShape(lineB);
+}
+});
+
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.g.a.1_1',4.3201,'4.g.a.1','');
 */
 var i_4_g_a_1__1 = new Class(
