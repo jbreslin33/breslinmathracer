6591ca069da97d712e2978fa54a07e8f3810cb0e
jbreslin33 <jbreslin33@localhost>
2015-05-05 12:09:28 -0400

    t3

--- src/src/core/math/g4_g_a_1.php
+++ src/src/core/math/g4_g_a_1.php
@@ -140,6 +140,41 @@ createQuestionShapes: function()
 });
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.g.a.1_3',4.3203,'4.g.a.1','');
+*/
+var i_4_g_a_1__3 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,95,100,50,425,100);
+        this.mRaphael = Raphael(10,150,500,350);
+	this.mChopWhiteSpace = false;
+        this.mType = '4.g.a.1_3';
+
+        this.setQuestion('What kind of angle is this? Acute or Obtuse or Right?');
+        this.setAnswer('' + 'Right',0);
+        this.setAnswer('' + 'Right angle',1);
+},
+
+createQuestionShapes: function()
+{
+	var angleA = 270;
+	var angleB = 360;
+
+        this.mRayA = new Ray (parseInt(this.mRaphael.width/2),parseInt(this.mRaphael.height/2),100,angleA,this,"#000000",.5,false);
+        this.addQuestionShape(this.mRayA);
+
+        this.mRayB = new Ray (parseInt(this.mRaphael.width/2),parseInt(this.mRaphael.height/2),100,angleB,this,"#000000",.5,false);
+        this.addQuestionShape(this.mRayB);
+
+        this.mAngleArc = new AngleArc(parseInt(this.mRaphael.width/2),parseInt(this.mRaphael.height/2),50, parseFloat(angleB),parseFloat(angleA),true,this,0,0,1,"none",.5,false);;
+        this.addQuestionShape(this.mAngleArc);
+}
+});
+
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.g.a.1_2',4.3202,'4.g.a.1','');
 */
 var i_4_g_a_1__2 = new Class(
