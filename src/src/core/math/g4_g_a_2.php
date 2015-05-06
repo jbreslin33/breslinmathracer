1848ba5e3ee3d93764e0031aa0442218a1037305
jbreslin33 <jbreslin33@localhost>
2015-05-06 18:32:16 -0400

    t21 rfm

--- src/src/core/math/g4_g_a_2.php
+++ src/src/core/math/g4_g_a_2.php
@@ -1,3 +1,43 @@
+//new Parallelogram(game,raphael,ax1,ay1,ax2,ay2,bx2,by2,bx1,by1,.5,.5,.5,"#000",1,false);
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.g.a.2_21',4.3321,'4.g.a.2','');
+*/
+var i_4_g_a_2__21 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,95,100,50,425,100);
+        this.mRaphael = Raphael(10,150,500,350);
+        this.mChopWhiteSpace = false;
+        this.mType = '4.g.a.2_21';
+
+        this.setQuestion('What kind of triangle is the shape below?');
+        this.setAnswer('' + 'Right',0);
+        this.setAnswer('' + 'Right triangle',1);
+},
+
+createQuestionShapes: function()
+{
+        var angleA = 270;
+        var angleB = 360;
+
+        this.mRayA = new Ray (parseInt(this.mRaphael.width/2),parseInt(this.mRaphael.height/2),100,angleA,this,"#000000",.5,false);
+        this.addQuestionShape(this.mRayA);
+
+        this.mRayB = new Ray (parseInt(this.mRaphael.width/2),parseInt(this.mRaphael.height/2),100,angleB,this,"#000000",.5,false);
+        this.addQuestionShape(this.mRayB);
+
+        this.mAngleArc = new AngleArc(parseInt(this.mRaphael.width/2),parseInt(this.mRaphael.height/2),50, parseFloat(angleB),parseFloat(angleA),true,this,0,0,1,"none",.5,false);;
+        this.addQuestionShape(this.mAngleArc);
+
+        //right square
+        var rectangle = new Rectangle(12,12,parseInt(this.mRaphael.width/2),parseInt(this.mRaphael.height/2 - 12),this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.3,false);
+        this.addQuestionShape(rectangle);
+}
+});
+
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('4.g.a.2_20',4.3320,'4.g.a.2','');
 */
