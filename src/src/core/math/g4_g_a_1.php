46fed18beab0ff9d9d9c0906aec5ef561956b14b
jbreslin33 <jbreslin33@localhost>
2015-05-05 21:53:54 -0400

    t10

--- src/src/core/math/g4_g_a_1.php
+++ src/src/core/math/g4_g_a_1.php
@@ -139,6 +139,33 @@ createQuestionShapes: function()
 });
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.g.a.1_10',4.3210,'4.g.a.1','');
+*/
+var i_4_g_a_1__10 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,95,100,50,425,100);
+        this.mRaphael = Raphael(10,150,500,350);
+        this.mType = '4.g.a.1_10';
+
+        this.setQuestion('How many line segments does the shape below have?');
+        this.setAnswer('' + '6',0);
+},
+
+createQuestionShapes: function()
+{
+        var rectangle = new Rectangle(50,50,100,150,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.5,false);
+        this.addQuestionShape(rectangle);
+
+        var triangle = new Triangle (125,125,100,150,150,150,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.5,false);
+        this.addQuestionShape(triangle);
+}
+});
+
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.g.a.1_9',4.3209,'4.g.a.1','');
 */
 var i_4_g_a_1__9 = new Class(
