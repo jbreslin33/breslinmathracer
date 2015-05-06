1f1a63fda2353e588b10706f783489c1b562f15c
jbreslin33 <jbreslin33@localhost>
2015-05-05 21:21:11 -0400

    t7 rfm

--- src/src/core/math/g4_g_a_1.php
+++ src/src/core/math/g4_g_a_1.php
@@ -132,10 +132,36 @@ createQuestionShapes: function()
 {
 	var rectangle = new Rectangle(50,50,100,150,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.5,false);
 	this.addQuestionShape(rectangle);
+
 	var triangle = new Triangle (125,125,100,150,150,150,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.5,false);
 	this.addQuestionShape(triangle);
 }
 });
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.g.a.1_7',4.3207,'4.g.a.1','');
+*/
+var i_4_g_a_1__7 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,95,100,50,425,100);
+        this.mRaphael = Raphael(10,150,500,350);
+        this.mChopWhiteSpace = false;
+        this.mType = '4.g.a.1_7';
+
+        this.setQuestion('How many acute angles are inside the shape below?');
+        this.setAnswer('' + '3',0);
+},
+
+createQuestionShapes: function()
+{
+	var triangle = new Triangle (125,125,100,150,150,150,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.5,false);
+	this.addQuestionShape(triangle);
+}
+});
+
 
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('4.g.a.1_6',4.3206,'4.g.a.1','');
