19a9a807babc254c3a9f57850b504b50cda0f604
jbreslin33 <jbreslin33@localhost>
2015-05-06 20:19:51 -0400

    t22 rfm

--- src/src/core/math/g4_g_a_2.php
+++ src/src/core/math/g4_g_a_2.php
@@ -1,6 +1,52 @@
 //new Parallelogram(game,raphael,ax1,ay1,ax2,ay2,bx2,by2,bx1,by1,.5,.5,.5,"#000",1,false);
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('4.g.a.2_22',4.3322,'4.g.a.2','');
+*/
+var i_4_g_a_2__22 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,95,100,50,425,100);
+        this.mRaphael = Raphael(10,150,500,350);
+        this.mChopWhiteSpace = false;
+        this.mType = '4.g.a.2_22';
+
+        this.setQuestion('What is the name of the figure below?');
+        this.setAnswer('' + 'Rhombus',0);
+},
+
+createQuestionShapes: function()
+{
+        var x = parseInt(this.mRaphael.width/2);
+        var y = parseInt(this.mRaphael.height/2);
+
+        var rhombus = new Rhombus(this.mSheet.mGame,this.mRaphael, parseInt(x-175),parseInt(y-150), parseInt(x+125),parseInt(y-150), parseInt(x+100),parseInt(y+25), parseInt(x-200),parseInt(y+25),.5,.5,.5,"#000",1,false);
+        this.addQuestionShape(rhombus);
+
+        var lineA = new LineOne (parseInt(x-20),parseInt(y-160),parseInt(x-20),parseInt(y-140),this.mGame,this.mRaphael,"#000",.5,false);
+        this.addQuestionShape(lineA);
+
+        var lineB = new LineOne (parseInt(x+100),parseInt(y-75),parseInt(x+130),parseInt(y-75),this.mGame,this.mRaphael,"#000",.5,false);
+        this.addQuestionShape(lineB);
+
+        var lineC = new LineOne (parseInt(x+100),parseInt(y-65),parseInt(x+130),parseInt(y-65),this.mGame,this.mRaphael,"#000",.5,false);
+        this.addQuestionShape(lineC);
+
+        var lineD = new LineOne (parseInt(x-20),parseInt(y+15),parseInt(x-20),parseInt(y+35),this.mGame,this.mRaphael,"#000",.5,false);
+        this.addQuestionShape(lineD);
+
+        var lineE = new LineOne (parseInt(x-200),parseInt(y-75),parseInt(x-170),parseInt(y-75),this.mGame,this.mRaphael,"#000",.5,false);
+        this.addQuestionShape(lineE);
+
+        var lineF = new LineOne (parseInt(x-200),parseInt(y-65),parseInt(x-170),parseInt(y-65),this.mGame,this.mRaphael,"#000",.5,false);
+        this.addQuestionShape(lineF);
+}
+});
+
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('4.g.a.2_21',4.3321,'4.g.a.2','');
 */
 var i_4_g_a_2__21 = new Class(
