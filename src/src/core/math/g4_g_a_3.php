afed448e702db7e28582f33839dc92c5e45e0666
jbreslin33 <jbreslin33@localhost>
2015-05-08 14:23:34 -0400

    t25 d

--- src/src/core/math/g4_g_a_3.php
+++ src/src/core/math/g4_g_a_3.php
@@ -1,3 +1,52 @@
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.g.a.3_25',4.3425,'4.g.a.3','');
+*/
+var i_4_g_a_3__25 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,95,100,50,425,100);
+        this.mRaphael = Raphael(10,150,500,350);
+        this.mChopWhiteSpace = false;
+        this.ns = new NameSampler();
+        this.mType = '4.g.a.3_25';
+
+        this.mRandom = Math.floor(Math.random()*2)+1;
+        
+	if (this.mRandom == 1)
+	{
+        	this.setQuestion('' + this.ns.mNameOne + ' wanted to make a symetrical cut in the triangle below with the grey line. Was ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' succesful? Answer yes or no.');
+        	this.setAnswer('' + 'yes',0);
+	}
+	if (this.mRandom == 2)
+	{
+        this.setQuestion('' + this.ns.mNameOne + ' wanted to make a symetrical cut in the triangle below with the grey line. Was ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' succesful? Answer yes or no.');
+        this.setAnswer('' + 'no',0);
+	
+	}
+},
+
+createQuestionShapes: function()
+{
+	if (this.mRandom == 1)
+	{
+        	var triangle = new Triangle (20,200, 100,100, 180,200, this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.5,false);
+        	this.addQuestionShape(triangle);
+
+        	var lineA = new LineOne (100,75, 100,225,this.mGame,this.mRaphael,"#000",.5,false);
+        	this.addQuestionShape(lineA);
+	}
+	if (this.mRandom == 2)
+	{
+        	var triangle = new Triangle (20,200, 100,100, 180,200, this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.5,false);
+        	this.addQuestionShape(triangle);
+
+        	var lineA = new LineOne (10,150,190,150,this.mGame,this.mRaphael,"#000",.5,false);
+        	this.addQuestionShape(lineA);
+	}
+}
+});
 
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('4.g.a.3_24',4.3424,'4.g.a.3','');
