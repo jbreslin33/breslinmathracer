78b9c55de4d8768a0028e0e28f13faefd82e3086
jbreslin33 <jbreslin33@localhost>
2015-05-08 11:50:25 -0400

    t21

--- src/src/core/math/g4_g_a_3.php
+++ src/src/core/math/g4_g_a_3.php
@@ -1,3 +1,32 @@
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('4.g.a.3_21',4.3421,'4.g.a.3','');
+*/
+var i_4_g_a_3__21 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,95,100,50,425,100);
+        this.mRaphael = Raphael(10,150,500,350);
+        this.mChopWhiteSpace = false;
+        this.mType = '4.g.a.3_21';
+
+        this.setQuestion('Does the grey line show a line of symmetry for the kid? Answer yes or no.');
+        this.setAnswer('' + 'no',0);
+},
+
+createQuestionShapes: function()
+{
+        var kid = new Shape(50,50,200,300,this.mSheet.mGame,"/images/bus/kid.png","","");
+        this.addQuestionShape(kid);
+
+        var lineA = new LineOne (100,150,300,150,this.mGame,this.mRaphael,"#000",.5,false);
+        this.addQuestionShape(lineA);
+}
+});
+
+
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('4.g.a.3_20',4.3420,'4.g.a.3','');
 */
@@ -11,7 +40,7 @@ initialize: function(sheet)
         this.mChopWhiteSpace = false;
         this.mType = '4.g.a.3_20';
 
-        this.setQuestion('Does the line show a line of symmetry? Answer yes or no.');
+        this.setQuestion('Does the grey line show a line of symmetry for the kid? Answer yes or no.');
         this.setAnswer('' + 'yes',0);
 },
 
@@ -22,27 +51,6 @@ createQuestionShapes: function()
 
         var lineA = new LineOne (190,100,190,200,this.mGame,this.mRaphael,"#000",.5,false);
         this.addQuestionShape(lineA);
-
-
-/*
-        var angleA = 270;
-        var angleB = 360;
-
-        this.mRayA = new Ray (parseInt(this.mRaphael.width/2),parseInt(this.mRaphael.height/2),100,angleA,this,"#000000",.5,false);
-        this.addQuestionShape(this.mRayA);
-
-        this.mRayB = new Ray (parseInt(this.mRaphael.width/2),parseInt(this.mRaphael.height/2),100,angleB,this,"#000000",.5,false);
-        this.addQuestionShape(this.mRayB);
-
-        this.mAngleArc = new AngleArc(parseInt(this.mRaphael.width/2),parseInt(this.mRaphael.height/2),50, parseFloat(angleB),parseFloat(angleA),true,this,0,0,1,"none",.5,false);;
-        this.addQuestionShape(this.mAngleArc);
-
-        //right square
-        var rectangle = new Rectangle(12,12,parseInt(this.mRaphael.width/2),parseInt(this.mRaphael.height/2 - 12),this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.3,false);
-        this.addQuestionShape(rectangle);
-*/
-
-
 }
 });
 
