24f5d9b4fa1c8fb66b89dac2aff0d6cdd9d1bc02
jbreslin33 <jbreslin33@localhost>
2015-05-08 13:30:05 -0400

    4.nf.a.1_10 glitch

--- src/src/core/math/g4_nf_a_1.php
+++ src/src/core/math/g4_nf_a_1.php
@@ -16,7 +16,7 @@ initialize: function(sheet)
         this.a = 0;
         this.b = 0;
 
-        while (this.a == this.b)
+        while (this.a == this.b || this.a % this.b == 0)
         {
                 this.a = Math.floor(Math.random()*9)+1;
                 this.b = Math.floor(Math.random()*9)+1;
