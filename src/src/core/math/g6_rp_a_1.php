37104c675a9038de4b0ff758e82f1a7526bc31cb
jbreslin33 <jbreslin33@localhost>
2015-05-12 21:18:49 -0400

    t3

--- src/src/core/math/g6_rp_a_1.php
+++ src/src/core/math/g6_rp_a_1.php
@@ -1,3 +1,24 @@
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('6.rp.a.1_3',6.0103,'6.rp.a.1','' );
+*/
+var i_6_rp_a_1__3 = new Class(
+{
+Extends: TextItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet,300,50,175,95,100,50,425,100);
+
+                this.mType = '6.rp.a.1_3';
+                var a = Math.floor((Math.random()*8)+2);
+                var answer = parseInt(a * 1000);
+
+                this.setQuestion('How many grams in ' + a + ' kilograms?');
+                this.setAnswer('' + answer,0);
+        }
+});
+
+
 var i_6_rp_a_1__picture = new Class(
 {
 Extends: TextItem,
