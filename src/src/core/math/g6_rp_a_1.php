3cc660028b6940bfb107ded8fff9e73323d5a7e6
jbreslin33 <jbreslin33@localhost>
2015-05-18 13:58:04 -0400

    t4

--- src/src/core/math/g6_rp_a_1.php
+++ src/src/core/math/g6_rp_a_1.php
@@ -1,5 +1,37 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('6.rp.a.1_5',6.0105,'6.rp.a.1','' );
+*/
+var i_6_rp_a_1__5 = new Class(
+{
+Extends: TextItemMixedNumber,
+initialize: function(sheet)
+{
+	this.parent(sheet, 320,100,200,95, 100,50,510,137, 100,50,625,100, 100,50,625,175,true);
+        this.mType = '6.rp.a.1_5';
+        this.ns = new NameSampler();
+        this.mChopWhiteSpace = false;
+
+         var a = 3;
+         var b = 2;
+         while (a % b != 0 || a == b)
+         {
+                a = Math.floor((Math.random()*98)+2);
+                b = Math.floor((Math.random()*98)+2);
+         }
+         c = parseInt( a / b );
+         this.setQuestion('' + 'Write ' + a + ' ' + this.ns.mThingOne + ' for every ' + b + ' ' + this.ns.mThingTwo + ' as a unit rate.');
+	this.setAnswer('' + c + ' ' + this.ns.mThingOne + ' per ' + this.ns.mNameMachine.getSingular(this.ns.mThingTwo),0);
+},
+createQuestionShapes: function()
+{
+	var txt = new Shape(150,50,600,240,this.mSheet.mGame,"","","");
+	this.addQuestionShape(txt);
+        txt.setText('' + this.ns.mThingOne + ' per ' + this.ns.mNameMachine.getSingular(this.ns.mThingTwo));
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('6.rp.a.1_4',6.0104,'6.rp.a.1','' );
 */
 var i_6_rp_a_1__4 = new Class(
