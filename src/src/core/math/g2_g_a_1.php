15e7894e0e783b2ccd8949a2f6a8414e76a8bea4
jbreslin33 <jbreslin33@localhost>
2015-05-23 17:19:19 -0400

    t5

--- src/src/core/math/g2_g_a_1.php
+++ src/src/core/math/g2_g_a_1.php
@@ -1,5 +1,21 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('2.g.a.1_5',4.0155,'2.g.a.1','' );
+*/
+var i_2_g_a_1__5 = new Class(
+{
+Extends: TextItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet,300,50,175,95, 200,50,475,100);
+                this.mType = '2.g.a.1_5';
+                this.setQuestion('' + 'What is the name of a shape with 2 equal faces? A sphere, cylinder, cone or cube?');
+                this.setAnswer('' + 'Cylinder',0);
+                this.setAnswer('' + 'A Cylinder',1);
+        }
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('2.g.a.1_4',4.0154,'2.g.a.1','' );
 */
 var i_2_g_a_1__4 = new Class(
