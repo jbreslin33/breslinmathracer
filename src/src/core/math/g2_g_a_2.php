095bbdf4b674f6944b7fb1bbf4a90704455e104a
jbreslin33 <jbreslin33@localhost>
2015-05-24 16:29:07 -0400

    t1

--- src/src/core/math/g2_g_a_2.php
+++ src/src/core/math/g2_g_a_2.php
@@ -11,15 +11,18 @@ initialize: function(sheet)
         this.mChopWhiteSpace = false;
         this.mType = '2.g.a.2_1';
    
-	this.r = Math.floor(Math.random()*4);
-
-
+	this.r = Math.floor( (Math.random()*4) +1);
         this.setQuestion('What rectangle has been divided into same-sized squares?');
-        this.setAnswer('' + 'D',0);
 },
 
 createQuestionShapes: function()
 {
+	var b1 = new Rectangle(25,75,300,25,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.3,false);
+        this.addQuestionShape(b1);
+	var b2 = new Rectangle(25,75,325,25,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.3,false);
+        this.addQuestionShape(b2);
+	this.mText1 = new RaphaelText(280,30,this,0,0,1,"#000000",.5,false,"" + "B.",16);
+        this.addQuestionShape(this.mText1);
 	
 	var a1 = new Rectangle(50,25,100,25,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.3,false);
         this.addQuestionShape(a1);
@@ -27,15 +30,8 @@ createQuestionShapes: function()
         this.addQuestionShape(a2);
 	var a3 = new Rectangle(50,25,100,75,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.3,false);
         this.addQuestionShape(a3);
-        var textA = new RaphaelText(80,30,this,0,0,1,"#000000",.5,false,"" + "A.",16);
-        this.addQuestionShape(textA);
-	
-	var b1 = new Rectangle(25,75,300,25,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.3,false);
-        this.addQuestionShape(b1);
-	var b2 = new Rectangle(25,75,325,25,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.3,false);
-        this.addQuestionShape(b2);
-	var textB = new RaphaelText(280,30,this,0,0,1,"#000000",.5,false,"" + "B.",16);
-        this.addQuestionShape(textB);
+        this.mText2 = new RaphaelText(80,30,this,0,0,1,"#000000",.5,false,"" + "A.",16);
+        this.addQuestionShape(this.mText2);
  
    	var c1 = new Rectangle(25,12.5,100,125,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.3,false);
         this.addQuestionShape(c1);
@@ -61,20 +57,8 @@ createQuestionShapes: function()
         this.addQuestionShape(c11);
         var c12 = new Rectangle(25,12.5,125,187.5,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.3,false);
         this.addQuestionShape(c12);
-
-
-/*
-   	var c3 = new Rectangle(50,25,100,150,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.3,false);
-        this.addQuestionShape(c3);
-   	var c4 = new Rectangle(50,25,150,150,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.3,false);
-        this.addQuestionShape(c4);
-   	var c5 = new Rectangle(50,25,100,175,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.3,false);
-        this.addQuestionShape(c5);
-   	var c6 = new Rectangle(50,25,150,175,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.3,false);
-        this.addQuestionShape(c6);
-*/
-	var textC = new RaphaelText(80,130,this,0,0,1,"#000000",.5,false,"" + "C.",16);
-        this.addQuestionShape(textC);
+	this.mText3 = new RaphaelText(80,130,this,0,0,1,"#000000",.5,false,"" + "C.",16);
+        this.addQuestionShape(this.mText3);
 
         var d1 = new Rectangle(25,25,300,125,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.3,false);
         this.addQuestionShape(d1);
@@ -88,12 +72,31 @@ createQuestionShapes: function()
         this.addQuestionShape(d5);
         var d6 = new Rectangle(25,25,325,175,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.3,false);
         this.addQuestionShape(d6);
-        var textD = new RaphaelText(280,130,this,0,0,1,"#000000",.5,false,"" + "D.",16);
-        this.addQuestionShape(textD);
-
-
-
-        
+        this.mText4 = new RaphaelText(280,130,this,0,0,1,"#000000",.5,false,"" + "D.",16);
+        this.addQuestionShape(this.mText4);
+	
+	if (this.r == 1) 
+	{
+		this.setAnswer('' + 'A',0);
+		this.mText4.setText('A.');
+		this.mText2.setText('D.');
+	}
+	if (this.r == 2) 
+	{
+		this.setAnswer('' + 'B',0);
+		this.mText4.setText('B.');
+		this.mText1.setText('D.');
+	}
+	if (this.r == 3) 
+	{
+		this.setAnswer('' + 'C',0);
+		this.mText4.setText('C.');
+		this.mText3.setText('D.');
+	}
+	if (this.r == 4) 
+	{
+		this.setAnswer('' + 'D',0);
+	}
 }
 });
 
