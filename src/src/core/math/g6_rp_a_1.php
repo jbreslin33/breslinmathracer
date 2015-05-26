6c533e16bc0703a6e33a3f6e7b621be7e2b37d6d
jbreslin33 <jbreslin33@localhost>
2015-05-26 15:03:34 -0400

    t17

--- src/src/core/math/g6_rp_a_1.php
+++ src/src/core/math/g6_rp_a_1.php
@@ -1,5 +1,28 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('6.rp.a.1_17',6.0117,'6.rp.a.1','' );
+*/
+
+var i_6_rp_a_1__17 = new Class(
+{
+Extends: TextItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet,300,50,175,95, 200,50,475,100);
+
+                this.mType = '6.rp.a.1_17';
+                this.ns = new NameSampler();
+
+                var a = Math.floor((Math.random()*20)+2);
+                var b = Math.floor((Math.random()*20)+1);
+
+                this.setQuestion('' + this.ns.mNameOne + ' has some balls. The ratio of his ' + this.ns.mColorOne + ' balls to ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,1) + ' ' + this.ns.mColorTwo + ' balls is ' + a + ' to ' + b + '. Write the ratio with a colon of ' + this.ns.mColorOne + ' balls to ' + this.ns.mColorTwo + ' balls.');
+
+                this.setAnswer('' + a + ':' + b ,0);
+        }
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('6.rp.a.1_16',6.0116,'6.rp.a.1','' );
 */
 
