efdf3fff1f11dc8f3fd8a5d68debd9ab7cf02b5d
Jim Breslin <jbreslin33@gmail.com>
2015-05-29 20:08:35 -0400

    t14 rfm

--- src/src/core/math/g2_nbt_b_7.php
+++ src/src/core/math/g2_nbt_b_7.php
@@ -1,5 +1,35 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('2.nbt.b.7_14',4.0181,'2.nbt.b.7','' );
+*/
+var i_2_nbt_b_7__14 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,500,25,275,50,50,50,625,100);
+        this.ns = new NameSampler();
+        this.mRaphael = Raphael(10,150,500,350);
+        this.mChopWhiteSpace = false;
+        this.mType = '2.nbt.b.7_14';
+
+        var a = Math.floor( (Math.random()*29)+30);
+        var b = Math.floor( (Math.random()*39)+60);
+
+        var c = parseInt(a + b);
+        this.setQuestion('' + this.ns.mNameOne + ' is adding ' + a + ' + ' + b + '. Use the empty number line below to find the sum by adding on from the larger number.');
+        this.setAnswer('' + c,0);
+},
+createQuestionShapes: function()
+{
+        this.addQuestionShape(new LineOne (100,150, 475,150,this.mGame,this.mRaphael,"#000",.5,false));
+        this.addQuestionShape(new Triangle(75,150,100,125,100,175,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.5,false));
+        this.addQuestionShape(new Triangle(500,150,475,125,475,175,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.5,false));
+
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('2.nbt.b.7_13',4.0180,'2.nbt.b.7','' );
 */
 var i_2_nbt_b_7__13 = new Class(
