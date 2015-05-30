0dec1e4c316902721698288bdfe2e919e9e717b8
Jim Breslin <jbreslin33@gmail.com>
2015-05-29 22:56:36 -0400

    3.nbt.a.2_3 d

--- src/src/core/math/g3_nbt_a_2.php
+++ src/src/core/math/g3_nbt_a_2.php
@@ -1,5 +1,36 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('3.nbt.a.2_3',4.0184,'3.nbt.a.2','');
+*/
+
+var i_3_nbt_a_2__3 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,400,50,280,75,100,50,500,180);
+
+        this.mType = '3.nbt.a.2_3';
+        this.ns = new NameSampler();
+
+        var x = 0;
+        var y = 0;
+        var z = 1001;
+
+        while (z > 999)
+        {
+                x = Math.floor(Math.random()*899)+100;
+                y = Math.floor(Math.random()*899)+100;
+                z = parseInt(x + y);
+        }
+
+        this.setQuestion('' + this.ns.mNameOne + ' dropped some ' + this.ns.mThingOne + '. ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,1,0) + ' picked up ' + x + ' and ' +  this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,1) + ' friend ' + this.ns.mNameTwo + ' picked up ' + y + '. ' + this.ns.mNameOne + ' used mental math to figure out how many ' + this.ns.mThingOne + ' ' +  this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' dropped. How many did ' +  this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,0) + ' drop?');
+
+        this.setAnswer('' + z,0);
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('3.nbt.a.2_2',4.0184,'3.nbt.a.2','');
 */
 
