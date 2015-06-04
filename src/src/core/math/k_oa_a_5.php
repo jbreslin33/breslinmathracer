bb74113b1676fb348e1ec0d25df2b7de05fc132f
Jim Breslin <jbreslin33@gmail.com>
2015-06-04 15:08:38 -0400

    home

--- src/src/core/math/k_oa_a_5.php
+++ src/src/core/math/k_oa_a_5.php
@@ -1,5 +1,115 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.5_7',0.1507,'k.oa.a.5','Add Subtract within 5.');
+*/
+var i_k_oa_a_5__7 = new Class(
+{
+Extends: NumberPadItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+        this.mType = 'k.oa.a.5_7';
+        this.mThresholdTime = 10000;
+
+        this.setQuestion('1 + 0 = ');
+        this.setAnswer('' + '1',0);
+}
+});
+
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.5_6',0.1506,'k.oa.a.5','Add Subtract within 5.');
+*/
+var i_k_oa_a_5__6 = new Class(
+{
+Extends: NumberPadItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+        this.mType = 'k.oa.a.5_6';
+        this.mThresholdTime = 10000;
+
+        this.setQuestion('0 + 5 = ');
+        this.setAnswer('' + '5',0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.5_5',0.1505,'k.oa.a.5','Add Subtract within 5.');
+*/
+var i_k_oa_a_5__5 = new Class(
+{
+Extends: NumberPadItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+        this.mType = 'k.oa.a.5_5';
+        this.mThresholdTime = 10000;
+
+        this.setQuestion('0 + 4 = ');
+        this.setAnswer('' + '4',0);
+}
+});
+
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.5_4',0.1504,'k.oa.a.5','Add Subtract within 5.');
+*/
+var i_k_oa_a_5__4 = new Class(
+{
+Extends: NumberPadItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+        this.mType = 'k.oa.a.5_4';
+        this.mThresholdTime = 10000;
+
+        this.setQuestion('0 + 3 = ');
+        this.setAnswer('' + '3',0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.5_3',0.1503,'k.oa.a.5','Add Subtract within 5.');
+*/
+var i_k_oa_a_5__3 = new Class(
+{
+Extends: NumberPadItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+        this.mType = 'k.oa.a.5_3';
+        this.mThresholdTime = 10000;
+
+        this.setQuestion('0 + 2 = ');
+        this.setAnswer('' + '2',0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.5_2',0.1502,'k.oa.a.5','Add Subtract within 5.');
+*/
+var i_k_oa_a_5__2 = new Class(
+{
+Extends: NumberPadItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+        this.mType = 'k.oa.a.5_2';
+        this.mThresholdTime = 10000;
+
+	this.setQuestion('0 + 1 = ');
+        this.setAnswer('' + '1',0);
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.5_1',0.1501,'k.oa.a.5','Add Subtract within 5.');
 */
 var i_k_oa_a_5__1 = new Class(
@@ -10,6 +120,26 @@ initialize: function(sheet)
 {
         this.parent(sheet);
         this.mType = 'k.oa.a.5_1';
+
+        this.mThresholdTime = 10000;
+
+        this.setQuestion('0 + 0 = ');
+        this.setAnswer('' + '0',0);
+}
+});
+
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.5_51',0.1551,'k.oa.a.5','Add Subtract within 5.');
+*/
+var i_k_oa_a_5__51 = new Class(
+{
+Extends: NumberPadItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+        this.mType = 'k.oa.a.5_51';
 
 	this.mThresholdTime = 10000;
 	
@@ -44,16 +174,16 @@ initialize: function(sheet)
 });
 
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.5_2',0.1502,'k.oa.a.5','Add Subtract 1 within 5.');
+insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.5_52',0.1552,'k.oa.a.5','Add Subtract 1 within 5.');
 */
-var i_k_oa_a_5__2 = new Class(
+var i_k_oa_a_5__52 = new Class(
 {
 Extends: NumberPadItem,
 
 initialize: function(sheet)
 {
         this.parent(sheet);
-        this.mType = 'k.oa.a.5_2';
+        this.mType = 'k.oa.a.5_52';
 
         this.mThresholdTime = 10000;
 
