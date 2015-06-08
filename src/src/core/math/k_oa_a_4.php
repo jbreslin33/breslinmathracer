8dec6be67ef5b66d6f7b67d1078fdcf6d2de4b91
Jim Breslin <jbreslin33@gmail.com>
2015-06-08 10:03:02 -0400

    move

--- src/src/core/math/k_oa_a_4.php
+++ src/src/core/math/k_oa_a_4.php
@@ -1,5 +1,47 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.4_11',0.14011,'k.oa.a.4','');
+*/
+
+var i_k_oa_a_4__11 = new Class(
+{
+Extends: ThreeButtonItem,
+        initialize: function(sheet)
+        {
+                this.parent(sheet);
+
+                this.mType = 'k.oa.a.4_11';
+
+		this.a = 0;
+		this.b = 0;
+		this.c = 1000;
+
+		this.x = 0;	 
+		this.y = 0;	 
+
+		while (this.a == this.b || this.a == this.y || this.b == this.y || this.c != 10 || this.a < 0 || this.b < 0)
+		{
+			//variables
+                	this.x = Math.floor(Math.random()*9)+2;
+                	this.y = Math.floor(Math.random()*9)+2;
+			this.c = parseInt(this.x + this.y);  
+	
+			//wrong answers 
+                	this.a = Math.floor(Math.random()*11);
+                	this.b = Math.floor(Math.random()*11);
+                }
+
+                this.setAnswer('' + parseInt(this.y),0);
+
+                this.mButtonA.setAnswer('' + this.a);
+                this.mButtonB.setAnswer('' + this.b);
+                this.mButtonC.setAnswer('' +this.y);
+
+		this.setQuestion('' + this.x + ' + _ = 10'); 
+                this.shuffle(10);
+        }
+});
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.4_9',0.1409,'k.oa.a.4','');
 */
 var i_k_oa_a_4__9 = new Class(
@@ -145,45 +187,3 @@ initialize: function(sheet)
 }
 });
 
-/*
-insert into item_types(id,progression,core_standards_id,description) values ('k.oa.a.4_11',0.14011,'k.oa.a.4','');
-*/
-
-var i_k_oa_a_4__11 = new Class(
-{
-Extends: ThreeButtonItem,
-        initialize: function(sheet)
-        {
-                this.parent(sheet);
-
-                this.mType = 'k.oa.a.4_11';
-
-		this.a = 0;
-		this.b = 0;
-		this.c = 1000;
-
-		this.x = 0;	 
-		this.y = 0;	 
-
-		while (this.a == this.b || this.a == this.y || this.b == this.y || this.c != 10 || this.a < 0 || this.b < 0)
-		{
-			//variables
-                	this.x = Math.floor(Math.random()*9)+2;
-                	this.y = Math.floor(Math.random()*9)+2;
-			this.c = parseInt(this.x + this.y);  
-	
-			//wrong answers 
-                	this.a = Math.floor(Math.random()*11);
-                	this.b = Math.floor(Math.random()*11);
-                }
-
-                this.setAnswer('' + parseInt(this.y),0);
-
-                this.mButtonA.setAnswer('' + this.a);
-                this.mButtonB.setAnswer('' + this.b);
-                this.mButtonC.setAnswer('' +this.y);
-
-		this.setQuestion('' + this.x + ' + _ = 10'); 
-                this.shuffle(10);
-        }
-});
