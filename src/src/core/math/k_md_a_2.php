daee0f41c7c2899ee47190b1af189276cc765e8c
Jim Breslin <jbreslin33@gmail.com>
2015-06-09 12:19:03 -0400

    home

--- src/src/core/math/k_md_a_2.php
+++ src/src/core/math/k_md_a_2.php
@@ -19,6 +19,7 @@ initialize: function(sheet)
 createQuestionShapes: function()
 {
         this.addQuestionShape(new Rectangle(10,240,360,10,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.3,false));
+	this.addQuestionShape(new Shape    (50,50,300,100,this.mGame,"/images/christmas/snowball.png","",""));
 }
 });
 
