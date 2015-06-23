ba576ff4bb80565fa1b555ddc9910df07c3dca21
Jim Breslin <jbreslin33@gmail.com>
2015-06-23 09:56:39 -0400

    fixed nulls in schools

--- src/src/database/patches.sql
+++ src/src/database/patches.sql
@@ -5,20 +5,12 @@ ALTER TABLE schools ADD zip text;
 ALTER TABLE schools ADD email text;
 ALTER TABLE schools ADD student_code text;
 
-ALTER TABLE schools ALTER COLUMN city set NOT NULL;
-ALTER TABLE schools ALTER COLUMN state set NOT NULL;
-ALTER TABLE schools ALTER COLUMN zip set NOT NULL;
-ALTER TABLE schools ALTER COLUMN email set NOT NULL;
-ALTER TABLE schools ALTER COLUMN student_code set NOT NULL;
-
-ALTER TABLE schools ADD UNIQUE (name,city,state,zip); 
-
 update schools set username = 'jfield' where id = 2; 
 update schools set city = 'Philadelphia' where id = 2; 
 update schools set state = 'Pennsylvannia' where id = 2; 
-update schools set state = '19125' where id = 2; 
-update schools set email = 'jfield@visitationbvm.com' where id = 1; 
-update schools set student_code = 'learn' where id = 1; 
+update schools set zip = '19125' where id = 2; 
+update schools set email = 'jfield@visitationbvm.com' where id = 2; 
+update schools set student_code = 'learn' where id = 2; 
 
 update schools set username = 'jbreslin' where id = 1; 
 update schools set city = 'Philadelphia' where id = 1; 
@@ -26,3 +18,11 @@ update schools set state = 'Pennsylvannia' where id = 1;
 update schools set zip = '19125' where id = 1; 
 update schools set email = 'jbreslin33@gmail.com' where id = 1; 
 update schools set student_code = 'mathcore' where id = 1; 
+
+ALTER TABLE schools ALTER COLUMN city set NOT NULL;
+ALTER TABLE schools ALTER COLUMN state set NOT NULL;
+ALTER TABLE schools ALTER COLUMN zip set NOT NULL;
+ALTER TABLE schools ALTER COLUMN email set NOT NULL;
+ALTER TABLE schools ALTER COLUMN student_code set NOT NULL;
+
+ALTER TABLE schools ADD UNIQUE (name,city,state,zip); 
