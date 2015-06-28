2d76cf173edac0e13101d26ddb180e62566d859b
Jim Breslin <jbreslin33@gmail.com>
2015-06-28 19:51:45 -0400

    add file

---
+++ src/web/navigation/reports.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+<html>
+
+<head>
+        <title>LEADER BOARDS</title>
+<link rel="stylesheet" type="text/css" href="<?php getenv("DOCUMENT_ROOT")?>/css/green_block.css" />
+</head>
+
+<body>
+<p><b> Leader Boards </p></b>
+<?php
+
+session_start();
+?>
+
+<ul>
+<li><a href="/web/navigation/main_menu_school.php">Main Menu</a></li>
+<li><a href="/web/php/logout.php">Logout</a></li>
+<li><a href="/web/stats/leaderboards.php">Normal Leader Boards</a></li>
+<li><a href="/web/stats/statsreports.php">Non Mastery Types</a></li>
+</ul>
+
+</body>
+</html>
