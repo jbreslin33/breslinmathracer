f6c79c5fee061e186874d50b7c1e570b68e436f9
Jim Breslin <jbreslin33@gmail.com>
2015-06-30 11:10:28 -0400

    t2 rfm

--- src/src/core/math/g1_oa_a_2.php
+++ src/src/core/math/g1_oa_a_2.php
@@ -1,5 +1,38 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('1.oa.a.2_2',1.0202,'1.oa.a.2','How many more.' );
+*/
+
+var i_1_oa_a_2__2 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+	this.parent(sheet,600,50,330,75,100,50,685,80);
+        this.mType = '1.oa.a.2_2';
+
+        this.mNameMachine = new NameMachine();
+        this.ns = new NameSampler();
+
+        //variables
+        this.a = 0;
+        this.b = 0;
+        this.c = 0;
+	this.d = 99; 
+	while(this.d > 20)
+	{
+        	this.a = Math.floor(Math.random()*8)+2;
+        	this.b = Math.floor(Math.random()*8)+2;
+        	this.c = Math.floor(Math.random()*8)+2;
+        	this.d = parseInt(this.a + this.b + this.c);
+	}
+
+        this.setQuestion(this.ns.mNameOne + ' has ' + this.a + ' ' + this.ns.mThingOne + ', ' + this.ns.mNameTwo + ' has ' + this.b + ' ' + this.ns.mThingOne + ' and ' + this.ns.mNameThree + ' has ' + this.c + ' ' + this.ns.mThingOne + '. How many ' + this.ns.mThingOne + ' do they have ' + this.ns.mSum + '?');
+        this.setAnswer('' + this.d,0);
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('1.oa.a.2_1',1.0201,'1.oa.a.2','How many more.' );
 */
 
