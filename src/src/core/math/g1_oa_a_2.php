c0dd2f86c671e98b777302d24ce4e0d922912cb3
Jim Breslin <jbreslin33@gmail.com>
2015-06-30 10:25:13 -0400

    t1

--- src/src/core/math/g1_oa_a_2.php
+++ src/src/core/math/g1_oa_a_2.php
@@ -6,22 +6,29 @@ insert into item_types(id,progression,core_standards_id,description) values ('1.
 var i_1_oa_a_2__1 = new Class(
 {
 Extends: TextItem,
-        initialize: function(sheet)
-        {
-                this.parent(sheet,600,50,330,75,100,50,685,80);
+initialize: function(sheet)
+{
+	this.parent(sheet,600,50,330,75,100,50,685,80);
+        this.mType = '1.oa.a.2_1';
 
-                this.mType = '1.oa.a.2_1';
+        this.mNameMachine = new NameMachine();
+        this.ns = new NameSampler();
 
-                this.mNameMachine = new NameMachine();
-                this.ns = new NameSampler();
+        //variables
+        this.a = 0;
+        this.b = 0;
+        this.c = 0;
+	this.d = 99; 
+	while(this.d > 20)
+	{
+        	this.a = Math.floor(Math.random()*8)+2;
+        	this.b = Math.floor(Math.random()*8)+2;
+        	this.c = Math.floor(Math.random()*8)+2;
+        	this.d = parseInt(this.a + this.b + this.c);
+	}
 
-                //variables
-                this.a = Math.floor(Math.random()*8)+13;
-                this.b = Math.floor(Math.random()*5)+5;
-                this.c = parseInt(this.a - this.b);
-
-                this.setQuestion(this.ns.mNameOne + ' has ' + this.a + ' ' + this.ns.mThings + '. ' + this.ns.mNameTwo + ' has ' + this.b + ' ' + this.ns.mThings + '. How many more ' + this.ns.mThings + ' does ' + this.ns.mNameOne + ' have than ' + this.ns.mNameTwo + '?');
-                this.setAnswer('' + this.c,0);
-        }
+        this.setQuestion(this.ns.mNameOne + ' has ' + this.a + ' ' + this.ns.mThingOne + '. ' + this.ns.mNameTwo + ' has ' + this.b + ' ' + this.ns.mThingOne + ' and ' + this.mNameThree + ' has ' + this.c + ' ' + this.ns.mThingOne + '. How many ' + this.ns.mThingOne + ' do they have ?');
+        this.setAnswer('' + this.d,0);
+}
 });
 
