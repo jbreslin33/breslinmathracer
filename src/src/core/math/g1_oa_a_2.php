1a78329ca08318cbd4b0879ec833243bd447abb4
Jim Breslin <jbreslin33@gmail.com>
2015-06-30 20:06:28 -0400

    t4

--- src/src/core/math/g1_oa_a_2.php
+++ src/src/core/math/g1_oa_a_2.php
@@ -1,5 +1,38 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('1.oa.a.2_4',1.0204,'1.oa.a.2','How many more.' );
+*/
+
+var i_1_oa_a_2__4 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,600,50,330,75,100,50,685,80);
+        this.mType = '1.oa.a.2_4';
+
+        this.mNameMachine = new NameMachine();
+        this.ns = new NameSampler();
+
+        //variables
+        this.a = 0;
+        this.b = 0;
+        this.c = 0;
+        this.d = 99;
+        while(this.d > 20)
+        {
+                this.a = Math.floor(Math.random()*8)+2;
+                this.b = Math.floor(Math.random()*8)+2;
+                this.c = Math.floor(Math.random()*8)+2;
+                this.d = parseInt(this.a + this.b + this.c);
+        }
+
+        this.setQuestion('' + 'At the zoo' + ' ' + this.ns.mNameOne + ' counted ' + this.a + ' ' + this.ns.mAnimalOne + ', ' + this.b + ' ' + this.ns.mAnimalTwo + ' and ' + this.c + ' ' + this.ns.mAnimalThree + '. How many animals did ' + this.ns.mNameOne + ' count ' + this.ns.mSum + '?');
+        this.setAnswer('' + this.d,0);
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('1.oa.a.2_3',1.0203,'1.oa.a.2','How many more.' );
 */
 
