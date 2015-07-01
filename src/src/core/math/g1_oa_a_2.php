4f61aef3d954a5b2284f7bfbd93db2cdf663bd05
Jim Breslin <jbreslin33@gmail.com>
2015-06-30 21:06:23 -0400

    t9

--- src/src/core/math/g1_oa_a_2.php
+++ src/src/core/math/g1_oa_a_2.php
@@ -1,5 +1,37 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('1.oa.a.2_9',1.0209,'1.oa.a.2','add 3 within 20' );
+*/
+
+var i_1_oa_a_2__9 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,600,50,330,75,100,50,685,80);
+        this.mType = '1.oa.a.2_9';
+
+        this.mNameMachine = new NameMachine();
+        this.ns = new NameSampler();
+
+        //variables
+        this.a = 0;
+        this.b = 0;
+        this.c = 0;
+        this.d = 99;
+        while(this.d > 20)
+        {
+                this.a = Math.floor(Math.random()*8)+2;
+                this.b = Math.floor(Math.random()*8)+2;
+                this.c = Math.floor(Math.random()*8)+2;
+                this.d = parseInt(this.a + this.b + this.c);
+        }
+	this.setQuestion('' + 'While playing ' + this.ns.mPointActivityOne + ' ' + this.ns.mNameOne + ' scored ' + this.a + ' points, ' + this.ns.mNameTwo + ' scored ' + this.b + ' points and ' + this.ns.mNameThree + ' scored ' + this.c + ' points. How many points did they score ' + this.ns.mSum + '?');       
+        this.setAnswer('' + this.d,0);
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('1.oa.a.2_8',1.0208,'1.oa.a.2','add 3 within 20' );
 */
 
