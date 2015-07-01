5809730412015bf1cb1b872fb5176ec926432759
Jim Breslin <jbreslin33@gmail.com>
2015-06-30 21:13:03 -0400

    t10 d

--- src/src/core/math/g1_oa_a_2.php
+++ src/src/core/math/g1_oa_a_2.php
@@ -1,5 +1,38 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('1.oa.a.2_10',1.0210,'1.oa.a.2','add 3 within 20' );
+*/
+
+var i_1_oa_a_2__10 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,600,50,330,75,100,50,685,80);
+        this.mType = '1.oa.a.2_10';
+
+        this.mNameMachine = new NameMachine();
+        this.ns = new NameSampler();
+
+        //variables
+        this.a = 0;
+        this.b = 0;
+        this.c = 0;
+        this.d = 99;
+        while(this.d > 20)
+        {
+                this.a = Math.floor(Math.random()*8)+2;
+                this.b = Math.floor(Math.random()*8)+2;
+                this.c = Math.floor(Math.random()*8)+2;
+                this.d = parseInt(this.a + this.b + this.c);
+        }
+        this.setQuestion('' + this.ns.mNameOne + ' used ' + this.a + ' pages in ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,1) + ' ' + this.ns.mSubjectOne + ' notebook, ' + this.b + ' pages in ' +  this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,1) + ' ' + this.ns.mSubjectTwo + ' notebook and ' + this.c + ' pages in ' +  this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,1) + ' ' + this.ns.mSubjectThree + ' notebook. How many pages did ' +  this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,1) + ' use ' + this.ns.mSum + '?');
+        this.setAnswer('' + this.d,0);
+}
+});
+
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('1.oa.a.2_9',1.0209,'1.oa.a.2','add 3 within 20' );
 */
 
