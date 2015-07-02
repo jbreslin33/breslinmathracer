b211761870c7dcfe0d358b142bda0c27de6d0258
Jim Breslin <jbreslin33@gmail.com>
2015-07-02 09:14:31 -0400

    t16

--- src/src/core/math/g1_oa_b_3.php
+++ src/src/core/math/g1_oa_b_3.php
@@ -1,5 +1,125 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('1.oa.b.3_16',1.0316,'1.oa.b.3','communative' );
+*/
+var i_1_oa_b_3__16 = new Class(
+{
+Extends: NumberPadItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+        this.mType = '1.oa.b.3_16';
+        this.mThresholdTime = 10000;
+
+        this.mQuestionLabel.setPosition(150,75);
+        this.mQuestionLabel.setSize(250,50);
+        
+        this.mUserAnswerLabel.setPosition(150,100);
+	this.mUserAnswerLabel.setSize(250,50);
+
+	this.a = Math.floor(Math.random()*9)+1;
+	this.b = Math.floor(Math.random()*9)+1;
+        this.c = parseInt(this.a + this.b);
+
+        this.setQuestion('' + this.a +  ' + ' + this.b + ' = ' + this.c + ' AND ' + this.b + ' + ' + this.a + ' = _' );
+        this.setAnswer('' + this.c,0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('1.oa.b.3_15',1.0315,'1.oa.b.3','associative' );
+*/
+var i_1_oa_b_3__15 = new Class(
+{
+Extends: NumberPadItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+        this.mType = '1.oa.b.3_15';
+        this.mThresholdTime = 10000;
+ 
+	this.mQuestionLabel.setPosition(220,75);
+	this.mQuestionLabel.setSize(100,50);
+
+	this.a = 0;
+	this.b = 0;
+	while (this.a == 0 || this.a == 1 || this.a == 9) 
+	{
+		this.a = Math.floor(Math.random()*9)+1;
+	}
+	this.b = parseInt(10 + this.a);
+
+	this.r = Math.floor(Math.random()*3);
+
+	if (this.r == 0)
+	{
+        	this.setQuestion('' + '6 + 4 + ' + this.a + ' =');
+	}
+        this.setAnswer('' + this.b,0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('1.oa.b.3_14',1.0314,'1.oa.b.3','associative' );
+*/
+var i_1_oa_b_3__14 = new Class(
+{
+Extends: NumberPadItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+        this.mType = '1.oa.b.3_14';
+        this.mThresholdTime = 10000;
+
+        this.mQuestionLabel.setPosition(220,75);
+        this.mQuestionLabel.setSize(100,50);
+
+        this.a = 0;
+        this.b = 0;
+        while (this.a == 0 || this.a == 1 || this.a == 9)
+        {
+                this.a = Math.floor(Math.random()*9)+1;
+        }
+        this.b = parseInt(10 + this.a);
+
+        this.setQuestion('' + '6 + ' + this.a + ' + 4 =');
+        this.setAnswer('' + this.b,0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('1.oa.b.3_13',1.0313,'1.oa.b.3','associative' );
+*/
+var i_1_oa_b_3__13 = new Class(
+{
+Extends: NumberPadItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+        this.mType = '1.oa.b.3_13';
+        this.mThresholdTime = 10000;
+
+        this.mQuestionLabel.setPosition(220,75);
+        this.mQuestionLabel.setSize(100,50);
+
+        this.a = 0;
+        this.b = 0;
+        while (this.a == 0 || this.a == 1 || this.a == 9)
+        {
+                this.a = Math.floor(Math.random()*9)+1;
+        }
+        this.b = parseInt(10 + this.a);
+
+        this.setQuestion('' + '6 + 4 + ' + this.a + ' =');
+        this.setAnswer('' + this.b,0);
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('1.oa.b.3_12',1.0312,'1.oa.b.3','communative' );
 */
 var i_1_oa_b_3__12 = new Class(
