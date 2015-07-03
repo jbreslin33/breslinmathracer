e6f8c6ae16f999b425537e493285578b9e5eac09
Jim Breslin <jbreslin33@gmail.com>
2015-07-03 06:07:35 -0400

    t9

--- src/src/core/math/g1_oa_c_6.php
+++ src/src/core/math/g1_oa_c_6.php
@@ -1,4 +1,197 @@
 
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('1.oa.c.6_9',1.0609,'1.oa.c.6','' );
+*/
+var i_1_oa_c_6__9 = new Class(
+{
+Extends: NumberPadItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+        this.mType = '1.oa.c.6_9';
+        this.mThresholdTime = 5000;
+
+        this.mQuestionLabel.setPosition(320,75);
+        this.mQuestionLabel.setSize(300,50);
+        this.mUserAnswerLabel.setPosition(220,150);
+        this.mAnswerTextBox.setPosition(370,75);
+        this.mAnswerTextBox.setSize(100,50);
+
+        this.setQuestion('' + '9 + _ = 10');
+        this.setAnswer('' + '1',0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('1.oa.c.6_8',1.0608,'1.oa.c.6','' );
+*/
+var i_1_oa_c_6__8 = new Class(
+{
+Extends: NumberPadItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+        this.mType = '1.oa.c.6_8';
+        this.mThresholdTime = 5000;
+
+        this.mQuestionLabel.setPosition(320,75);
+        this.mQuestionLabel.setSize(300,50);
+        this.mUserAnswerLabel.setPosition(220,150);
+        this.mAnswerTextBox.setPosition(370,75);
+        this.mAnswerTextBox.setSize(100,50);
+
+        this.setQuestion('' + '8 + _ = 10');
+        this.setAnswer('' + '2',0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('1.oa.c.6_7',1.0607,'1.oa.c.6','' );
+*/
+var i_1_oa_c_6__7 = new Class(
+{
+Extends: NumberPadItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+        this.mType = '1.oa.c.6_7';
+        this.mThresholdTime = 5000;
+
+        this.mQuestionLabel.setPosition(320,75);
+        this.mQuestionLabel.setSize(300,50);
+        this.mUserAnswerLabel.setPosition(220,150);
+        this.mAnswerTextBox.setPosition(370,75);
+        this.mAnswerTextBox.setSize(100,50);
+
+        this.setQuestion('' + '7 + _ = 10');
+        this.setAnswer('' + '3',0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('1.oa.c.6_6',1.0606,'1.oa.c.6','' );
+*/
+var i_1_oa_c_6__6 = new Class(
+{
+Extends: NumberPadItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+        this.mType = '1.oa.c.6_6';
+        this.mThresholdTime = 5000;
+
+        this.mQuestionLabel.setPosition(320,75);
+        this.mQuestionLabel.setSize(300,50);
+        this.mUserAnswerLabel.setPosition(220,150);
+        this.mAnswerTextBox.setPosition(370,75);
+        this.mAnswerTextBox.setSize(100,50);
+
+        this.setQuestion('' + '6 + _ = 10');
+        this.setAnswer('' + '4',0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('1.oa.c.6_5',1.0605,'1.oa.c.6','' );
+*/
+var i_1_oa_c_6__5 = new Class(
+{
+Extends: NumberPadItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+        this.mType = '1.oa.c.6_5';
+        this.mThresholdTime = 5000;
+
+        this.mQuestionLabel.setPosition(320,75);
+        this.mQuestionLabel.setSize(300,50);
+        this.mUserAnswerLabel.setPosition(220,150);
+        this.mAnswerTextBox.setPosition(370,75);
+        this.mAnswerTextBox.setSize(100,50);
+
+        this.setQuestion('' + '5 + _ = 10');
+        this.setAnswer('' + '5',0);
+}
+});
+
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('1.oa.c.6_4',1.0604,'1.oa.c.6','' );
+*/
+var i_1_oa_c_6__4 = new Class(
+{
+Extends: NumberPadItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+        this.mType = '1.oa.c.6_4';
+        this.mThresholdTime = 5000;
+
+        this.mQuestionLabel.setPosition(320,75);
+        this.mQuestionLabel.setSize(300,50);
+        this.mUserAnswerLabel.setPosition(220,150);
+        this.mAnswerTextBox.setPosition(370,75);
+        this.mAnswerTextBox.setSize(100,50);
+
+        this.setQuestion('' + '4 + _ = 10');
+        this.setAnswer('' + '6',0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('1.oa.c.6_3',1.0603,'1.oa.c.6','' );
+*/
+var i_1_oa_c_6__3 = new Class(
+{
+Extends: NumberPadItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+        this.mType = '1.oa.c.6_3';
+        this.mThresholdTime = 5000;
+
+        this.mQuestionLabel.setPosition(320,75);
+        this.mQuestionLabel.setSize(300,50);
+        this.mUserAnswerLabel.setPosition(220,150);
+        this.mAnswerTextBox.setPosition(370,75);
+        this.mAnswerTextBox.setSize(100,50);
+
+        this.setQuestion('' + '3 + _ = 10');
+        this.setAnswer('' + '7',0);
+}
+});
+
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('1.oa.c.6_2',1.0602,'1.oa.c.6','' );
+*/
+var i_1_oa_c_6__2 = new Class(
+{
+Extends: NumberPadItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+        this.mType = '1.oa.c.6_2';
+        this.mThresholdTime = 5000;
+
+        this.mQuestionLabel.setPosition(320,75);
+        this.mQuestionLabel.setSize(300,50);
+        this.mUserAnswerLabel.setPosition(220,150);
+        this.mAnswerTextBox.setPosition(370,75);
+        this.mAnswerTextBox.setSize(100,50);
+
+        this.setQuestion('' + '2 + _ = 10');
+        this.setAnswer('' + '8',0);
+}
+});
 
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('1.oa.c.6_1',1.0601,'1.oa.c.6','' );
@@ -11,16 +204,13 @@ initialize: function(sheet)
 {
         this.parent(sheet);
         this.mType = '1.oa.c.6_1';
-        this.mThresholdTime = 15000;
-/*
-        this.mQuestionLabel.setPosition(170,75);
-        this.mQuestionLabel.setSize(300,50);
-        
-	this.mUserAnswerLabel.setPosition(220,150);
+        this.mThresholdTime = 5000;
 
+        this.mQuestionLabel.setPosition(320,75);
+        this.mQuestionLabel.setSize(300,50);
+	this.mUserAnswerLabel.setPosition(220,150);
     	this.mAnswerTextBox.setPosition(370,75);
     	this.mAnswerTextBox.setSize(100,50);
-*/
 
         this.setQuestion('' + '1 + _ = 10');
         this.setAnswer('' + '9',0);
