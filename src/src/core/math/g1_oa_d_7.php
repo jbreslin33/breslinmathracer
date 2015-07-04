cc25403fb21cf09ee9c7c29b9cd31657322ac9f5
Jim Breslin <jbreslin33@gmail.com>
2015-07-04 13:39:26 -0400

    t1

--- src/src/core/math/g1_oa_d_7.php
+++ src/src/core/math/g1_oa_d_7.php
@@ -5,22 +5,19 @@ insert into item_types(id,progression,core_standards_id,description) values ('1.
 var i_1_oa_d_7__1 = new Class(
 {
 Extends: TextItem,
-        initialize: function(sheet)
-        {
-                this.parent(sheet,600,50,330,75,100,50,685,80);
+initialize: function(sheet)
+{
+	this.parent(sheet,600,50,330,75,100,50,685,80);
 
-                this.mType = '1.oa.d.7_1';
+        this.mType = '1.oa.d.7_1';
+        this.mNameMachine = new NameMachine();
+        this.ns = new NameSampler();
+        
+	this.a = Math.floor(Math.random()*9)+1;
+        this.b = this.a;
 
-                this.mNameMachine = new NameMachine();
-                this.ns = new NameSampler();
-
-                //variables
-                this.a = Math.floor(Math.random()*8)+13;
-                this.b = Math.floor(Math.random()*5)+5;
-                this.c = parseInt(this.a - this.b);
-
-                this.setQuestion(this.ns.mNameOne + ' has ' + this.a + ' ' + this.ns.mThings + '. ' + this.ns.mNameTwo + ' has ' + this.b + ' ' + this.ns.mThings + '. How many more ' + this.ns.mThings + ' does ' + this.ns.mNameOne + ' have than ' + this.ns.mNameTwo + '?');
-                this.setAnswer('' + this.c,0);
-        }
+        this.setQuestion('' + 'Help ' + this.ns.mNameOne + ' answer whether ' + this.a + ' = ' + this.b + ' is true of false.');
+        this.setAnswer('' + 'true',0);
+}
 });
 
