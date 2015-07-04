831cfa02349f71e008f019d9689926882f2b1582
Jim Breslin <jbreslin33@gmail.com>
2015-07-04 13:45:42 -0400

    t3

--- src/src/core/math/g1_oa_d_7.php
+++ src/src/core/math/g1_oa_d_7.php
@@ -1,3 +1,65 @@
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('1.oa.d.7_3',1.0703,'1.oa.d.7','');
+*/
+
+var i_1_oa_d_7__3 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,600,50,330,75,100,50,685,80);
+
+        this.mType = '1.oa.d.7_3';
+        this.mNameMachine = new NameMachine();
+        this.ns = new NameSampler();
+
+        this.a = 0;
+        this.b = 0;
+        this.c = 0;
+        this.d = 99;
+
+        while (this.a != this.d)
+        {
+                this.a = Math.floor(Math.random()*9)+1;
+                this.b = Math.floor(Math.random()*9)+1;
+                this.c = Math.floor(Math.random()*9)+1;
+		this.d = parseInt(this.b + this.c);
+        }
+        this.setQuestion('' + 'Help ' + this.ns.mNameOne + ' answer whether ' + this.a + ' = ' + this.b + ' + ' + this.c + ' is true of false.');
+        this.setAnswer('' + 'true',0);
+}
+});
+
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('1.oa.d.7_2',1.0702,'1.oa.d.7','');
+*/
+
+var i_1_oa_d_7__2 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,600,50,330,75,100,50,685,80);
+
+        this.mType = '1.oa.d.7_2';
+        this.mNameMachine = new NameMachine();
+        this.ns = new NameSampler();
+
+	this.a = 0;
+	this.b = 0;
+
+	while (this.a == this.b)	
+	{
+        	this.a = Math.floor(Math.random()*9)+1;
+        	this.b = Math.floor(Math.random()*9)+1;
+	}
+        this.setQuestion('' + 'Help ' + this.ns.mNameOne + ' answer whether ' + this.a + ' = ' + this.b + ' is true of false.');
+        this.setAnswer('' + 'false',0);
+}
+});
+
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('1.oa.d.7_1',1.0701,'1.oa.d.7','');
 */
