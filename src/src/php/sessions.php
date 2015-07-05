56e5bade68ebe01d1f55772bc6ee5ca1ccda8be3
Jim Breslin <jbreslin33@gmail.com>
2015-07-05 19:55:03 -0400

    normal

--- src/src/php/sessions.php
+++ src/src/php/sessions.php
@@ -38,56 +38,34 @@ public function process()
 	{
 		$_SESSION["subject_id"] = 1;
 	}
-	//get the latest evaluations_attempts one that is not complete so that if you were doing a test or tables or normal you go back there...
-	$query = "select evaluations_attempts.id, evaluations.description from evaluations_attempts JOIN evaluations ON evaluations.id=evaluations_attempts.evaluations_id where evaluations_attempts.end_time is null AND user_id = ";
-	$query .= $_SESSION["user_id"];
-	$query .= " order by start_time desc limit 1;";
-	
-        $result = pg_query($this->mDatabaseConnection->getConn(),$query) or die('Could not connect: ' . pg_last_error());
+	//normalTime
+	$normalStartTime = "8:30:00";
+	$normalEndTime   = "14:50:00";
+	if (time() >= strtotime($normalStartTime) && time() <= strtotime($normalEndTime))
+ 	{
+		if ($ref_id == 'normal')
+		{
+			$normal = new Normal(1);
+		}
 
-        $num = pg_num_rows($result);
-
-        if ($num > 0)
-        {
-                $evaluations_attempts_id = pg_Result($result, 0, 'id');
-                $ref_id                  = pg_Result($result, 0, 'description');
-
-		$_SESSION["evaluations_attempts_id"] = $evaluations_attempts_id;
-                $_SESSION["ref_id"]  = $ref_id;
-
-		//normalTime
-		$normalStartTime = "8:30:00";
-		$normalEndTime   = "14:50:00";
-		if (time() >= strtotime($normalStartTime) && time() <= strtotime($normalEndTime))
- 		{
-			if ($ref_id == 'normal')
+		// this is just for dev user and has no effect on anyone else
+		if ($ref_id == 'practice')
+		{
+			if ($_SESSION["first_name"] == 'dev' && $_SESSION["last_name"] == 'user')
+			{
+				$practice = new Practice('',0,0);
+			}
+			else
 			{
 				$normal = new Normal(1);
 			}
-
-			// this is just for dev user and has no effect on anyone else
-			if ($ref_id == 'practice')
-			{
-				if ($_SESSION["first_name"] == 'dev' && $_SESSION["last_name"] == 'user')
-				{
-					$practice = new Practice('',0,0);
-				}
-				else
-				{
-					$normal = new Normal(1);
-				}
-			}
-		}
-		else //after school
-		{
-			//$assessment = new Assessment();
-			$normal = new Normal(1);
-                	//$_SESSION["ref_id"]  = 'assessment';
 		}
 	}
-	else
+	else //after school
 	{
+		//$assessment = new Assessment();
 		$normal = new Normal(1);
+               	//$_SESSION["ref_id"]  = 'assessment';
 	}
 }
 //end class
