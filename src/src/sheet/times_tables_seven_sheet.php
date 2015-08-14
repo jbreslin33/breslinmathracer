f5fda4c36bf340d056b518e3c8d4a7d46bc59e1b
Jim Breslin <jbreslin33@gmail.com>
2015-08-14 14:57:06 -0400

    copied 2 to other tables

--- src/src/sheet/times_tables_seven_sheet.php
+++ src/src/sheet/times_tables_seven_sheet.php
@@ -1,67 +1,30 @@
-var TimesTablesSevenSheet = new Class(
+var TimesTablesTwoSheet = new Class(
 {
-Extends: Sheet,
+Extends: TimesTablesSheet,
 
 initialize: function(game)
 {
 	this.parent(game);
-},
-
-createItem: function()
-{
-	//would love to loop till we got no dup
-        while (APPLICATION.mQuestionTypeLast == APPLICATION.mQuestionTypeCurrent)
-        {
-        	var r = Math.floor(Math.random()*3);
-
-                if (r == 0)
-                {
-                        APPLICATION.getFirst();
-                        APPLICATION.mQuestionTypeCurrent = APPLICATION.mFirst;
-                }
-                if (r == 1)
-                {
-                        APPLICATION.getLeastAsked();
-                        APPLICATION.mQuestionTypeCurrent = APPLICATION.mLeastAsked;
-                }
-                if (r == 2)
-                {
-                        APPLICATION.getLeastCorrect();
-                        APPLICATION.mQuestionTypeCurrent = APPLICATION.mLeastCorrect;
-                }
-	}
-
-        var pick = 0;
-
-        if (pick == 0)
-        {
-                pick = this.mPicker.getItem(APPLICATION.mQuestionTypeCurrent);
-        }
-        if (pick == 0)
-        {
-                pick = this.mPickerBrian.getItem(APPLICATION.mQuestionTypeCurrent);
-        }
-        if (pick == 0)
-        {
-                pick = this.mPickerJim.getItem(APPLICATION.mQuestionTypeCurrent);
-        }
-
-        //if you got an item then add it to sheet
-        if (pick != 0)
-        {
-                this.setItem(pick);
-                var itemAttempt = new ItemAttempt();
-                APPLICATION.mItemAttemptsArray.push(itemAttempt);
-                pick.setItemAttempt(itemAttempt);
-              	itemAttempt.mType = pick.mType;
-               	itemAttempt.setEvaluationsID(8);
-        }
-        else
-        {
-                APPLICATION.log('no item picked by pickers!');
-        }
-
-        //set this as last for next run
- 	APPLICATION.mQuestionTypeLast = APPLICATION.mQuestionTypeCurrent;
+	this.mIDArray.push('3.oa.c.7_82');
+	this.mIDArray.push('3.oa.c.7_1');
+	this.mIDArray.push('3.oa.c.7_2');
+	this.mIDArray.push('3.oa.c.7_4');
+	this.mIDArray.push('3.oa.c.7_6');
+	this.mIDArray.push('3.oa.c.7_8');
+	this.mIDArray.push('3.oa.c.7_10');
+	this.mIDArray.push('3.oa.c.7_12');
+	this.mIDArray.push('3.oa.c.7_14');
+	this.mIDArray.push('3.oa.c.7_16');
+	this.mIDArray.push('3.oa.c.7_92');
+	this.mIDArray.push('3.oa.c.7_1');
+	this.mIDArray.push('3.oa.c.7_3');
+	this.mIDArray.push('3.oa.c.7_5');
+	this.mIDArray.push('3.oa.c.7_7');
+	this.mIDArray.push('3.oa.c.7_9');
+	this.mIDArray.push('3.oa.c.7_11');
+	this.mIDArray.push('3.oa.c.7_13');
+	this.mIDArray.push('3.oa.c.7_15');
+	this.mIDArray.push('3.oa.c.7_17');
 }
+
 });
