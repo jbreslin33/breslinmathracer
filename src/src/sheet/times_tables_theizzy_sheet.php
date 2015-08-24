f934d3f8e6bae5af60168ddc0ec50a4128bb7419
Jim Breslin <jbreslin33@gmail.com>
2015-08-24 13:36:52 -0400

    the izzy needs tt questions

--- src/src/sheet/times_tables_theizzy_sheet.php
+++ src/src/sheet/times_tables_theizzy_sheet.php
@@ -5,26 +5,22 @@ Extends: TimesTablesSheet,
 initialize: function(game)
 {
 	this.parent(game);
-	this.mIDArray.push('3.oa.c.7_82');
-	this.mIDArray.push('3.oa.c.7_1');
-	this.mIDArray.push('3.oa.c.7_2');
-	this.mIDArray.push('3.oa.c.7_4');
-	this.mIDArray.push('3.oa.c.7_6');
-	this.mIDArray.push('3.oa.c.7_8');
-	this.mIDArray.push('3.oa.c.7_10');
-	this.mIDArray.push('3.oa.c.7_12');
-	this.mIDArray.push('3.oa.c.7_14');
-	this.mIDArray.push('3.oa.c.7_16');
-	this.mIDArray.push('3.oa.c.7_92');
-	this.mIDArray.push('3.oa.c.7_1');
-	this.mIDArray.push('3.oa.c.7_3');
-	this.mIDArray.push('3.oa.c.7_5');
-	this.mIDArray.push('3.oa.c.7_7');
-	this.mIDArray.push('3.oa.c.7_9');
-	this.mIDArray.push('3.oa.c.7_11');
-	this.mIDArray.push('3.oa.c.7_13');
-	this.mIDArray.push('3.oa.c.7_15');
-	this.mIDArray.push('3.oa.c.7_17');
-}
+ 	for (i = 1; i < 43; i++)
+        {
+                var a = 'k.oa.a.5_';
+                var b = '' + i;
+                var c = '' + a + b;
+                this.mIDArray.push('' + c);
+        }
+},
 
+pickItem: function()
+{
+	var length = this.mIDArray.length;
+        while (APPLICATION.mQuestionTypeLast == APPLICATION.mQuestionTypeCurrent)
+        {
+                var r = Math.floor(Math.random()*length);
+                APPLICATION.mQuestionTypeCurrent = this.mIDArray[r];
+        }
+}
 });
