a0e0ae5c39460de25289e43c0fa620c6965284ed
Jim Breslin <jbreslin33@gmail.com>
2015-09-06 20:55:15 -0400

    homework page

--- src/web/navigation/reports.php
+++ src/web/navigation/reports.php
@@ -18,6 +18,7 @@ session_start();
 <li><a href="/web/navigation/main_menu_school.php">Main Menu</a></li>
 <li><a href="/web/php/logout.php">Logout</a></li>
 <li><a href="/web/stats/leaderboards.php">Normal Leader Boards</a></li>
+<li><a href="/web/stats/homework.php">Home Work</a></li>
 <li><a href="/web/stats/statsreports.php">Non Mastery Types</a></li>
 </ul>
 
