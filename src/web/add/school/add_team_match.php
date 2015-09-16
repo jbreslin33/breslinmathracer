e1913bb8702ba8ca60fe8a7eccccc5a257841c1a
Jim Breslin <jbreslin33@gmail.com>
2015-09-16 10:25:49 -0400

    teamsmatches

--- src/web/add/school/add_team_match.php
+++ src/web/add/school/add_team_match.php
@@ -17,6 +17,56 @@ $conn = dbConnect();
 echo "<br>";
 ?>
 	<p><b> ADD TEAM TO MATCH: </p></b>
+
+<?php
+echo '<table border=\"1\">';
+        echo '<tr>';
+
+        echo '<td> match_id';
+        echo '</td>';
+        echo '<td> team_name';
+        echo '</td>';
+        echo '<td> start_time';
+        echo '</td>';
+        echo '<td> end_time';
+        echo '</td>';
+
+        echo '</tr>';
+
+$q = "select matches.id, teams.name, matches.start_time, matches.end_time from matches JOIN teams_matches ON matches.id=teams_matches.matches_id JOIN teams ON teams_matches.team_id=teams.id where matches.school_id = ";
+$q .= $_SESSION["school_id"];
+$q .= " order by matches.id desc;";
+
+$r = pg_query($conn,$q);
+$n = pg_numrows($r);
+
+        for($i = 0; $i < $n; $i++)
+        {
+                $row = pg_fetch_array($r, $i);
+                $match_id = $row[0];
+                $team_name = $row[1];
+                $start_time = $row[2];
+                $end_time = $row[3];
+
+                echo '<tr>';
+                echo '<td>';
+                echo $match_id;
+                echo '</td>';
+                echo '<td>';
+                echo $team_name;
+                echo '</td>';
+                echo '<td>';
+                echo $start_time;
+                echo '</td>';
+                echo '<td>';
+                echo $end_time;
+                echo '</td>';
+                echo '</tr>';
+        }
+
+        pg_free_result($r);
+        echo '</table>';
+?>
 	
 	<form method="post" action="/web/add/school/addteammatch.php">
 <select name="match_id">
