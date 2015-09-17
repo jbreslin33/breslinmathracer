90b9f8c41a78f4040006b6de3801421f508d79ff
Jim Breslin <jbreslin33@gmail.com>
2015-09-17 14:40:24 -0400

    2

--- src/src/core/math/g1_oa_b_4.php
+++ src/src/core/math/g1_oa_b_4.php
@@ -1,5 +1,36 @@
+
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('1.oa.b.4_1',1.0401,'1.oa.b.4','associative' );
+insert into item_types(id,progression,core_standards_id,description) values ('1.oa.b.4_2',1.0402,'1.oa.b.4','' );
+*/
+var i_1_oa_b_4__2 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+       	this.parent(sheet,600,50,330,75,100,50,685,80);
+
+        this.mNameMachine = new NameMachine();
+        this.ns = new NameSampler();
+
+        this.mType = '1.oa.b.4_2';
+
+	this.a = 0;	
+	this.b = 0;	
+	while (this.a <= this.b)	
+	{
+      		this.a = Math.floor(Math.random()*9)+1;
+       		this.b = Math.floor(Math.random()*9)+1;
+	}
+	this.c = parseInt(this.a - this.b);        
+
+        this.setQuestion('' + 'Subtract' + this.a + ' - ' + this.b + ' by finding the number that makes ' + this.a + ' when added to ' + this.b + '.');
+        this.setAnswer('' + this.c,0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('1.oa.b.4_1',1.0401,'1.oa.b.4','' );
 */
 var i_1_oa_b_4__1 = new Class(
 {
