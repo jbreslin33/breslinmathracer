c8e8356dd94969abc815aaefe4d5b9eece22eba3
Jim Breslin <jbreslin33@gmail.com>
2015-09-17 09:11:02 -0400

    dev

---
+++ src/src/database/dev_user.sql
@@ -0,0 +1,2 @@
+insert into schools (name, username, password,city,state,zip,email,student_code) VALUES ('j','j','j','j','j','j','j','j');
+insert into users (username, password, first_name, last_name, school_id, core_standards_id) VALUES ('j','j','dev','user',1,'5.oa.a.1');
