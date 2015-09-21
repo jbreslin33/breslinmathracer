d7b1758e6ad91fb5becca6df2a2c053e3342e6ab
Jim Breslin <jbreslin33@gmail.com>
2015-09-21 17:57:23 -0400

    matches

--- src/web/reports/student/matches.php
+++ src/web/reports/student/matches.php
@@ -16,47 +16,7 @@ $conn = dbConnect();
 
 echo "<br>";
 ?>
-	<p><b> ADD TEAM TO MATCH: </p></b>
-
-	
-	<form method="post" action="/web/add/school/addteammatch.php">
-<select name="match_id">
-
-<?php
-$query = "select id, start_time, end_time from matches where school_id = ";
-$query .= $_SESSION["school_id"];
-$query .= " order by start_time desc;";
-
-$result = pg_query($conn,$query);
-$numrows = pg_numrows($result);
-
-for($i = 0; $i < $numrows; $i++)
-{
-        $row = pg_fetch_array($result, $i);
-        echo "<option value=\"$row[0]\">$row[0]</option>";
-}
-?>
-
-</select>
-<select name="team_id">
-
-<?php
-$query = "select id, name from teams where school_id = ";
-$query .= $_SESSION["school_id"];
-$query .= " order by name asc;";
-
-$result = pg_query($conn,$query);
-$numrows = pg_numrows($result); 
-
-for($i = 0; $i < $numrows; $i++)
-{
-        $row = pg_fetch_array($result, $i);
-        echo "<option value=\"$row[0]\">$row[1]</option>";
-}
-?>
-</select>
-	<p><input type="submit" value="UPDATE" /></p>
-	</form>
+	<p><b> MATCHES: </p></b>
 <?php
 echo '<table border=\"1\">';
         echo '<tr>';
