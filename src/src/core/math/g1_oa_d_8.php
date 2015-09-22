6c67c71e22994bf014ff70233a7ac833c29d0076
Jim Breslin <jbreslin33@gmail.com>
2015-09-22 08:13:07 -0400

    fix

--- src/src/core/math/g1_oa_d_8.php
+++ src/src/core/math/g1_oa_d_8.php
@@ -1,5 +1,108 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('1.oa.d.8_12',1.0812,'1.oa.d.8','' );
+*/
+var i_1_oa_d_8__12 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+       	this.parent(sheet,600,50,330,75,100,50,685,80);
+
+        this.mType = '1.oa.d.8_12';
+
+	this.c = -1;
+	while (this.c < 0)
+	{		
+		this.a = Math.floor(Math.random()*9)+1;
+		this.b = Math.floor(Math.random()*9)+1;
+		this.c = parseInt(this.a - this.b);        
+	}
+
+        this.setQuestion('' + 'Determine the unknown number that makes the equation true: ' + this.a + ' - ' + this.b + ' = _');
+        this.setAnswer('' + this.c,0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('1.oa.d.8_11',1.0811,'1.oa.d.8','' );
+*/
+var i_1_oa_d_8__11 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+       	this.parent(sheet,600,50,330,75,100,50,685,80);
+
+        this.mType = '1.oa.d.8_11';
+
+	this.c = -1;
+	while (this.c < 0)
+	{		
+		this.a = Math.floor(Math.random()*9)+1;
+		this.b = Math.floor(Math.random()*9)+1;
+		this.c = parseInt(this.a - this.b);        
+	}
+
+        this.setQuestion('' + 'Determine the unknown number that makes the equation true: ' + this.a + ' - _ = ' + this.c);
+        this.setAnswer('' + this.b,0);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('1.oa.d.8_10',1.0810,'1.oa.d.8','' );
+*/
+var i_1_oa_d_8__10 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+       	this.parent(sheet,600,50,330,75,100,50,685,80);
+
+        this.mType = '1.oa.d.8_10';
+
+	this.c = -1;
+	while (this.c < 0)
+	{		
+		this.a = Math.floor(Math.random()*9)+1;
+		this.b = Math.floor(Math.random()*9)+1;
+		this.c = parseInt(this.a - this.b);        
+	}
+
+        this.setQuestion('' + 'Determine the unknown number that makes the equation true: _ - ' + this.b + ' = ' + this.c);
+        this.setAnswer('' + this.a,0);
+}
+});
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('1.oa.d.8_9',1.0809,'1.oa.d.8','' );
+*/
+var i_1_oa_d_8__9 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+       	this.parent(sheet,600,50,330,75,100,50,685,80);
+
+        this.mType = '1.oa.d.8_9';
+
+	this.c = -1;
+	while (this.c < 0)
+	{		
+		this.a = Math.floor(Math.random()*9)+1;
+		this.b = Math.floor(Math.random()*9)+1;
+		this.c = parseInt(this.a - this.b);        
+	}
+
+        this.setQuestion('' + 'Determine the unknown number that makes the equation true: ' + this.a + ' - ' + this.b + ' = ?');
+        this.setAnswer('' + this.c,0);
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('1.oa.d.8_8',1.0808,'1.oa.d.8','' );
 */
 var i_1_oa_d_8__8 = new Class(
