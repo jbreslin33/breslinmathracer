09b16aec989532b3b22955fb12e40bcd2c60a9f3
Jim Breslin <jbreslin33@gmail.com>
2015-09-23 15:54:30 -0400

    t3

--- src/src/core/math/g1_nbt_b_2_b.php
+++ src/src/core/math/g1_nbt_b_2_b.php
@@ -9,17 +9,14 @@ initialize: function(sheet)
 {
         this.parent(sheet,300,50,175,95,100,50,425,100);
         this.mType = '1.nbt.b.2.b_3';
-        this.ns = new NameSampler();
 
-        var totalDigits = 6;
-
-        this.mAnswerTextBox.setPosition(425,140);
-        this.mAnswerTextBox2.setPosition(595,140);
+        this.mAnswerTextBox.setPosition(425,160);
+        this.mAnswerTextBox2.setPosition(595,160);
         this.mAnswerTextBox.setSize(75,25);
         this.mAnswerTextBox2.setSize(75,25);
 
-        this.mHeadingAnswerLabel.setText('Blue<br> Digit<br> Value');
-        this.mHeadingAnswerLabel2.setText('Red<br> Digit<br> Value');
+        this.mHeadingAnswerLabel.setText('Tens<br>In Tens<br>Place');
+        this.mHeadingAnswerLabel2.setText('Ones<br>In Ones<br>Place');
         this.mHeadingAnswerLabel.setPosition(425,70);
         this.mHeadingAnswerLabel2.setPosition(595,70);
         this.mHeadingAnswerLabel.setSize(50,50);
@@ -27,6 +24,14 @@ initialize: function(sheet)
 
         this.mQuestionLabel.setSize(220,250);
         this.mQuestionLabel.setPosition(225,180);
+        
+	this.tens = 1;
+        this.ones = Math.floor(Math.random()*9)+1;
+        this.a = parseInt(10 + this.ones);
+ 
+	this.setQuestion('How many tens in the tens place and how many ones in the ones place in ' + this.a + '?');
+        this.setAnswer('' + '1',0);
+        this.setAnswer('' + this.ones,1);
 }
 });
 
