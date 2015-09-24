869c7a1301b6f11f70cd9148ea496e015c69a87d
Jim Breslin <jbreslin33@gmail.com>
2015-09-24 11:18:55 -0400

    t4

--- src/src/core/math/g1_nbt_b_3.php
+++ src/src/core/math/g1_nbt_b_3.php
@@ -1,5 +1,69 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('1.nbt.b.3_4',1.1404,'1.nbt.b.3','');
+*/
+var i_1_nbt_b_3__4 = new Class(
+{
+Extends: ThreeButtonItem,
+
+initialize: function(sheet)
+{
+	this.parent(sheet);
+        this.mType = '1.nbt.b.3_4';
+
+        //BUTTON A
+        this.mButtonA.setPosition(380,100);
+        this.mButtonB.setPosition(380,200);
+        this.mButtonC.setPosition(380,300);
+
+	this.onesA = 9;
+	this.tensA = 10;
+	this.onesB = 10;  
+	this.tensB = 9;  
+        
+	while (this.tensB <= this.tensA || this.onesB >= this.onesA)
+	{	
+		this.onesA = Math.floor(Math.random()*9+1);
+		this.tensA = Math.floor(Math.random()*9+1);
+		this.onesB = Math.floor(Math.random()*9+1);
+		this.tensB = Math.floor(Math.random()*9+1);
+	}
+	this.a = (parseInt(this.tensA * 10 + this.onesA));
+	this.b = (parseInt(this.tensB * 10 + this.onesB));
+
+        this.setQuestion('Compare.');
+	if (this.a > this.b) 	
+	{
+        	this.setAnswer('&gt;',0);
+	}
+	if (this.a == this.b) 	
+	{
+        	this.setAnswer('=',0);
+	}
+	if (this.a < this.b) 	
+	{
+        	this.setAnswer('&lt;',0);
+	}
+
+        this.mButtonA.setAnswer('&gt;');
+        this.mButtonB.setAnswer('=');
+        this.mButtonC.setAnswer('&lt;');
+},
+
+createQuestionShapes: function()
+{
+	var shapeA = new Shape(100,100,240,200,this.mSheet.mGame,"","","");
+        var shapeB = new Shape(100,100,530,200,this.mSheet.mGame,"","","");
+
+        shapeA.setText('' + this.a);
+        shapeB.setText('' + this.b);
+
+        this.addQuestionShape(shapeA);
+        this.addQuestionShape(shapeB);
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('1.nbt.b.3_3',1.1403,'1.nbt.b.3','');
 */
 var i_1_nbt_b_3__3 = new Class(
