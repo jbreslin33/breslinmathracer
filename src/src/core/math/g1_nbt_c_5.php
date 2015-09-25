5c633626bab96d866b2bf5a3ff1acf261e45dc44
Jim Breslin <jbreslin33@gmail.com>
2015-09-25 12:30:15 -0400

    t4

--- src/src/core/math/g1_nbt_c_5.php
+++ src/src/core/math/g1_nbt_c_5.php
@@ -1,5 +1,71 @@
 
 /* 
+insert into item_types(id,progression,core_standards_id,description) values ('1.nbt.c.5_4',1.1604,'1.nbt.c.5','');
+*/
+var i_1_nbt_c_5__4 = new Class(
+{
+Extends: NumberPadItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+
+	this.mQuestionLabel.setSize(100,50);
+  	this.mQuestionLabel.setPosition(250,75);
+
+        this.mType = '1.nbt.c.5_4';
+
+        this.mThresholdTime = 5000;
+
+	this.tensA = 0;
+	this.onesA = 0;
+	this.b = 10;
+	this.answer = 100;
+	
+	while (this.answer > 99)
+	{
+		this.tensA = Math.floor(Math.random()*9)+1;
+		this.onesA = Math.floor(Math.random()*9)+1;
+		this.answer = parseInt(this.tensA * 10 + this.onesA + this.b);
+	}
+
+        this.setQuestion('' + this.tensA + '' + this.onesA + ' + 10 =');
+        this.setAnswer('' + this.answer,0);
+}
+});
+
+/* 
+insert into item_types(id,progression,core_standards_id,description) values ('1.nbt.c.5_3',1.1603,'1.nbt.c.5','');
+*/
+var i_1_nbt_c_5__3 = new Class(
+{
+Extends: NumberPadItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+
+	this.mQuestionLabel.setSize(100,50);
+  	this.mQuestionLabel.setPosition(250,75);
+        this.mType = '1.nbt.c.5_3';
+        this.mThresholdTime = 5000;
+
+	this.answer = 101;
+	this.a = 0;
+	this.b = 10;
+
+	while (this.answer > 99)
+	{
+		this.a = Math.floor(Math.random()*9)+1;
+        	this.answer = parseInt(this.a * 10 + 10);
+	}
+
+        this.setQuestion('' + this.b + ' + ' + this.a + '0 = ');
+        this.setAnswer('' + this.answer,0);
+}
+});
+
+/* 
 insert into item_types(id,progression,core_standards_id,description) values ('1.nbt.c.5_2',1.1602,'1.nbt.c.5','');
 */
 var i_1_nbt_c_5__2 = new Class(
--- src/src/core/math/picker_jim.php
+++ src/src/core/math/picker_jim.php
@@ -12,7 +12,7 @@ initialize: function(sheet)
 
 getDev: function()
 {
-	return '1.nbt.c.5_1';
+	return '1.nbt.c.5_4';
 },
 
 getItem: function(id)
