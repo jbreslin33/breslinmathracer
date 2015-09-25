bd795538cdeff6a3be11610c85800f1e0b57c452
Jim Breslin <jbreslin33@gmail.com>
2015-09-25 12:53:45 -0400

    t6

--- src/src/core/math/g1_nbt_c_5.php
+++ src/src/core/math/g1_nbt_c_5.php
@@ -1,5 +1,40 @@
 
 /* 
+insert into item_types(id,progression,core_standards_id,description) values ('1.nbt.c.5_6',1.1606,'1.nbt.c.5','');
+*/
+var i_1_nbt_c_5__6 = new Class(
+{
+Extends: NumberPadItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+
+	this.mQuestionLabel.setSize(100,50);
+  	this.mQuestionLabel.setPosition(250,75);
+
+        this.mType = '1.nbt.c.5_6';
+
+        this.mThresholdTime = 5000;
+
+	this.tensA = 0;
+	this.onesA = 0;
+	this.b = 10;
+	this.answer = 100;
+	
+	while (this.answer > 99)
+	{
+		this.tensA = Math.floor(Math.random()*9)+1;
+		this.onesA = Math.floor(Math.random()*9)+1;
+		this.answer = parseInt(this.tensA * 10 + this.onesA + this.b);
+	}
+
+        this.setQuestion('' + '10 + ' + this.tensA + '' + this.onesA + ' =');
+        this.setAnswer('' + this.answer,0);
+}
+});
+
+/* 
 insert into item_types(id,progression,core_standards_id,description) values ('1.nbt.c.5_5',1.1605,'1.nbt.c.5','');
 */
 var i_1_nbt_c_5__5 = new Class(
--- src/src/core/math/picker_jim.php
+++ src/src/core/math/picker_jim.php
@@ -12,7 +12,7 @@ initialize: function(sheet)
 
 getDev: function()
 {
-	return '1.nbt.c.5_5';
+	return '1.nbt.c.5_6';
 },
 
 getItem: function(id)
