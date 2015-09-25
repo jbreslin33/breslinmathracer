e44715bb509e885c9f218e6afb4853c9a546d0c6
Jim Breslin <jbreslin33@gmail.com>
2015-09-25 12:20:10 -0400

    t2

--- src/src/core/math/g1_nbt_c_5.php
+++ src/src/core/math/g1_nbt_c_5.php
@@ -1,3 +1,37 @@
+
+/* 
+insert into item_types(id,progression,core_standards_id,description) values ('1.nbt.c.5_2',1.1602,'1.nbt.c.5','');
+*/
+var i_1_nbt_c_5__2 = new Class(
+{
+Extends: NumberPadItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet);
+
+	this.mQuestionLabel.setSize(100,50);
+  	this.mQuestionLabel.setPosition(250,75);
+
+        this.mType = '1.nbt.c.5_2';
+
+        this.mThresholdTime = 5000;
+
+	this.a = 0;
+	this.b = 10;
+	this.answer = -1;
+	
+	while (this.answer < 1)
+	{
+		this.a = Math.floor(Math.random()*9)+1;
+		this.answer = parseInt(this.a * 10 - 10);
+	}
+
+        this.setQuestion('' + this.a + '0' + ' - ' + this.b + ' =');
+        this.setAnswer('' + this.answer,0);
+}
+});
+
 /* 
 insert into item_types(id,progression,core_standards_id,description) values ('1.nbt.c.5_1',1.1601,'1.nbt.c.5','');
 */
@@ -11,18 +45,21 @@ initialize: function(sheet)
 
 	this.mQuestionLabel.setSize(100,50);
   	this.mQuestionLabel.setPosition(250,75);
-
-
         this.mType = '1.nbt.c.5_1';
-
         this.mThresholdTime = 5000;
 
-
-	this.a = Math.floor(Math.random()*9)+1;
+	this.answer = 101;
+	this.a = 0;
 	this.b = 10;
 
+	while (this.answer > 99)
+	{
+		this.a = Math.floor(Math.random()*9)+1;
+        	this.answer = parseInt(this.a * 10 + 10);
+	}
+
         this.setQuestion('' + this.a + '0' + ' + ' + this.b + ' =');
-        this.setAnswer('' + parseInt(this.a * 10 + 10),0);
+        this.setAnswer('' + this.answer,0);
 }
 });
 
