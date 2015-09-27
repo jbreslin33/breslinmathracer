9dcb62ad3da0b6ab537b247a2af414465ddce3f0
Jim Breslin <jbreslin33@gmail.com>
2015-09-26 21:30:49 -0400

    t26

--- src/src/core/math/g1_md_b_3.php
+++ src/src/core/math/g1_md_b_3.php
@@ -1,3 +1,64 @@
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('1.md.b.3_26',1.2026,'1.md.b.3','' );
+*/
+var i_1_md_b_3__26 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,600,50,330,75,100,50,685,80);
+
+        this.mNameMachine = new NameMachine();
+        this.mRaphael = Raphael(25,200,200, 200);
+
+        this.ns = new NameSampler();
+        this.mType = '1.md.b.3_26';
+
+        this.hour = Math.floor(Math.random()*12)+1;
+        this.minute = "30";
+
+        this.setQuestion('' + 'What time is it?');
+        this.setAnswer('' + this.hour + ':30',0);
+},
+createQuestionShapes: function()
+{
+        this.mAnalogClock = new AnalogClock (this,100,20,20,APPLICATION.mGame,this.mRaphael,0,0,0,"#000000",.5,false);
+        this.addQuestionShape(this.mAnalogClock);
+        this.mAnalogClock.set(this.hour,this.minute);
+}
+});
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('1.md.b.3_25',1.2025,'1.md.b.3','' );
+*/
+var i_1_md_b_3__25 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,600,50,330,75,100,50,685,80);
+
+        this.mNameMachine = new NameMachine();
+        this.mRaphael = Raphael(25,200,200, 200);
+
+        this.ns = new NameSampler();
+        this.mType = '1.md.b.3_25';
+
+        this.hour = Math.floor(Math.random()*12)+1;
+        this.minute = "00";
+
+        this.setQuestion('' + 'What time is it?');
+        this.setAnswer('' + this.hour + ':00',0);
+},
+createQuestionShapes: function()
+{
+        this.mAnalogClock = new AnalogClock (this,100,20,20,APPLICATION.mGame,this.mRaphael,0,0,0,"#000000",.5,false);
+        this.addQuestionShape(this.mAnalogClock);
+        this.mAnalogClock.set(this.hour,this.minute);
+}
+});
 
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('1.md.b.3_24',1.2024,'1.md.b.3','' );
--- src/src/core/math/picker_jim.php
+++ src/src/core/math/picker_jim.php
@@ -12,7 +12,7 @@ initialize: function(sheet)
 
 getDev: function()
 {
-	return '1.md.b.3_24';
+	return '1.md.b.3_26';
 },
 
 getItem: function(id)
