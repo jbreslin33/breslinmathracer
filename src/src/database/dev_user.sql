a00f339f326bd88ba7005775edda334437d90929
Jim Breslin <jbreslin33@gmail.com>
2015-09-27 19:17:37 -0400

    brian

--- src/src/database/dev_user.sql
+++ src/src/database/dev_user.sql
@@ -1,2 +1,3 @@
 insert into schools (name, username, password,city,state,zip,email,student_code) VALUES ('j','j','j','j','j','j','j','j');
-insert into users (username, password, first_name, last_name, school_id, core_standards_id) VALUES ('j','j','dev','user',1,'5.oa.a.1');
+insert into users (username, password, first_name, last_name, school_id, core_standards_id) VALUES ('j','j','jim','dev',1,'5.oa.a.1');
+insert into users (username, password, first_name, last_name, school_id, core_standards_id) VALUES ('b','b','brian','dev',1,'5.oa.a.1');
