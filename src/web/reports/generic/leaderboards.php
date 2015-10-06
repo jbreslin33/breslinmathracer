04d832ef7a750873db419ee2e40d4e28ab03efe7
Jim Breslin <jbreslin33@gmail.com>
2015-10-06 14:59:31 -0400

    in middle

--- src/web/reports/generic/leaderboards.php
+++ src/web/reports/generic/leaderboards.php
@@ -32,24 +32,9 @@ else
 include(getenv("DOCUMENT_ROOT") . "/src/database/db_connect.php");
 $conn = dbConnect();
 
-$school_id = 0;
 $room_id = 0;
 $category = 0;
 $id = 0;
-
-if (isset($_POST["school_id"]))
-{
-        $school_id = $_POST["school_id"];
-}
-
-else if (isset($_GET['school_id']))
-{
-        $school_id = $_GET['school_id'];
-}
-else
-{
-
-}
 
 if (isset($_POST["room_id"]))
 {
@@ -87,37 +72,10 @@ echo "<br>";
 
 <form method="post" action="/web/reports/generic/leaderboards.php">
 
-<select id="school_id" name="school_id" onchange="loadAgain()">
-<?php
-$query = "select * from schools";
-//$query .= intval($_SESSION["school_id"]);
-$query .= " order by name asc;";
-$result = pg_query($conn,$query);
-$numrows = pg_numrows($result);
-
-echo "<option selected=\"selected\" value=\"0\"> \"All Schools\" </option>";
-
-for($i = 0; $i < $numrows; $i++)
-{
-        $row = pg_fetch_array($result, $i);
-        if ($row[0] == $school_id)
-        {
-                echo "<option selected=\"selected\" value=\"$row[0]\"> $row[1] </option>";
-        }      
-        else
-        {
-                echo "<option value=\"$row[0]\"> $row[1] </option>";
-        }
-}
-?>
-</select>
-
-
 <select id="room_id" name="room_id" onchange="loadAgain()">
 <?php
 $query = "select id, name from rooms where school_id = ";
-//$query .= intval($_SESSION["school_id"]);
-$query .= $school_id;
+$query .= $_SESSION["school_id"];
 $query .= " order by name asc;";
 $result = pg_query($conn,$query);
 $numrows = pg_numrows($result);
@@ -138,9 +96,6 @@ for($i = 0; $i < $numrows; $i++)
 }
 ?>
 </select>
-
-
-
 
 <select id="category" name="category" onchange="loadAgain()">
 <?php
@@ -178,13 +133,11 @@ for($i = 0; $i < sizeof($category_array); $i++)
 <script>
 function loadAgain()
 {
-    	var x = document.getElementById("school_id").value;
     	var y = document.getElementById("room_id").value;
     	var z = document.getElementById("category").value;
-	document.location.href = '/web/reports/generic/leaderboards.php?school_id=' + x + '&room_id=' + y + '&category=' + z; 
+	document.location.href = '/web/reports/generic/leaderboards.php?room_id=' + y + '&category=' + z; 
 }
 </script>
-
 
 <?php
 
@@ -212,11 +165,10 @@ echo '<table border=\"1\">';
         $lastName = '';
         $score = '';
 
-
         $query = "select last_activity, first_name, last_name, ";
 	$query .= $category;
 	$query .= " from users where banned_id = 0 AND school_id = ";
-        $query .= $school_id;
+        $query .= $_SESSION["school_id"];
 	if ($room_id != 0)
 	{
 		$query .= " AND room_id = ";
@@ -256,7 +208,6 @@ echo '<table border=\"1\">';
         echo '</table>';
 }
 ?>
-
 
 </body>
 </html>
