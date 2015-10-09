40806482a515ff7f451683dd5f4a9f02c3826202
Jim Breslin <jbreslin33@gmail.com>
2015-10-08 22:16:18 -0400

    hard code time

--- src/web/reports/generic/daily_homework_check.php
+++ src/web/reports/generic/daily_homework_check.php
@@ -123,7 +123,14 @@ for($s = 0; $s < $numrowsStudents; $s++)
 
 	$queryOne = "select * from evaluations_attempts where user_id = ";
 	$queryOne .= $rowStudents[0];
-	$queryOne .= " AND (evaluations_id = 17 OR evaluations_id = 18) AND ( extract(hour from evaluations_attempts.start_time) < 9 OR extract(hour from evaluations_attempts.start_time) > 14) order by start_time desc;";
+	$queryOne .= " AND (evaluations_id = 17 OR evaluations_id = 18) ";
+
+ 	//$queryOne .= "AND ( extract(hour from evaluations_attempts.start_time) < 9 OR extract(hour from evaluations_attempts.start_time) > 14)";
+	$queryOne .= "AND evaluations_attempts.start_time > '";
+	$queryOne .= "2015-10-01 14:55:00";
+	$queryOne .= "'"; 
+
+ 	$queryOne .= " order by start_time desc;";
 	$resultOne = pg_query($conn,$queryOne);
 	$numrowsOne = pg_numrows($resultOne);
 
