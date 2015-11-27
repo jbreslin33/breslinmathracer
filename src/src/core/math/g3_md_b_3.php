697c71d49171088d94f94916f8a70ad3ff88a89f
Jim Breslin <jbreslin33@gmail.com>
2015-11-27 17:14:15 -0500

    need key

--- src/src/core/math/g3_md_b_3.php
+++ src/src/core/math/g3_md_b_3.php
@@ -22,11 +22,11 @@ initialize: function(sheet)
  	this.totalC = Math.floor(Math.random()*18)+2;  
  	this.totalT = Math.floor(Math.random()*18)+2;  
 
-	this.box1Total = parseInt(this.totalR + this.totalS);
-	this.box2Total = parseInt(this.totalC + this.totalT);
+	this.area1Total = parseInt(this.totalR + this.totalS);
+	this.area2Total = parseInt(this.totalC + this.totalT);
 
 	this.setAnswer('a',0);
-	this.setQuestion('There are 2 different boxes for shapes. Box 1 has enough space for ' + this.box1Total + ' shapes. Box 2 has enough space for ' + this.box2Total + ' shapes.'  );
+	this.setQuestion('There are 2 different areas at the zoo for bears. Area 1 has enough space for ' + this.area1Total + ' bears. Area 2 has enough space for ' + this.area2Total + ' bears.'  );
     
 	this.mButtonB.setAnswer('a');
 	this.mButtonB.setAnswer('b');
@@ -38,14 +38,45 @@ initialize: function(sheet)
 createQuestionShapes: function()
 {
 	//rectangles
-	this.x = 10;
-	for (i = 0; i < this.totalR; i++) 
+
+	this.remainder = this.totalR % 2;
+	this.half = parseInt(this.totalR / 2);
+	
+	APPLICATION.log('t:' + this.totalR);
+	APPLICATION.log('h:' + this.half);
+	APPLICATION.log('r:' + this.remainder);
+	
+	this.x = 100;
+	labelOne = new Shape(100,50,80,105,this.mSheet.mGame,"","","");	
+	labelOne.setText('Black');
+       	this.addQuestionShape(labelOne);
+	
+	labelTwo = new Shape(100,50,80,130,this.mSheet.mGame,"","","");	
+	labelTwo.setText('Brown');
+       	this.addQuestionShape(labelTwo);
+
+	labelThree = new Shape(100,50,80,160,this.mSheet.mGame,"","","");	
+	labelThree.setText('Polar');
+       	this.addQuestionShape(labelThree);
+	
+	labelFour = new Shape(100,50,80,190,this.mSheet.mGame,"","","");	
+	labelFour.setText('Grizzly');
+       	this.addQuestionShape(labelFour);
+
+	for (i = 0; i < this.half; i++) 
 	{
         	this.rArray.push(new Rectangle(40,20,this.x,45,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.3,true));
 		this.x = this.x + 50;
 	}
-	for (i = 0; i < this.totalR; i++) 
+	var i = 0;
+	while (i < this.half)
 	{
+        	this.addQuestionShape(this.rArray[i]);
+		i++;
+	}
+	if (this.remainder == 1)
+	{
+        	this.rArray.push(new Rectangle(20,20,this.x,45,this.mSheet.mGame,this.mRaphael,.5,.5,.5,"#000",.3,true));
         	this.addQuestionShape(this.rArray[i]);
 	}
 }
