a3ca1009efea347e6f24db47d6cae86f72ea486c
Jim Breslin <jbreslin33@gmail.com>
2015-12-07 15:32:42 -0500

    t15

--- src/src/database/patches.sql
+++ src/src/database/patches.sql
@@ -3,3 +3,4 @@ update item_types set active_code = 1 where id = '5.oa.a.1_6';
 update item_types set active_code = 1 where id = '5.oa.a.1_7';
 update item_types set active_code = 1 where id = '5.oa.a.1_8';
 update item_types set active_code = 1 where id = '5.oa.a.1_9';
+update item_types set active_code = 1 where id = '5.oa.a.1_10';
