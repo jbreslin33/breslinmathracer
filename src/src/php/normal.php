49d5233d997408b1c09de07cb6a24c5f97e78df1
Jim Breslin <jbreslin33@gmail.com>
2015-12-23 08:56:34 -0500

    added 16

--- src/src/php/normal.php
+++ src/src/php/normal.php
@@ -149,7 +149,7 @@ public function fillItemAttemptsArray()
                 for ($i = 0; $i < $num; $i++)
                 {
                         $evalID = pg_Result($result,$i,'evaluations_id');
-			//if ($evalID == 1 || $evalID == 3 || $evalID == 4 || $evalID == 5 || $evalID == 6 || $evalID == 7 || $evalID == 8 || $evalID == 9 || $evalID == 10 || $evalID == 11 || $evalID == 12 || $evalID == 13 || $evalID == 14 || $evalID == 15 || $evalID == 16 || $evalID == 17 || $eval18)
+			//if ($evalID == 1 || $evalID == 3 || $evalID == 4 || $evalID == 5 || $evalID == 6 || $evalID == 7 || $evalID == 8 || $evalID == 9 || $evalID == 10 || $evalID == 11 || $evalID == 12 || $evalID == 13 || $evalID == 14 || $evalID == 15 || $evalID == 16 || $evalID == 17 || $evalID == 16 || $eval18)
 			if ($evalID == 1 || $evalID == 18)
 			{
                         	$this->mItemAttemptsTypeArrayOne[] = pg_Result($result,$i,'item_types_id');
