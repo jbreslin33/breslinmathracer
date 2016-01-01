b2ea383bc7e615bd512c41ed57e5db10705e2050
Jim Breslin <jbreslin33@gmail.com>
2016-01-01 09:44:12 -0500

    t24

--- src/src/core/math/g2_oa_a_1.php
+++ src/src/core/math/g2_oa_a_1.php
@@ -12,31 +12,10 @@ initialize: function(sheet)
 
         this.mType = '2.oa.a.1_27';
 
-        // graph coords
-        var startX = 10;
-        var endX = 300;
-        var startY = 10;
-        var endY = 280;
-        var width = endX - startX;
-        var height = endY - startY;
-        var range = [0,10];
-
-        var rX1 = 10;
-        var rY1 = 50;
-        var rX2 = 350;
-        var rY2 = 300;
-
-        this.raphael = Raphael(rX1, rY1, rX2, rY2);
-
-        this.raphaelSizeX = rX2;
-        this.raphaelSizeY = rY2;
-
         this.nm = new NameMachine();
         this.ns = new NameSampler();
 
         var r = Math.floor(Math.random()*3);
-	r = 2;
-        var l = Math.floor(Math.random()*3)+1;
 
        	var a = Math.floor(Math.random()*1000)+100000;
        	var b = Math.floor(Math.random()*1000)+a;
@@ -48,35 +27,6 @@ initialize: function(sheet)
 	g = g + 10;
 
         var answer = '' + g;
-
-        // create ratioTable[rows][cols] to pass in to Table
-        var ratioTable = [['type of things','number of things'],['type of vegetable','number of vegetables'],['type of fruit','number of fruit']];
-
-        var head1 = ratioTable[r][0];
-        var head2 = ratioTable[r][1];
-
-        var tableData   = [[head1,head2],[1,''+a],[2,''+b],[3,''+c]];
-	if (r == 0)
-	{
-		tableData[1][0] = '' + this.ns.mThingOne;
-		tableData[2][0] = '' + this.ns.mThingTwo;
-		tableData[3][0] = '' + this.ns.mThingThree;
-	}
-	if (r == 1)
-	{
-		tableData[1][0] = '' + this.ns.mVegetableOne;
-		tableData[2][0] = '' + this.ns.mVegetableTwo;
-		tableData[3][0] = '' + this.ns.mVegetableThree;
-	}
-	if (r == 2)
-	{
-		tableData[1][0] = '' + this.ns.mFruitOne;
-		tableData[2][0] = '' + this.ns.mFruitTwo;
-		tableData[3][0] = '' + this.ns.mFruitThree;
-	}
-
-        // create Table object
-        var table = new Table (this.mSheet.mGame,this,this.raphael,startX, startY, endX, endY,tableData,rX1,rY1,tableData,"#000000",false);
 
         if (r == 0)
         {
@@ -99,15 +49,6 @@ initialize: function(sheet)
         this.mButtonD.setAnswer('' + f);
         
 	this.shuffle(10);
-  
-	this.mQuestionLabel.setPosition(560,155);
-	this.mQuestionLabel.setSize(350,200);
-
-        this.mButtonA.setPosition(450,250);
-        this.mButtonB.setPosition(650,250);
-        this.mButtonC.setPosition(450,325);
-        this.mButtonD.setPosition(650,325);
-
 }
 });
 
@@ -147,7 +88,6 @@ initialize: function(sheet)
         this.ns = new NameSampler();
 
         var r = Math.floor(Math.random()*3);
-	r = 2;
         var l = Math.floor(Math.random()*3)+1;
 
        	var a = Math.floor(Math.random()*1000)+100000;
