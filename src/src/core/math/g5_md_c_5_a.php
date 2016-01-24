6a3e109059c7e40a5d849bc3ef1ed611d999ced3
Jim Breslin <jbreslin33@gmail.com>
2016-01-24 15:20:46 -0500

    t35

--- src/src/core/math/g5_md_c_5_a.php
+++ src/src/core/math/g5_md_c_5_a.php
@@ -1,60 +1,50 @@
 
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('5.md.c.5.a_4',5.2804,'5.md.c.5','Terra Nova 35');
+insert into item_types(id,progression,core_standards_id,description) values ('5.md.c.5.a_4',5.2804,'5.md.c.5.a','');
 */
 var i_5_md_c_5_a__4 = new Class(
 {
-Extends: TextItem2,
+Extends: TextItem,
 initialize: function(sheet)
 {
- 	this.parent(sheet,575,50,320,75,720,50,380,150);
+        this.parent(sheet,450,200,255,145,100,50,580,100);
+
         this.mType = '5.md.c.5.a_4';
-        this.mNameMachine = new NameMachine();
-        this.mDist1     = this.mNameMachine.getDistanceIncrement();
-        this.mDist2     = this.mNameMachine.getDistanceAbbreviation(this.mDist1);
-  
-	this.mAnswerTextBox.setPosition(225,180);
-        this.mAnswerTextBox2.setPosition(550,140);
-        this.mAnswerTextBox.setSize(75,25);
-        this.mAnswerTextBox2.setSize(75,25);
-
-        this.mHeadingAnswerLabel.setText('What is the volume of the right rectangular prism on the right in ' + this.mDist2 + '?');
-        this.mHeadingAnswerLabel.setPosition(525,70);
-        this.mHeadingAnswerLabel.setSize(200,50);
-
-        this.mQuestionLabel.setSize(220,250);
-        this.mQuestionLabel.setPosition(225,180);
-
+        this.ns = new NameSampler();
         var rx = 10;
         var ry = 120;
         this.mRaphael = Raphael(rx,ry,400,600);
 
-        // position of rubix cube
-        var x = 35;
-        var y = 90;
+// position of rubix cube
+var x = 35;
+var y = 220;
 
-        // dimensions of a single cube
-        var w1 = Math.floor(Math.random()*4)+1;
-        var h1 = Math.floor(Math.random()*4)+1;
-        var d1 = Math.floor(Math.random()*4)+1;
+// dimensions of a single cube
+var w1 = 40;
+var h1 = 40;
+var d1 = 40;
 
-        answer = w1*h1*d1; //Math.floor(Math.random()*3+1);
-        w1 = w1*40;
-        h1 = h1*40;
-        d1 = d1*40;
+// dimensions of rubix cube
+var w2 = Math.floor(Math.random()*3+2);
+var h2 = Math.floor(Math.random()*3+2);
+var d2 = Math.floor(Math.random()*3+2);
 
-        var cube = new Cube(this,this.mSheet.mGame,this.mRaphael,rx,ry,x,y,w1,h1,d1,.5,.5,.5,"#000",1,false,this.mDist2);
-        this.addQuestionShape(cube);
+var rCube = new RubixCube(this,this.mSheet.mGame,this.mRaphael,rx,ry,x,y,w1,h1,d1,w2,h2,d2,.5,.5,.5,"#000",1,false);
 
-	var answer = parseInt(w1 * h1 * d1);
+        answer = w2*h2*d2;
 
-        this.setQuestion('What is the volume of the right rectangular prism on the left in cubic ' + this.mDist2 + '?');
+        this.setQuestion('The right rectangular prism is made of centimeter cubes. What is the volume of the prism in cubic centimeters?');
         this.setAnswer('' + answer,0);
 
-	this.mUserAnswerLabel.setPosition(625,150);
-	this.mCorrectAnswerLabel.setPosition(625,250);
+this.mUserAnswerLabel.setPosition(625,150);
+this.mCorrectAnswerLabel.setPosition(625,250);
+
 }
+
+
 });
+
+
 
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('5.md.c.5.a_3',5.2803,'5.md.c.5','');
