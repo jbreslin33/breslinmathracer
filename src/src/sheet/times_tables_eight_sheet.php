d1693afb4f2da3cd6e5a6a05fb9eafffb67b3142
Jim Breslin <jbreslin33@gmail.com>
2016-02-22 15:16:49 -0500

    8

--- src/src/sheet/times_tables_eight_sheet.php
+++ src/src/sheet/times_tables_eight_sheet.php
@@ -5,7 +5,7 @@ Extends: TimesTablesSheet,
 initialize: function(game)
 {
 	this.parent(game);
-	this.mIDArray.push('3.oa.c.7_88');
+
 	this.mIDArray.push('3.oa.c.7_13');
 	this.mIDArray.push('3.oa.c.7_28');
 	this.mIDArray.push('3.oa.c.7_41');
@@ -14,17 +14,13 @@ initialize: function(game)
 	this.mIDArray.push('3.oa.c.7_68');
 	this.mIDArray.push('3.oa.c.7_73');
 	this.mIDArray.push('3.oa.c.7_74');
-	this.mIDArray.push('3.oa.c.7_76');
-	this.mIDArray.push('3.oa.c.7_98');
 	this.mIDArray.push('3.oa.c.7_12');
 	this.mIDArray.push('3.oa.c.7_27');
 	this.mIDArray.push('3.oa.c.7_40');
 	this.mIDArray.push('3.oa.c.7_51');
 	this.mIDArray.push('3.oa.c.7_60');
 	this.mIDArray.push('3.oa.c.7_67');
-	this.mIDArray.push('3.oa.c.7_73');
 	this.mIDArray.push('3.oa.c.7_75');
-	this.mIDArray.push('3.oa.c.7_72');
 
 	this.mCurrentElement = 0;
 	this.shuffle(50);
