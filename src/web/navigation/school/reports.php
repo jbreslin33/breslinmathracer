0a703aba52117a28e62d0e8c2f1573bd9873e037
Jim Breslin <jbreslin33@gmail.com>
2016-03-02 11:14:53 -0500

    reports

--- src/web/navigation/school/reports.php
+++ src/web/navigation/school/reports.php
@@ -20,14 +20,9 @@ session_start();
 <li><a href="/web/reports/generic/leaderboards.php">Leader Boards</a></li>
 <li><a href="/web/reports/generic/item_attempts_realtime.php">Playing</a></li>
 <li><a href="/web/reports/generic/incorrect_attempts_realtime.php">Live Incorrect</a></li>
-<li><a href="/web/reports/generic/tests.php">Evaluations</a></li>
+<li><a href="/web/reports/generic/tests.php">Evaluation Answers and Questions</a></li>
 <li><a href="/web/reports/generic/grades.php">Grades</a></li>
-<li><a href="/web/reports/generic/class_grades.php">Class Grades</a></li>
-<li><a href="/web/reports/generic/homeworks.php">Daily Homework</a></li>
-<li><a href="/web/reports/generic/homework_grades.php">Homework Grades</a></li>
-<li><a href="/web/reports/generic/homework_class_grades.php">Class Homework Grades</a></li>
-<li><a href="/web/reports/generic/daily_homework_check.php">Daily Homework Check</a></li>
-<li><a href="/web/reports/generic/any_homework_check.php">Any Homework Check</a></li>
+<li><a href="/web/reports/generic/any_homework_check.php">Quick Homework Check</a></li>
 </ul>
 
 </body>
