8380f380e5330adcddeaee05c7a4bb138ac612f1
Jim Breslin <jbreslin33@gmail.com>
2016-03-13 19:29:48 -0400

    t3

--- src/src/core/math/g6_ee_a_1.php
+++ src/src/core/math/g6_ee_a_1.php
@@ -1,19 +1,20 @@
+
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('6.ee.a.1_1',6.2301,'6.ee.a.1','');
+insert into item_types(id,progression,core_standards_id,description) values ('6.ee.a.1_3',6.2303,'6.ee.a.1','');
 */
-var i_6_ee_a_1__1 = new Class(
+var i_6_ee_a_1__3 = new Class(
 {
 Extends: TextItem,
 initialize: function(sheet)
 {
         this.parent(sheet,300,50,175,75,100,50,425,100);
-        this.mType = '6.ee.a.1_1';
+        this.mType = '6.ee.a.1_3';
 
         var x = Math.floor(Math.random()*9)+1;
         var y = Math.floor(Math.random()*8)+2;
 
-        this.setQuestion('' + 'In the expression ' + x + '<sup>' + y + '</sup>' + ' what is the base?');
-        this.setAnswer('' + x,0);
+        this.setQuestion('' + 'In the expression ' + x + '<sup>' + y + '</sup>' + ' what is the exponent?');
+        this.setAnswer('' + y,0);
 }
 });
 
@@ -35,3 +36,23 @@ initialize: function(sheet)
         this.setAnswer('' + y,0);
 }
 });
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('6.ee.a.1_1',6.2301,'6.ee.a.1','');
+*/
+var i_6_ee_a_1__1 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,75,100,50,425,100);
+        this.mType = '6.ee.a.1_1';
+
+        var x = Math.floor(Math.random()*9)+1;
+        var y = Math.floor(Math.random()*8)+2;
+
+        this.setQuestion('' + 'In the expression ' + x + '<sup>' + y + '</sup>' + ' what is the base?');
+        this.setAnswer('' + x,0);
+}
+});
+
