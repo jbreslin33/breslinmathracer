4693012981de1be7c2309b87066d55ccec09f322
Jim Breslin <jbreslin33@gmail.com>
2016-03-13 19:41:05 -0400

    t4

--- src/src/core/math/g6_ee_a_1.php
+++ src/src/core/math/g6_ee_a_1.php
@@ -1,5 +1,30 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('6.ee.a.1_4',6.2304,'6.ee.a.1','');
+*/
+var i_6_ee_a_1__4 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,75,100,50,425,100);
+        this.mType = '6.ee.a.1_4';
+	this.nm = new NameMachine();
+
+        var x = 0;
+        var y = 0;
+	while (x == y)
+	{
+        	var x = Math.floor(Math.random()*26);
+        	var y = Math.floor(Math.random()*26);
+	}
+
+        this.setQuestion('' + 'In the expression ' + this.nm.mLowerLetterArray[x] + '<sup>' + this.nm.mLowerLetterArray[y] + '</sup>' + ' what is the base?');
+        this.setAnswer('' + this.nm.mLowerLetterArray[x],0);
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('6.ee.a.1_3',6.2303,'6.ee.a.1','');
 */
 var i_6_ee_a_1__3 = new Class(
