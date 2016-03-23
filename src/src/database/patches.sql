f3e0f0b7a53182559b6d7676be637d68b47399c2
Jim Breslin <jbreslin33@gmail.com>
2016-03-23 13:37:27 -0400

    db

--- src/src/database/patches.sql
+++ src/src/database/patches.sql
@@ -1,2 +1,3 @@
 update item_types set active_code = 2 where id = '5.oa.a.2_0_11';
 update item_types set active_code = 2 where id = '5.oa.a.2_0_10';
+update item_types set active_code = 2 where id = '6.rp.a.3.a_3';
