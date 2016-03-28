524049b0fabbe5d1e2bda65a2f5d6ced56b3c2a0
Jim Breslin <jbreslin33@gmail.com>
2016-03-28 13:04:47 -0400

    t2

--- src/src/core/math/g6_ee_a_2.php
+++ src/src/core/math/g6_ee_a_2.php
@@ -1,5 +1,26 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('6.ee.a.2_2',6.2502,'6.ee.a.2','');
+*/
+var i_6_ee_a_2__2 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,75,100,50,425,100);
+        this.mType = '6.ee.a.2_2';
+        this.ns = new NameSampler();
+
+        var x = Math.floor(Math.random()*8)+2;
+	var n = this.ns.mLowerLetterOne;
+
+        this.setQuestion('' + 'Write an expression that represents the product of a number and ' + x + '. Let ' + n + ' = number.'  );
+
+        this.setAnswer('' + n + '*' + x ,0);
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('6.ee.a.2_1',6.2501,'6.ee.a.2','');
 */
 var i_6_ee_a_2__1 = new Class(
