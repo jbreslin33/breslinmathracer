ed760d6418ed51eed7888fc26a1e1a2326a41a05
Jim Breslin <jbreslin33@gmail.com>
2016-03-30 09:13:33 -0400

    t7

--- src/src/core/math/g6_ee_a_2.php
+++ src/src/core/math/g6_ee_a_2.php
@@ -1,4 +1,26 @@
 
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('6.ee.a.2_7',6.2507,'6.ee.a.2','');
+*/
+var i_6_ee_a_2__7 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,75,100,50,425,100);
+        this.mType = '6.ee.a.2_7';
+        this.ns = new NameSampler();
+
+        var x = Math.floor(Math.random()*8)+2;
+	var n = this.ns.mLowerLetterOne;
+
+        this.setQuestion('' + 'Write an expression that represents ' + n + ' divided by ' + x + '.');
+
+        this.setAnswer('' + n + '/' + x ,0);
+}
+});
+
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('6.ee.a.2_6',6.2506,'6.ee.a.2','');
 */
--- src/src/core/math/picker_jim.php
+++ src/src/core/math/picker_jim.php
@@ -12,7 +12,7 @@ initialize: function(sheet)
 
 getDev: function()
 {
-	return '6.ee.a.2_6';
+	return '6.ee.a.2_7';
 },
 
 getItem: function(id)
