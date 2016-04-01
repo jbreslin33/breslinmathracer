f8a9b7acf7581f702784400817fd7676a46898c7
Jim Breslin <jbreslin33@gmail.com>
2016-03-31 20:39:09 -0400

    fractions

--- src/src/core/math/g6_ee_a_2_c.php
+++ src/src/core/math/g6_ee_a_2_c.php
@@ -1,5 +1,35 @@
 
 /*
+insert into item_types(id,progression,core_standards_id,description) values ('6.ee.a.2.c_4',6.2704,'6.ee.a.2.c','');
+*/
+var i_6_ee_a_2_c__4 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,75,100,50,425,100);
+        this.mType = '6.ee.a.2.c_4';
+        this.ns = new NameSampler();
+   
+        var x = this.ns.mLowerLetterOne;
+	var y = Math.floor(Math.random()*8)+2;
+	var z = Math.floor(Math.random()*8)+2;
+	
+	var n = Math.floor(Math.random()*8)+2;
+	var d = Math.floor(Math.random()*8)+2;
+
+ 	var fA = new Fraction(n,d,false);
+ 	var fB = new Fraction(y,1,false);
+
+	var fC = fA.subtract(fB);
+
+        this.setQuestion('' + 'Evaluation the expression ' + x + ' - ' + fA.getString() + ' where ' + x + ' = ' + y);
+
+	this.setAnswer('' + fC.getOneLineString(),0);
+}
+});
+
+/*
 insert into item_types(id,progression,core_standards_id,description) values ('6.ee.a.2.c_3',6.2703,'6.ee.a.2.c','');
 */
 var i_6_ee_a_2_c__3 = new Class(
--- src/src/core/math/picker_jim.php
+++ src/src/core/math/picker_jim.php
@@ -12,7 +12,7 @@ initialize: function(sheet)
 
 getDev: function()
 {
-	return '6.ee.a.2.c_3';
+	return '6.ee.a.2.c_4';
 },
 
 getItem: function(id)
