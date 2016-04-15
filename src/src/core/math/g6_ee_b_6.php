113adedddf176670163084e125098fbdb1ac3c30
Brian Breslin <bbreslin99@gmail.com>
2016-04-14 22:18:25 -0400

    more items for 6.ee.b.6

--- src/src/core/math/g6_ee_b_6.php
+++ src/src/core/math/g6_ee_b_6.php
@@ -14,13 +14,41 @@ initialize: function(sheet)
 
         var c = a + b;
 
-        this.setQuestion('Write an equation that represents the following situation:</br></br>Joey ate ' + a + ' red grapes. After that he ate some green grapes. In all he ate ' + c + ' grapes. How many green grapes did he eat? (Use n as variable)');
+        this.setQuestion('Write an equation that represents the following situation (Use n as variable):</br></br>Joey ate ' + a + ' red grapes. After that he ate some green grapes. In all he ate ' + c + ' grapes. How many green grapes did he eat?');
               
         this.setAnswer('' + a + '+n=' + c,0);
-        this.setAnswer('22=n+6',1);
-        this.setAnswer('6+n=22',2);
-        this.setAnswer('22=6+n',3);
+        this.setAnswer('' + c + '=' + a + '+n',1);
+        this.setAnswer('' + c + '= n+' + a,2);
+        this.setAnswer('' + 'n+' + a + '=' + c,3);
 
+this.mCorrectAnswerLabel.setPosition(530,300);
+this.mUserAnswerLabel.setPosition(230,300);
+
+}
+});
+
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('6.ee.a.6_2',6.3202,'6.ee.a.3','');
+*/
+var i_6_ee_a_6__2 = new Class(
+{
+Extends: TextItem,
+initialize: function(sheet)
+{
+        this.parent(sheet,300,50,175,75,100,50,425,100);
+        this.mType = '6.ee.a.6_2';
+
+        var a = Math.floor(Math.random()*4+3);
+        var b = Math.floor(Math.random()*4+3);
+
+        var c = a * b;
+
+        this.setQuestion('Write an equation that represents the following situation (Use n as variable):</br></br>Mary earned $' + a + ' for each box of cookies she sold. She sold several boxes. In all Mary earned $' + c + '. How many boxes did Mary sell?');
+              
+        this.setAnswer('' + a + 'n=' + c,0);
+        this.setAnswer('' + c + '=' + a + 'n',1);
+       
 this.mCorrectAnswerLabel.setPosition(530,300);
 this.mUserAnswerLabel.setPosition(230,300);
 
