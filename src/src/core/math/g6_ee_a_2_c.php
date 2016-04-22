82fbc43c4bc0c787e6be1809cad0c4b48a0fefba
Jim Breslin <jbreslin33@gmail.com>
2016-04-21 21:25:37 -0400

    t9 rfm

--- src/src/core/math/g6_ee_a_2_c.php
+++ src/src/core/math/g6_ee_a_2_c.php
@@ -1,14 +1,14 @@
 
 /*
-insert into item_types(id,progression,core_standards_id,description) values ('6.ee.a.2.c_8',6.2708,'6.ee.a.2.c','');
+insert into item_types(id,progression,core_standards_id,description) values ('6.ee.a.2.c_9',6.2709,'6.ee.a.2.c','');
 */
-var i_6_ee_a_2_c__8 = new Class(
+var i_6_ee_a_2_c__9 = new Class(
 {
 Extends: TextItem,
 initialize: function(sheet)
 {
         this.parent(sheet,300,50,175,75,100,50,425,100);
-        this.mType = '6.ee.a.2.c_8';
+        this.mType = '6.ee.a.2.c_9';
 
         var a = Math.floor(Math.random()*8)+2;
         var b = Math.floor(Math.random()*8)+2;
