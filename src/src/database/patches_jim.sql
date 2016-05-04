bf0dcb80dfedda342599dd3d31612f29d4533f8a
Jim Breslin <jbreslin33@gmail.com>
2016-05-03 22:45:14 -0400

    5th db fixes and bugfix

--- src/src/database/patches_jim.sql
+++ src/src/database/patches_jim.sql
@@ -2,3 +2,6 @@ insert into evaluations (id,description) values (19,'The Super Izzy');
 alter table users add alltimesuperizzy integer;
 insert into evaluations (id,description) values (20,'Basic Skills Fourth');
 alter table users add alltimebasicskillsfourth integer;
+insert into evaluations (id,description) values (21,'Basic Skills Fifth');
+alter table users add alltimebasicskillsfifth integer;
+update evaluations set description = 'Basic Skills 5th' where id = 21;
--- src/src/php/application/core_application.php
+++ src/src/php/application/core_application.php
@@ -46,7 +46,7 @@ if ($APPLICATION->mCode == 217)
 	error_log($APPLICATION->mLoginStudent->mLastName);
 }
 //add_game_K
-if ($APPLICATION->mCode == 1 || $APPLICATION->mCode == 3 || $APPLICATION->mCode == 4 || $APPLICATION->mCode == 5 || $APPLICATION->mCode == 6 || $APPLICATION->mCode == 7 || $APPLICATION->mCode == 8 || $APPLICATION->mCode == 9 || $APPLICATION->mCode == 10 || $APPLICATION->mCode == 12 || $APPLICATION->mCode == 13 || $APPLICATION->mCode == 14 || $APPLICATION->mCode == 15 || $APPLICATION->mCode == 16 || $APPLICATION->mCode == 17 || $APPLICATION->mCode == 18 || $APPLICATION->mCode == 19 || $APPLICATION->mCode == 20 || $evalID == 21)
+if ($APPLICATION->mCode == 1 || $APPLICATION->mCode == 3 || $APPLICATION->mCode == 4 || $APPLICATION->mCode == 5 || $APPLICATION->mCode == 6 || $APPLICATION->mCode == 7 || $APPLICATION->mCode == 8 || $APPLICATION->mCode == 9 || $APPLICATION->mCode == 10 || $APPLICATION->mCode == 12 || $APPLICATION->mCode == 13 || $APPLICATION->mCode == 14 || $APPLICATION->mCode == 15 || $APPLICATION->mCode == 16 || $APPLICATION->mCode == 17 || $APPLICATION->mCode == 18 || $APPLICATION->mCode == 19 || $APPLICATION->mCode == 20 || $APPLICATION->mCode == 21)
 {
         $APPLICATION->mDataArray[] = $APPLICATION->mCode;
         $APPLICATION->mDataArray[] = $_GET["itemtypesid"];
