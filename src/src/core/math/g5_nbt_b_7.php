9a98e3e146b99a9c30eddb196ce12e5f78c22374
Jim Breslin <jbreslin33@gmail.com>
2016-08-05 15:41:40 -0400

    t38 d

--- src/src/core/math/g5_nbt_b_7.php
+++ src/src/core/math/g5_nbt_b_7.php
@@ -15,7 +15,7 @@ xx. xx.00 t35
 -------------------
 .xx x.xx0 t36  
 .xx xx.x0 t37  
-x.x xx.x0   
+x.x xx.x0 t38  
 x.x x.xx0   
 xx. x.xx0  
 xx. xx.x0  
@@ -29,6 +29,56 @@ xx. xx.xx
                 //q = parseInt( (a * 1000) + (b * 100) + (c * 10) + d);
 -------------------
 */
+
+/*
+insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.7_38',5.1138,'5.nbt.b.7','');
+*/
+
+var i_5_nbt_b_7__38 = new Class(
+{
+Extends: TextItem,
+
+initialize: function(sheet)
+{
+        this.parent(sheet,575,50,320,75,720,50,380,150);
+
+        this.mType = '5.nbt.b.7_38';
+
+        this.ns = new NameSampler();
+        var a = 0;
+
+	var b = 0;
+        var c = 0;
+
+        var d = 0;
+        var e = 0;
+        var f = 0;
+        var x = 0;
+        var y = 0;
+        var z = 0;
+	var r = 1;
+
+	//x.x xx.x0 t38  
+
+        while(y % x != 0 || z < 99 || r == 0 || x % 10 == 0 || y % 100 == 0)
+        {
+                x = Math.floor((Math.random()*90)+10);
+                y = Math.floor((Math.random()*890)+100);
+		y = y * 10;
+		z = parseInt(y / x);      
+		r = parseInt(z%10);	 
+	}                
+
+	y = y / 100;
+
+	x = x / 10; 
+
+	z = z / 10;
+
+        this.setQuestion('Find the Quotient: ' + y + ' &divide ' + x);
+        this.setAnswer('' + z,0);
+}
+});
 
 /*
 insert into item_types(id,progression,core_standards_id,description) values ('5.nbt.b.7_37',5.1137,'5.nbt.b.7','');
