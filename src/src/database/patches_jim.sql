b0188a37d305a8ef41a66e47ef5f36cc92de884b
Jim Breslin <jbreslin33@gmail.com>
2016-09-12 17:08:45 -0400

    1 nbt works so should all thru 3rd

--- src/src/database/patches_jim.sql
+++ src/src/database/patches_jim.sql
@@ -2,7 +2,16 @@
 update evaluations SET (questions,score_needed,standard_jump_id,progression) = (13,13,'k.oa.a.4',1) where description = 'k_cc';
 update evaluations SET (questions,score_needed,standard_jump_id,progression) = (18,18,'k.oa.a.5',2) where description = 'k_oa_a_4';
 update evaluations SET (questions,score_needed,standard_jump_id,progression) = (20,20,'1.oa.a.1',3) where description = 'k_oa_a_5';
+
 update evaluations SET (questions,score_needed,standard_jump_id,progression) = (39,39,'1.oa.c.6',4) where description = '1_oa_b_3';
+update evaluations SET (questions,score_needed,standard_jump_id,progression) = (70,70,'1.oa.c.6',5) where description = '1_oa_c_6';
+update evaluations SET (questions,score_needed,standard_jump_id,progression) = (12,12,'2.oa.a.1',6) where description = '1_nbt';
+
+update evaluations SET (questions,score_needed,standard_jump_id,progression) = (36,36,'2.nbt.a.1',7) where description = '2_oa_b_2';
+update evaluations SET (questions,score_needed,standard_jump_id,progression) = (13,13,'3.oa.a.1',8) where description = '2_nbt';
+
+update evaluations SET (questions,score_needed,standard_jump_id,progression) = (64,64,'3.nbt.a.1',9) where description = '3_oa_c_7';
+update evaluations SET (questions,score_needed,standard_jump_id,progression) = (13,13,'4.oa.a.1',10) where description = '3_nbt';
 
 
 --just drop all milestones 
