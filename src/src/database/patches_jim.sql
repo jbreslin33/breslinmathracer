faf1d606223b41eaa34e524841ea3c97e1a536f1
Jim Breslin <jbreslin33@gmail.com>
2016-10-17 19:22:59 -0400

    g6 ms reset

--- src/src/database/patches_jim.sql
+++ src/src/database/patches_jim.sql
@@ -166,6 +166,11 @@ update users set core_standards_overide_id = '5.oa.a.1' where first_name = 'Bray
 update users set core_grades_id = 5 where first_name = 'Kirian' and last_name = 'Vargas';
 update users set core_standards_overide_id = '5.oa.a.1' where first_name = 'Kirian' and last_name = 'Vargas'; 
 
+update users set g6_rp = 0;
+update users set g6_ns = 0;
+update users set g6_ee = 0;
+update users set g6_g = 0;
+update users set g6_sp = 0;
 
 
 
