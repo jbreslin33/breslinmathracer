80223ec758448dc6a583600d970d8347879e42fb
Jim Breslin <jbreslin33@gmail.com>
2016-11-16 16:02:42 -0500

    ms

--- src/web/reports/student/milestone_attempts.php
+++ src/web/reports/student/milestone_attempts.php
@@ -54,7 +54,7 @@ echo '<table border=\"1\">';
 	$query = "select evaluations_attempts.start_time, evaluations.description, evaluations.score_needed, count(*), case when count(*) = evaluations.score_needed THEN 1 ELSE 0 END from item_attempts join evaluations_attempts on evaluations_attempts.id=item_attempts.evaluations_attempts_id join evaluations on evaluations.id=evaluations_attempts.evaluations_id join users on evaluations_attempts.user_id=users.id where evaluations_id != 1 AND user_id = ";
 
         $query .= $_SESSION["user_id"];
-        $query .= " group by evaluations_attempts, score_needed, evaluations_attempts.start_time, evaluations.description, users.first_name, users.last_name order by evaluations_attempts.start_time desc limit 20;";
+        $query .= " group by evaluations_attempts, score_needed, evaluations_attempts.start_time, evaluations.description, users.first_name, users.last_name order by evaluations_attempts.start_time desc;";
 
        	$result = pg_query($conn,$query);
         $numrows = pg_numrows($result);
@@ -62,16 +62,20 @@ echo '<table border=\"1\">';
         for($i = 0; $i < $numrows; $i++)
         {
                 $row = pg_fetch_array($result, $i);
-                $match_id = $row[0];
-                $team_name = $row[1];
-                $start_time = $row[2];
-                $end_time = $row[3];
-                $score = $row[4];
+                
+		if ($row[4] == 1)
+                {
+                        echo '<tr bgcolor="green">';
+                }
+                else
+                {
+                        echo '<tr bgcolor="red">';
+                }
+                //echo '';
 
-                echo '<tr>';
  		
 		echo '<td>';
-                echo $row[0];
+                echo mb_strimwidth($row[0], 0, 19, "");
                 echo '</td>';
 
                 echo '<td>';
