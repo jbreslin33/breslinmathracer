ed2d7bc25b3ea3f2de396d0da0bcc0ba11158a06
Jim Breslin <jbreslin33@gmail.com>
2016-12-19 23:28:12 -0500

    dont know

--- src/web/reports/generic/school_milestones.php
+++ src/web/reports/generic/school_milestones.php
@@ -306,8 +306,8 @@ function calc_raw_grade_new($core_grades_id,&$row)
         return $rg;
 }
 
-//ms
-        $query_m = "select distinct sub.id, sub.first_name, sub.last_name, sub.description, sub.progression, sub.room_id FROM ( select users.id, users.first_name, users.last_name, users.room_id, evaluations.description, evaluations.progression, evaluations.score_needed,   COUNT(CASE WHEN item_attempts.transaction_code = 2 then 1 ELSE NULL END) as incorrect,
+//m
+        $query_m = "select distinct sub.id, sub.first_name, sub.last_name, sub.description, sub.progression, sub.room_id FROM ( select users.id, users.first_name, users.last_name, users.room_id, evaluations.description, evaluations.progression, evaluations.score_needed,  COUNT(CASE WHEN item_attempts.transaction_code = 2 then 1 ELSE NULL END) as incorrect, 
     COUNT(CASE WHEN item_attempts.transaction_code = 1 then 1 ELSE NULL END) as correct   from evaluations_attempts join users on evaluations_attempts.user_id=users.id JOIN item_attempts ON item_attempts.evaluations_attempts_id=evaluations_attempts.id JOIN evaluations ON evaluations.id=evaluations_attempts.evaluations_id where evaluations_attempts.start_time > '2016-09-10 09:28:27.777635' AND evaluations_attempts.evaluations_id != 1 AND (";
 
 	for($r = 0; $r < $num_rooms; $r++)
