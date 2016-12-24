a3feba1610197e022376f51132b92f1190e10a81
Jim Breslin <jbreslin33@gmail.com>
2016-12-24 14:10:12 -0500

    main menu

--- src/src/core/math/main_menu.php
+++ src/src/core/math/main_menu.php
@@ -5,15 +5,11 @@ Extends: TextItem,
         {
                 this.parent(sheet,300,50,175,95, 200,50,475,100);
 
-                this.mType = '6.rp.a.1_17';
-                this.ns = new NameSampler();
+                this.mType = '';
 
-                var a = Math.floor((Math.random()*20)+2);
-                var b = Math.floor((Math.random()*20)+1);
+                this.setQuestion('Welcome to Mathcore! Menu is in drop down at top left.');
 
-                this.setQuestion('' + this.ns.mNameOne + ' has some balls. The ratio of his ' + this.ns.mColorOne + ' balls to ' + this.ns.mNameMachine.getPronoun(this.ns.mNameOne,0,1) + ' ' + this.ns.mColorTwo + ' balls is ' + a + ' to ' + b + '. Write the ratio with a colon of ' + this.ns.mColorOne + ' balls to ' + this.ns.mColorTwo + ' balls.');
-
-                this.setAnswer('' + a + ':' + b ,0);
+                this.setAnswer('',0);
         }
 });
 
