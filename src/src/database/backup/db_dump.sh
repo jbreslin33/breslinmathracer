e3ac650d9b189db45324a9122a45bba7032aad60
Jim Breslin <jbreslin33@gmail.com>
2017-01-03 23:02:48 -0500

    backup is not right

--- src/src/database/backup/db_dump.sh
+++ src/src/database/backup/db_dump.sh
@@ -1,3 +1,4 @@
 #OLD sudo -u postgres pg_dump -Fc jamesanthonybreslin > src/database/jamesanthonybreslin.dump
-sudo -u postgres pg_dump jamesanthonybreslin | split -b 40m - src/database/backup/db.dump
 #pg_dump dbname | split -b 1m - filename
+sudo rm src/database/backup/db.dump
+sudo -u postgres pg_dump jamesanthonybreslin | split -b 40m - src/database/backup/db.dump
--- src/src/database/db_restore.sh
+++
@@ -1,6 +0,0 @@
-#sudo -u postgres psql -d jamesanthonybreslin -f src/database/drop_db.sql
-#sudo -u postgres dropdb jamesanthonybreslin
-#sudo -u postgres pg_restore -C -d postgres src/database/jamesanthonybreslin.dump
-
-#createdb jamesanthonybreslin cat src/database/backup/jamesanthonybreslin* | psql jamesanthonybreslin
-rm db.dump 
