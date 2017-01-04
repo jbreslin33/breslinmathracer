1a677cb20c7c347f59943f9212bb5764e4bad297
Jim Breslin <jbreslin33@gmail.com>
2017-01-04 16:17:09 -0500

    backup

--- src/src/database/backup/db_dump.sh
+++ src/src/database/backup/db_dump.sh
@@ -1,4 +1,19 @@
 #OLD sudo -u postgres pg_dump -Fc jamesanthonybreslin > src/database/jamesanthonybreslin.dump
 #pg_dump dbname | split -b 1m - filename
-sudo rm src/database/backup/db.dump
-sudo -u postgres pg_dump jamesanthonybreslin | split -b 40m - src/database/backup/db.dump
+
+#remove files
+#sudo rm *.dump*
+#sudo rm db.sql
+
+#make backup and split it into files
+sudo -u postgres pg_dump jamesanthonybreslin > full.sql 
+
+split -b 40m full.sql mini 
+
+
+#cat files into 1 big one db.sql
+#cat miniaa  miniab  miniac  miniad  miniae  miniaf  miniag  miniah  miniai  miniaj  miniak > db.sql
+
+#split big work file db.sql into small permanent files to put to git
+#split -b 40m db.sql big.data
+
