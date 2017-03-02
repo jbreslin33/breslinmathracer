53dc216ff641e31a02083736843db2692a86c030
Jim Breslin <jbreslin33@gmail.com>
2017-03-02 00:21:46 -0500

    live

--- src/web/reports/generic/live_evaluations.php
+++ src/web/reports/generic/live_evaluations.php
@@ -39,6 +39,10 @@ echo '<table border=\"1\">';
 
 
         echo '<tr>';
+        echo '<td>first_name';
+        echo '</td>';
+        echo '<td>last_name';
+        echo '</td>';
         echo '<td>start time';
         echo '</td>';
         echo '<td>description';
@@ -51,8 +55,8 @@ echo '<table border=\"1\">';
         echo '</td>';
         echo '</tr>';
 
-	$query = "select evaluations_attempts.start_time, evaluations.description, evaluations.score_needed, count(*), case when count(*) = evaluations.score_needed THEN 1 ELSE 0 END from item_attempts join evaluations_attempts on evaluations_attempts.id=item_attempts.evaluations_attempts_id join evaluations on evaluations.id=evaluations_attempts.evaluations_id join users on evaluations_attempts.user_id=users.id where evaluations_id != 1 ";
-        $query .= " AND item_attempts.transaction_code != 0 AND item_attempts.transaction_code != 2 group by evaluations_attempts, score_needed, evaluations_attempts.start_time, evaluations.description, users.first_name, users.last_name order by evaluations_attempts.start_time desc limit 20;";
+	$query = "select (select first_name from users where id = evaluations_attempts.user_id) as captain_firstname, (select last_name from users where id = evaluations_attempts.user_id) as captain_lastname, evaluations_attempts.start_time, evaluations.description, evaluations.score_needed, count(*), case when count(*) = evaluations.score_needed THEN 1 ELSE 0 END from item_attempts join evaluations_attempts on evaluations_attempts.id=item_attempts.evaluations_attempts_id join evaluations on evaluations.id=evaluations_attempts.evaluations_id join users on evaluations_attempts.user_id=users.id where evaluations_id != 1 ";
+        $query .= " AND item_attempts.transaction_code != 0 AND item_attempts.transaction_code != 2 group by evaluations_attempts, score_needed, evaluations_attempts.start_time, evaluations.description, users.first_name, users.last_name, captain_firstname, captain_lastname order by evaluations_attempts.start_time desc limit 20;";
 
        	$result = pg_query($conn,$query);
         $numrows = pg_numrows($result);
@@ -71,25 +75,32 @@ echo '<table border=\"1\">';
                 {
                         echo '<tr bgcolor="red">';
                 }
- 		
-		echo '<td>';
-                echo mb_strimwidth($row[0], 0, 19, "");
-                echo '</td>';
-
                 echo '<td>';
+                echo $row[0];
+                echo '</td>';
+                
+		echo '<td>';
                 echo $row[1];
                 echo '</td>';
-
-                echo '<td>';
-                echo $row[2];
+ 		
+		echo '<td>';
+                echo mb_strimwidth($row[2], 0, 19, "");
                 echo '</td>';
 
                 echo '<td>';
                 echo $row[3];
-               	echo '</td>';
+                echo '</td>';
 
                 echo '<td>';
                 echo $row[4];
+                echo '</td>';
+
+                echo '<td>';
+                echo $row[5];
+               	echo '</td>';
+
+                echo '<td>';
+                echo $row[6];
                 echo '</td>';
 
                 echo '</tr>';
