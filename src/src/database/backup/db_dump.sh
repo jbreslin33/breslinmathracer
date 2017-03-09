be6935051ee56edf52a1423d071c00ac5faab4ab
Jim Breslin <jbreslin33@gmail.com>
2017-03-08 21:40:25 -0500

    backup

--- src/src/database/backup/db_dump.sh
+++ src/src/database/backup/db_dump.sh
@@ -1,1 +1,3 @@
-sudo -u postgres pg_dump -Fc jamesanthonybreslin > src/database/backup/presplit.dump
+sudo -u postgres pg_dump -Fc jamesanthonybreslin > src/database/backup/full
+split -b 40m src/database/backup/full src/database/backup/partial
+git add src/database/backup/partial*
