1582a21e194f83e44af5a9ba920f0ff5f2e64190
Jim Breslin <jbreslin33@gmail.com>
2017-03-08 19:11:46 -0500

    db files

--- src/src/database/backup/db_restore.sh
+++ src/src/database/backup/db_restore.sh
@@ -1,3 +1,3 @@
 sudo -u postgres psql -d jamesanthonybreslin -f src/database/backup/drop_db.sql
 sudo -u postgres dropdb jamesanthonybreslin
-sudo -u postgres pg_restore -C -d postgres src/database/backup/jamesanthonybreslin.dump
+sudo -u postgres pg_restore -C -d postgres src/database/backup/db.dump
