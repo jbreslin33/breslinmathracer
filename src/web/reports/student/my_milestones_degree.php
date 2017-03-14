b3b1d96f4b2f65780a4576c5d93c12ce29e0f488
Jim Breslin <jbreslin33@gmail.com>
2017-03-14 19:37:05 -0400

    edit ms

--- src/web/reports/student/my_milestones_degree.php
+++ src/web/reports/student/my_milestones_degree.php
@@ -121,7 +121,7 @@ echo '<table border=\"1\">';
         echo '</tr>';
 
         //------------------MILESTONES------------------------------
-	$query_m = "select distinct sub.id, sub.first_name, sub.last_name, sub.description, sub.progression, sub.inner_grade FROM ( select users.id, users.first_name, users.last_name, evaluations.description, evaluations.progression, evaluations.score_needed, COUNT(CASE WHEN item_attempts.transaction_code = 0 then 1 ELSE NULL END) as not_answered,  COUNT(CASE WHEN item_attempts.transaction_code = 2 then 1 ELSE NULL END) as incorrect,
+	$query_m = "select distinct sub.description, sub.progression, sub.inner_grade FROM ( select evaluations.description, evaluations.progression, evaluations.score_needed, COUNT(CASE WHEN item_attempts.transaction_code = 0 then 1 ELSE NULL END) as not_answered,  COUNT(CASE WHEN item_attempts.transaction_code = 2 then 1 ELSE NULL END) as incorrect,
 COUNT(CASE WHEN item_attempts.transaction_code = 1 then 1 ELSE NULL END) as correct, COUNT(CASE WHEN item_attempts.transaction_code = 0 then 1 ELSE NULL END) + COUNT(CASE WHEN item_attempts.transaction_code = 1 then 1 ELSE NULL END) + COUNT(CASE WHEN item_attempts.transaction_code = 2 then 1 ELSE NULL END) as total_answered,";
 
 	$query_m .= " COUNT(CASE WHEN item_attempts.transaction_code = 1 then 1 ELSE NULL END) / (COUNT(CASE WHEN item_attempts.transaction_code = 0 then 1 ELSE NULL END) + COUNT(CASE WHEN item_attempts.transaction_code = 1 then 1 ELSE NULL END) + COUNT(CASE WHEN item_attempts.transaction_code = 2 then 1 ELSE NULL END))::float * 100 as inner_grade   from evaluations_attempts join users on evaluations_attempts.user_id=users.id JOIN item_attempts ON item_attempts.evaluations_attempts_id=evaluations_attempts.id JOIN evaluations ON evaluations.id=evaluations_attempts.evaluations_id where evaluations_attempts.start_time > '2016-09-10 09:28:27.777635' AND evaluations_attempts.evaluations_id != 1 "; 
@@ -132,7 +132,7 @@ COUNT(CASE WHEN item_attempts.transaction_code = 1 then 1 ELSE NULL END) as corr
         	$query_m .= $_SESSION["user_id"];
 	}
 
-	$query_m .= " AND evaluations.progression > 0.9 group by evaluations_attempts, evaluations.progression, evaluations.description, users.id, users.first_name, users.last_name, evaluations.score_needed) sub WHERE sub.total_answered >= sub.score_needed order by sub.last_name, sub.progression;";
+	$query_m .= " AND evaluations.progression > 0.9 group by evaluations_attempts, evaluations.progression, evaluations.description, evaluations.score_needed) sub WHERE sub.total_answered >= sub.score_needed order by sub.progression;";
         $result_m = pg_query($conn,$query_m);
         $numrows_m = pg_numrows($result_m);
 
@@ -150,16 +150,16 @@ COUNT(CASE WHEN item_attempts.transaction_code = 1 then 1 ELSE NULL END) as corr
 	for($m = 0; $m < $numrows_m; $m++)
 	{
                	$row_m = pg_fetch_array($result_m, $m);
-		$row_m[5] = intval($row_m[5]);
+		$row_m[2] = intval($row_m[2]);
 	
 		for ($p = 0; $p < $numrows_e; $p++)
 		{
                		$row_e = pg_fetch_array($result_e, $p);
-			if ($row_m[3] == $row_e[1]) //do we have a user and ms match?
+			if ($row_m[0] == $row_e[1]) //do we have a user and ms match?
 			{
-				if ($row[$p] < $row_m[5]) //less than new comming in
+				if ($row[$p] < $row_m[2]) //less than new comming in
 				{
-					$row[$p] = $row_m[5];
+					$row[$p] = $row_m[2];
 				}
 			}
 		}	
