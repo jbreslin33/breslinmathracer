31ea3b71d30a4b71785ba5fa50b6b2a81b877d05
Jim Breslin <jbreslin33@gmail.com>
2017-11-27 22:00:41 -0500

    ms

--- src/web/navigation/student/reports.php
+++ src/web/navigation/student/reports.php
@@ -18,19 +18,8 @@ session_start();
 <li><a href="/web/navigation/student/main_menu.php">Main Menu</a></li>
 <li><a href="/web/php/logout.php">Logout</a></li>
 <li><a href="/web/reports/generic/live.php">Live</a></li>
-<li><a href="/web/reports/generic/live_evaluations.php">Live Evaluations</a></li>
-<li><a href="/web/reports/student/matches.php">Matches</a></li>
-<li><a href="/web/reports/student/teams.php">Teams</a></li>
-<li><a href="/web/reports/generic/any_homework_check.php">Quick Homework Check</a></li>
-<li><a href="/web/reports/generic/leaderboards.php">Leader Boards</a></li>
-<li><a href="/web/reports/generic/tests.php">Evaluation Answers and Questions</a></li>
-<li><a href="/web/reports/generic/grades.php">Grades</a></li>
-<li><a href="/web/reports/generic/milestones_degree.php">Any Class Milestones</a></li>
-<li><a href="/web/reports/generic/evaluations.php">Any Class Evaluations</a></li>
-<li><a href="/web/reports/generic/work_today.php">Any Class Work Today</a></li>
 <li><a href="/web/reports/student/milestone_attempts.php">My Milestone Attempts</a></li>
 <li><a href="/web/reports/student/my_milestones_degree.php">My Milestones</a></li>
-<li><a href="/web/reports/student/my_team_milestones_degree.php">My Team Milestones</a></li>
 <li><a href="/web/reports/student/my_work_today.php">My Work Today</a></li>
 
 </ul>
