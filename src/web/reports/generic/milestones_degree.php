8cfd24f7a0f15305d13bf424d02a3d369f76c121
Jim Breslin <jbreslin33@gmail.com>
2018-04-15 20:36:42 -0400

    milestons_degree

--- src/web/reports/generic/milestones_degree.php
+++ src/web/reports/generic/milestones_degree.php
@@ -144,23 +144,23 @@ $pre_end[] = 0;
 $pre_end[] = 0;
 $pre_end[] = 8;
 $pre_end[] = 11;
-$pre_end[] = 14;
-$pre_end[] = 24;
-$pre_end[] = 29; //5
-$pre_end[] = 34;
-$pre_end[] = 34;
-$pre_end[] = 34;
+$pre_end[] = 16;
+$pre_end[] = 26;
+$pre_end[] = 31; //5
+$pre_end[] = 36;
+$pre_end[] = 36;
+$pre_end[] = 36;
 
 $new_end[] = 0;
 $new_end[] = 0;
 $new_end[] = 11;
-$new_end[] = 14;
-$new_end[] = 24;
-$new_end[] = 29;
-$new_end[] = 34; //5
-$new_end[] = 34;
-$new_end[] = 34;
-$new_end[] = 34;
+$new_end[] = 16;
+$new_end[] = 26;
+$new_end[] = 31;
+$new_end[] = 36; //5
+$new_end[] = 36;
+$new_end[] = 36;
+$new_end[] = 36;
         
 //------------------EVALUATIONS------------------------------
 $query_e = "select * from evaluations where progression > 0.9 AND progression < 21 order by progression asc;";
@@ -362,6 +362,10 @@ echo '<table border=\"1\">';
         echo '<td>b<br>a<br>b<br>y<br>9<br>s';
         echo '</td>';
         echo '<td>b<br>a<br>b<br>y<br>8<br>s';
+        echo '</td>';
+        echo '<td>b<br>a<br>b<br>y<br>7<br>s';
+        echo '</td>';
+        echo '<td>b<br>a<br>b<br>y<br>6<br>s';
         echo '</td>';
         echo '<td>2<br> o<br>a<br> b<br> 2';
         echo '</td>';
